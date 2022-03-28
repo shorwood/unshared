46d740e737717fed43f07026c81c72f8bdfa78a9
Stanley Horwood <stanley@hsjm.io>
2022-03-28 02:44:37 +0200

    chore: finished `buildLicences` script

--- scripts/buildLicences.ts
+++ scripts/buildLicences.ts
@@ -1,13 +1,17 @@
-import { copyFileSync, readdirSync } from 'node:fs'
-import { resolve } from 'node:path'
+import { copyFileSync } from 'node:fs'
+import { join, relative, resolve } from 'node:path'
+import { sync as glob } from 'fast-glob'
+import consola from 'consola'
 
 // --- Compute paths.
 const root = resolve(__dirname, '../')
 const distPath = resolve(root, 'dist')
-const distPackages = readdirSync(distPath)
+const distPackages = glob('*', { cwd: distPath, onlyDirectories: true, absolute: true })
 
 // --- Get root licence path.
-const licencePath = resolve(__dirname, 'LICENSE')
+const licencePath = glob('LI(C|S)ENCE(.md)?', { cwd: root, onlyFiles: true, absolute: true })[0]
 
-for (const distPackage in distPackages)
-  copyFileSync(licencePath, distPackage)
+for (const distPackage of distPackages) {
+  consola.success(`Copied LICENCE to ${relative(root, distPackage)}`)
+  copyFileSync(licencePath, join(distPackage, 'LICENCE'))
+}
