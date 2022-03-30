66e8d111b768a64e2bd1caf9ae05d98513d68f32
Stanley Horwood <stanley@hsjm.io>
2022-03-30 19:04:46 +0200

    chore: restricted glob depth

--- scripts/buildLicences.ts
+++ scripts/buildLicences.ts
@@ -6,7 +6,7 @@ import consola from 'consola'
 // --- Compute paths.
 const root = resolve(__dirname, '../')
 const distPath = resolve(root, 'dist')
-const distPackages = glob('*', { cwd: distPath, onlyDirectories: true, absolute: true })
+const distPackages = glob('./*', { cwd: distPath, onlyDirectories: true, absolute: true })
 
 // --- Get root licence path.
 const licencePath = glob('LI(C|S)ENCE(.md)?', { cwd: root, onlyFiles: true, absolute: true })[0]
