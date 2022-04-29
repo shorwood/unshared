da80e574a7d69858b0a5cfdcbbcec13230105d64
Stanley Horwood <stanley@hsjm.io>
2022-04-29 19:54:38 +0200

    feat: introduced `mapKeys` and `mapValues`

--- packages/shared/collection.ts
+++ packages/shared/collection.ts
@@ -1,3 +1,5 @@
+type Key = string | number | symbol
+
 export const chunk = <T>(array: T[], chunkSize: number): T[][] => {
   const arrayChunked = []
   for (let index = 0; index < array.length; index += chunkSize)
@@ -11,12 +13,24 @@ export const uniq = <T>(array: T[]) =>
 export const compact = <T>(array: T[]) =>
   uniq(array.filter(Boolean))
 
-export const omit = <T>(object: Record<string, T>, iterator: ((value: T, key: string) => boolean)) => {
-  const entries = Object.entries(object).filter(([key, value]) => iterator(value, key))
-  return Object.fromEntries(entries)
+// export const get = <T>(object: Record)
+
+export const omit = <T, K extends Key>(object: Record<K, T>, iterator: ((value: T, key: K) => boolean)) => {
+  const entries = Object.entries(object).filter(([key, value]) => iterator(<T>value, <K>key))
+  return <Record<K, T>>Object.fromEntries(entries)
 }
 
-export const pick = <T>(object: Record<string, T>, iterator: ((value: T, key: string) => boolean)) => {
-  const entries = Object.entries(object).filter(([key, value]) => !iterator(value, key))
-  return Object.fromEntries(entries)
+export const pick = <T, K extends Key>(object: Record<Key, T>, iterator: ((value: T, key: Key) => boolean)) => {
+  const entries = Object.entries(object).filter(([key, value]) => !iterator(<T>value, <K>key))
+  return <Record<K, T>>Object.fromEntries(entries)
+}
+
+export const mapValues = <T, K extends Key, TResult>(object: Record<K, T>, iterator: ((value: T, key: K) => TResult)) => {
+  const entries = Object.entries(object).map(([key, value]) => [key, iterator(<T>value, <K>key)])
+  return <Record<K, TResult>>Object.fromEntries(entries)
+}
+
+export const mapKeys = <T, K extends Key, KResult extends Key>(object: Record<K, T>, iterator: ((value: T, key: K) => KResult)) => {
+  const entries = Object.entries(object).map(([key, value]) => [iterator(<T>value, <K>key), value])
+  return <Record<KResult, T>>Object.fromEntries(entries)
 }
