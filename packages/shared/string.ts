d53bb9e864a73de1ef255e27429c23945b6ca9e2
Stanley Horwood <stanley@hsjm.io>
2022-05-03 23:48:26 +0200

    feat: added common string transformation utilities

--- packages/shared/string.ts
+++ packages/shared/string.ts
@@ -1,4 +1,31 @@
+export const trim = (value: string) => value
+  .trim()
 
-export const camelCase = (string: string) => string
+export const lowerCase = (value: string) => value
+  .toLowerCase()
+
+export const upperCase = (value: string) => value
+  .toUpperCase()
+
+export const snakeCase = (value: string) => value
+  .toLowerCase()
+  .replace(/[^a-z]+/gi, '_')
+
+export const kebabCase = (value: string) => value
+  .toLowerCase()
+  .replace(/[^a-z]+/gi, '-')
+
+export const camelCase = (value: string) => value
+  .toLowerCase()
   .replace(/^\w|[A-Z]|\b\w/g, (word, index) => (index === 0 ? word.toLowerCase() : word.toUpperCase()))
-  .replace(/\s+/g, '')
+  .replace(/[^a-z]+/gi, '')
+
+export const pascalCase = (value: string) => value
+  .toLowerCase()
+  .replace(/^\w|[A-Z]|\b\w/g, word => word
+    .toUpperCase()).replace(/[^a-z]+/gi, '')
+
+export const capitalize = (value: string) => value
+  .toLowerCase()
+  .replace(/^\w|[A-Z]|\b\w/g, word => word.toUpperCase())
+  .replace(/[^a-z]+/gi, ' ')
