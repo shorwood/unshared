c2ef08f121acd61f2d429821a6bffb1d4467d4fc
Stanley Horwood <stanley@hsjm.io>
2022-05-03 23:47:56 +0200

    feat: introduced `isBrowser`, `isNode` and `isWebWorker`

--- packages/shared/environment.ts
+++ packages/shared/environment.ts
@@ -1,4 +1,13 @@
 // @ts-expect-error: Property 'env' does not exist on type 'ImportMeta'.
 export const environment = import.meta?.env ?? typeof process !== 'undefined' ? process.env : {}
-export const isDevelopment = environment.DEV ?? environment.NODE_ENV !== 'production'
+export const isDevelopment = !!environment.DEV || environment.NODE_ENV !== 'production'
 export const isProduction = !isDevelopment
+
+/** Check if running in browser. */
+export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined'
+
+/** Check if running in NodeJs instance. */
+export const isNode = typeof process !== 'undefined' && typeof process.versions?.node !== 'undefined'
+
+/** Check if running in a web worker instance. */
+export const isWebWorker = typeof self === 'object' && self.constructor?.name === 'DedicatedWorkerGlobalScope'
