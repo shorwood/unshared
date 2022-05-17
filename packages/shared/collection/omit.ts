be0de56a2e03c397cbf67daad920a4a12d1d56b7
Stanley Horwood <stanley@hsjm.io>
2022-05-17 19:29:03 +0200

    feat(shared): restored `omit` and `pick` (was deleted because of my smooth brain)

---
+++ packages/shared/collection/omit.ts
@@ -0,0 +1,23 @@
+
+interface IOmit {
+  <T extends object, K extends keyof T>(object: T, path: K): Omit<T, K>
+  <T extends object, K extends keyof T>(object: T, iterator: (value: T[K], key: K, object: T) => boolean): Partial<T>
+  <T extends object>(object: T, path: any): Partial<T>
+}
+
+/**
+ *
+ * @param object
+ * @param iterator
+ */
+export const omit: IOmit = (object: any, iterator?: any): any => {
+  // --- If iterator is a path, cast as getter function.
+  if (typeof iterator !== 'function') {
+    const path = iterator
+    iterator = (value: any, key: any) => key === path
+  }
+
+  // --- Filter entries.
+  const entries = Object.entries(object).filter(([key, value]) => !iterator(value, key, object))
+  return Object.fromEntries(entries)
+}
