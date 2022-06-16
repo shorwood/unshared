4ccc3791ac4ef072757bb2612e04aed04fa3e663
Stanley Horwood <stanley@hsjm.io>
2022-06-16 21:24:56 +0200

    feat(shared): improved typing

--- packages/shared/collection/mapKeys.ts
+++ packages/shared/collection/mapKeys.ts
@@ -1,22 +1,19 @@
-import { Key, MaybeArray } from '../types'
 import { get } from './get'
 
 interface MapKeys {
-  <T>(object: Array<T>, path: MaybeArray<Key>): Record<string, T>
+  <T>(object: Array<T>, path: keyof T): Record<string, T>
   <T>(object: Array<T>, iterator: (value: T, key: keyof T, array: Array<T>) => string): Record<string, T>
-  <T>(object: Array<T>, iterator: any): Record<string, T>
-  <T>(object: Record<string, T>, path: MaybeArray<Key>): Record<string, T>
+  <T>(object: Record<string, T>, path: keyof T): Record<string, T>
   <T>(object: Record<string, T>, iterator: (value: T, key: keyof T, object: Record<string, T>) => string): Record<string, T>
-  <T>(object: Record<string, T>, iterator: any): Record<string, T>
 }
 
 /**
  * Maps keys in an object or array.
- * @param {Array|Object} object The object or array to map keys for
- * @param {Function|string[]} iterator The iterator function or path
- * @returns {Object} The new object with mapped keys
+ * @param {Array<T>|Record<string, T>} object The object or array to map keys for
+ * @param {Function|keyof T} iterator The iterator function or path
+ * @returns {Record<string, T>} The new object with mapped keys
  */
-export const mapKeys: MapKeys = (object: any, iterator: any) => {
+export const mapKeys: MapKeys = (object: any, iterator: any): any => {
   // --- If iterator is a path, cast as getter function.
   if (typeof iterator !== 'function') {
     const path = iterator
--- packages/shared/collection/mapValues.ts
+++ packages/shared/collection/mapValues.ts
@@ -1,20 +1,17 @@
-import { Key, MaybeArray } from '../types'
 import { get } from './get'
 
 interface MapValues {
-  <T>(object: Array<T>, path: MaybeArray<Key>): Record<string, T>
+  <T, K extends keyof T>(object: Array<T>, path: K): Record<string, T[K]>
   <T, U>(object: Array<T>, iterator: (value: T, key: keyof T, array: Array<T>) => U): Record<string, U>
-  <T>(object: Array<T>, iterator: any): Record<string, T>
-  <T>(object: Record<string, T>, path: MaybeArray<Key>): Record<string, T>
+  <T, K extends keyof T>(object: Record<string, T>, path: keyof T): Record<string, T[K]>
   <T, U>(object: Record<string, T>, iterator: (value: T, key: keyof T, object: Record<string, T>) => U): Record<string, U>
-  <T>(object: Record<string, T>, iterator: any): Record<string, T>
 }
 
 /**
  * Maps values of an object or array according to an iterator function or a key path.
- * @param {Array|Record} object The object to map
- * @param {Function|Array} iterator The iterator function or key path
- * @returns {Array|Record} A new object or array
+ * @param object The object to map
+ * @param iterator The iterator function or key path
+ * @returns A new object or array
  */
 export const mapValues: MapValues = (object: any, iterator: any) => {
   // --- If iterator is a path, cast as getter function.
