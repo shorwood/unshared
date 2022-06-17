6055d32216930db0386b19e7bc4a456e31c3eb2f
Stanley Horwood <stanley@hsjm.io>
2022-06-17 17:44:54 +0200

    feat(shared)!: improved typing and removed `get` dependency

--- packages/shared/collection/map.ts
+++ packages/shared/collection/map.ts
@@ -1,11 +1,8 @@
-import { Key, MaybeArray } from '../types'
-import { get } from './get'
-
 interface Map {
-  <U, T = any>(object: Array<T>, path: MaybeArray<Key>): U[]
-  <U, T = any>(object: Array<T>, iterator: (value: T, key: number, array: Array<T>) => U): U[]
-  <U, T = any>(object: Record<string, T>, path: MaybeArray<Key>): U[]
-  <U, T = any>(object: Record<string, T>, iterator: (value: T, key: number, object: Record<string, T>) => U): U[]
+  <T, K extends keyof T>(object: Array<T>, path: K): T[K]
+  <T, U>(object: Array<T>, iterator: (value: T, key: number, array: Array<T>) => U): U[]
+  <T, K extends keyof T>(object: Record<string, T>, path: K): T[K]
+  <T, U>(object: Record<string, T>, iterator: (value: T, key: keyof T, object: Record<string, T>) => U): U[]
 }
 
 /**
@@ -17,15 +14,15 @@ interface Map {
  * If a callback is supplied, it will be invoked for each item in the object or array.
  * The callback can return a new value to be added to the new object or array.
  *
- * @param {Array|Object} object The object or array to iterate over
- * @param {MaybeArray<Key>|Function} iterator The callback function or path to iterate over
- * @returns {Array|Object} A new array consisting of the results of the callback function
+ * @param object The object or array to iterate over
+ * @param iterator The callback function or path to iterate over
+ * @returns A new array consisting of the results of the callback function
  */
-export const map: Map = (object, iterator: any) => {
+export const map: Map = (object: any, iterator: any) => {
   // --- If iterator is a path, cast as getter function.
   if (typeof iterator !== 'function') {
     const path = iterator
-    iterator = (value: any) => get(value, path)
+    iterator = (value: any) => value[path]
   }
 
   // --- Map values.
