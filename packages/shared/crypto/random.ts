9282674c4bbe73d78a2b787749b182f6fbcf929c
Stanley Horwood <stanley@hsjm.io>
2022-06-24 22:31:12 +0200

    chore: documented lines

--- packages/shared/crypto/random.ts
+++ packages/shared/crypto/random.ts
@@ -10,11 +10,17 @@ import { isBrowser, requireSafe } from '../misc'
  * @see https://github.com/brix/crypto-js/blob/develop/src/core.js#L47
  */
 export const random = (): number => {
+  // --- If browser environment
   if (isBrowser) {
+    // --- Get global object.
     const global = window ?? globalThis
+
+    // --- Use `crypto.getRandomValues()`, falls back to `Math.random()`
     return global.crypto
       ? global.crypto.getRandomValues(new Uint32Array(1))[0]
       : Math.trunc(Math.random() * 0xFFFFFFFF)
   }
+
+  // --- If NodeJS, use `node:crypto`.
   return requireSafe('node:crypto').randomBytes(4).readInt32LE()
 }
