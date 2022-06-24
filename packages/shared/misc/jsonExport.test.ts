73dd8437863192d93491d190f4119a385c0af190
Stanley Horwood <stanley@hsjm.io>
2022-06-24 02:11:56 +0200

    feat(shared)!: removed unused tests and functions

--- packages/shared/misc/jsonExport.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, it } from 'vitest'
-import { jsonExport } from './jsonExport'
-import { jsonImport } from './jsonImport'
-
-it.todo('should export the object to a JSON file', () => {
-  const object = { foo: 'bar' }
-  jsonExport('./test.json', object)
-  expect(jsonImport('./test.json')).toEqual(object)
-})
--- packages/shared/misc/workerizeBrowser.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from 'vitest'
-import { isBrowser } from './environment'
-import { workerizeBrowser } from './workerizeBrowser'
-
-it.skipIf(!isBrowser)('should wrap a function and return a promise', async() => {
-  const wrapped = workerizeBrowser((x: number) => x + 5)
-  expect(await wrapped(10)).toEqual(15)
-})
