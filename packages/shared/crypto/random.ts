24c58f02f854d3ae1eeb73327d04764b6bf90677
Stanley Horwood <stanley@hsjm.io>
2022-06-24 22:29:11 +0200

    feat(shared): added fallback if `window.crypto` is `undefined`

--- packages/shared/crypto/random.ts
+++ packages/shared/crypto/random.ts
@@ -1,13 +1,20 @@
 import { isBrowser, requireSafe } from '../misc'
 
 /**
- * Returns a cryptographically secure pseudorandom number.
+ * Returns a cryptographically secure pseudorandom 32-bit number.
  * As `Math.random()` is cryptographically not safe to use.
+ *
+ * On browser, if `window.crypto` is undefined, falls back
+ * to using Math.random() to generate an integer
  * @returns {number} a random number
  * @see https://github.com/brix/crypto-js/blob/develop/src/core.js#L47
  */
-export const random = (): number => (
-  isBrowser
-    ? window.crypto.getRandomValues(new Uint32Array(1))[0]
-    : requireSafe('node:crypto').randomBytes(4).readInt32LE()
-)
+export const random = (): number => {
+  if (isBrowser) {
+    const global = window ?? globalThis
+    return global.crypto
+      ? global.crypto.getRandomValues(new Uint32Array(1))[0]
+      : Math.trunc(Math.random() * 0xFFFFFFFF)
+  }
+  return requireSafe('node:crypto').randomBytes(4).readInt32LE()
+}
