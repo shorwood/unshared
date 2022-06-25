d327b588d2b75a83be3ad770a8b93df3f2131d11
Stanley Horwood <stanley@hsjm.io>
2022-06-25 05:52:49 +0200

    feat: allowed definite user typing

--- packages/shared/misc/requireSafe.ts
+++ packages/shared/misc/requireSafe.ts
@@ -3,7 +3,8 @@
  * @param {string} moduleId The id of the module to require
  * @returns {T} The required module, or undefined if it couldn't be required
  */
-export const requireSafe = <T = any>(moduleId: string): T | undefined => {
+// @ts-expect-error: behavior wanted.
+export const requireSafe = <T = any>(moduleId: string): T => {
   try { return require(moduleId) }
   catch {}
 }
