3750efe574c2da770c338d047a020e4d6b2b7cc2
Stanley Horwood <stanley@hsjm.io>
2022-06-26 04:32:26 +0200

    fix(shared): array are now deeply copyied

--- packages/shared/collection/mergeDeep.test.ts
+++ packages/shared/collection/mergeDeep.test.ts
@@ -22,9 +22,27 @@ it('should deeply nested merge arrays of objects', () => {
   )).toEqual({ a: { b: [{ a: 1 }, { a: 2 }] } })
 })
 
-it('should not merge objects and arrays', () => {
+it('should not merge objects with arrays', () => {
   expect(mergeDeep(
     { a: [3, 4] },
     { a: { b: { c: 1 } } },
   )).toEqual({ a: { b: { c: 1 } } })
+})
+
+it('should not modify the original object', () => {
+  const object1 = { a: { b: { c: 1 } } }
+  const object2 = { a: { b: { d: 2 } } }
+  mergeDeep(object1, object2)
+  expect(object1).toEqual({ a: { b: { c: 1 } } })
+})
+
+it('should not modify the child objects and arrays', () => {
+  const array = [1, 2]
+  const object = { a: 1 }
+  expect(mergeDeep(
+    { a: array, o: object },
+    { a: [3, 4], o: { a: 2 } },
+  )).toEqual({ a: [ 1, 2, 3, 4 ], o: { a: 2 } })
+  expect(object).toEqual({ a: 1 })
+  expect(array).toEqual([1, 2])
 })
--- packages/shared/collection/mergeDeep.ts
+++ packages/shared/collection/mergeDeep.ts
@@ -1,13 +1,18 @@
+export interface MergeDeep {
+  <T1 extends object, T2 extends object>(object1: T1, object2: T2): T1 extends T2 ? T1 & T2 : T1
+  <T extends object>(...objects: T[]): T
+}
+
 /**
  * Merge multiple objects together deeply and concat colliding arrays.
  * @param {...Record<string, any>} objects Objects to merge
  * @returns {Record<string, any>} Merged object
  */
-export const mergeDeep = <T extends Record<string, any>>(...objects: T[]): T => {
-  const result = {} as T
+export const mergeDeep: MergeDeep = (...objects: Record<string, any>[]): Record<string, any> => {
+  const result = { ...objects[0] }
 
   // --- Iterate over each objects and their properties.
-  for (const object of objects) {
+  for (const object of objects.slice(1)) {
     for (const key in object) {
       const value = object[key]
 
@@ -15,7 +20,7 @@ export const mergeDeep = <T extends Record<string, any>>(...objects: T[]): T => 
       const isValueArray = Array.isArray(value)
       const isResultArray = Array.isArray(result[key])
       if (isValueArray && isResultArray) {
-        result[key].push(...value as any[])
+        result[key] = [...result[key], ...value]
         continue
       }
 
