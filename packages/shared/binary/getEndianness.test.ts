0bc2237bacad067c0e3cc56b8d0f690ab95a7f2d
Stanley Horwood <stanley@hsjm.io>
2022-06-27 02:56:09 +0200

    feat(shared): refacted `getEndianness` and introduced `endianness`, `isLittleEndian` and `isBigEndian`

--- packages/shared/binary/getEndianness.test.ts
+++ packages/shared/binary/getEndianness.test.ts
@@ -1,6 +1,11 @@
+import { endianness as osEndianness } from 'node:os'
 import { expect, it } from 'vitest'
-import { getEndianness } from './getEndianness'
+import { endianness, getEndianness, isBigEndian, isLittleEndian } from './getEndianness'
 
 it('returns the endianness of the machine', () => {
-  expect(getEndianness()).toMatch(/^(LE|BE)$/)
+  const expected = osEndianness()
+  expect(getEndianness()).toEqual(expected)
+  expect(endianness).toEqual(expected)
+  expect(isLittleEndian).toEqual(expected === 'LE')
+  expect(isBigEndian).toEqual(expected === 'BE')
 })
--- packages/shared/binary/getEndianness.ts
+++ packages/shared/binary/getEndianness.ts
@@ -1,12 +1,20 @@
-let endianness: 'LE' | 'BE'
 
 /**
- * Get the endianness of the current machine
- * @returns {string} The endianness of the machine
+ * Get the endianness of the current runtime
+ * @warning Prefer `endianness` over this function as the result won't change.
+ * @returns {string} The endianness of the runtime
  */
 export const getEndianness = (): 'LE' | 'BE' => {
-  if (endianness) return endianness
   const uint8Array = new Uint8Array([0x01, 0x02, 0x03, 0x04])
   const uint32Array = new Uint32Array(uint8Array.buffer)
-  return uint32Array[0] === 0x04030201 ? (endianness = 'LE') : (endianness = 'BE')
+  return uint32Array[0] === 0x04030201 ? 'LE' : 'BE'
 }
+
+/** Endianness of the current runtime. */
+export const endianness = getEndianness()
+
+/** Current runtime is little-endien. */
+export const isLittleEndian = endianness === 'LE'
+
+/** Current runtime is big-endien. */
+export const isBigEndian = endianness === 'BE'
