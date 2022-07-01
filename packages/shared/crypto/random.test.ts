03d2adf2928d9a3a9f083804f832d097e8f2a182
Stanley Horwood <stanley@hsjm.io>
2022-07-01 21:58:12 +0200

    feat(shared): introduced `randomString` and `randomFloat`

--- packages/shared/crypto/random.test.ts
+++ packages/shared/crypto/random.test.ts
@@ -1,9 +1,42 @@
 import { expect, it } from 'vitest'
-import { random } from './random'
+import { random, randomFloat, randomString } from './random'
 
-it.each(Array.from({ length: 10 }))('returns a random number (#%#)', () => {
+it.each(Array.from({ length: 10 }))('returns a random Uint32 (#%#)', () => {
   const result = random()
   expect(result).toBeTypeOf('number')
-  expect(result).toBeGreaterThanOrEqual(Number.MIN_SAFE_INTEGER)
-  expect(result).toBeLessThanOrEqual(Number.MAX_SAFE_INTEGER)
+  expect(result).toBeGreaterThanOrEqual(0x00000000)
+  expect(result).toBeLessThanOrEqual(0xFFFFFFFF)
+  expect(Number.isInteger(result)).toEqual(true)
+})
+
+it.each(Array.from({ length: 10 }))('returns a random float between 0 and 1 (#%#)', () => {
+  const result = randomFloat()
+  expect(result).toBeTypeOf('number')
+  expect(result).toBeGreaterThanOrEqual(0)
+  expect(result).toBeLessThanOrEqual(1)
+})
+
+it.each(Array.from({ length: 10 }))('returns a random float between min and max (#%#)', () => {
+  const result = randomFloat(10, 20)
+  expect(result).toBeTypeOf('number')
+  expect(result).toBeGreaterThanOrEqual(10)
+  expect(result).toBeLessThanOrEqual(20)
+})
+
+it.each(Array.from({ length: 10 }))('returns a random alphanum string of length 16 (#%#)', () => {
+  const result = randomString()
+  expect(result).toBeTypeOf('string')
+  expect(result.length).toEqual(16)
+  expect(result).toMatch(/^[\dA-Za-z]+$/)
+})
+
+it.each([
+  [16, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+  [8, '0123456789abcdef'],
+  [8, '01'],
+])('returns a random string (#%#)', (n, chars) => {
+  const result = randomString(n, chars)
+  expect(result).toBeTypeOf('string')
+  expect(result.length).toEqual(n)
+  expect([...result].every(char => chars.includes(char))).toEqual(true)
 })
--- packages/shared/crypto/random.ts
+++ packages/shared/crypto/random.ts
@@ -1,23 +1,42 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
-import { isBrowser, requireSafe } from '../misc'
+import { requireSafe } from '../misc/requireSafe'
+import { isNode } from '../environment/runtime'
 
 /**
  * Returns a cryptographically secure pseudorandom 32-bit number.
- * As `Math.random()` is cryptographically not safe to use.
- *
- * On browser, if `window.crypto` is undefined, falls back
- * to using Math.random() to generate an integer
- * @returns {number} a random number
- * @see https://github.com/brix/crypto-js/blob/develop/src/core.js#L47
+ * @returns {number} A random number
  */
 export const random = (): number => {
-  // --- If browser environment, use `globalThis.crypto`, falls back to `Math.random()`
-  if (isBrowser) {
-    return globalThis.crypto
-      ? globalThis.crypto.getRandomValues(new Uint32Array(1))[0]
-      : Math.trunc(Math.random() * 0xFFFFFFFF)
+  if (isNode) return requireSafe('node:crypto')?.randomBytes(4).readUint32LE(0)
+  return globalThis.crypto.getRandomValues(new Uint32Array(1))[0]
+}
+
+/**
+ * Returns a cryptographically secure pseudorandom float.
+ * @param {number} [min] The minimum value
+ * @param {number} [max] The maximum value
+ * @returns {number} A random float
+ */
+export const randomFloat = (min = 0, max = 1): number => {
+  const randomInt = random()
+  const randomFloat = randomInt / 0xFFFFFFFF
+  return min + (max - min) * randomFloat
+}
+
+/**
+ * Generate a random string of `n` length
+ * @param {number} [n] length of the string
+ * @param {string} [chars] characters to use in the string
+ */
+export const randomString = (n = 16, chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string => {
+  let result = ''
+
+  // --- Generate the string
+  for (let index = 0; index < n; index++) {
+    const char = Math.trunc(randomFloat(0, chars.length))
+    result += chars[char]
   }
 
-  // --- If NodeJS, use `node:crypto`.
-  return requireSafe<typeof import('node:crypto')>('node:crypto').randomBytes(4).readInt32LE()
+  // --- Return the string
+  return result
 }
