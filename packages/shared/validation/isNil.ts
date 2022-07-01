7b33d8ef5a6b062e561c1721429462cdc12c6d35
Stanley Horwood <stanley@hsjm.io>
2022-07-01 22:24:21 +0200

    feat(shared): improved types

--- packages/shared/validation/isNil.ts
+++ packages/shared/validation/isNil.ts
@@ -1,7 +1,9 @@
+import { NotNil, NotNull, NotUndefined } from '../types'
+
 /**
  * Check if value is null
  * @param value The value to check
- * @returns {value is null} Returns true if value is null, false otherwise
+ * @returns {boolean} Returns true if value is null, false otherwise
  */
 export const isNull = (value: any): value is null => value === null
 
@@ -10,12 +12,12 @@ export const isNull = (value: any): value is null => value === null
  * @param value The value to check
  * @returns {boolean} Returns true if value is not null, false otherwise
  */
-export const isNotNull = (value: any): boolean => value !== null
+export const isNotNull = <T>(value: T): value is NotNull<T> => value !== null
 
 /**
  * Check if value is undefined
  * @param value The value to check
- * @returns {value is undefined} Returns true if value is undefined, false otherwise
+ * @returns {boolean} Returns true if value is undefined, false otherwise
  */
 export const isUndefined = (value: any): value is undefined => typeof value === 'undefined'
 
@@ -24,12 +26,12 @@ export const isUndefined = (value: any): value is undefined => typeof value === 
  * @param value The value to check
  * @returns {boolean} Returns true if value is not undefined, false otherwise
  */
-export const isNotUndefined = (value: any): boolean => typeof value !== 'undefined'
+export const isNotUndefined = <T>(value: T): value is NotUndefined<T> => typeof value !== 'undefined'
 
 /**
  * Check if value is null or undefined
  * @param value The value to check
- * @returns {value is null | undefined} Returns true if value is null or undefined, false otherwise
+ * @returns {boolean} Returns true if value is null or undefined, false otherwise
  */
 export const isNil = (value: any): value is null | undefined => typeof value === 'undefined' || value === null
 
@@ -38,4 +40,4 @@ export const isNil = (value: any): value is null | undefined => typeof value ===
  * @param value The value to check
  * @returns {boolean} Returns true if value is not null and not undefined, false otherwise
  */
-export const isNotNil = (value: any): boolean => typeof value !== 'undefined' && value !== null
+export const isNotNil = <T>(value: T): value is NotNil<T> => typeof value !== 'undefined' && value !== null
