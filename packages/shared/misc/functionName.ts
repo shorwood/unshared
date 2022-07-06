5c1325cba1c679cb2474ba55344f5dfed7580ded
Stanley Horwood <stanley@hsjm.io>
2022-07-06 14:02:40 +0200

    feat(shared)!: removed `functionName`

--- packages/shared/misc/functionName.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns the name of the function that called the function.
- * @param {Function} target An optional target function
- * @returns {string} The name of the function that called the function. If no target is provided, returns the name of the function that called functionName.
- */
-export const functionName = (target?: Function): string => (target
-  ? target.name
-  : functionName.caller.name
-)
