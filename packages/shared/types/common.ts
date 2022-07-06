a38e7f3b56972a0217c924511d9eaf28a2e1388c
Stanley Horwood <stanley@hsjm.io>
2022-07-06 14:00:20 +0200

    feat(shared)!: improved common and function types

--- packages/shared/types/common.ts
+++ packages/shared/types/common.ts
@@ -1,28 +1,42 @@
-/* eslint-disable unicorn/prevent-abbreviations */
 
-/** Is of type `Any` */
-export type Any = null | undefined | number | string | boolean | symbol | bigint | object
+/** Primitive types */
+export type Primitive = null | undefined | number | string | boolean | symbol | bigint
 
-/** Is not of type `Any` */
-export type NotAny<T> = Any extends T ? never : T
+/** Not a primitive */
+export type NotPrimitive<U = any> = U extends Primitive ? never : U
+
+/** All types exept unknown */
+export type Any = Primitive | object
+
+/** Not `any` */
+export type NotAny<T = any> = any extends T ? never : T
 
 /** Might be an array. */
 export type MaybeArray<U = any> = U | U[]
 
+/** Not an `Array` */
+export type NotArray<U = any> = U extends any[] ? never : U
+
+/** Might be a promise. */
+export type MaybePromise<U = any> = U | Promise<U>
+
+/** Not a `Promise` */
+export type NotPromise<U = any> = U extends Promise<any> ? never : U
+
 /** Generic 2D vector. */
-export type Vector<U> = U[][]
+export type Vector<U = any> = U[][]
 
 /** Generic 3D matrix. */
-export type Matrix<U> = U[][][]
+export type Matrix<U = any> = U[][][]
 
 /** Not `null`. */
-export type NotNull<U> = U extends null ? never : U
+export type NotNull<U = any> = U extends null ? never : U
 
 /** Not `undefined`. */
-export type NotUndefined<U> = U extends undefined ? never : U
+export type NotUndefined<U = any> = U extends undefined ? never : U
 
 /** Not `null` or `undefined`. */
-export type NotNil<U> = U extends null | undefined ? never : U
+export type NotNil<U = any> = U extends null | undefined ? never : U
 
 /** Replace `undefined` with `U`. */
 export type Default<T, U> = T extends undefined ? NotUndefined<T> | U : T
--- packages/shared/types/function.ts
+++ packages/shared/types/function.ts
@@ -1,11 +1,14 @@
 import { Key, Values } from './collection'
 import { Any } from './common'
 
-/** Function to iterate over an array or object values. */
-export type IteratorFunction<T, R> = (value: Values<T>, key: Key<T>, object: T) => R
+/** A function */
+export type Function<A extends any[] = any[], T = any> = (...parameters: A) => T
 
 /** Anything that is not a function. */
 export type NotFunction = Any & { apply?: never }
+
+/** Function to iterate over an array or object values. */
+export type IteratorFunction<T, R> = (value: Values<T>, key: Key<T>, object: T) => R
 
 /** Remove first argument from a function */
 export type OmitFirstParameter<F> = F extends (x: any, ...parameters: infer P) => infer U ? (...parameters: P) => U : never
