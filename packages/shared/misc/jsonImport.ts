2ed42b041e11f6f69e8a68c4b51852b15055098d
Stanley Horwood <stanley@hsjm.io>
2022-07-08 07:02:44 +0200

    feat(shared)!: safer execution

--- packages/shared/misc/jsonImport.ts
+++ packages/shared/misc/jsonImport.ts
@@ -2,12 +2,19 @@ import { requireSafe } from './requireSafe'
 import { jsonParse } from './jsonParse'
 
 /**
- * Imports a JSON file.
+ * Imports data from a JSON file.
  * @param {string} path The path to the JSON file
  * @returns {any} The parsed JSON data
  */
 export const jsonImport = <T>(path: string): T | undefined => {
+  // --- Import `fs` safely
   const fs = requireSafe('node:fs')
+
+  // --- Make sure FS is available and the file exists
   if (!fs) return undefined
-  return jsonParse<T>(fs.readFileSync(path).toString())
+  if (!fs.existsSync(path)) return undefined
+
+  // --- Get the JSON data and parse it
+  const json = fs.readFileSync(path, 'utf8')
+  return jsonParse<T>(json)
 }
