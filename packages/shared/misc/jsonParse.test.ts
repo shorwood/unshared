b8c22962f663e6ed68130c3d389d714908e46837
Stanley Horwood <stanley@hsjm.io>
2022-07-08 06:57:18 +0200

    feat(shared)!: improved `jsonParse`

--- packages/shared/misc/jsonParse.test.ts
+++ packages/shared/misc/jsonParse.test.ts
@@ -2,27 +2,24 @@
 import { expect, it } from 'vitest'
 import { jsonParse } from './jsonParse'
 
-it('should parse from JSON value', () => {
-  expect(jsonParse('true')).toEqual(true)
-  expect(jsonParse('false')).toEqual(false)
-  expect(jsonParse('null')).toEqual(null)
-  expect(jsonParse('nan')).toEqual(Number.NaN)
-  expect(jsonParse('infinity')).toEqual(Number.POSITIVE_INFINITY)
-  expect(jsonParse('undefined')).toEqual(undefined)
-  expect(jsonParse('42')).toEqual(42)
-})
-
-it('should parse from JSON object', () => {
-  expect(jsonParse('{"foo": "bar"}')).toEqual({ foo: 'bar' })
-})
-
-it('should exclude "__proto__" and "constructor"', () => {
-  expect(jsonParse('{"foo": "bar", "__proto__": "foobar" }')).toEqual({ foo: 'bar' })
-  expect(jsonParse('{"foo": "bar", "constructor": "foobar" }')).toEqual({ foo: 'bar' })
-})
-
-it('should fail on invalid JSON', () => {
-  expect(() => { jsonParse(0 as any) }).toThrow()
-  expect(() => { jsonParse(false as any) }).toThrow()
-  expect(() => { jsonParse('{ foo: bar }') }).toThrow()
+it.each([
+  [true, 'true'],
+  [false, 'false'],
+  [null, 'NULL'],
+  [undefined, 'undefined'],
+  [42, '42.0'],
+  [Number.NaN, 'NaN'],
+  [Number.POSITIVE_INFINITY, 'infinity'],
+  [{}, '{     }'],
+  [{ foo: 'bar' }, '{"foo": "bar", "__proto__": "foobar" }'],
+  [{ foo: 'bar' }, '{"foo": "bar", "constructor": "foobar" }'],
+  [undefined, 0],
+  [undefined, false],
+  [undefined, null],
+  [undefined, undefined],
+  [undefined, 'salami'],
+  [undefined, '{ foo: bar }'],
+])('should parse %s from "%s"', (value, json: any) => {
+  const result = jsonParse(json)
+  expect(result).toEqual(value)
 })
