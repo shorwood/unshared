bf7be9c10f55b70ab7f7baf036eb8378c69c2a46
Stanley Horwood <stanley@hsjm.io>
2022-07-08 07:02:10 +0200

    chore: simpler tests

--- packages/shared/crypto/hashMd5.test.ts
+++ packages/shared/crypto/hashMd5.test.ts
@@ -4,14 +4,14 @@ import { toHex } from '../binary/toHex'
 import { hashMd5 } from './hashMd5'
 
 it.each([
+  // --- Small values
   ['', 'd41d8cd98f00b204e9800998ecf8427e'],
   ['a', '0cc175b9c0f1b6a831c399e269772661'],
-  ['abc', '900150983cd24fb0d6963f7d28e17f72'],
-  ['message digest', 'f96b697d7cb7938d525a2f31aaf161d0'],
-  ['abcdefghijklmnopqrstuvwxyz', 'c3fcd3d76192e4007dfb496cca67e13b'],
-  ['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 'd174ab98d277d9f5a5611c2c9f419d9f'],
-  ['12345678901234567890123456789012345678901234567890123456789012345678901234567890', '57edf4a22be3c955ac49da2e2107b67a'],
+
+  // --- UTF-8
   ['The quick brown fox jumps over the lazy dog', '9e107d9d372bb6826bd81d3542a419d6'],
+  ['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 'd174ab98d277d9f5a5611c2c9f419d9f'],
+
 ])('should hash "%s" into an ArrayBuffer equal to "0x%s"', (input, expected) => {
   const inputBuffer = toArrayBuffer(input, 'utf8')
   const hash = hashMd5(inputBuffer)
