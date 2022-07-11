b84c59bcd25485e74ee50ba5a1bec6c449d3d4d9
Stanley Horwood <stanley@hsjm.io>
2022-07-11 08:23:07 +0200

    fix(shared): fixed throttled functions arguments

--- packages/shared/misc/throttle.test.ts
+++ packages/shared/misc/throttle.test.ts
@@ -5,19 +5,19 @@ import { delay } from './delay'
 
 it('throttles a function so it is only called once every delay', async() => {
   let counter = 0
-  const throttled = throttle(() => counter++, 10)
+  const throttled = throttle((n = 1) => counter += n, 10)
 
   // --- First cycle
-  throttled()
-  throttled()
+  throttled(1)
+  throttled(1)
   expect(counter).toEqual(1)
 
   // --- Second cycle (Not unlocked yet)
   await delay(5)
   expect(counter).toEqual(1)
-  
+
   // --- Third cycle (Now unlocked)
   await delay(10)
-  throttled()
-  expect(counter).toEqual(2)
+  throttled(10)
+  expect(counter).toEqual(11)
 })
--- packages/shared/misc/throttle.ts
+++ packages/shared/misc/throttle.ts
@@ -2,10 +2,18 @@
 export type ThrottledFn<T extends (...args: any) => any> = (...args: Parameters<T>) => void
 
 /**
- * Returns a function, that, when invoked, will only be triggered at most once during a given window of time.
+ * Wrap a function in a throttle function.
+ *
+ * A throttled function will only execute once per delay.
+ * If the function is called again before the delay has passed,
+ * the timer will be reset. Useful for implementing spam protection.
+ *
  * @param {Function} callback The function to throttle
- * @param {number} delay The amount of time in milliseconds to throttle invocations
- * @returns {Function} A function throttled function.
+ * @param {number} [delay] The throttle delay in milliseconds
+ * @returns {Function} A throttled function
+ * @example
+ * const getUsers = (id: string) => axios.get(`/users/${id}`)
+ * const throttled = throttle(getUser)
  */
 export const throttle = <T extends (...args: any) => any>(callback: T, delay: number): ThrottledFn<T> => {
   // --- Handle edge cases.
@@ -13,18 +21,23 @@ export const throttle = <T extends (...args: any) => any>(callback: T, delay: nu
 
   // --- Initialize variables.
   let timeout: NodeJS.Timeout
-  let last: number
+  let last = 0
 
   // --- Instantiate and return a thottled function.
   return (...args: any[]) => {
     const now = Date.now()
-    if (last && now <= last + delay) {
+    const canExecute = (now - last) > delay
+
+    // --- If the delay has passed, or if the function is being called for the first time.
+    if (canExecute) {
+      last = now
+      callback(...args)
+    }
+
+    // --- If the delay has not passed, reset the timeout.
+    else {
       clearTimeout(timeout)
       timeout = setTimeout(() => last = now, delay)
-    }
-    else {
-      last = now
-      callback(args)
     }
   }
 }
