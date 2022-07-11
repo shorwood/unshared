163ffcffb91d69a80857a79756497b01b05fd5a9
Stanley Horwood <stanley@hsjm.io>
2022-07-11 08:23:32 +0200

    chore: improved docs

--- packages/shared/misc/debounce.ts
+++ packages/shared/misc/debounce.ts
@@ -2,10 +2,18 @@
 export type DebouncedFn<T extends (...args: any) => any> = (...args: Parameters<T>) => void
 
 /**
- * Returns a debounced version of the passed function.
+ * Wrap a function in a debounce function.
+ *
+ * A debounced function will only execute after the specified delay.
+ * If the function is called again before the delay has passed,
+ * the timer will be reset. Useful for implementing spam protection.
+ *
  * @param {Function} callback The function to debounce
  * @param {number} [delay] The debounce delay in milliseconds
  * @returns {Function} A debounced function
+ * @example
+ * const getUser = (id: string) => axios.get(`/users/${id}`)
+ * const debounced = debounce(getUser)
  */
 export const debounce = <T extends (...args: any) => any>(callback: T, delay: number): DebouncedFn<T> => {
   // --- Handle edge cases.
