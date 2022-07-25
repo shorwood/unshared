06c78da69230702728e9d6da0c18e656791b5caa
Stanley Horwood <stanley@hsjm.io>
2022-07-25 08:51:06 +0200

    chore: updated exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,15 @@ import { isProduction } from './packages/shared'
 export default defineConfig({
   outDir: 'dist',
   format: ['esm', 'cjs'],
+  entry: ['index.ts', '*/index.ts'],
+  external: [
+    /vue/,
+    /faker/,
+    /firebase/,
+    /milsymbol/,
+    /leaflet/,
+    /@hsjm/,
+  ],
   treeshake: isProduction,
   splitting: isProduction,
   minify: isProduction,
