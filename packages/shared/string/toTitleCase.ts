321b2d178d128e82ff2e11ae9f192c080e7d7d6a
Stanley Horwood <stanley@hsjm.io>
2022-10-18 21:25:19 +0200

    chore: removed useless `\`\`\``

--- packages/shared/string/toTitleCase.ts
+++ packages/shared/string/toTitleCase.ts
@@ -5,10 +5,8 @@ import { tokenize } from './tokenize'
  * @param {string} value The string to convert
  * @returns {string} The converted string
  * @example
- * ```
  * toCamelCase('foo_bar') // returns 'Foo Bar'
  * toCamelCase('FOO_BAR') // returns 'Foo Bar'
- * ```
  */
 export const toTitleCase = (value: string): string => tokenize(value)
   .map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase())
