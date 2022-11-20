a3e532c88d175ed00363fee23d5ca41d215e217e
Stanley Horwood <stanley@hsjm.io>
2022-11-20 23:48:19 +0100

    chore: upgraded packages + build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'tsup'
-import { isProduction } from './packages/shared/environment'
+import { isProduction } from './packages/shared/environment/isProduction'
 
 export default defineConfig({
+  name: 'HsjmShared',
+  globalName: 'HsjmShared',
+  onSuccess: 'pnpm build:post && pnpm build:lint',
   outDir: 'dist',
-  format: ['esm', 'cjs'],
-  entry: ['index.ts', '*/index.ts'],
-  treeshake: isProduction,
-  splitting: isProduction,
-  minify: isProduction,
+  format: ['esm', 'cjs', 'iife'],
+  entry: ['index.ts'],
+  treeshake: isProduction(),
+  splitting: isProduction(),
+  minify: isProduction(),
   sourcemap: true,
   clean: true,
   shims: true,
