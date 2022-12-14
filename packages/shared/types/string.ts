e7b5f8840b8ebbc8463f0e9a5f95047bf5c41633
Stanley Horwood <stanley@hsjm.io>
2022-12-14 18:44:36 +0100

    feat(shared): introduced `Split` type

--- packages/shared/types/string.ts
+++ packages/shared/types/string.ts
@@ -19,3 +19,16 @@ export type Literal<T> = string extends T
 export type StringConstraint<C, L extends number, T extends string = ''> = number extends L
   ? string
   : L extends 0 ? T: `${T}${Literal<C>}${StringConstraint<C, Decrease<L>>}`
+
+/**
+ * Extract litteral types of strings separated by a delimiter.
+ * @param S String to extract litteral types from.
+ * @param D Delimiter to split string with.
+ * @param N Maximum number of splits. (default: `128`)
+ */
+export type Split<S extends string, D extends string = ',', N extends number = 128> =
+  N extends 0
+    ? never
+    : S extends `${infer A}${D}${infer B}`
+      ? `${A}` | Split<B, D, Decrease<N>>
+      : `${S}`
