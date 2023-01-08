911d15a25e9baa6be4eb2e0faf2a5d107a083a0c
Stanley Horwood <stanley@hsjm.io>
2023-01-08 19:15:04 +0100

    feat(shared): proxyfied return object of `resolvable` to allow `await` keyword

--- packages/shared/misc/resolvable.test.ts
+++ packages/shared/misc/resolvable.test.ts
@@ -1,3 +1,4 @@
+import { nextTick } from 'node:process'
 import { expect, it } from 'vitest'
 import { resolvable } from './resolvable'
 
@@ -5,38 +6,49 @@ it('should initialize', () => {
   const state = resolvable()
   expect(state.pending).toEqual(true)
   expect(state.resolved).toEqual(false)
+  expect(state.promise).toBeInstanceOf(Promise)
 })
 
 it('should resolve a value', async() => {
   const value = 'test'
   const state = resolvable<string>()
-  setTimeout(() => state.resolve(value), 10)
-  expect(await state.promise).toEqual(value)
-  expect(state.pending).toEqual(false)
-  expect(state.resolved).toEqual(true)
-}, 20)
+  nextTick(() => state.resolve(value))
+  expect(state).resolves.toEqual(value)
+  expect(state.promise).resolves.toEqual(value)
+  expect(state).resolves.toHaveProperty('pending', false)
+  expect(state).resolves.toHaveProperty('resolved', true)
+})
 
 it('should reject a value', async() => {
   const value = 'test'
   const state = resolvable<string>()
-  setTimeout(() => state.reject(value), 10)
-  expect(await state.promise.catch(error => error)).toEqual(value)
-  expect(state.pending).toEqual(false)
-  expect(state.resolved).toEqual(false)
-}, 20)
+  nextTick(() => state.reject(value))
+  expect(state).rejects.toEqual(value)
+  expect(state.promise).rejects.toEqual(value)
+  expect(state).rejects.toHaveProperty('pending', false)
+  expect(state).rejects.toHaveProperty('resolved', false)
+})
 
 it('should be resolved after reset is called if already resolved', () => {
   const state = resolvable()
   state.resolve()
   state.reset()
-  expect(state.pending).toEqual(true)
-  expect(state.resolved).toEqual(false)
+  expect(state).resolves.toHaveProperty('pending', true)
+  expect(state).resolves.toHaveProperty('resolved', false)
 })
 
 it('should be pending after reset is called if already rejected', () => {
   const state = resolvable()
   state.reject()
   state.reset()
-  expect(state.pending).toEqual(true)
-  expect(state.resolved).toEqual(false)
+  expect(state).resolves.toHaveProperty('pending', true)
+  expect(state).resolves.toHaveProperty('resolved', false)
+})
+
+it('should be awaitable', async() => {
+  const value = 'test'
+  const state = resolvable<string>()
+  nextTick(() => state.resolve(value))
+  expect(state).resolves.toEqual(value)
+  expect(state.promise).resolves.toEqual(value)
 })
--- packages/shared/misc/resolvable.ts
+++ packages/shared/misc/resolvable.ts
@@ -1,17 +1,25 @@
-export interface Resolvable<T> {
+export interface Resolvable<T> extends PromiseLike<T> {
+  /** Whether the promise has been resolved. */
   resolved: boolean
+  /** Whether the promise has been rejected. */
   pending: boolean
+  /** The resolvable promise. */
   promise: Promise<T>
+  /** Resolve the promise with a value. */
   resolve: (value: T | PromiseLike<T>) => void
+  /** Reject the promise with a value. */
   reject: (reason?: any) => void
+  /** Reset the promise to its initial state. */
   reset: () => void
 }
-
-// TODO: Proxyfy the state object so that it can be used as a promise.
 
 /**
  * Creates a new resolvable object with convenience methods for Promise resolution and state management.
  * @return A new resolvable object
+ * @example
+ * const state = resolvable()
+ * eventEmitter.on('event', state.resolve)
+ * await state
  */
 export const resolvable = <T = void>(): Resolvable<T> => {
   // --- Initialize state.
@@ -44,5 +52,12 @@ export const resolvable = <T = void>(): Resolvable<T> => {
   state.reset()
 
   // --- Return variable.
-  return state
+  return new Proxy(state, {
+    get(target: any, key) {
+      if (key === 'then') return state.promise.then.bind(state.promise)
+      if (key === 'catch') return state.promise.catch.bind(state.promise)
+      if (key === 'finally') return state.promise.finally.bind(state.promise)
+      if (key in target) return target[key]
+    },
+  })
 }
