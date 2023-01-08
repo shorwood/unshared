737726481043dc80364afdb9f4456a217500aa06
Stanley Horwood <stanley@hsjm.io>
2023-01-08 19:15:04 +0100

    chore: updated `@ts-expect-error` message

--- packages/shared/function/bindThis.ts
+++ packages/shared/function/bindThis.ts
@@ -16,5 +16,5 @@ import { OmitFirstParameter } from '../types/function'
  *
  * new Person('Joe').greet() // 'Hello, I am Joe'
  */
-// @ts-expect-error: ignore `this` error
+// @ts-expect-error: ignores undefined `this` argument
 export const bindThis = <F extends Function>(fn: F): OmitFirstParameter<F> => function(...args: any[]) { return fn(this, ...args) }
