6ae68b3e79f33b0d8d574c850c91ddf60eba81f7
Stanley Horwood <stanley@hsjm.io>
2023-01-08 19:15:04 +0100

    feat(shared)!: introduced `allowUnsafe` flag

--- packages/shared/crypto/randomString.ts
+++ packages/shared/crypto/randomString.ts
@@ -1,18 +1,45 @@
 import { randomFloat } from './randomFloat'
 
-/**
- * Generate a random string of `n` length
- * @param n length of the string
- * @param chars characters to use in the string
- */
+export interface RandomStringOptions {
+  /** Characters to use in the string
+   * @default 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+   */
+  characters?: string
+  /**
+   * Allow unsafe random number generation if `node:crypto` and `globalThis.crypto` is not available.
+   * @default false
+   */
+  allowUnsafe?: boolean
+}
 
-export const randomString = (n = 16, chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string => {
+/**
+ * Generate a random string of a specific length.
+ * @param length length of the string (default: `16`)
+ * @param charactersOrOptions characters to use in the string or options object
+ * @return A random string
+ * @throws If `node:crypto` and `globalThis.crypto` is not available and `allowUnsafe` is `false`
+ * @example
+ * randomString() // 'a1b2c3d4e5f6g7h8'
+ */
+export function randomString(length?: number, options?: RandomStringOptions): string
+export function randomString(length?: number, characters?: string): string
+export function randomString(length = 16, charactersOrOptions?: string | RandomStringOptions): string {
   let result = ''
 
+  // --- Parse options
+  if (typeof charactersOrOptions === 'string')
+    charactersOrOptions = { characters: charactersOrOptions }
+
+  // --- Destructure options
+  const {
+    characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    allowUnsafe = false,
+  } = charactersOrOptions ?? {}
+
   // --- Generate the string
-  for (let index = 0; index < n; index++) {
-    const char = Math.trunc(randomFloat(0, chars.length))
-    result += chars[char]
+  for (let index = 0; index < length; index++) {
+    const char = Math.trunc(randomFloat(0, characters.length, allowUnsafe))
+    result += characters[char]
   }
 
   // --- Return the string
