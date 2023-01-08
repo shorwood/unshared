d246c9cbcc5eec87bc6c99c173bbd1e8eed565b2
Stanley Horwood <stanley@hsjm.io>
2023-01-08 19:15:04 +0100

    chore: fixed missing `ROOT_PATH` variable in build script

--- scripts/buildLicence.ts
+++ scripts/buildLicence.ts
@@ -1,7 +1,8 @@
 import { copyFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { sync as glob } from 'fast-glob'
-import { ROOT_PATH } from './utils'
+
+export const ROOT_PATH = join(__dirname, '..')
 
 export const buildLicence = (cwd: string) => {
   const licencePath = glob('LI(C|S)ENCE(.md)?', { cwd: ROOT_PATH, onlyFiles: true, absolute: true, caseSensitiveMatch: false })[0]
