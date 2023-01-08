c52bba32ade54f934d0c7193b131e5ba7972fffc
Stanley Horwood <stanley@hsjm.io>
2023-01-08 22:26:21 +0100

    chore: tweaked vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
   test: {
     api: { port: 9000 },
     environment: 'node',
     setupFiles: '/__mocks__/setup.ts',
-    include: ['./packages/**/*.test.ts', './packages/**/*.test.*.ts'],
-    coverage: {
-      all: true,
-      reporter: ['clover', 'cobertura', 'lcov', 'text'],
-      include: ['./packages'],
-      extension: ['ts'],
-      excludeNodeModules: true,
-      reportsDirectory: './coverage',
-    },
+    include: [
+      './packages/**/*.test.ts',
+      './packages/**/*.test.*.ts',
+    ],
   },
 })
