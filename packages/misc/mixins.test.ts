723dfee43a1d5fe5a4b2bb6e8e958b7b7017e87f
Stanley Horwood <stanley@hsjm.io>
2023-01-19 21:42:43 +0100

    feat(misc)!: improved `mixins` function
    - better static properties mixing
    - added new test cases
    - added typing support when passing fewer than 2 classes
    - added runtime assertments
    - mixins now apply from right to left

--- packages/misc/mixins.test.ts
+++ packages/misc/mixins.test.ts
@@ -5,20 +5,20 @@ it('should mix classes into a single class', () => {
   class ClassA { foo = 'foo' }
   class ClassB { bar = 'bar' }
   class ClassC { baz = 'baz' }
-  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {}
   const result = new Result()
   expect(result).toHaveProperty('foo', 'foo')
   expect(result).toHaveProperty('bar', 'bar')
   expect(result).toHaveProperty('baz', 'baz')
 })
 
-it('should override properties from left to right', () => {
+it('should override properties from right to left', () => {
   class ClassA { foo = 'foo' }
   class ClassB { foo = 'bar' }
   class ClassC { foo = 'baz' }
-  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {}
   const result = new Result()
-  expect(result).toHaveProperty('foo', 'baz')
+  expect(result).toHaveProperty('foo', 'foo')
 })
 
 it('should preserve the prototype chain', () => {
@@ -27,30 +27,119 @@ it('should preserve the prototype chain', () => {
   class ClassC { baz = 'baz' }
   class Result extends mixins(ClassA, ClassB, ClassC) {}
   const result = new Result()
-  expect(result).toBeInstanceOf(ClassA)
-  expect(result).toBeInstanceOf(ClassB)
-  expect(result).toBeInstanceOf(ClassC)
   expect(result).toBeInstanceOf(Result)
+  expect(result).toBeInstanceOf(ClassC)
+  expect(result).toBeInstanceOf(ClassB)
+  expect(result).toBeInstanceOf(ClassA)
+})
+
+it('should preserve the prototype chain of nested mixins', () => {
+  class ClassA { foo = 'foo' }
+  class ClassB { bar = 'bar' }
+  class ClassC { baz = 'baz' }
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {}
+  const result = new Result()
+  expect(result).toBeInstanceOf(Result)
+  expect(result).toBeInstanceOf(ClassC)
+  expect(result).toBeInstanceOf(ClassB)
+  expect(result).toBeInstanceOf(ClassA)
 })
 
 it('should preserve the `this` context', () => {
-  class ClassA { foo = 'foo'; getFoo() { return this.foo } }
-  class ClassB { bar = 'bar'; getBar() { return this.bar } }
-  class ClassC { baz = 'baz'; getBaz() { return this.baz } }
-  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  class ClassA { _foo = 'foo'; get foo() { return this._foo }; constructor() { this._foo = this.foo.toUpperCase() } }
+  class ClassB { _bar = 'bar'; get bar() { return this._bar }; constructor() { this._bar = this.bar.toUpperCase() } }
+  class ClassC { _baz = 'baz'; get baz() { return this._baz }; constructor() { this._baz = this.baz.toUpperCase() } }
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {
+    fooBarBaz = ''
+    constructor() {
+      super()
+      this.fooBarBaz = this.foo + this.bar + this.baz
+    }
+  }
   const result = new Result()
-  expect(result.getFoo()).toBe('foo')
-  expect(result.getBar()).toBe('bar')
-  expect(result.getBaz()).toBe('baz')
+  expect(result).toHaveProperty('foo', 'FOO')
+  expect(result).toHaveProperty('bar', 'BAR')
+  expect(result).toHaveProperty('baz', 'BAZ')
+  expect(result).toHaveProperty('fooBarBaz', 'FOOBARBAZ')
 })
 
 it('should preserve private properties', () => {
   class ClassA { private foo = 'foo' }
   class ClassB { private bar = 'bar' }
   class ClassC { private baz = 'baz' }
-  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {}
   const result = new Result()
   expect(result).toHaveProperty('foo', 'foo')
   expect(result).toHaveProperty('bar', 'bar')
   expect(result).toHaveProperty('baz', 'baz')
+})
+
+it('should just return the class if only one class is passed', () => {
+  class ClassA { foo = 'foo' }
+  class Result extends mixins(ClassA) {}
+  const result = new Result()
+  expect(result).toHaveProperty('foo', 'foo')
+  expect(result).toBeInstanceOf(ClassA)
+})
+
+it('should preserve getters and setters', () => {
+  class ClassA { _foo = 'foo'; get foo() { return this._foo } set foo(value) { this._foo = value } }
+  class ClassB { _bar = 'bar'; get bar() { return this._bar } set bar(value) { this._bar = value } }
+  class ClassC { _baz = 'baz'; get baz() { return this._baz } set baz(value) { this._baz = value } }
+  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  const result = new Result()
+  result.foo = 'FOO'
+  result.bar = 'BAR'
+  result.baz = 'BAZ'
+  expect(result.foo).toEqual('FOO')
+  expect(result.bar).toEqual('BAR')
+  expect(result.baz).toEqual('BAZ')
+})
+
+it('should preserve static properties', () => {
+  class ClassA { static FOO = 'foo' }
+  class ClassB { static BAR = 'bar' }
+  class ClassC { static BAZ = 'baz' }
+  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  new Result()
+  expect(Result.FOO).toEqual('foo')
+  expect(Result.BAR).toEqual('bar')
+  expect(Result.BAZ).toEqual('baz')
+})
+
+it('should preserve static getters and setters', () => {
+  class ClassA { static FOO = 'foo'; static get foo() { return this.FOO } static set foo(value) { this.FOO = value } }
+  class ClassB { static BAR = 'bar'; static get bar() { return this.BAR } static set bar(value) { this.BAR = value } }
+  class ClassC { static BAZ = 'baz'; static get baz() { return this.BAZ } static set baz(value) { this.BAZ = value } }
+  class Result extends mixins(ClassA, ClassB, ClassC) {}
+  new Result()
+  Result.foo = 'FOO'
+  Result.bar = 'BAR'
+  Result.baz = 'BAZ'
+  expect(Result.foo).toEqual('FOO')
+  expect(Result.bar).toEqual('BAR')
+  expect(Result.baz).toEqual('BAZ')
+})
+
+it('should preserve statics when nesting mixins', () => {
+  class ClassA { static getFoo() { return 'foo' }; static FOO = 'foo' }
+  class ClassB { static getBar() { return 'bar' }; static BAR = 'bar' }
+  class ClassC { static getBaz() { return 'baz' }; static BAZ = 'baz' }
+  class Result extends mixins(mixins(ClassA, ClassB), ClassC) {}
+  new Result()
+  expect(Result).toHaveProperty('FOO', 'foo')
+  expect(Result).toHaveProperty('BAR', 'bar')
+  expect(Result).toHaveProperty('BAZ', 'baz')
+})
+
+it('should throw if no class is passed', () => {
+  // @ts-expect-error: invalid arguments
+  const shouldThrow = () => mixins()
+  expect(shouldThrow).toThrow()
+})
+
+it('should throw if a non-class is passed', () => {
+  // @ts-expect-error: invalid arguments
+  const shouldThrow = () => mixins('foo')
+  expect(shouldThrow).toThrow()
 })
--- packages/misc/mixins.ts
+++ packages/misc/mixins.ts
@@ -1,11 +1,17 @@
 /** A typed constructor.  */
 export type Constructor<P extends any[] = any[], R = any> = new (...parameters: P) => R
 
+/** Extract static properties from a class. */
+export type StaticProperties<T extends Constructor> =
+  T extends Constructor
+    ? { [K in Exclude<keyof T, 'prototype' | 'length' | 'name'>]: T[K] }
+    : never
+
 /** Extends one class with another.  */
 export type Extends<T1 extends Constructor, T2 extends Constructor> =
   T1 extends Constructor<unknown[], infer R1>
     ? T2 extends Constructor<infer P, infer R2>
-      ? Constructor<P, R1 & Omit<R2, keyof R1>>
+      ? Constructor<P, R1 & Omit<R2, keyof R1>> & StaticProperties<T1> & StaticProperties<T2>
       : never
     : never
 
@@ -18,10 +24,18 @@ export type Mixins<T extends Constructor[]> =
       ? Extends<T1, T2>
       : never : never
 
+    // --- If there is only one class, then return the class.
+    : T extends [infer T1] ? T1 extends Constructor
+      ? T1
+      : never
+
     // --- If there is more than two classes, recursively extend the first class with the rest.
-      : T extends [infer T1, ...infer TRest] ? T1 extends Constructor ? TRest extends Constructor[]
-        ? Extends<T1, Mixins<TRest>>
-        : never : never : never : never
+    : T extends [infer T1, ...infer TRest] ? T1 extends Constructor ? TRest extends Constructor[]
+      ? Extends<T1, Mixins<TRest>>
+      : never : never : never
+
+  // --- Fallback to never.
+  : never
 
 /**
  * Mix multiple classes into a single class.
@@ -44,25 +58,37 @@ export type Mixins<T extends Constructor[]> =
  * class Baz { baz = 'baz' }
  * class FooBarBaz extends mixins(Foo, Bar, Baz) {}
  */
-export function mixins<T extends Constructor[]>(...mixins: T): Mixins<T> {
-  return class {
+export function mixins<T extends [Constructor, ...Constructor[]]>(...mixins: T): Mixins<T> {
+  // --- Handle the edge cases.
+  if (mixins.length === 0) throw new TypeError('No mixins were provided.');
+  if (mixins.some(mixin => typeof mixin !== 'function')) throw new TypeError('All mixins must be class constructors.');
+  if (mixins.length === 1) return mixins[0] as Mixins<T>;
+
+  // --- Reverse the mixins to preserve the prototype chain.
+  mixins.reverse()
+
+  // --- Create a new class extending all the mixins.
+  class Mixed {
     constructor(...parameters: unknown[]) {
-      let parentInstance: unknown
+
+      let thisPrototype = Object.getPrototypeOf(this)
+      let parentPrototype = thisPrototype
+
+      // --- Apply the mixins.
       for (const Ctor of mixins) {
-        // --- Create and apply the current mixin.
-        Ctor.constructor = Ctor.constructor.bind(this)
         const instance = new Ctor(...parameters)
         Object.assign(this, instance)
 
-        // --- Preserve the prototype chain.
-        Object.setPrototypeOf(
-          Object.getPrototypeOf(parentInstance ?? this),
-          Object.getPrototypeOf(instance),
-        )
-
-        // --- Update the parent instance.
-        parentInstance = instance
+        // --- Preserve prototype descriptors and prototype chain.
+        const instancePrototype = Object.getPrototypeOf(instance)
+        const instanceDescriptors = Object.getOwnPropertyDescriptors(instancePrototype)
+        Object.defineProperties(thisPrototype, instanceDescriptors)
+        Object.setPrototypeOf(parentPrototype, instancePrototype)
+        parentPrototype = instancePrototype
       }
     }
-  } as Mixins<T>
+  }
+
+  // --- Preserve static properties and return the mixed class.
+  return Object.assign(Mixed, ...mixins) as Mixins<T>
 }
