46dc48937e43cc9035178e29105d65e8fff4511b
Stanley Horwood <stanley@hsjm.io>
2023-02-06 02:35:23 +0100

    chore: enabled `vitest` globals and coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    globals: true,
+    coverage: {
+      enabled: true,
+      skipFull: true,
+    },
     setupFiles: './packages/setupTest.ts',
     include: [
       './packages/**/*.test.ts',
