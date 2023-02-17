81b3eee7250d89d7dd69a9251db362fb6232b9cb
Stanley Horwood <stanley@hsjm.io>
2023-02-17 21:47:03 +0100

    feat(string): introduced `toDotCase`

---
+++ packages/string/toDotCase.ts
@@ -0,0 +1,41 @@
+import { tokenize } from './tokenize'
+
+/**
+ * Converts a string to dot case.
+ *
+ * @param value The string to convert.
+ * @returns The converted string.
+ * @throws If argument is not a string.
+ * @example toDotCase('fooBar') // returns 'foo.bar'
+ */
+export function toDotCase(value: string): string {
+  if (typeof value !== 'string')
+    throw new TypeError('Expected a string')
+
+  // --- Tokenize, lowercase and join with dots.
+  return tokenize(value)
+    .map(token => token.toLowerCase())
+    .join('.')
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  it.each([
+    ['camelCase', 'camel.case'],
+    ['PascalCase', 'pascal.case'],
+    ['snake_case', 'snake.case'],
+    ['kebab-case', 'kebab.case'],
+    ['Title Case', 'title.case'],
+    ['UPPER CASE', 'upper.case'],
+    ['lower case', 'lower.case'],
+  ])('should convert %s to %s', (input, expected) => {
+    const result = toDotCase(input)
+    expect(result).toEqual(expected)
+  })
+
+  it('should throw if value is not a string', () => {
+    // @ts-expect-error: invalid argument type.
+    const shouldThrow = () => toDotCase(1)
+    expect(shouldThrow).toThrow(TypeError)
+  })
+}
