157f6fd574ba54591de7b8c4350a2e52ed9f5403
Stanley Horwood <stanley@hsjm.io>
2023-02-18 02:08:40 +0100

    chore(workspace): enabled vitest "in-source" testing
    Added global declaration `import.meta.vitest` for in-source testing. Also tweaked build automatically discard test code and avoid side-effects.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { existsSync, readdirSync } from 'node:fs'
+import { readFileSync, readdirSync } from 'node:fs'
 import { resolve } from 'node:path'
-import { defineConfig, Options } from 'tsup'
+import { Options, defineConfig } from 'tsup'
 import { toPascalCase } from './packages/string'
 
-const outExtension = ({ format, options }) => {
+const outExtension: Options['outExtension'] = ({ format }) => {
   let js = '.js'
   if (format === 'cjs') js = '.cjs'
   if (format === 'esm') js = '.js'
@@ -14,37 +14,44 @@ const outExtension = ({ format, options }) => {
 const configs = readdirSync('./packages', { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
   .map((dirent): Options => {
-
     // --- Define the input and output paths.
     const entry = resolve(__dirname, `./packages/${dirent.name}/*.ts`)
-    const outDir = resolve(__dirname, `./packages/${dirent.name}/dist`)
+    const outDirectory = resolve(__dirname, `./packages/${dirent.name}/dist`)
     const globalName = toPascalCase(`Unshared_${dirent.name}`)
+
+    const packageJsonPath = resolve(__dirname, `./packages/${dirent.name}/package.json`)
+    const packageJsonUtf8 = readFileSync(packageJsonPath, 'utf8')
+    const packageJson = JSON.parse(packageJsonUtf8)
 
     // --- Compute the configuration.
     return <Options>({
       name: 'Unshared',
       format: ['esm', 'cjs'],
-      entry: [entry, '!**/*.test.ts','!**/*.test.*.ts'],
-      outDir,
+      entry: [entry, '!**/*.test.ts', '!**/*.test.*.ts'],
+      outDir: outDirectory,
       globalName,
       platform: 'node',
-      target: [
-        'chrome58',
-        'firefox57',
-      ],
+      target: ['node18'],
       splitting: true,
       bundle: false,
-      external: [/^@unshared-dev\/.*/],
+      external: [/^@unshared\/.*/],
       sourcemap: true,
       clean: true,
       shims: false,
       dts: true,
       outExtension,
+      treeshake: true,
+      minifySyntax: true,
+      define: { 'import.meta.vitest': 'false' },
       esbuildOptions: (options) => {
         options.chunkNames = 'chunks/[hash]'
-      }
+        options.ignoreAnnotations = true
+      },
+
+      // --- Merge the package.json configuration.
+      ...(packageJson ? packageJson.tsup : {}),
     })
   })
-    
+
 // --- Export Tsup configuration.
 export default defineConfig(configs)
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,23 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
+      100: true,
       enabled: true,
-      skipFull: true,
+      excludeNodeModules: true,
+      reporter: ['text', 'lcov', 'html'],
     },
     setupFiles: './packages/setupTest.ts',
+    passWithNoTests: true,
     include: [
       './packages/**/*.test.ts',
       './packages/**/*.test.*.ts',
+    ],
+    includeSource: [
+      './packages/**/*.ts',
+    ],
+    exclude: [
+      './packages/setupTest.ts',
+      './packages/**/node_modules/**',
     ],
   },
 })
