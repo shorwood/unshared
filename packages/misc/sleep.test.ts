525c74915a6983b19195be200293f572a2da1e38
Stanley Horwood <stanley@hsjm.io>
2023-02-18 02:15:32 +0100

    feat(misc)!: deleted `@unshared/misc` package
    DIspatched it's functions into other sub-packages.

--- packages/misc/sleep.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from 'vitest'
-import { sleep } from './sleep'
-
-it('delay execution by ms milliseconds', async() => {
-  const start = Date.now()
-  await sleep(100)
-  expect(Date.now() - start).toBeGreaterThanOrEqual(100)
-})
--- packages/misc/sleep.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Create a promise that resolves after a given delay.
- * @param ms The amount of milliseconds to delay
- * @return A promise that resolves in `ms` milliseconds
- * @example
- * await sleep(1000)
- */
-export const sleep = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms))
