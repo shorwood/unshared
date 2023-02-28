4689d7c2b80454e4259a393e95b5ea16539f20ca
Stanley Horwood <stanley@hsjm.io>
2023-02-28 15:26:40 +0100

    feat(environment): introduced `isTsx` function
    Checks if the process running with [`tsx`](https://github.com/esbuild-kit/tsx) or using the `--loader=tsx` flag.

---
+++ packages/environment/isTsx.ts
@@ -0,0 +1,47 @@
+/**
+ * Checks if the process running with [`tsx`](https://github.com/esbuild-kit/tsx) or
+ * using the `--loader=tsx` flag.
+ *
+ * @returns `true` if the process is running with `tsx`.
+ * @example isTsx() // true
+ */
+export function isTsx() {
+  // --- Detect through environment variable.
+  if (process.env && 'ESBK_TSCONFIG_PATH' in process.env)
+    return process.env.ESBK_TSCONFIG_PATH !== ''
+
+  // --- Detect through the `execArgv` property.
+  if (process.execArgv) {
+    for (const argument of process.execArgv)
+      if (/--loader\s*[ =]\s*tsx/.test(argument)) return true
+  }
+
+  // --- Detect through the `argv` property.
+  return false
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  it('should return true when ESBK_TSCONFIG_PATH is set', () => {
+    vi.stubEnv('ESBK_TSCONFIG_PATH', '/path/to/tsconfig.json')
+    const result = isTsx()
+    expect(result).toEqual(true)
+  })
+
+  it('should return false when ESBK_TSCONFIG_PATH is not set', () => {
+    vi.stubEnv('ESBK_TSCONFIG_PATH', '')
+    const result = isTsx()
+    expect(result).toEqual(false)
+  })
+
+  it.each([
+    [true, '--loader=tsx'],
+    [true, '--loader = tsx'],
+    [true, '--loader tsx'],
+    [false, '--loader=ts-node/esm'],
+  ])('should return true when execArgv contains %s', (expected, loader) => {
+    vi.stubGlobal('process', { execArgv: [loader] })
+    const result = isTsx()
+    expect(result).toEqual(expected)
+  })
+}
