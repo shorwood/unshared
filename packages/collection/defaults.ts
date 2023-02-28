410bf68f0ac96a80144b12d3bdf4efdf86056d37
Stanley Horwood <stanley@hsjm.io>
2023-02-28 03:36:56 +0100

    feat(collection)!: improved type inference on `defaults` function
    Refactored tests to test the return type is correcly inferred

--- packages/collection/defaults.ts
+++ packages/collection/defaults.ts
@@ -1,25 +1,29 @@
-import { isNil } from '@unshared/predicate/isNil'
-
-export interface Defaults {
-  <T>(object: Partial<T>, defaults: T, depth?: number): T
-  <T>(object?: T, defaults?: Partial<T>, depth?: number): T
-  // <T1, T2>(object: T1, defaults: T2, depth?: number): Defaults<T1, T2>
-}
-
-// TODO: [collection/defaults] Improve return type in overloads
+import { Default } from '@unshared/types/Default'
 
 /**
- * Defaut property values of a collection.
+ * Defaut a value or object by another value or object. Meaning that if the
+ * first value is `undefined` or `null`, the second value will be used instead.
  *
- * @param object The object to apply the default values to.
- * @param from The default values to apply.
- * @param depth The maximum depth to apply the default values to.
+ * You can also defaults to nested objects by setting the `depth` to a
+ * positive number.
+ *
+ * @param object The object to default.
+ * @param defaults The object to default with.
+ * @param depth If the object is nested, the depth to the defaults.
  * @returns The object with the default values applied.
+ * @example defaults({ a: 1 }, { b: 2 }) // { a: 1, b: 2 }
  */
-export const defaults: Defaults = (object: any, from: any, depth = 0): any => {
+export function defaults<T extends object = object>(object: Partial<T>, defaults: T, depth?: number): T
+export function defaults<T extends object = object>(object: T, defaults: Partial<T>, depth?: number): T
+export function defaults<T1, T2, N extends number = 0>(object: T1, defaults: T2, depth?: N): Default<T1, T2, N>
+export function defaults(object: any, from: any, depth = 0): any {
   // --- Handle edge cases.
-  if (typeof depth !== 'number') throw new TypeError('Depth must be a number.')
-  if (typeof object !== 'object') return object ?? from
+  if (typeof depth !== 'number')
+    throw new TypeError('Expected depth to be a number')
+  if (depth < 0)
+    throw new RangeError('Expected depth to be a positive number')
+  if (typeof object !== 'object' || object === null)
+    return object ?? from
   if (!from) return { ...object }
 
   // --- Clone the object.
@@ -29,8 +33,8 @@ export const defaults: Defaults = (object: any, from: any, depth = 0): any => {
 
   // --- Iterate over the object's properties.
   for (const key in from) {
-    // --- If the value is nil, apply the default value.
-    if (isNil(object[key]))
+    // --- If the value is nil, the default value.
+    if (object[key] === undefined || object[key] === null)
       object[key] = from[key]
 
     // --- Apply defaults to nested objects.
@@ -44,21 +48,60 @@ export const defaults: Defaults = (object: any, from: any, depth = 0): any => {
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should apply default values', () => {
-    // eslint-disable-next-line unicorn/no-null
-    const object = { a: undefined, b: null, c: 0, d: false, e: '', f: {}, g: [] }
-    const defaultValue = { a: 1, b: 2, c: 3, d: 4, e: 5, f: { a: 1 }, g: [1], h: 'h' }
-    const expected = { a: 1, b: 2, c: 0, d: false, e: '', f: {}, g: [], h: 'h' }
-    const result = defaults<any>(object, defaultValue)
-    expect(result).toEqual(expected)
+  it('should default object values to undefined properties', () => {
+    const result = defaults({ a: 1 }, { a: 2, b: 3 })
+    expect(result).toEqual({ a: 1, b: 3 })
+    expectTypeOf(result).toEqualTypeOf<{ a: number; b: number }>()
   })
 
-  it('should apply default values to nested objects', () => {
+  it('should default object values to null properties', () => {
     // eslint-disable-next-line unicorn/no-null
-    const object = { a: undefined, b: null, c: 0, d: false, e: '', f: {}, g: [] }
-    const defaultValue = { a: 1, b: 2, c: 3, d: 4, e: 5, f: { a: 1 }, g: [1] }
-    const expected = { a: 1, b: 2, c: 0, d: false, e: '', f: { a: 1 }, g: [1] }
-    const result = defaults<any>(object, defaultValue, 1)
-    expect(result).toEqual(expected)
+    const result = defaults({ a: null }, { a: 2, b: 3 })
+    expect(result).toEqual({ a: 2, b: 3 })
+    expectTypeOf(result).toEqualTypeOf<{ a: number; b: number }>()
+  })
+
+  it('should not default falsy object values', () => {
+    const result = defaults({ a: 0 }, { a: 2, b: 3 })
+    expect(result).toEqual({ a: 0, b: 3 })
+    expectTypeOf(result).toEqualTypeOf<{ a: number; b: number }>()
+  })
+
+  it('should defaults nested objects', () => {
+    const result = defaults({ a: { b: 1 } }, { a: { b: 2, c: 3 } }, 1)
+    expect(result).toEqual({ a: { b: 1, c: 3 } })
+    expectTypeOf(result).toEqualTypeOf<{ a: { b: number; c: number } }>()
+  })
+
+  it('should not default nested objects if depth is 0', () => {
+    const result = defaults({ a: { b: 1 } }, { a: { b: 2, c: 3 } }, 0)
+    expect(result).toEqual({ a: { b: 1 } })
+    expectTypeOf(result).toEqualTypeOf<{ a: { b: number } }>()
+  })
+
+  it('should default array values to undefined properties', () => {
+    const result = defaults([1], [2, 3])
+    expect(result).toEqual([1, 3])
+    expectTypeOf(result).toEqualTypeOf<number[]>()
+  })
+
+  it('should default array values to null properties', () => {
+    // eslint-disable-next-line unicorn/no-null
+    const result = defaults([null], [2, 3])
+    expect(result).toEqual([2, 3])
+    expectTypeOf(result).toEqualTypeOf<number[]>()
+  })
+
+  it('should defaults a primitive and keep the value', () => {
+    const result = defaults(1, '2')
+    expect(result).toEqual(1)
+    expectTypeOf(result).toEqualTypeOf<number>()
+  })
+
+  it('should defaults a primitive and use the default', () => {
+    const value = undefined as number | undefined
+    const result = defaults(value, '2')
+    expect(result).toEqual('2')
+    expectTypeOf(result).toEqualTypeOf<number | string>()
   })
 }
