b69e5537f8c442e67244ac3e63e5fc22b6501b79
Stanley Horwood <stanley@hsjm.io>
2023-07-13 16:24:37 +0200

    feat(encode); introduced the `@unshared/encode` package

---
+++ packages/encode/decodeUtf8.ts
@@ -0,0 +1,21 @@
+/**
+ * Convert an UTF8 string to an ArrayBuffer
+ *
+ * @param value The string to convert
+ * @returns A new ArrayBuffer
+ * @example
+ * fromUtf8('Hello World') // ArrayBuffer([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64])
+ * fromUtf8('こんにちは') // ArrayBuffer([0xe3, 0x81, 0x93, 0xe3, 0x82, 0x93, 0xe3, 0x81, 0xab, 0xe3, 0x81, 0xa1, 0xe3, 0x81, 0xaf])
+ */
+export const fromUtf8 = (value: string): ArrayBuffer => {
+  // --- Create a new view on the string.
+  const result = new ArrayBuffer(value.length)
+  const view = new Uint8Array(result)
+
+  // --- Convert the string to bytes.
+  for (let index = 0; index < value.length; index++)
+    view[index] = value.charCodeAt(index)
+
+  // --- Return the result.
+  return result
+}
---
+++ packages/encode/encodeUtf8.ts
@@ -0,0 +1,12 @@
+/**
+ * Convert an ArrayBuffer to a UTF8 string
+ *
+ * @param buffer The buffer to convert
+ * @returns The UTF8 string
+ */
+export const toUtf8 = (buffer: ArrayBuffer): string => {
+  const view = new Uint8Array(buffer)
+  const result: string[] = []
+  for (const element of view) result.push(String.fromCharCode(element))
+  return result.join('')
+}
