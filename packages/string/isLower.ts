d6f4960e2ad8621cf51cce5b30c05545f278c5c4
Stanley Horwood <stanley@hsjm.io>
2023-07-13 16:28:22 +0200

    feat(string)!: restricted input parameters to `number`

--- packages/string/isLower.ts
+++ packages/string/isLower.ts
@@ -4,15 +4,10 @@
  *
  * @param value The string or character code to check.
  * @returns `true` if the string or character code is lower case.
- * @example isLower('a') // returns true
+ * @example isLower('a'.charCodeAt(0)) // true
  */
-export function isLower(value: string): value is Lowercase<string>
-export function isLower(value: string | number): boolean
-export function isLower(value: string | number): boolean {
-  if (typeof value === 'number') return value >= 97 && value <= 122
-  if (value.length !== 1) throw new SyntaxError('Expected a single character')
-  const char = value.charCodeAt(0)
-  return char >= 97 && char <= 122
+export function isLower(value: number): boolean {
+  return value >= 97 && value <= 122
 }
 
 /* c8 ignore next */
@@ -21,20 +16,6 @@ if (import.meta.vitest) {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
   const numbers = '0123456789'
   const symbols = '!@#$%^&*()_+-=[]{};:,./<>?`~'
-
-  it('should return true for lower case characters', () => {
-    for (const char of lowerCase) {
-      const result = isLower(char)
-      expect(result).toEqual(true)
-    }
-  })
-
-  it('should return false for non lower case characters', () => {
-    for (const char of upperCase + numbers + symbols) {
-      const result = isLower(char)
-      expect(result).toEqual(false)
-    }
-  })
 
   it('should return true for lower case character codes', () => {
     for (const char of lowerCase) {
@@ -48,16 +29,5 @@ if (import.meta.vitest) {
       const result = isLower(char.charCodeAt(0))
       expect(result).toEqual(false)
     }
-  })
-
-  it('should throw an error for a string with more than one character', () => {
-    const shouldThrow = () => isLower('foo')
-    expect(shouldThrow).toThrow(/single character/)
-  })
-
-  it('should predicate guard for lower case string', () => {
-    const string = 'a' as 'a' | 'A'
-    const result = isLower(string)
-    if (result) expectTypeOf(string).toEqualTypeOf<'a'>()
   })
 }
--- packages/string/isUpper.ts
+++ packages/string/isUpper.ts
@@ -4,15 +4,10 @@
  *
  * @param value The string or character code to check.
  * @returns `true` if the string or character code is upper case.
- * @example isUpper('A') // returns true
+ * @example isUpper('A'.charCodeAt(0)) // true
  */
-export function isUpper(value: string): value is Uppercase<string>
-export function isUpper(value: string | number): boolean
-export function isUpper(value: string | number): boolean {
-  if (typeof value === 'number') return value >= 97 && value <= 122
-  if (value.length !== 1) throw new SyntaxError('Expected a single character')
-  const char = value.charCodeAt(0)
-  return char >= 97 && char <= 122
+export function isUpper(value: number): boolean {
+  return value >= 97 && value <= 122
 }
 
 /* c8 ignore next */
@@ -21,20 +16,6 @@ if (import.meta.vitest) {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
   const numbers = '0123456789'
   const symbols = '!@#$%^&*()_+-=[]{};:,./<>?`~'
-
-  it('should return true for upper case characters', () => {
-    for (const char of upperCase) {
-      const result = isUpper(char)
-      expect(result).toEqual(true)
-    }
-  })
-
-  it('should return false for non upper case characters', () => {
-    for (const char of lowerCase + numbers + symbols) {
-      const result = isUpper(char)
-      expect(result).toEqual(false)
-    }
-  })
 
   it('should return true for upper case character codes', () => {
     for (const char of upperCase) {
@@ -48,16 +29,5 @@ if (import.meta.vitest) {
       const result = isUpper(char.charCodeAt(0))
       expect(result).toEqual(false)
     }
-  })
-
-  it('should throw an error for a string with more than one character', () => {
-    const shouldThrow = () => isUpper('foo')
-    expect(shouldThrow).toThrow(/single character/)
-  })
-
-  it('should predicate guard for upper case string', () => {
-    const string = 'a' as 'a' | 'A'
-    const result = isUpper(string)
-    if (result) expectTypeOf(string).toEqualTypeOf<'A'>()
   })
 }
