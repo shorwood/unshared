ba672bcd5e0bc51810b05f3ce7541a79f3b18424
Stanley Horwood <stanley@hsjm.io>
2023-07-13 18:18:37 +0200

    feat(string): introduced `escapeAnsiSequences` function

---
+++ packages/string/escapeAnsiSequences.ts
@@ -0,0 +1,33 @@
+const ANSI_REGEX = new RegExp([
+  '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+  '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
+].join('|'), 'g')
+
+/**
+ * Escapes ANSI sequences from a string.
+ *
+ * @param text The string to escape.
+ * @returns The escaped string.
+ * @example escapeAnsiSequences('\u001B[1mHello\u001B[22m') // 'Hello'
+ */
+export function escapeAnsiSequences(text: string): string {
+  return text.replace(ANSI_REGEX, '')
+}
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should escape ANSI sequences from a string', () => {
+    const result = escapeAnsiSequences('\u001B[1mHello\u001B[22m')
+    expect(result).toEqual('Hello')
+  })
+
+  it('should escape ANSI sequences from a string with multiple sequences', () => {
+    const result = escapeAnsiSequences('\u001B[1mHello\u001B[22m \u001B[2mWorld\u001B[22m')
+    expect(result).toEqual('Hello World')
+  })
+
+  it('should escape hyperlinks', () => {
+    const result = escapeAnsiSequences('\u001B]8;;https://example.com\u0007Hello\u001B]8;;\u0007')
+    expect(result).toEqual('Hello')
+  })
+}
