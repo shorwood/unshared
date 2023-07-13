3b476b007e07a7e3d7ac17a7539c09aa18fc8a0f
Stanley Horwood <stanley@hsjm.io>
2023-07-13 16:28:28 +0200

    feat(types): introduced the `OverloadsParameters` and `OverloadsReturnType` types

---
+++ packages/types/OverloadsParameters.ts
@@ -0,0 +1,38 @@
+import { Function } from './Function'
+import { Overloads } from './Overloads'
+
+/**
+ * Extract the parameters of all functions in a tuple.
+ */
+type ExtractTupleParameters<T extends Function[]> = {
+  [K in keyof T]: T[K] extends (...args: infer P) => any ? P : never
+}[number]
+
+/**
+ * Extract the parameters from the signatures of a function with overloads.
+ *
+ * @template T The function type.
+ * @example
+ * function add(a: number, b: number): number
+ * function add(a: string, b: string): string
+ * type Result = OverloadsParameters<typeof add> // [number, number] | [string, string]
+ */
+export type OverloadsParameters<T extends Function> = ExtractTupleParameters<Overloads<T>>
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should infer the parameters of a function', () => {
+    type Method = (a: number, b: string) => boolean
+    type Result = OverloadsParameters<Method>
+    expectTypeOf<Result>().toEqualTypeOf<[number, string]>()
+  })
+
+  it('should infer the parameters of a function with overloads', () => {
+    interface Method {
+      (a: number, b: string): boolean
+      (a: string, b: number): boolean
+    }
+    type Result = OverloadsParameters<Method>
+    expectTypeOf<Result>().toEqualTypeOf<[number, string] | [string, number]>()
+  })
+}
---
+++ packages/types/OverloadsReturnTypes.ts
@@ -0,0 +1,38 @@
+import { Function } from './Function'
+import { Overloads } from './Overloads'
+
+/**
+ * Extract the return types of all functions in a tuple.
+ */
+type ExtractTupleReturnTypes<T extends Function[]> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => infer P ? P : never
+}[number]
+
+/**
+ * Extract the return types from the signatures of a function with overloads.
+ *
+ * @template T The function type.
+ * @example
+ * function add(a: number, b: number): number
+ * function add(a: string, b: string): string
+ * type Result = OverloadsParameters<typeof add> // [number, number] | [string, string]
+ */
+export type OverloadsReturnTypes<T extends Function> = ExtractTupleReturnTypes<Overloads<T>>
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should infer the parameters of a function', () => {
+    type Method = (a: number, b: string) => boolean
+    type Result = OverloadsReturnTypes<Method>
+    expectTypeOf<Result>().toEqualTypeOf<boolean>()
+  })
+
+  it('should infer the parameters of a function with overloads', () => {
+    interface Method {
+      (a: number, b: string): string
+      (a: string, b: number): number
+    }
+    type Result = OverloadsReturnTypes<Method>
+    expectTypeOf<Result>().toEqualTypeOf<string | number>()
+  })
+}
