1345b14c3038718a3ff9bbdf106fd65604492739
Stanley Horwood <stanley@hsjm.io>
2023-09-12 03:38:55 +0200

    chore(collection): rename `arrayify` to `castArray` and update types
    The `arrayify` function within the `collection` package has been renamed to `castArray`. Along with this modification, the import of types `Array`, `MaybeArray`, `Nil` has been simplified and brought from `@unshared/types`.
    
    The tests and usages reflect these changes as well and have been updated correspondingly. This renaming provides a better understanding of the function's purpose and usage.

--- packages/collection/castArray.ts
+++ packages/collection/castArray.ts
@@ -1,6 +1,4 @@
-import { Array } from '@unshared/types/Array'
-import { MaybeArray } from '@unshared/types/MaybeArray'
-import { Nil } from '@unshared/types/Nil'
+import { Array, MaybeArray, Nil } from '@unshared/types'
 
 /**
  * Wrap a value into an array if it is not one already. If the value is
@@ -8,40 +6,44 @@ import { Nil } from '@unshared/types/Nil'
  *
  * @param value The value to wrap.
  * @returns The arrayified value.
- * @example arrayify(1) // [1]
+ * @example castArray(1) // [1]
  */
-export function arrayify(value?: Nil): []
-export function arrayify<U>(value?: Array<U>): Array<U>
-export function arrayify<U>(value?: MaybeArray<U>): Array<U>
-export function arrayify(value?: MaybeArray): unknown[] {
+export function castArray(value?: Nil): []
+export function castArray<U>(value?: MaybeArray<U>): Array<U>
+export function castArray(value?: unknown): unknown[] {
+  // --- If the value is undefined or null, return an empty array.
   if (value === undefined || value === null) return []
+
+  // --- Return array as-is.
   if (Array.isArray(value)) return value
+
+  // --- Wrap primitive value in an array.
   return [value]
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should return the array if the value is an array', () => {
-    const result = arrayify([1, 2, 3])
+    const result = castArray([1, 2, 3])
     expect(result).toEqual([1, 2, 3])
     expectTypeOf(result).toEqualTypeOf<Array<number>>()
   })
 
   it('should return an array with the value if the value is not an array', () => {
-    const result = arrayify(1)
+    const result = castArray(1)
     expect(result).toEqual([1])
     expectTypeOf(result).toEqualTypeOf<Array<number>>()
   })
 
   it('should return an empty array if the value is undefined', () => {
-    const result = arrayify()
+    const result = castArray()
     expect(result).toEqual([])
     expectTypeOf(result).toEqualTypeOf<[]>
   })
 
   it('should return an empty array if the value is null', () => {
     // eslint-disable-next-line unicorn/no-null
-    const result = arrayify(null)
+    const result = castArray(null)
     expect(result).toEqual([])
     expectTypeOf(result).toEqualTypeOf<[]>
   })
