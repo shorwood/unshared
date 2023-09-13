3481cd628419d26557e83f8dd6e43dd5864ea628
Stanley Horwood <stanley@hsjm.io>
2023-09-13 05:27:43 +0200

    feat(string): add support for negative limits in `split` function
    This commit adds support for negative limits in the `split` function. The `split` function now accepts a negative limit to get the last N elements from the split result. It also includes a type check in the function signature to ensure the limit is an integer. The associated test cases have been updated accordingly to verify the new feature.

--- packages/string/split.ts
+++ packages/string/split.ts
@@ -1,7 +1,10 @@
-import { StringSplit } from '@unshared/types/StringSplit'
+import { NumberInteger } from '@unshared/types'
 
 /**
- * Split the string into an array of substrings using the specified delimiter.
+ * Split the string into an array of substrings using the specified delimiter. The
+ * main difference between this function and the native `String.prototype.split` is
+ * that this function supports negative limits. Meaning that you can use a negative
+ * limit to get the last N elements of the string.
  *
  * @param value The string to split.
  * @param delimiter The character to use as a separator.
@@ -9,12 +12,10 @@ import { StringSplit } from '@unshared/types/StringSplit'
  * @returns An array of substrings.
  * @example split('a,b,c', ',') // => ['a', 'b', 'c']
  */
-export function split<S extends string, D extends string, N extends number = number>(value: S, delimiter: D, limit?: N): StringSplit<S, D, N>
-export function split(value: string, delimiter: string | RegExp, limit?: number): string[]
-export function split(value: string, delimiter: string | RegExp, limit?: number): string[] {
-  return limit > 0
-    ? value.split(delimiter, limit)
-    : value.split(delimiter).slice(limit)
+export function split<N extends number = number>(value: string, delimiter: string | RegExp, limit?: NumberInteger<N>): string[] {
+  return limit !== undefined && limit < 0
+    ? value.split(delimiter).slice(limit)
+    : value.split(delimiter, limit)
 }
 
 /* c8 ignore next */
@@ -32,16 +33,5 @@ if (import.meta.vitest) {
   it('should split a string with a negative limit', () => {
     const result = split('a,b,c', ',', -2)
     expect(result).toEqual(['b', 'c'])
-  })
-
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => split(1, ',')
-    expect(shouldThrow).toThrow(TypeError)
-  })
-
-  it('should throw if limit is not an integer', () => {
-    const shouldThrow = () => split('a,b,c', ',', 1.1)
-    expect(shouldThrow).toThrow(TypeError)
   })
 }
