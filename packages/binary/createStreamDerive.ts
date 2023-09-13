39b1b9dc0a1783c77507269edf04d5778734f0cc
Stanley Horwood <stanley@hsjm.io>
2023-09-13 19:33:20 +0200

    feat(binary): create `createStreamDerive` function
    Added a new `createStreamDerive` function to derive a value from a stream without consuming the stream. This is useful for computing checksums, digests, and other values from a stream without having to buffer the entire stream in memory. Added tests to ensure its correct functionality.

---
+++ packages/binary/createStreamDerive.ts
@@ -0,0 +1,109 @@
+import { Readable, Transform } from 'node:stream'
+import { Awaitable, awaitable } from '@unshared/functions/awaitable'
+import { streamRead } from './streamRead'
+
+/** Callback that is executed with the stream chunks. */
+export type StreamDeriveFunction<T> = (value: { chunk: Buffer; encoding: BufferEncoding; value: T }) => T
+
+/** A transform stream that derives a value from the stream chunks. */
+class Derive<T> extends Transform {
+  /**
+   * Create a transform stream that derives a value from the stream chunks.
+   * The chunks are passed-through to the stream, and the derived value is
+   * resolved once the stream has been consumed.
+   *
+   * @param derive The callback to execute with the stream chunks.
+   * @param value The initial value of the derived value.
+   * @example
+   * const stream = fs.createReadStream('file.txt')
+   * const length = new Derive(({ chunk, value }) => value + chunk.length, 0)
+   *
+   * // Pipe the stream to a file.
+   * const writeStream = fs.createWriteStream('file.txt')
+   * stream.pipe(writeStream)
+   *
+   * // Once the stream has been consumed, get the length of the file.
+   * const length = await length.finalValue // 13
+   */
+  constructor(private readonly derive: StreamDeriveFunction<T>, private value: T) {
+    super()
+
+    // --- Initialize a promise that can be resolved from outside the constructor.
+    this.finalValue = new Promise(resolve => this.resolveDerivedValue = resolve)
+  }
+
+  /** The chunks read from the stream. */
+  private resolveDerivedValue!: (value: T) => void
+
+  /** Promise that resolves to the value returned from the callback. */
+  public finalValue: Promise<T>
+
+  // --- Intercepts the chunks read from the stream and executes the callback.
+  override async _transform(chunk: Buffer, encoding: BufferEncoding, callback: () => void) {
+    this.value = this.derive({ chunk, encoding, value: this.value })
+    this.push(chunk, encoding)
+    callback()
+  }
+
+  // --- Resolves the promise once the stream has been consumed.
+  override _final(callback: () => void) {
+    this.resolveDerivedValue(this.value!)
+    callback()
+  }
+}
+
+/**
+ * Derive a value from a stream of bytes without consuming the stream. This
+ * function will intercept the chunks read from subsequent calls to `read()`,
+ * execute the given callback, and then pass the chunks back into the stream.
+ *
+ * This function returns an `Awaitable` value, which means that the synchronous
+ * value is the stream itself, and the asynchronous value is the value returned
+ * from the callback once the stream has been consumed.
+ *
+ * This function is useful for computing checksums, digests, and other values
+ * from a stream without consuming the stream. For example, you can pipe the
+ * stream to a file, and then get the checksum once the stream has been consumed
+ * without having to buffer the entire stream in memory.
+ *
+ * @param derive The callback to execute with the stream chunks.
+ * @param initialValue The initial value of the derived value.
+ * @returns A promise that resolves to the value returned from the callback.
+ * @example
+ * const stream = fs.createReadStream('file.txt')
+ * const derivedLength = createStreamDerive(({ chunk, value }) => value + chunk.length, 0)
+ * stream.pipe(derivedLength)
+ *
+ * // Pipe the stream to a file.
+ * const writeStream = fs.createWriteStream('file.txt')
+ * stream.pipe(writeStream)
+ *
+ * // Once the stream has been consumed, get the length of the file.
+ * const length = await derivedLength // 13
+ */
+export function createStreamDerive<T>(derive: StreamDeriveFunction<T>, initialValue: T): Awaitable<Transform, T> {
+  const stream = new Derive(derive, initialValue)
+  return awaitable(stream, () => stream.finalValue)
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  const valueUtf8 = 'Hello, world!'
+  const valueBuffer = Buffer.from(valueUtf8, 'utf8')
+
+  it('should passthrough the stream chunks', async() => {
+    const stream = Readable.from(valueBuffer)
+    const derivedLength = createStreamDerive(({ chunk, value }) => value! + chunk.length, 0)
+    stream.pipe(derivedLength)
+    const buffer = await streamRead(derivedLength, 'utf8')
+    expect(buffer).toEqual(valueUtf8)
+  })
+
+  it('should derive a value from the stream chunks', async() => {
+    const stream = Readable.from(valueBuffer)
+    const derivedLength = createStreamDerive(({ chunk, value }) => value! + chunk.length, 0)
+    stream.pipe(derivedLength)
+    const length = await derivedLength
+    expect(length).toEqual(13)
+  })
+}
