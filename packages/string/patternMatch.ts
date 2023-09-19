3882e11158cd407e33b23d83fd423ea116ba5d9b
Stanley Horwood <stanley@hsjm.io>
2023-09-19 17:26:35 +0200

    feat(string)!: Changed parameter order in `patternMatch` and `patternMatches` functions
    These changes reverse the parameter order for both the `patternMatch` and `patternMatches` functions in the string package. In addition, both functions now use the `createPatternRegexp` function instead of `patternRegex`. This change conforms with many functional programming patterns where the data to be operated on is provided last. This is a breaking change for any implementation that uses these functions.

--- packages/string/patternMatch.ts
+++ packages/string/patternMatch.ts
@@ -1,21 +1,21 @@
-import { patternRegex } from './patternRegex'
+import { createPatternRegexp } from './createPatternRegexp'
 
 /**
  * Returns `true` if the given string matches the given glob pattern.
  *
- * @param string The string to match.
  * @param pattern The glob pattern to match against.
+ * @param match The string to match against the pattern.
  * @returns `true` if the string matches the pattern.
- * @example patternMatch('Foo', '[fF]o*') // true
+ * @example patternMatch('{f,F}o*', 'Foo') // true
  */
-export function patternMatch(string: string, pattern: string): boolean {
-  return patternRegex(pattern).test(string)
+export function patternMatch(pattern: string, match: string): boolean {
+  return createPatternRegexp(pattern).test(match)
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should match a string by pattern', () => {
-    const result = patternMatch('foo', 'fo*')
+    const result = patternMatch('{f,F}o*', 'Foo')
     expect(result).toEqual(true)
   })
 }
