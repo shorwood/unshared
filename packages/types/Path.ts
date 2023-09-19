95bbbdf62591bdc6cbc42c6fa8471e2bf30be5d7
Stanley Horwood <stanley@hsjm.io>
2023-09-19 19:20:00 +0200

    feat(types): improve `Path` helper to match non literal and optimize typchecking
    The `Path` type helper in the `types` package is now improved to match against non literal strings and is now using lighter type helpers(`IsZero` and `Substract`). This would make type checking more efficient and would allow the use of non-literal strings. These changes were reflected in each specific test case.

--- packages/types/Path.ts
+++ packages/types/Path.ts
@@ -1,7 +1,10 @@
-import { MathDecrease } from './MathDecrease'
+import { IsZero, Substract } from './utils'
 
 /**
- * Extract nested paths of an object.
+ * Extract all of the nested keys of an object. The keys are returned as a
+ * string literal union. This is useful for creating a type that can be used to
+ * access nested properties of an object allowing comprehensive autocompletion
+ * and type checking.
  *
  * @template T Object type
  * @template N Number of nested keys to explore
@@ -13,7 +16,7 @@ export type Path<T, N extends number = 8, P extends string = ''> =
   T extends string | any[] ? P extends '' ? number : `${P}${number}` | `${P}${number}.${string}`
 
     // --- Recursion threshold reached, return loose path.
-    : N extends 0 ? `${P}${Extract<keyof T, string | number>}`
+    : IsZero<N> extends true ? `${P}${Extract<keyof T, string | number>}`
       : { [K in keyof T]-?: K extends string | number
 
         // --- Concat the current path with the keys of T
@@ -22,20 +25,20 @@ export type Path<T, N extends number = 8, P extends string = ''> =
         // --- Check for recursion threshold.
         // --- If value is an object, recurse
         | (T[K] extends object
-          ? Path<T[K], MathDecrease<N>, `${P}${K}.`>
+          ? Path<T[K], Substract<N, 1>, `${P}${K}.`>
           : `${P}${K}.${string}`)
 
         // --- Ignore symbol if nested.
         : P extends '' ? K : never
 
       // --- Extract keys only.
-      }[keyof T]
+      }[keyof T] | string & {}
 
 /** c8 ignore next */
 if (import.meta.vitest) {
   it('should infer the path of a nested object', () => {
     type Result = Path<{ foo: { bar: { baz: string } } }>
-    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | 'foo.bar.baz' | `foo.bar.baz.${string}`
+    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | 'foo.bar.baz' | `foo.bar.baz.${string}` | string & {}
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
@@ -47,25 +50,25 @@ if (import.meta.vitest) {
 
   it('should infer the path of a nested array', () => {
     type Result = Path<{ foo: { bar: { baz: string[] } } }>
-    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | `foo.bar.baz.${number}` | `foo.bar.baz.${number}.${string}`
+    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | `foo.bar.baz.${number}` | `foo.bar.baz.${number}.${string}` | string & {}
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should infer the path of a nested object with a number key', () => {
     type Result = Path<{ foo: string; [x: number]: string }>
-    type expeted = number | 'foo' | `${number}.${string}` | `foo.${string}`
+    type expeted = number | 'foo' | `${number}.${string}` | `foo.${string}` | string & {}
     expectTypeOf<Result>().toEqualTypeOf<expeted>()
   })
 
   it('should infer the path of a nested object with a symbol key', () => {
     type Result = Path<{ foo: string; [x: symbol]: string }>
-    type expeted = symbol | 'foo' | `foo.${string}`
+    type expeted = symbol | 'foo' | `foo.${string}` | string & {}
     expectTypeOf<Result>().toEqualTypeOf<expeted>()
   })
 
   it('should ignore nested symbol keys', () => {
     type Result = Path<{ foo: Record<symbol, string> }>
-    type Expected = 'foo'
+    type Expected = 'foo' | string & {}
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
