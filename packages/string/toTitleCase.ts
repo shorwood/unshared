61dbf4ff2b7a70c38de570817db91ece7b779ab8
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:59:55 +0200

    feat(string): allow `toTitleCase` to handle multiple words
    `toTitleCase` function in `string` package has been updated to allow conversion of multiple strings into title case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toTitleCase.ts
+++ packages/string/toTitleCase.ts
@@ -1,41 +1,73 @@
 import { tokenize } from './tokenize'
 
 /**
- * Converts a string to title case.
+ * Convert a string to title case.
  *
- * @param value The string to convert
- * @returns The converted string
- * @throws If argument is not a string
- * @example toCamelCase('fooBar') // returns 'Foo Bar'
+ * @param values The string(s) to convert.
+ * @returns The converted string to title case.
+ * @example toTitleCase('FOO_BAR', 'Baz') // 'Foo Bar Baz'
  */
-export function toTitleCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Tokenize and capitalize the first letter of each token.
-  return tokenize(value)
+export function toTitleCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase())
     .join(' ')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'Camel Case'],
-    ['PascalCase', 'Pascal Case'],
-    ['snake_case', 'Snake Case'],
-    ['kebab-case', 'Kebab Case'],
-    ['Title Case', 'Title Case'],
-    ['UPPER CASE', 'Upper Case'],
-    ['lower case', 'Lower Case'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toTitleCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to title-case', () => {
+    const result = toTitleCase('camelCase')
+    expect(result).toEqual('Camel Case')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toTitleCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to title-case', () => {
+    const result = toTitleCase('CONSTANT_CASE')
+    expect(result).toEqual('Constant Case')
+  })
+
+  it('should convert dot-case to title-case', () => {
+    const result = toTitleCase('dot.case')
+    expect(result).toEqual('Dot Case')
+  })
+
+  it('should convert header-case to title-case', () => {
+    const result = toTitleCase('Header-Case')
+    expect(result).toEqual('Header Case')
+  })
+
+  it('should convert kebab-case to title-case', () => {
+    const result = toTitleCase('kebab-case')
+    expect(result).toEqual('Kebab Case')
+  })
+
+  it('should convert pascal-case to title-case', () => {
+    const result = toTitleCase('PascalCase')
+    expect(result).toEqual('Pascal Case')
+  })
+
+  it('should convert path-case to title-case', () => {
+    const result = toTitleCase('path/case')
+    expect(result).toEqual('Path Case')
+  })
+
+  it('should convert snake_case to title-case', () => {
+    const result = toTitleCase('snake_case')
+    expect(result).toEqual('Snake Case')
+  })
+
+  it('should convert title-case to title-case', () => {
+    const result = toTitleCase('Title Case')
+    expect(result).toEqual('Title Case')
+  })
+
+  it('should convert lower-case to title-case', () => {
+    const result = toTitleCase('lower case')
+    expect(result).toEqual('Lower Case')
+  })
+
+  it('should convert multiple strings to title-case', () => {
+    const result = toTitleCase('foo', 'bar', 'baz')
+    expect(result).toEqual('Foo Bar Baz')
   })
 }
