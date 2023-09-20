3107a079c3536e219a4ff70b1d65925f8051be74
Stanley Horwood <stanley@hsjm.io>
2023-09-20 18:05:51 +0200

    chore(string): refactor `toLowerCase` and `toUpperCase` functions
    Improved the `toLowerCase` and `toUpperCase` functions in the `string` package. Now, these functions provide better type inference with less code. Also, the test cases have been updated to reflect these changes. The new string handling matches the built-in String methods but gives TypeScript enhanced understanding of the types.

--- packages/string/toLowerCase.ts
+++ packages/string/toLowerCase.ts
@@ -1,9 +1,11 @@
 /**
- * Changes the case of a string to lower case.
+ * Changes the case of a string to lower case. Is the same as
+ * `String.prototype.toLowerCase` but provides type inference for
+ * the new string.
  *
  * @param value The string to convert to lower case.
  * @returns The lower case string.
- * @example toCamelCase('FOO_BAR') // returns 'foo_bar'
+ * @example toLowerCase('FOO_BAR_1') // 'foo_bar_1'
  */
 export function toLowerCase<S extends string>(value: S): Lowercase<S> {
   return value.toLowerCase() as Lowercase<S>
--- packages/string/toUpperCase.ts
+++ packages/string/toUpperCase.ts
@@ -1,29 +1,21 @@
 /**
- * Converts a string to upper case.
+ * Changes the case of a string to upper case. Is the same as
+ * `String.prototype.toUpperCase` but provides type inference for
+ * the new string.
  *
- * @param value The string to convert.
- * @returns The upper cased string.
- * @throws If argument is not a string.
- * @example toCamelCase('fooBar') // returns 'FOOBAR'
+ * @param value The string to convert to upper case.
+ * @returns The upper case string.
+ * @example toUpperCase('foo_bar_1') // 'FOO_BAR_1'
  */
-export function toUpperCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Convert to upper case.
-  return value.toUpperCase()
+export function toUpperCase<S extends string>(value: S): Uppercase<S> {
+  return value.toUpperCase() as Uppercase<S>
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it('should convert a string to lower case', () => {
+  it('should convert a string to upper case', () => {
     const result = toUpperCase('foo_bar_1')
     expect(result).toEqual('FOO_BAR_1')
-  })
-
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toUpperCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+    expectTypeOf(result).toEqualTypeOf<'FOO_BAR_1'>()
   })
 }
