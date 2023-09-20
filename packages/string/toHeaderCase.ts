0947866265b26a586a65dabab0ca7a263e4f6f56
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:57:56 +0200

    feat(string): allow `toHeaderCase` to handle multiple words
    `toHeaderCase` function in `string` package has been updated to allow conversion of multiple strings into header case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toHeaderCase.ts
+++ packages/string/toHeaderCase.ts
@@ -1,41 +1,73 @@
 import { tokenize } from './tokenize'
 
 /**
- * Converts a string to header case.
+ * Convert a string to header case.
  *
- * @param value The string to convert.
- * @returns The converted string.
- * @throws If argument is not a string.
- * @example toHeaderCase('fooBar') // returns 'Foo-Bar'
+ * @param values The string(s) to convert.
+ * @returns The converted string to header case.
+ * @example toHeaderCase('FOO_BAR', 'Baz') // 'Foo-Bar-Baz'
  */
-export function toHeaderCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Convert to header case.
-  return tokenize(value)
+export function toHeaderCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase())
     .join('-')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'Camel-Case'],
-    ['PascalCase', 'Pascal-Case'],
-    ['snake_case', 'Snake-Case'],
-    ['kebab-case', 'Kebab-Case'],
-    ['Title Case', 'Title-Case'],
-    ['UPPER CASE', 'Upper-Case'],
-    ['lower case', 'Lower-Case'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toHeaderCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to header-case', () => {
+    const result = toHeaderCase('camelCase')
+    expect(result).toEqual('Camel-Case')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toHeaderCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to header-case', () => {
+    const result = toHeaderCase('CONSTANT_CASE')
+    expect(result).toEqual('Constant-Case')
+  })
+
+  it('should convert dot-case to header-case', () => {
+    const result = toHeaderCase('dot.case')
+    expect(result).toEqual('Dot-Case')
+  })
+
+  it('should convert header-case to header-case', () => {
+    const result = toHeaderCase('Header-Case')
+    expect(result).toEqual('Header-Case')
+  })
+
+  it('should convert kebab-case to header-case', () => {
+    const result = toHeaderCase('kebab-case')
+    expect(result).toEqual('Kebab-Case')
+  })
+
+  it('should convert pascal-case to header-case', () => {
+    const result = toHeaderCase('PascalCase')
+    expect(result).toEqual('Pascal-Case')
+  })
+
+  it('should convert path-case to header-case', () => {
+    const result = toHeaderCase('path/case')
+    expect(result).toEqual('Path-Case')
+  })
+
+  it('should convert snake_case to header-case', () => {
+    const result = toHeaderCase('snake_case')
+    expect(result).toEqual('Snake-Case')
+  })
+
+  it('should convert title-case to header-case', () => {
+    const result = toHeaderCase('Title Case')
+    expect(result).toEqual('Title-Case')
+  })
+
+  it('should convert lower-case to header-case', () => {
+    const result = toHeaderCase('lower case')
+    expect(result).toEqual('Lower-Case')
+  })
+
+  it('should convert multiple strings to header-case', () => {
+    const result = toHeaderCase('foo', 'bar', 'baz')
+    expect(result).toEqual('Foo-Bar-Baz')
   })
 }
