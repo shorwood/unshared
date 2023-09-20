cf6bd5b1f5464f0a72e9cb43443f28c1e17be80f
Stanley Horwood <stanley@hsjm.io>
2023-09-20 18:06:54 +0200

    feat(string): enhance trim function with type inference
    The `trim` function within the `string` package has been upgraded to support type inference for the resulting string. Now, the function correctly infers the type of the trimmed string, which can be beneficial in type-safe string manipulations. The relevant tests have been updated to check this behavior.

--- packages/string/trim.ts
+++ packages/string/trim.ts
@@ -1,22 +1,37 @@
+import type { CharacterWhitespace } from '@unshared/types'
+
+/**
+ * Removes the leading and trailing white space and line terminator
+ * characters from a string.
+ *
+ * @template S The string to trim.
+ * @returns The trimmed string.
+ * @example trim(' Hello world ') // 'Hello world'
+ */
+export type Trimmed<S extends string> =
+  S extends `${CharacterWhitespace}${infer R}` ? Trimmed<R> :
+    S extends `${infer L}${CharacterWhitespace}` ? Trimmed<L> :
+      S
+
 /**
  * Removes the leading and trailing white space and line terminator
  * characters from a string. This function is a wrapper around the
  * `String.prototype.trim` method but allows to use it in a functional
- * manner.
+ * manner. It also provides type inference for the new string.
  *
- * @param string The string to trim
- * @returns The trimmed string
- * @throws If argument is not a string
- * @example trim('\nfoo bar\t') // returns 'foo bar'
+ * @param string The string to trim.
+ * @returns The trimmed string.
+ * @example trim(' Hello world ') // 'Hello world'
  */
-export function trim(string: string): string {
-  return string.trim()
+export function trim<S extends string>(string: S): Trimmed<S> {
+  return string.trim() as Trimmed<S>
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should remove whitespace from both sides of a string', () => {
-    const result = trim(' foo bar ')
-    expect(result).toEqual('foo bar')
+    const result = trim(' Hello world ')
+    expect(result).toEqual('Hello world')
+    expectTypeOf(result).toEqualTypeOf<'Hello world'>()
   })
 }
