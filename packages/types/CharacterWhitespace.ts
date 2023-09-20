b6334e001fb2bf6e1215bbedcf04ca57e0335d0b
Stanley Horwood <stanley@hsjm.io>
2023-09-20 18:55:10 +0200

    feat(types): add `CharacterWhitespace` type
    A new type `CharacterWhitespace` has been added to the types package. This type is used to represent a whitespace character. Test cases have been added to validate that it does not match with lowercase letters, uppercase letters, digits or symbols but only with whitespace characters.

---
+++ packages/types/CharacterWhitespace.ts
@@ -0,0 +1,34 @@
+import { CharacterDigit } from './CharacterDigit'
+import { CharacterLower } from './CharacterLower'
+import { CharacterSymbol } from './CharacterSymbol'
+import { CharacterUpper } from './CharacterUpper'
+
+/**
+ * A whitespace character. This includes the space character, the tab character,
+ * the line feed character, the carriage return character, and the vertical
+ * tab character.
+ */
+export type CharacterWhitespace = ' ' | '\n' | '\t' | '\r' | '\v'
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should not match lowercase letters', () => {
+    expectTypeOf<CharacterLower>().not.toMatchTypeOf<CharacterWhitespace>()
+  })
+
+  it('should not match uppercase letters', () => {
+    expectTypeOf<CharacterUpper>().not.toMatchTypeOf<CharacterWhitespace>()
+  })
+
+  it('should not match a digit', () => {
+    expectTypeOf<CharacterDigit>().not.toMatchTypeOf<CharacterWhitespace>()
+  })
+
+  it('should not match a symbol', () => {
+    expectTypeOf<CharacterSymbol>().not.toMatchTypeOf<CharacterWhitespace>()
+  })
+
+  it('should match a whitespace character', () => {
+    expectTypeOf<' ' | '\n' | '\t' | '\r' | '\v'>().toMatchTypeOf<CharacterWhitespace>()
+  })
+}
