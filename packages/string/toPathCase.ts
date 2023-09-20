b884d75846033b7a361681a0889ce123ba5d74ef
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:59:11 +0200

    feat(string): allow `toPathCase` to handle multiple words
    `toPathCase` function in `string` package has been updated to allow conversion of multiple strings into path case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toPathCase.ts
+++ packages/string/toPathCase.ts
@@ -1,41 +1,73 @@
 import { tokenize } from './tokenize'
 
 /**
- * Converts a string to path case.
+ * Convert a string to path case.
  *
- * @param value The string to convert.
- * @returns The converted string.
- * @throws If argument is not a string.
- * @example toPathCase('fooBar') // returns 'foo/bar'
+ * @param values The string(s) to convert.
+ * @returns The converted string to path case.
+ * @example toPathCase('FOO_BAR', 'Baz') // 'foo/bar/baz'
  */
-export function toPathCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Tokenize, lowercase and join with underscores.
-  return tokenize(value)
+export function toPathCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.toLowerCase())
     .join('/')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'camel/case'],
-    ['PascalCase', 'pascal/case'],
-    ['snake_case', 'snake/case'],
-    ['kebab-case', 'kebab/case'],
-    ['Title Case', 'title/case'],
-    ['UPPER CASE', 'upper/case'],
-    ['lower case', 'lower/case'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toPathCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to path-case', () => {
+    const result = toPathCase('camelCase')
+    expect(result).toEqual('camel/case')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toPathCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to path-case', () => {
+    const result = toPathCase('CONSTANT_CASE')
+    expect(result).toEqual('constant/case')
+  })
+
+  it('should convert dot-case to path-case', () => {
+    const result = toPathCase('dot.case')
+    expect(result).toEqual('dot/case')
+  })
+
+  it('should convert header-case to path-case', () => {
+    const result = toPathCase('Header-Case')
+    expect(result).toEqual('header/case')
+  })
+
+  it('should convert kebab-case to path-case', () => {
+    const result = toPathCase('kebab-case')
+    expect(result).toEqual('kebab/case')
+  })
+
+  it('should convert pascal-case to path-case', () => {
+    const result = toPathCase('PascalCase')
+    expect(result).toEqual('pascal/case')
+  })
+
+  it('should convert path-case to path-case', () => {
+    const result = toPathCase('path/case')
+    expect(result).toEqual('path/case')
+  })
+
+  it('should convert snake_case to path-case', () => {
+    const result = toPathCase('snake_case')
+    expect(result).toEqual('snake/case')
+  })
+
+  it('should convert title-case to path-case', () => {
+    const result = toPathCase('Title Case')
+    expect(result).toEqual('title/case')
+  })
+
+  it('should convert lower-case to path-case', () => {
+    const result = toPathCase('lower case')
+    expect(result).toEqual('lower/case')
+  })
+
+  it('should convert multiple strings to path-case', () => {
+    const result = toPathCase('foo', 'bar', 'baz')
+    expect(result).toEqual('foo/bar/baz')
   })
 }
