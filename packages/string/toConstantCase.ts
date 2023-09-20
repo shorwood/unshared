1d2cb56e9e3553d4d94d589a1e1faf928164e81d
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:57:12 +0200

    feat(string): allow `toConstantCase` to handle multiple words
    `toConstantCase` function in `string` package has been updated to allow conversion of multiple strings into constant case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toConstantCase.ts
+++ packages/string/toConstantCase.ts
@@ -3,39 +3,71 @@ import { tokenize } from './tokenize'
 /**
  * Convert a string to constant case.
  *
- * @param value The string to convert.
- * @returns The converted string.
- * @throws If `value` is not a string.
- * @example toConstantCase('fooBar') // returns 'FOO_BAR'
+ * @param values The string(s) to convert.
+ * @returns The converted string to constant case.
+ * @example toConstantCase('FOO_BAR', 'Baz') // 'FOO_BAR_BAZ'
  */
-export function toConstantCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Convert to constant case.
-  return tokenize(value)
+export function toConstantCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.toUpperCase())
     .join('_')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'CAMEL_CASE'],
-    ['PascalCase', 'PASCAL_CASE'],
-    ['snake_case', 'SNAKE_CASE'],
-    ['kebab-case', 'KEBAB_CASE'],
-    ['Title Case', 'TITLE_CASE'],
-    ['UPPER CASE', 'UPPER_CASE'],
-    ['lower case', 'LOWER_CASE'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toConstantCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to constant-case', () => {
+    const result = toConstantCase('camelCase')
+    expect(result).toEqual('CAMEL_CASE')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toConstantCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to constant-case', () => {
+    const result = toConstantCase('CONSTANT_CASE')
+    expect(result).toEqual('CONSTANT_CASE')
+  })
+
+  it('should convert dot-case to constant-case', () => {
+    const result = toConstantCase('dot.case')
+    expect(result).toEqual('DOT_CASE')
+  })
+
+  it('should convert header-case to constant-case', () => {
+    const result = toConstantCase('Header-Case')
+    expect(result).toEqual('HEADER_CASE')
+  })
+
+  it('should convert kebab-case to constant-case', () => {
+    const result = toConstantCase('kebab-case')
+    expect(result).toEqual('KEBAB_CASE')
+  })
+
+  it('should convert pascal-case to constant-case', () => {
+    const result = toConstantCase('PascalCase')
+    expect(result).toEqual('PASCAL_CASE')
+  })
+
+  it('should convert path-case to constant-case', () => {
+    const result = toConstantCase('path/case')
+    expect(result).toEqual('PATH_CASE')
+  })
+
+  it('should convert snake_case to constant-case', () => {
+    const result = toConstantCase('snake_case')
+    expect(result).toEqual('SNAKE_CASE')
+  })
+
+  it('should convert title-case to constant-case', () => {
+    const result = toConstantCase('Title Case')
+    expect(result).toEqual('TITLE_CASE')
+  })
+
+  it('should convert lower-case to constant-case', () => {
+    const result = toConstantCase('lower case')
+    expect(result).toEqual('LOWER_CASE')
+  })
+
+  it('should convert multiple strings to constantcamel-case', () => {
+    const result = toConstantCase('foo', 'bar', 'baz')
+    expect(result).toEqual('FOO_BAR_BAZ')
   })
 }
