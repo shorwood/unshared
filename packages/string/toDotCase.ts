db987252f51428d3b80e47293a71e3ce4a83ac27
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:57:33 +0200

    feat(string): allow `toDotCase` to handle multiple words
    `toDotCase` function in `string` package has been updated to allow conversion of multiple strings into dot case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toDotCase.ts
+++ packages/string/toDotCase.ts
@@ -1,41 +1,73 @@
 import { tokenize } from './tokenize'
 
 /**
- * Converts a string to dot case.
+ * Convert a string to dot case.
  *
- * @param value The string to convert.
- * @returns The converted string.
- * @throws If argument is not a string.
- * @example toDotCase('fooBar') // returns 'foo.bar'
+ * @param values The string(s) to convert.
+ * @returns The converted string to dot case.
+ * @example toDotCase('FOO_BAR', 'Baz') // 'foo.bar.baz'
  */
-export function toDotCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Tokenize, lowercase and join with dots.
-  return tokenize(value)
+export function toDotCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.toLowerCase())
     .join('.')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'camel.case'],
-    ['PascalCase', 'pascal.case'],
-    ['snake_case', 'snake.case'],
-    ['kebab-case', 'kebab.case'],
-    ['Title Case', 'title.case'],
-    ['UPPER CASE', 'upper.case'],
-    ['lower case', 'lower.case'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toDotCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to dot-case', () => {
+    const result = toDotCase('camelCase')
+    expect(result).toEqual('camel.case')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toDotCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to dot-case', () => {
+    const result = toDotCase('CONSTANT_CASE')
+    expect(result).toEqual('constant.case')
+  })
+
+  it('should convert dot-case to dot-case', () => {
+    const result = toDotCase('dot.case')
+    expect(result).toEqual('dot.case')
+  })
+
+  it('should convert header-case to dot-case', () => {
+    const result = toDotCase('Header-Case')
+    expect(result).toEqual('header.case')
+  })
+
+  it('should convert kebab-case to dot-case', () => {
+    const result = toDotCase('kebab-case')
+    expect(result).toEqual('kebab.case')
+  })
+
+  it('should convert pascal-case to dot-case', () => {
+    const result = toDotCase('PascalCase')
+    expect(result).toEqual('pascal.case')
+  })
+
+  it('should convert path-case to dot-case', () => {
+    const result = toDotCase('path/case')
+    expect(result).toEqual('path.case')
+  })
+
+  it('should convert snake_case to dot-case', () => {
+    const result = toDotCase('snake_case')
+    expect(result).toEqual('snake.case')
+  })
+
+  it('should convert title-case to dot-case', () => {
+    const result = toDotCase('Title Case')
+    expect(result).toEqual('title.case')
+  })
+
+  it('should convert lower-case to dot-case', () => {
+    const result = toDotCase('lower case')
+    expect(result).toEqual('lower.case')
+  })
+
+  it('should convert multiple strings to dot-case', () => {
+    const result = toDotCase('foo', 'bar', 'baz')
+    expect(result).toEqual('foo.bar.baz')
   })
 }
