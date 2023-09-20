06d526bf7f4abc832fd478120d6c7e52471e8944
Stanley Horwood <stanley@hsjm.io>
2023-09-20 17:58:33 +0200

    feat(string): allow `toKebabCase` to handle multiple words
    `toKebabCase` function in `string` package has been updated to allow conversion of multiple strings into kebab case. Additionally, test cases have been expanded to cover more edge cases and to check the new ability of processing multiple strings. This change improves the versatility of the function while maintaining its original functionality.

--- packages/string/toKebabCase.ts
+++ packages/string/toKebabCase.ts
@@ -1,40 +1,73 @@
 import { tokenize } from './tokenize'
 
 /**
- * Converts a string to kebab case.
+ * Convert a string to kebab case.
  *
- * @param value The string to convert
- * @returns The converted string
- * @example toKebabCase('fooBar') // returns 'foo-bar'
+ * @param values The string(s) to convert.
+ * @returns The converted string to kebab case.
+ * @example toKebabCase('FOO_BAR', 'Baz') // 'foo-bar-baz'
  */
-export function toKebabCase(value: string): string {
-  if (typeof value !== 'string')
-    throw new TypeError('Expected a string')
-
-  // --- Tokenize, lowercase each token and join with a dash.
-  return tokenize(value)
+export function toKebabCase(...values: string[]): string {
+  return values
+    .flatMap(tokenize)
     .map(token => token.toLowerCase())
     .join('-')
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it.each([
-    ['camelCase', 'camel-case'],
-    ['PascalCase', 'pascal-case'],
-    ['snake_case', 'snake-case'],
-    ['kebab-case', 'kebab-case'],
-    ['Title Case', 'title-case'],
-    ['UPPER CASE', 'upper-case'],
-    ['lower case', 'lower-case'],
-  ])('should convert %s to %s', (input, expected) => {
-    const result = toKebabCase(input)
-    expect(result).toEqual(expected)
+  it('should convert camel-case to kebab-case', () => {
+    const result = toKebabCase('camelCase')
+    expect(result).toEqual('camel-case')
   })
 
-  it('should throw if value is not a string', () => {
-    // @ts-expect-error: invalid argument type.
-    const shouldThrow = () => toKebabCase(1)
-    expect(shouldThrow).toThrow(TypeError)
+  it('should convert constant-case to kebab-case', () => {
+    const result = toKebabCase('CONSTANT_CASE')
+    expect(result).toEqual('constant-case')
+  })
+
+  it('should convert dot-case to kebab-case', () => {
+    const result = toKebabCase('dot.case')
+    expect(result).toEqual('dot-case')
+  })
+
+  it('should convert header-case to kebab-case', () => {
+    const result = toKebabCase('Header-Case')
+    expect(result).toEqual('header-case')
+  })
+
+  it('should convert kebab-case to kebab-case', () => {
+    const result = toKebabCase('kebab-case')
+    expect(result).toEqual('kebab-case')
+  })
+
+  it('should convert pascal-case to kebab-case', () => {
+    const result = toKebabCase('PascalCase')
+    expect(result).toEqual('pascal-case')
+  })
+
+  it('should convert path-case to kebab-case', () => {
+    const result = toKebabCase('path/case')
+    expect(result).toEqual('path-case')
+  })
+
+  it('should convert snake_case to kebab-case', () => {
+    const result = toKebabCase('snake_case')
+    expect(result).toEqual('snake-case')
+  })
+
+  it('should convert title-case to kebab-case', () => {
+    const result = toKebabCase('Title Case')
+    expect(result).toEqual('title-case')
+  })
+
+  it('should convert lower-case to kebab-case', () => {
+    const result = toKebabCase('lower case')
+    expect(result).toEqual('lower-case')
+  })
+
+  it('should convert multiple strings to kebab-case', () => {
+    const result = toKebabCase('foo', 'bar', 'baz')
+    expect(result).toEqual('foo-bar-baz')
   })
 }
