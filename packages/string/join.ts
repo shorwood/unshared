b42ab76ba937e7948c600796d3904d0754f54ebd
Stanley Horwood <stanley@hsjm.io>
2023-09-22 01:43:04 +0200

    chore(string): updated comments for `join` function
    The comments for the `join` function in the `string` package were slightly out of date. This commit updates them to more accurately describe the function's behavior and use. Also, the native function that it wraps and its advantages are mentioned.

--- packages/string/join.ts
+++ packages/string/join.ts
@@ -1,5 +1,7 @@
 /**
- * Join all values in an array into a single string.
+ * Join all values in an array into a single string. This is a wrapper for the native
+ * `Array.join()` but with a default separator of an empty string. It also allows for
+ * a more functional style of programming.
  *
  * @param values The values to join.
  * @param separator The character to use as a separator.
