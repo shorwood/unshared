f0064124b7d3bfb8fb336147cff86733fe3e86ce
Stanley Horwood <stanley@hsjm.io>
2023-09-21 15:27:12 +0200

    feat(string): improved `decodeBase64` function
    The `decodeBase64` function in the string package has been improved. It now uses the native `ArrayBuffer` API and no longer relies on Node.js, which makes it more suitable for cross-platform use. The function has been cleaned up and asserts that the alphabet is Base64. Error handling has also been improved, making it more robust. Corresponding test cases have also been added.

--- packages/string/decodeBase64.ts
+++ packages/string/decodeBase64.ts
@@ -1,94 +1,78 @@
-import { Buffer } from 'node:buffer'
-
-/* eslint-disable unicorn/prevent-abbreviations */
-export const base64Symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+import { base64Alphabet } from './encodeBase64'
 
 /**
- * Decode a Base64 string into a `Buffer`.
+ * Decode a Base64-encoded string into an `ArrayBuffer`. Since this implementation is
+ * using the native `ArrayBuffer` API, it does not rely on Node.js, this makes it ideal
+ * for use in cross-platform libraries.
  *
- * @param value The Base64 string to decode.
- * @returns The `Buffer` representation of the Base64 string.
- * @example decodeBase64('SGVsbG8sIFdvcmxkIQ==') // <Buffer 48 65 6c 6c 6f 2c 20 57 6f 72 6c 64 21>
- */
-export function decodeBase64(value: string): Buffer
-/**
- * Decode a Base64 string into a string.
+ * @param base64 The Base64 string to decode.
+ * @returns The decoded `ArrayBuffer`.
+ * @example
  *
- * @param value The Base64 string to decode.
- * @param encoding The encoding of the returned string.
- * @returns The string representation of the Base64 string.
- * @example decodeBase64('SGVsbG8sIFdvcmxkIQ==', 'utf8') // 'Hello, World!'
- */
-export function decodeBase64(value: string, encoding: BufferEncoding): string
-/**
- * Decode a Base64 string into a `Buffer` or a string.
+ * // Create a Base64 string
+ * const base64 = 'VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw=='
  *
- * @param value The Base64 string to decode.
- * @param encoding The encoding of the returned string. If omitted, the function returns a `Buffer`.
- * @returns The `Buffer` or string representation of the Base64 string.
- * @example decodeBase64('SGVsbG8sIFdvcmxkIQ==') // <Buffer 48 65 6c 6c 6f 2c 20 57 6f 72 6c 64 21>
+ * // Decode a Base64 string to an ArrayBuffer
+ * const buffer = decodeBase64(base64) // <ArrayBuffer 54 68 65 20 71 75 69 63 6b 20 62 72 ...>
+ *
+ * // Get the string from the buffer
+ * const string = new TextDecoder().decode(buffer) // 'The quick brown fox jumps over the lazy dog'
  */
-export function decodeBase64(value: string, encoding?: BufferEncoding): Buffer | string
-export function decodeBase64(value: string, encoding?: BufferEncoding): Buffer | string {
-  // --- Check if string is valid
-  if (typeof value !== 'string')
-    throw new TypeError('Value must be a string')
-  if (value.length % 4 !== 0)
-    throw new Error('String must have a length that is a multiple of 4')
-  if (!/^[\d+/=a-z]*$/i.test(value))
-    throw new Error('String must only contain base64 digits')
-  value = value.replace(/=/g, '')
+export function decodeBase64(base64: string): ArrayBuffer {
+  if (base64.length % 4 !== 0)
+    throw new Error('Could not decode string as Base64: Length is not a multiple of 4')
 
-  // --- Initialize buffer
-  const buffer = new ArrayBuffer(value.length / 4 * 3)
-  const bufferView = new Uint8Array(buffer)
+  // --- Remove padding.
+  base64 = base64.replace(/=/g, '')
 
-  // --- Handle each group of 4 characters
-  for (let i = 0, j = 0; i < value.length; i += 4) {
-    // --- Convert 4 characters to 3 bytes
-    const bytes = [
-      base64Symbols.indexOf(value[i]),
-      base64Symbols.indexOf(value[i + 1]),
-      base64Symbols.indexOf(value[i + 2]),
-      base64Symbols.indexOf(value[i + 3]),
-    ]
+  // --- Assert alphabet is base64.
+  if (![...base64].every(x => base64Alphabet.includes(x)))
+    throw new Error('Could not decode string as Base64: Invalid characters')
 
-    // --- Skip padding
-    if (bytes[2] === 64) {
-      bufferView[j++] = (bytes[0] << 2) | (bytes[1] >> 4)
-    }
-    else if (bytes[3] === 64) {
-      bufferView[j++] = (bytes[0] << 2) | (bytes[1] >> 4)
-      bufferView[j++] = ((bytes[1] & 15) << 4) | (bytes[2] >> 2)
-    }
-    else {
-      bufferView[j++] = (bytes[0] << 2) | (bytes[1] >> 4)
-      bufferView[j++] = ((bytes[1] & 15) << 4) | (bytes[2] >> 2)
-      bufferView[j++] = ((bytes[2] & 3) << 6) | bytes[3]
-    }
+  // --- Handle decode of every 4 characters into 3 bytes.
+  const bytes: number[] = []
+  for (let k = 0, v = 0; k < base64.length; k += 4) {
+    const byte0 = base64Alphabet.indexOf(base64[k])
+    const byte1 = base64Alphabet.indexOf(base64[k + 1])
+    const byte2 = base64Alphabet.indexOf(base64[k + 2])
+    const byte3 = base64Alphabet.indexOf(base64[k + 3])
+
+    // --- Set the bytes in the buffer.
+    bytes[v++] = ((byte0 << 2) & 0b11111100) | ((byte1 >> 4) & 0b00000011)
+    if (byte2 === -1) continue
+    bytes[v++] = ((byte1 << 4) & 0b11110000) | ((byte2 >> 2) & 0b00001111)
+    if (byte3 === -1) continue
+    bytes[v++] = ((byte2 << 6) & 0b11000000) | (byte3 & 0b00111111)
   }
 
-  // --- Return buffer.
-  return encoding
-    ? Buffer.from(buffer)
-    : Buffer.from(buffer).toString(encoding)
+  // --- Copy the bytes into a new buffer.
+  const bufferView = new Uint8Array(bytes)
+  return bufferView.buffer
 }
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  const inputUtf8 = 'The quick brown fox jumps over the lazy dog'
-  const inputBase64 = 'VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw=='
-  const inputUtf8Buffer = Buffer.from(inputUtf8)
-
-  it('should convert a base64 string to a buffer', () => {
-    const result = decodeBase64(inputBase64)
-    expect(result).toEqual(inputUtf8Buffer)
-    expectTypeOf(result).toEqualTypeOf<Buffer>()
+  it('should decode a Base64 encoded string with padding into a buffer', () => {
+    const result = decodeBase64('VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw==')
+    const resultUtf8 = new TextDecoder().decode(result)
+    const expected = 'The quick brown fox jumps over the lazy dog'
+    expect(resultUtf8).toEqual(expected)
   })
 
-  it('should convert a base64 string to a string', () => {
-    const result = decodeBase64(inputBase64, 'utf8')
-    expect(result).toEqual(inputUtf8)
-    expectTypeOf(result).toEqualTypeOf<string>()
+  it('should decode a Base64 encoded string without padding into a buffer', () => {
+    const result = decodeBase64('SGVsbG8sIFdvcmxk')
+    const resultUtf8 = new TextDecoder().decode(result)
+    const expected = 'Hello, World'
+    expect(resultUtf8).toEqual(expected)
+  })
+
+  it('should throw if the string is not a multiple of 4', () => {
+    const shouldThrow = () => decodeBase64('SGVsbG8sIFdvcmxkIQ=')
+    expect(shouldThrow).toThrow(Error)
+  })
+
+  it('should throw if the string contains invalid characters', () => {
+    const shouldThrow = () => decodeBase64('SGVsbG8sIFdvcmxkIQ!@')
+    expect(shouldThrow).toThrow(Error)
   })
 }
