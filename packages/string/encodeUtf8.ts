a0813b3f977685ada872228aa6228dcc7736a3cc
Stanley Horwood <stanley@hsjm.io>
2023-09-22 04:12:10 +0200

    feat(string): improved `encodeUtf8` function and added test
    The `encodeUtf8` function, formerly `toUtf8`, now uses native ArrayBuffer API for better cross-platform support. Detailed explanatory comments and usage example were added for better documentation. The function was also renamed to more accurately reflect its purpose. Additionally, a test case to verify proper functionality was written.

--- packages/string/encodeUtf8.ts
+++ packages/string/encodeUtf8.ts
@@ -1,12 +1,33 @@
 /**
- * Convert an ArrayBuffer to a UTF8 string
+ * Encode a `ArrayBuffer` to an UTF-8 encoded string. Since this implementation is
+ * using the native `ArrayBuffer` API, it does not rely on Node.js, this makes it ideal
+ * for use in cross-platform libraries.
  *
- * @param buffer The buffer to convert
- * @returns The UTF8 string
+ * @param buffer The `ArrayBuffer` to convert.
+ * @returns The UTF-8 encoded string.
+ * @example
+ *
+ * // Create a buffer from a string.
+ * const buffer = new TextEncoder().encode('The quick brown fox jumps over the lazy dog')
+ *
+ * // Encode the buffer into UTF-8.
+ * encodeUtf8(buffer) // 'The quick brown fox jumps over the lazy dog'
  */
-export const toUtf8 = (buffer: ArrayBuffer): string => {
+export function encodeUtf8(buffer: ArrayBuffer): string {
   const view = new Uint8Array(buffer)
   const result: string[] = []
-  for (const element of view) result.push(String.fromCharCode(element))
+  for (const element of view) {
+    const char = String.fromCharCode(element)
+    result.push(char)
+  }
   return result.join('')
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  it('should encode a buffer into a UTF-8 string and omit padding', () => {
+    const buffer = new TextEncoder().encode('Hello, World!').buffer
+    const result = encodeUtf8(buffer)
+    expect(result).toEqual('Hello, World!')
+  })
 }
