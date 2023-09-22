d15a9300a95fb9de3f337a171aa4bfa7d2bc3964
Stanley Horwood <stanley@hsjm.io>
2023-09-22 04:31:57 +0200

    chore(string): move `parseDistinguishedName` from network to string
    The `parseDistinguishedName` function has been moved from the network package to the string package. The documentation in the function has been updated to better describe its behavior, and an example has been added.

--- packages/string/parseDistinguishedName.ts
+++ packages/string/parseDistinguishedName.ts
@@ -1,35 +1,43 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import { MaybeArray } from '@unshared/types'
 
+/** A distinguished name attributes. */
 export type DistinguishedName = Record<string, MaybeArray<string>>
 
 /**
  * Parse a [distinguished name](https://en.wikipedia.org/wiki/Lightweight_Directory_Access_Protocol) into
- * an object with the attributes. The attributes are case-insensitive and should
- * respect the [RFC 4514](https://www.rfc-editor.org/rfc/rfc4514) standard.
+ * an map of attributes. The attributes are case-sensitive and the values are always strings or arrays of strings.
+ *
+ * This implementation is loosely based on [RFC 4514](https://tools.ietf.org/html/rfc4514#section-2).
  *
  * @param name The distinguished name to parse.
- * @deprecated Yet to respect the RFC 4514 standard.
  * @returns An object with the attributes.
+ * @example
+ *
+ * // Create a distinguished name.
+ * const dName = 'CN=example.com,CN=example.dev,O=Example,C=US,ST=California,L=Los Angeles'
+ *
+ * // Parse the distinguished name.
+ * const dNameMap = parseDistinguishedName(dName)
+ *
+ * dNameMap.CN // ['example.com', 'example.dev']
+ * dNameMap.O // 'Example'
+ * dNameMap.C // 'US'
+ * dNameMap.ST // 'California'
+ * dNameMap.L // 'Los Angeles'
  */
+// TODO: Actually implement the RFC 4514.
 export function parseDistinguishedName(name: string): DistinguishedName {
-  if (typeof name !== 'string')
-    throw new TypeError('Expected the DN to be a string')
-
-  // --- Split the name into parts
   const attributes: DistinguishedName = {}
   const parts = name.split(/(?<!\\),/)
   for (const part of parts) {
     const parts = part.split(/(?<!\\)=/)
     const key = parts[0].trim()
-    const value = parts[1]
-      .replace(/\\,/g, ',')
-      .replace(/\\=/g, '=')
-      .trim()
+    const value = parts[1].replace(/\\(,)|\\(=)/g, '$1$2').trim()
 
     if (attributes[key] === undefined) attributes[key] = value
-    else if (Array.isArray(attributes[key])) attributes[key].push(value)
-    else attributes[key] = [attributes[key], value]
+    else if (typeof attributes[key] === 'string') attributes[key] = [attributes[key] as string, value]
+    else (attributes[key] as string[]).push(value)
   }
 
   // --- Return the attributes
@@ -62,7 +70,7 @@ if (import.meta.vitest) {
     })
   })
 
-  it('should parse a DN with escaped equals', () => {
+  it('should parse a DN with escaped equal sign', () => {
     const result = parseDistinguishedName('CN=example\\=com,O=Example')
     expect(result).toEqual({
       CN: 'example=com',
