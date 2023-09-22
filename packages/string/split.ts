ec23072835872b7bf1425ed5c487dab95c48b0b9
Stanley Horwood <stanley@hsjm.io>
2023-09-22 03:01:49 +0200

    feat(string!): enhanced `split` functionality and added tests
    The `split` function now has default arguments for the delimiter and limit parameters. The default behaviour has been changed to filter out empty strings. The delimiter now defaults to an empty string. The limit can be negative to limit the number of elements from the end of the string. New tests have been added to accommodate these changes.

--- packages/string/split.ts
+++ packages/string/split.ts
@@ -1,10 +1,17 @@
 import { NumberInteger } from '@unshared/types'
 
 /**
- * Split the string into an array of substrings using the specified delimiter. The
- * main difference between this function and the native `String.prototype.split` is
- * that this function supports negative limits. Meaning that you can use a negative
- * limit to get the last N elements of the string.
+ * Split the string into an array of substrings using the specified delimiter. This
+ * function is similar to `String.prototype.split` but has a few differences:
+ *
+ * - The `limit` parameter can be a negative number to limit the number of elements
+ *   returned from the end of the string.
+ *
+ * - The empty string are filtered out by default. This means that `split('ab', 'a')`
+ *   will return `['b']` instead of `['', 'b']`.
+ *
+ * - The `delimiter` parameter is defaulted to an empty string. This means that
+ *   `split('a b')` will return `['a', 'b']` instead of `['a b']`.
  *
  * @param value The string to split.
  * @param delimiter The character to use as a separator.
@@ -12,15 +19,20 @@ import { NumberInteger } from '@unshared/types'
  * @returns An array of substrings.
  * @example split('a,b,c', ',') // => ['a', 'b', 'c']
  */
-export function split<N extends number>(value: string, delimiter: string | RegExp, limit = Number.POSITIVE_INFINITY as NumberInteger<N>): string[] {
-  return limit < 0
-    ? value.split(delimiter).slice(limit)
-    : value.split(delimiter, limit)
+export function split<N extends number>(value: string, delimiter: string | RegExp = '', limit?: NumberInteger<N>): string[] {
+  return limit !== undefined && limit < 0
+    ? value.split(delimiter).slice(limit).filter(Boolean)
+    : value.split(delimiter, limit).filter(Boolean)
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it('should split a string', () => {
+  it('should split a string with empty delimiter', () => {
+    const result = split('abc')
+    expect(result).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should split a string with a delimiter', () => {
     const result = split('a,b,c', ',')
     expect(result).toEqual(['a', 'b', 'c'])
   })
@@ -33,6 +45,11 @@ if (import.meta.vitest) {
   it('should split a string with a negative limit', () => {
     const result = split('a,b,c', ',', -2)
     expect(result).toEqual(['b', 'c'])
+  })
+
+  it('should filter out empty strings', () => {
+    const result = split('a,,b,c', ',')
+    expect(result).toEqual(['a', 'b', 'c'])
   })
 
   it('should expose a type error when the limit is not an integer', () => {
