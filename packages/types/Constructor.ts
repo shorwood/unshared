06aec7928164260aa25d21c6966e8bff2602e436
Stanley Horwood <stanley@hsjm.io>
2023-10-06 17:52:02 +0200

    chore(types): refactor `Constructor` tests and improve type declaration
    The `Constructor` type now properly extends `any[]` instead of `unknown[]`. The tests for `Constructor` were also refactored to check the constructor parameters, statics, and instance type separately. This results in more granular and accurate tests. The `ConstructorStatics` type was imported and utilized in the tests for improved testing accuracy.

--- packages/types/Constructor.ts
+++ packages/types/Constructor.ts
@@ -1,4 +1,6 @@
 /* eslint-disable unicorn/no-static-only-class */
+import { ConstructorStatics } from './ConstructorStatics'
+
 /**
  * A constructor with parameters, instance properties and static properties. This is a type that
  * can be used to create a class with a specific constructor signature, instance properties and
@@ -14,7 +16,7 @@
  * type S = { c: number } // Static properties
  * type Foo = Constructor<P, R, S> // (new (a: number, b: string) => { a: number; b?: string }) & { c: number }
  */
-export type Constructor<P extends unknown[] = any[], R extends object = {}, S extends object = {}> =
+export type Constructor<P extends any[] = any[], R extends object = {}, S extends object = {}> =
   (new (...parameters: P) => R) & S
 
 /** c8 ignore next */
@@ -22,30 +24,32 @@ if (import.meta.vitest) {
   it('should build the parameters of a class', () => {
     type Result = Constructor<[number, string]>
     class Expected { constructor(_a: number, _b: string) {} }
-    expectTypeOf<Result>().toEqualTypeOf<typeof Expected>()
+    expectTypeOf<ConstructorParameters<Result>>().toEqualTypeOf<ConstructorParameters<typeof Expected>>()
+    expectTypeOf<ConstructorStatics<Result>>().toEqualTypeOf<ConstructorStatics<typeof Expected>>()
+    expectTypeOf<InstanceType<Result>>().toEqualTypeOf<InstanceType<typeof Expected>>()
   })
 
   it('should build the instance properties of a class', () => {
     type Result = Constructor<[], { a: number }>
     class Expected { a = 1 }
-    expectTypeOf<Result>().toEqualTypeOf<typeof Expected>()
+    expectTypeOf<ConstructorParameters<Result>>().toEqualTypeOf<ConstructorParameters<typeof Expected>>()
+    expectTypeOf<ConstructorStatics<Result>>().toEqualTypeOf<ConstructorStatics<typeof Expected>>()
+    expectTypeOf<InstanceType<Result>>().toEqualTypeOf<InstanceType<typeof Expected>>()
   })
 
   it('should build the type of a class with static properties', () => {
     type Result = Constructor<[], {}, { a: number }>
     class Expected { static a = 1 }
-    expectTypeOf<Result>().toEqualTypeOf<typeof Expected>()
+    expectTypeOf<ConstructorParameters<Result>>().toEqualTypeOf<ConstructorParameters<typeof Expected>>()
+    expectTypeOf<ConstructorStatics<Result>>().toEqualTypeOf<ConstructorStatics<typeof Expected>>()
+    expectTypeOf<InstanceType<Result>>().toEqualTypeOf<InstanceType<typeof Expected>>()
   })
 
   it('should build an empty class', () => {
     type Result = Constructor
     class Expected { constructor(..._: any[]) {} }
-    expectTypeOf<Result>().toEqualTypeOf<typeof Expected>()
-  })
-
-  it('should not build a function', () => {
-    function foo() {}
-    type Result = Constructor<[], {}>
-    expectTypeOf<typeof foo>().not.toMatchTypeOf<Result>()
+    expectTypeOf<ConstructorParameters<Result>>().toEqualTypeOf<ConstructorParameters<typeof Expected>>()
+    expectTypeOf<ConstructorStatics<Result>>().toEqualTypeOf<ConstructorStatics<typeof Expected>>()
+    expectTypeOf<InstanceType<Result>>().toEqualTypeOf<InstanceType<typeof Expected>>()
   })
 }
