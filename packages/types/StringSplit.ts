84645b449e5782baaf5ef63653717d15e7254675
Stanley Horwood <stanley@hsjm.io>
2023-10-06 17:56:21 +0200

    fix(types): simplify and improve typing of `StringSplit` function
    Simplifies the `StringSplit` type by removing unnecessary conditionals and variables, specifically the maximum number of splits (`N`) has been removed which simplified the recursion check. Also, improved type template names for better readability. Adjusted related test cases accordingly.

--- packages/types/StringSplit.ts
+++ packages/types/StringSplit.ts
@@ -1,33 +1,27 @@
-import { IsDecimal, IsEmptyString, IsPositive, IsString, IsZero, Substract } from './utils'
+import { IsEmptyString, IsString } from './utils'
 
 /**
  * Extract litteral types of strings separated by a delimiter.
  *
- * @template S String to extract litteral types from.
- * @template D Delimiter to split string with.
- * @template N Maximum number of splits. (Default: `128`)
+ * @template T String to extract litteral types from.
+ * @template S Delimiter to split string with.
  * @template R The list of tokens extracted so far. (Used internally)
  * @returns Litteral types of strings separated by a delimiter.
  * @example StringSplit<'Hello,World'> // ['Hello', 'World']
  */
-export type StringSplit<S extends string, D extends string = ',', N extends number = 128, R extends string[] = []> =
-  IsString<S> extends true ? string[]
-    : IsEmptyString<S> extends true ? []
-      : IsDecimal<N> extends true ? never
-        : IsZero<N> extends true ? R
+export type StringSplit<T extends string, S extends string = ',', R extends string[] = []> =
+  IsString<T> extends true ? string[]
+    : IsEmptyString<T> extends true ? []
 
-          // --- Split string.
-          : S extends `${infer A extends string}${D}${infer Rest extends string}`
+      // --- Split string by separator.
+      : T extends `${infer A extends string}${S}${infer B extends string}`
 
-            // --- Skip empty strings.
-            ? IsEmptyString<A> extends true ? StringSplit<Rest, D, N, R>
+        // --- Skip empty strings.
+        ? IsEmptyString<A> extends true ? StringSplit<B, S, R>
 
-              // --- Recurse.
-              : IsPositive<N> extends true
-                ? [...R, A, ...StringSplit<Rest, D, Substract<N, 1>>]
-                : never
-
-            : [...R, S]
+          // --- Recursively split string.
+          : [...R, A, ...StringSplit<B, S, R>]
+        : [...R, T]
 
 /** c8 ignore next */
 if (import.meta.vitest) {
@@ -39,12 +33,6 @@ if (import.meta.vitest) {
 
   it('should return literrals split by a dash', () => {
     type Result = StringSplit<'Hello-World', '-'>
-    type Expected = ['Hello', 'World']
-    expectTypeOf<Result>().toEqualTypeOf<Expected>()
-  })
-
-  it('should return literrals split by a comma with a maximum of 2 splits', () => {
-    type Result = StringSplit<'Hello,World,Goodbye', ',', 2>
     type Expected = ['Hello', 'World']
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
