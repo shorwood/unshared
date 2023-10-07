ab5618516e6d5dfcc47c5d1c95dce11c8a72ed37
Stanley Horwood <stanley@hsjm.io>
2023-10-07 19:23:33 +0200

    chore(build): move build scripts from separate file to `tsup.config.ts`
    Removed the `scripts/build.ts` file and moved its contents directly into `tsup.config.ts`. This change simplifies the build process by having all build configurations in a single location. It may also have optimization benefits since the build scripts are no longer separated into a different file. Other small changes have been made to tweak the configuration.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,1 +1,43 @@
-export { default } from './scripts/build'
+import { defineConfig } from 'tsup'
+
+// --- Export Tsup configuration.
+export default defineConfig(async() => ({
+  name: 'Unshared',
+  outDir: './dist',
+  platform: 'node',
+  target: ['node18'],
+  format: ['esm', 'cjs'],
+  entry: ['./*.ts'],
+  clean: true,
+  shims: false,
+  silent: true,
+  bundle: true,
+  splitting: true,
+  sourcemap: true,
+  minifySyntax: true,
+  treeshake: 'smallest',
+  skipNodeModulesBundle: true,
+  external: [/@unshared\/.+/],
+  define: { 'import.meta.vitest': 'false' },
+
+  dts: {
+    compilerOptions: {
+      // moduleResolution: 'bundler',
+      strict: true,
+    },
+  },
+
+  esbuildOptions: (options) => {
+    options.entryNames = '[name]'
+    options.chunkNames = 'chunks/[hash]'
+    options.assetNames = 'assets/[hash]'
+    options.packages = 'external'
+  },
+
+  // esbuildPlugins: [
+  // PluginGlobSpecifier(),
+  // PluginDeclaration(),
+  // PluginMetaUrl(),
+  // PluginWorker(),
+  // ],
+}))
