257f9466a4de4b62f819e0b1827953021798030c
Stanley Horwood <stanley@hsjm.io>
2023-10-07 19:22:30 +0200

    feat(types): improved `Path` type for explicit and nested nest extraction
    The `Path` type has been significantly improved. The new implementation avoids recursion for non-object types, provides better handling of Array types, more accurate typings, and is more readable. Test cases have been updated to cover major use cases and ensure correctness. Additionally, template comments were updated for better clarity.

--- packages/types/Path.ts
+++ packages/types/Path.ts
@@ -6,9 +6,9 @@ import type { IsZero, Substract } from './utils'
  * access nested properties of an object allowing comprehensive autocompletion
  * and type checking.
  *
- * @template T Object type
- * @template N Number of nested keys to explore
- * @template P Current path
+ * @template T Object type to extract keys from.
+ * @template N Number of nested keys to explore before returning a loose path.
+ * @template P Current path to prepend to the keys. Used internally for recursion.
  * @returns List of possible paths.
  * @example
  * interface Contact {
@@ -16,71 +16,72 @@ import type { IsZero, Substract } from './utils'
  *  email: string
  * }
  *
- * type Result = Path<Contact> // 'name' | 'name.first' | 'name.last' | 'email' | (string & {})
+ * type Result = Path<Contact> // 'name' | 'name.first' | 'name.last' | 'email'
  */
-export type Path<T, N extends number = 8, P extends string = ''> =
-  // --- If T is a string or an array, return loose path.
-  T extends string | any[] ? P extends '' ? number : `${P}${number}` | `${P}${number}.${string}`
+export type Path<T extends object, N extends number = 10, P extends string = ''> =
+  // --- On recursion threshold, return the current path(s) without the trailing dot.
+  IsZero<N> extends true ? P extends `${infer P1}.` ? P1 : P
 
-    // --- Recursion threshold reached, return loose path.
-    : IsZero<N> extends true ? `${P}${Extract<keyof T, string | number>}`
-      : { [K in keyof T]-?: K extends string | number
+    // --- If T is an Array and its items are objects, recurse.
+    : T extends Array<infer U>
+      ? U extends object ? Path<U, Substract<N, 1>, `${P}${number}.`>
+        : (P extends '' ? `${number}` : `${P}${number}`)
 
-        // --- Concat the current path with the keys of T
-        ? (P extends '' ? K : `${P}${K}`)
+      // --- For each key of T, collect the path.
+      : Extract<{ [K in keyof T]-?: K extends string | number
 
-        // --- Check for recursion threshold.
-        // --- If value is an object, recurse
-        | (T[K] extends object
-          ? Path<T[K], Substract<N, 1>, `${P}${K}.`>
-          : `${P}${K}.${string}`)
+        // --- Collect the path and prepend the current path.
+        ? (P extends '' ? `${K}` : `${P}${K}`)
 
-        // --- Ignore symbol if nested.
-        : P extends '' ? K : never
+        // --- If the value is an object, recurse.
+        | (T[K] extends object ? Path<T[K], Substract<N, 1>, `${P}${K}.`> : `${P}${K}`)
 
-      // --- Extract keys only.
-      }[keyof T] | (string & {})
+        // --- Ignore symbols.
+        : never
+
+      }[keyof T], string>
 
 /** c8 ignore next */
 if (import.meta.vitest) {
   it('should infer the path of a nested object', () => {
     type Result = Path<{ foo: { bar: { baz: string } } }>
-    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | 'foo.bar.baz' | `foo.bar.baz.${string}` | string & {}
+    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | 'foo.bar.baz'
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should not infer nested paths if the recursion threshold is reached', () => {
-    type Result = Path<{ foo: { bar: { baz: string } } }, 0>
+    type Result = Path<{ foo: { bar: { baz: string } } }, 1>
     type Expected = 'foo'
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should infer the path of a nested array', () => {
     type Result = Path<{ foo: { bar: { baz: string[] } } }>
-    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | `foo.bar.baz.${number}` | `foo.bar.baz.${number}.${string}` | string & {}
+    type Expected = 'foo' | 'foo.bar' | 'foo.bar.baz' | `foo.bar.baz.${number}`
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should infer the path of a nested object with a number key', () => {
     type Result = Path<{ foo: string; [x: number]: string }>
-    type expeted = number | 'foo' | `${number}.${string}` | `foo.${string}` | string & {}
-    expectTypeOf<Result>().toEqualTypeOf<expeted>()
+    type Expected = 'foo' | `${number}`
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should infer the path of a nested object with a symbol key', () => {
     type Result = Path<{ foo: string; [x: symbol]: string }>
-    type expeted = symbol | 'foo' | `foo.${string}` | string & {}
-    expectTypeOf<Result>().toEqualTypeOf<expeted>()
+    type Expected = 'foo'
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should ignore nested symbol keys', () => {
     type Result = Path<{ foo: Record<symbol, string> }>
-    type Expected = 'foo' | string & {}
+    type Expected = 'foo'
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
-  it('should ignore prototype properties', () => {
-    type Result = Path<string>
-    expectTypeOf<Result>().toEqualTypeOf<number>()
+  it('should infer the path of an array of strings', () => {
+    type Result = Path<string[]>
+    type Expected = `${number}`
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 }
