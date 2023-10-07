e14fb482170c7e943d21fcd827a69a9f07cba3db
Stanley Horwood <stanley@hsjm.io>
2023-10-08 00:05:29 +0200

    feat(types): new type `NotStringEmpty`
    This adds a new TypeScript utility type `NotStringEmpty` to the types package. This type is a union that prevents the user from using an empty string. Test cases have been provided to ensure the correct functioning of the new type. This new type is useful for creating type constraints to prevent use of empty strings.

---
+++ packages/types/NotStringEmpty.ts
@@ -0,0 +1,28 @@
+/**
+ * Wrap a string type in a union with `string` to make it matchable with a
+ * any string type that is not empty. This is useful for creating a type that
+ * forces the user to not use an empty string.
+ *
+ * @template T String type to wrap.
+ * @returns String constraint to a non-empty string.
+ * @example NotStringEmpty<'foo' | ''> // 'foo'
+ */
+export type NotStringEmpty<T extends string> = T extends '' ? never : T
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should match a string type', () => {
+    type Result = NotStringEmpty<string>
+    expectTypeOf<Result>().toEqualTypeOf<string>()
+  })
+
+  it('should match a literal string', () => {
+    type Result = NotStringEmpty<'foo' | 'bar'>
+    expectTypeOf<Result>().toEqualTypeOf<'foo' | 'bar'>()
+  })
+
+  it('should exclude an empty string', () => {
+    type Result = NotStringEmpty<''>
+    expectTypeOf<Result>().toEqualTypeOf<never>()
+  })
+}
