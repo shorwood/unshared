a9a96431c7752c6a6ed0926bcf68f94d12a20cca
Stanley Horwood <stanley@hsjm.io>
2023-10-08 00:05:02 +0200

    chore(types): refactor `Get` type to support nested values and remove `Value` type
    The `Get` type was enhanced with support for extracting nested values from an object. As a result, the `Value` type no longer needed and was removed. Additionally, test cases of the `Get` type were updated to reflect the new capabilities.

--- packages/types/Get.ts
+++ packages/types/Get.ts
@@ -1,51 +1,57 @@
 /**
- * Extract value of an object at a given key. This is useful for creating a type
- * that can be used to access nested properties of an object allowing
+ * Extract nested value of an object recursively. This is useful for creating a
+ * type that can be used to access nested properties of an object allowing
  * comprehensive autocompletion and type checking.
  *
  * @template T Object type to extract value from.
- * @template K Key of the value to extract.
- * @returns The value at the key.
- * @example Get<{ foo: 'bar' }, 'foo'> // 'bar'
+ * @template P Path to the value to extract.
+ * @returns The value at the path.
+ * @example Get<{ foo: { bar: { baz: 'baz' } } }, 'foo.bar.baz'> // 'baz'
  */
-export type Get<T, K extends string | number | symbol> =
-  // --- Handle symbol keys
-  K extends symbol ? K extends keyof T ? T[K] : never
+export type Get<T, P extends string> =
+    // --- Extract the key of the first segment of the path.
+    P extends `${infer K}.${infer N}`
 
-    // --- Handle string and number keys
-    : K extends keyof T ? T[K]
+      // --- If the segment matches the key of the object, continue.
+      ? K extends keyof T ? Get<T[K], N> : never
 
-      // --- Handle string number keys
-      : K extends string ? `${K}` extends `${infer U extends number}` ? U extends keyof T ? T[U]
-
-        // --- Key is invalid
-        : never : never : never
+      // --- Otherwise, return the value at the current path.
+      : P extends keyof T ? T[P] : never
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should extract the value in an object', () => {
-    type Result = Get<{ foo: 'bar' }, 'foo'>
+  it('should extract the value of a nested object', () => {
+    interface Object { foo: { bar: { baz: 'baz' } } }
+    type Result = Get<Object, 'foo.bar.baz'>
+    expectTypeOf<Result>().toEqualTypeOf<'baz'>()
+  })
+
+  it('should extract the value of a nested object in an array', () => {
+    type Object = [{ foo: 'foo' }]
+    type Result = Get<Object, '0.foo'>
+    expectTypeOf<Result>().toEqualTypeOf<'foo'>()
+  })
+
+  it('should extract the value of a nested array in an object', () => {
+    interface Object { foo: { bar: { baz: [true] } } }
+    type Result = Get<Object, 'foo.bar.baz.0'>
+    expectTypeOf<Result>().toEqualTypeOf<true>()
+  })
+
+  it('should extract the value of a nested readonly array', () => {
+    type Object = Readonly<{ foo: readonly ['bar'] }>
+    type Result = Get<Object, 'foo.0'>
     expectTypeOf<Result>().toEqualTypeOf<'bar'>()
   })
 
-  it('should extract the value in an object with a string number key', () => {
-    type Result = Get<['a', 'b', 'c'], '1'>
-    expectTypeOf<Result>().toEqualTypeOf<'b'>()
+  it('should extract the value of a nested readonly object', () => {
+    type Object = Readonly<{ foo: Readonly<{ bar: 'baz' }> }>
+    type Result = Get<Object, 'foo.bar'>
+    expectTypeOf<Result>().toEqualTypeOf<'baz'>()
   })
 
-  it('should extract the value in an object with a number key', () => {
-    type Result = Get<['a', 'b', 'c'], 1>
-    expectTypeOf<Result>().toEqualTypeOf<'b'>()
-  })
-
-  it('should extract the value in an object with a symbol key', () => {
-    const symbol = Symbol('foo')
-    type Result = Get<{ [symbol]: 'x' }, typeof symbol>
-    expectTypeOf<Result>().toEqualTypeOf<'x'>()
-  })
-
-  it('should return `never` if the key does not exist', () => {
-    type Result = Get<{ foo: 'bar' }, 'bar'>
+  it('should return never if the path is invalid', () => {
+    type Result = Get<{ foo: string }, 'bar'>
     expectTypeOf<Result>().toEqualTypeOf<never>()
   })
 }
