5aa4ee2d60ba0d17e270c7dafd27a0bb99b734b8
Stanley Horwood <stanley@hsjm.io>
2023-10-07 18:34:24 +0200

    feat(collection): improve `compact` function typing and implementation
    The `compact` function now has a better type signature to prevent typescript errors. The implementation has also been improved to use a function declaration rather than an arrow function for consistency throughout the codebase. Additionally, we now explicitly typecast the result as `Array<NotNil<T>>` to ensure type safety. Related tests have been updated accordingly, checking for the correct type as well as the correct output.

--- packages/collection/compact.ts
+++ packages/collection/compact.ts
@@ -1,30 +1,34 @@
-import { NotNil } from '@unshared/types/NotNil'
+import type { NotNil } from '@unshared/types'
 
 /**
  * Filters out `undefined` and `null` values from an array.
  *
  * @param value The array to filter
  * @returns The filtered array
+ * @example compact([0, 1, undefined, 2, null, 3]) // => [0, 1, 2, 3]
  */
-export const compact = <T>(value: Array<T>): Array<NotNil<T>> =>
-  // @ts-expect-error: `filter` is not typed correctly.
-  value.filter(x => x !== undefined && x !== null)
+export function compact<T>(value: Array<T>): Array<NotNil<T>> {
+  return value.filter(x => x !== undefined && x !== null) as Array<NotNil<T>>
+}
 
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should filter out undefined values', () => {
-    const result = compact([true, undefined])
-    expect(result).toEqual([true])
+    const result = compact([1, undefined])
+    expect(result).toEqual([1])
+    expectTypeOf(result).toEqualTypeOf<Array<number>>()
   })
 
   it('should filter out null values', () => {
     // eslint-disable-next-line unicorn/no-null
     const result = compact([true, null])
     expect(result).toEqual([true])
+    expectTypeOf(result).toEqualTypeOf<Array<boolean>>()
   })
 
   it('should not filter out falsy values', () => {
     const result = compact([true, false])
     expect(result).toEqual([true, false])
+    expectTypeOf(result).toEqualTypeOf<Array<boolean>>()
   })
 }
