31eca2d1d2e45804e22186fe2ec66ed62e216845
Stanley Horwood <stanley@hsjm.io>
2023-10-07 19:22:01 +0200

    feat(types): introduce `MaybeLiteral` and remove `StringNotLiteral`
    Introduced a new `MaybeLiteral` type that wraps a literal type in a union with `string` to allow for comprehensive autocompletion and type checking. This new type replaces the now-deleted `StringNotLiteral` type.
    
    Tests were implemented to ensure `MaybeLiteral` behaves as expected. The `MaybeLiteral` type improves the usage of nested properties of an object and can be matched with any string type.

---
+++ packages/types/MaybeLiteral.ts
@@ -0,0 +1,24 @@
+/**
+ * Wrap a literal type in a union with `string` to make it matchable with a
+ * any string type. This is useful for creating a type that can be used to
+ * access nested properties of an object allowing comprehensive autocompletion
+ * and type checking without forcing the user to use a string literal union.
+ *
+ * @template T Literal type to wrap.
+ * @returns Literal type wrapped in a union with `string & {}`.
+ * @example MaybeLiteral<'foo'> // 'foo' | (string & {})
+ */
+export type MaybeLiteral<T extends string> = T | (string & {})
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  it('should match a string type', () => {
+    type Result = MaybeLiteral<string>
+    expectTypeOf<Result>().toEqualTypeOf<string>()
+  })
+
+  it('should match a literal string', () => {
+    type Result = MaybeLiteral<'foo' | 'bar'>
+    expectTypeOf<Result>().toEqualTypeOf<'foo' | 'bar' | (string & {})>()
+  })
+}
