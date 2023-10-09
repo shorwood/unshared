87f4312dff1898432fdd931853d052828e406b60
Stanley Horwood <stanley@hsjm.io>
2023-10-09 13:53:40 +0200

    chore(vitest): restructure test configuration
    The Vitest configuration file has been restructured for better clarity and maintainability. The changes include moving all package names to an array for easier modifications, implementing a `resolve` helper function, transforming `reporters` to 'basic', and using absolute paths in `setupFiles`, `include`, and `exclude`. Also, added `useAtomics: true` to enable Atomics support and adjusted the API configuration for Vitest UI. Lastly, reorganized the `coverage` and `typecheck` configurations for better readability.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,36 +1,54 @@
 import { defineConfig } from 'vitest/config'
 
+const modules = [
+  'binary',
+  'boolean',
+  'collection',
+  'color',
+  'environment',
+  'fs',
+  'function',
+  'math',
+  'network',
+  'string',
+  'reactivity',
+  'types',
+]
+
+const resolve = (path: string) => new URL(path, import.meta.url).pathname
+
 export default defineConfig({
   test: {
     open: false,
     globals: true,
+    useAtomics: true,
     testTimeout: 100,
-    reporters: ['dot'],
+    reporters: ['basic'],
+    setupFiles: resolve('packages/setupTest.ts'),
+    includeSource: modules.map(m => `packages/${m}/*.ts`).map(resolve),
+    include: modules.map(m => `packages/${m}/*.test.ts`).map(resolve),
+    exclude: ['**/node_modules/**'].map(resolve),
+
+    // --- Vitest UI configuration.
+    api: {
+      host: '0.0.0.0',
+      port: 4000,
+      strictPort: true,
+    },
+
+    // --- Type-checking configuration.
     typecheck: {
       checker: 'tsc',
       ignoreSourceErrors: false,
       include: ['./**/*.ts'],
     },
+
     coverage: {
+      enabled: false,
       100: true,
       clean: true,
-      enabled: false,
       cleanOnRerun: true,
-      excludeNodeModules: true,
       reporter: ['html'],
     },
-    setupFiles: new URL('packages/setupTest.ts', import.meta.url).pathname,
-    passWithNoTests: true,
-    include: [
-      './**/*.test.ts',
-      './**/*.test.*.ts',
-    ],
-    includeSource: [
-      './packages/**/*.ts',
-    ],
-    exclude: [
-      new URL('packages/setupTest.ts', import.meta.url).pathname,
-      new URL('**/node_modules/**', import.meta.url).pathname,
-    ],
   },
 })
