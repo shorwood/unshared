3095beaf009280c02f286475d9bc868c603c1be2
Stanley Horwood <stanley@hsjm.io>
2023-10-13 22:31:26 +0200

    feat(types): extend Collection type to include iterators
    The `Collection` type in the `types` package has been extended to
    include iterators by adding `{ [Symbol.iterator](): IterableIterator<T> }`
    to its union type. This allows for types that implement the
    iterator protocol to be considered collections, improving type flexibility
    and functionality.

--- packages/types/Collection.ts
+++ packages/types/Collection.ts
@@ -9,18 +9,19 @@ export type Collection<T = unknown> =
   Record<number | string | symbol, T>
   | T[]
   | readonly T[]
+  | { [Symbol.iterator](): IterableIterator<T> }
 
 /** c8 ignore next */
 if (import.meta.vitest) {
   it('should return a collection of numbers', () => {
     type Result = Collection<number>
-    type Expected = number[] | Record<number | string | symbol, number> | readonly number[]
+    type Expected = number[] | Record<number | string | symbol, number> | readonly number[] | { [Symbol.iterator](): IterableIterator<number> }
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 
   it('should return a collection of unknowns', () => {
     type Result = Collection
-    type Expected = Record<number | string | symbol, unknown> | unknown[] | readonly unknown[]
+    type Expected = Record<number | string | symbol, unknown> | unknown[] | readonly unknown[] | { [Symbol.iterator](): IterableIterator<unknown> }
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 }
