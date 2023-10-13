3fa0e2e2b2232f5fc6423c0dc089fb6da4158374
Stanley Horwood <stanley@hsjm.io>
2023-10-13 22:53:46 +0200

    chore(collection): rename `castArray` to `toArray`
    This commit renames the `castArray` function to `toArray` in the collection package to improve clarity. All references to `castArray` in the package have been updated to `toArray`.

--- packages/collection/toArray.ts
+++ packages/collection/toArray.ts
@@ -6,11 +6,11 @@ import { MaybeArray, Nil } from '@unshared/types'
  *
  * @param value The value to wrap.
  * @returns The arrayified value.
- * @example castArray(1) // [1]
+ * @example toArray(1) // [1]
  */
-export function castArray(value?: Nil): []
-export function castArray<U>(value?: MaybeArray<U>): U[]
-export function castArray(value?: unknown): unknown[] {
+export function toArray(value?: Nil): []
+export function toArray<U>(value?: MaybeArray<U>): U[]
+export function toArray(value?: unknown): unknown[] {
   if (value === undefined || value === null) return []
   return Array.isArray(value) ? value : [value]
 }
@@ -18,26 +18,26 @@ export function castArray(value?: unknown): unknown[] {
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should return the array if the value is an array', () => {
-    const result = castArray([1, 2, 3])
+    const result = toArray([1, 2, 3])
     expect(result).toEqual([1, 2, 3])
     expectTypeOf(result).toEqualTypeOf<number[]>()
   })
 
   it('should return an array with the value if the value is not an array', () => {
-    const result = castArray(1)
+    const result = toArray(1)
     expect(result).toEqual([1])
     expectTypeOf(result).toEqualTypeOf<number[]>()
   })
 
   it('should return an empty array if the value is undefined', () => {
-    const result = castArray()
+    const result = toArray()
     expect(result).toEqual([])
     expectTypeOf(result).toEqualTypeOf<[]>
   })
 
   it('should return an empty array if the value is null', () => {
     // eslint-disable-next-line unicorn/no-null
-    const result = castArray(null)
+    const result = toArray(null)
     expect(result).toEqual([])
     expectTypeOf(result).toEqualTypeOf<[]>
   })
