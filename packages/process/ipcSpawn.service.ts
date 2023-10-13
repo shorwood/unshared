75503cffccffdb425b1cfe3c86b59a8cb25686a6
Stanley Horwood <stanley@hsjm.io>
2023-10-13 15:04:07 +0200

    chore(process): remove deprecated IPC methods
    The old IPC methods are deprecated, and no longer in use in favor of the newly introduced WorkerService for improved inter-process communication (IPC). This includes ipcCall, ipcHandle, ipcImport, ipcPublish, ipcRequest, ipcRespond, ipcSpawn and ipcSubscribe. They are hereby removed to maintain the codebase clean and avoid confusion.

--- packages/process/ipcSpawn.service.ts
+++
@@ -1,9 +0,0 @@
-import { parentPort } from 'node:worker_threads'
-import { ipcRespond } from './ipcRespond'
-import { call } from './utils/moduleCall'
-
-// --- Listen for calls to `spawn` and respond with the result of `call`.
-ipcRespond('spawn', call)
-
-// --- Listen for calls to `close` and close the worker thread.
-ipcRespond('close', () => parentPort?.close())
--- packages/process/ipcSpawn.worker.ts
+++
@@ -1,5 +0,0 @@
-import { ipcHandle } from './ipcHandle'
-import { call } from './utils/moduleCall'
-
-// --- Throw an error if this file is run in the main thread.
-ipcHandle('spawn', call)
