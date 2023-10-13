3845b112613cfb98ae0ce8c6ac323b3c998979ea
Stanley Horwood <stanley@hsjm.io>
2023-10-13 14:53:52 +0200

    chore(functions): refactor debounce function to return void
    Refactored the debounce function to return void instead of a Promise. This is a cleaner and simpler implementation. Additionally, test cases are also updated to reflect this change.

--- packages/functions/debounce.ts
+++ packages/functions/debounce.ts
@@ -1,46 +1,34 @@
-import { Function } from '@unshared/types/Function'
-
-/* eslint-disable unicorn/prevent-abbreviations */
-export type Debounced<T extends Function> =
-  (...parameters: Parameters<T>) => ReturnType<T> extends Promise<unknown>
-    ? ReturnType<T>
-    : Promise<ReturnType<T>>
+import { NumberIntegerPositive, Function } from '@unshared/types'
 
 /**
- * Debounce a function so that it will only execute after the specified delay.
- * If the function is called again before the delay has passed,
- * the timer will be reset. Useful for implementing spam protection.
+ * A debounced function that executes once within a specified delay.
  *
- * If the delay is `0`, the returned function will be the original function.
+ * @template T The type of the function.
+ * @example Debounced<(a: number) => number> = (a: number) => void
+ */
+export type Debounced<T extends Function> = (...parameters: Parameters<T>) => void
+
+/**
+ * Debounce a function so that it will only execute after the specified delay. If the function
+ * is called multiple times before the delay has passed, the function will only execute once
+ * after the delay has passed. The function will be called with the parameters of the last call.
  *
  * @param fn The function to debounce.
  * @param delay The debounce delay in milliseconds.
- * @returns A debounced function that will only execute after the specified delay.
- * @example
- * const getUserNow = (id: string) => fetch(`/users/${id}`)
- * const getUser = debounce(getUser, 1000)
- * const user = await getUser('123') // Will be ignored.
- * const user = await getUser('123') // Will be ignored.
- * const user = await getUser('123') // Will only execute after 1000ms.
+ * @returns The debounced function.
+ * @example debounce(() => console.log('Hello'), 1000)
  */
-export const debounce = <T extends Function>(fn: T, delay: number): Debounced<T> => {
-  if (delay < 0) throw new RangeError('Expected delay to be a positive number.')
-  if (delay === 0) return fn as Debounced<T>
-
-  // --- Initialize timeout.
-  let timeout: NodeJS.Timeout
-  let result: unknown
+export function debounce<T extends Function, N extends number>(fn: T, delay: NumberIntegerPositive<N>): Debounced<T> {
+  let timeout: NodeJS.Timeout | undefined
 
   // --- Instantiate and return a debounced function.
-  // @ts-expect-error: override the return type.
-  return (...parameters: Parameters<T>[]) =>
-    new Promise((resolve) => {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        result = fn(...parameters)
-        resolve(result as ReturnType<T>)
-      }, delay)
-    })
+  const debounced = (...parameters: Parameters<T>): void =>{
+    clearTimeout(timeout)
+    timeout = setTimeout(() => fn(...parameters), delay)
+  }
+
+  // --- Return the debounced function.
+  return debounced as unknown as Debounced<T>
 }
 
 /* c8 ignore next */
@@ -51,65 +39,55 @@ if (import.meta.vitest) {
 
   it('should debounces a function so it is only called once after delay', async() => {
     let count = 0
-    const debounced = debounce(() => count++, 1000)
+    const debounced = debounce(() => count++, 10)
     debounced()
     debounced()
     debounced()
-    vi.advanceTimersByTime(1000)
+    vi.advanceTimersByTime(100)
     expect(count).toEqual(1)
   })
 
-  it('should pass the parameters to the debounced function', async() => {
+  it('should pass the parameters of the last call to the debounced function', async() => {
     let count = 0
-    const debounced = debounce((value: number) => (count += value), 1000)
-    const result = debounced(10)
-    expect(result).resolves.toEqual(10)
+    const debounced = debounce((n: number) => count = n, 10)
+    debounced(1)
+    debounced(2)
+    debounced(3)
+    vi.advanceTimersByTime(100)
+    expect(count).toEqual(3)
   })
 
-  it('should return an async function that resolves once the delay has passed', async() => {
+  it('should be able to execute the debounced function again after the delay has passed', async() => {
     let count = 0
-    const debounced = debounce(() => count++, 1000)
-    const result = Promise.all([debounced(), debounced(), debounced()])
-    expect(result).resolves.toEqual([1, 1, 1])
+    const debounced = debounce(() => count++, 10)
+    debounced()
+    vi.advanceTimersByTime(100)
+    expect(count).toEqual(1)
+    debounced()
+    vi.advanceTimersByTime(100)
+    expect(count).toEqual(2)
   })
 
-  it('should wrap the function result in a promise', async() => {
-    const debounced = debounce(() => 1, 1000)
+  it('should return undefined', async() => {
+    const debounced = debounce(async() => 1, 10)
     const result = debounced()
-    expect(result).resolves.toEqual(1)
+    vi.advanceTimersByTime(100)
+    expect(result).toBeUndefined()
   })
 
-  it('should wrap the function result in a promise if it is async', async() => {
-    const debounced = debounce(async() => 1, 1000)
-    const result = debounced()
-    expect(result).resolves.toEqual(1)
+  it('should not execute the function before the delay has passed', async() => {
+    let count = 0
+    const debounced = debounce(() => count++, 100)
+    debounced()
+    debounced()
+    debounced()
+    vi.advanceTimersByTime(50)
+    expect(count).toEqual(0)
   })
 
-  it('should pass the function if the delay is 0', async() => {
-    const fn = () => 1
-    const debounced = debounce(fn, 0)
-    expect(debounced).toBe(fn)
-  })
-
-  it('should throw if delay is lower than 1', () => {
-    // eslint-disable-next-line unicorn/consistent-function-scoping
-    const shouldThrow = () => debounce(() => {}, 0)
-    expect(shouldThrow).toThrow(RangeError)
-  })
-
-  it('should throw if the function is not a function', () => {
-    // @ts-expect-error: invalid parameter type.
-    const shouldThrow = () => debounce('function', 1000)
-    expect(shouldThrow).toThrow(TypeError)
-  })
-
-  it('should infer the return type of the debounced function', async() => {
-    const debounced = debounce(() => 1, 1000)
-    expectTypeOf(debounced).toEqualTypeOf<() => Promise<number>>()
-  })
-
-  it('should infer the return type of the debounced function if it is async', async() => {
-    const debounced = debounce(async() => 1, 1000)
-    expectTypeOf(debounced).toEqualTypeOf<() => Promise<number>>()
+  it('should transform the return type of the debounced function', async() => {
+    type Result = Debounced<(a: number) => number>
+    type Expected = (a: number) => void
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
   })
 }
