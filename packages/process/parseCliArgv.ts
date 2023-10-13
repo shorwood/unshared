09616ec03f0cd939f2cd0b3db27aa28393df6de7
Stanley Horwood <stanley@hsjm.io>
2023-10-13 15:08:11 +0200

    feat(process): add parseCliArgv and parseCliOption utilities
    Added `parseCliArgv` and `parseCliOption` utilities in the `process` package. These will be used for parsing command line arguments and options, respectively.
    
    `parseCliArgv` function parses command line argv and extracts options, non-option arguments, Node.js executable path, and current script path. It also has extensive test cases to ensure its reliability.
    
    `parseCliOption` is responsible for parsing a specific command line option and returning its object representation. It caters different option cases e.g., flags, nested options, and simple options. This utility, as well, includes test cases for validating its functionality.
    
    Adding these two utilities will make command-line parsing structured and easier.

---
+++ packages/process/parseCliArgv.ts
@@ -0,0 +1,96 @@
+/* eslint-disable unicorn/prevent-abbreviations */
+import { parseOption } from './parseCliOption'
+
+export interface ParseArgvReturnType<T extends Record<string, any> = any> {
+  /**
+   * Extracted options.
+   *
+   * @example
+   * parseArgv(['-f', '-b', '-q', '--foo', 'bar', 'baz']) // returns { f: true, b: true, q: true, foo: 'bar' }
+   */
+  options: T
+  /**
+   * Arguments that are not options.
+   *
+   * @example
+   * parseArgv(['-f', '-b', '-q', '--foo', 'bar', 'baz']) // returns ['baz']
+   */
+  args: string[]
+  /**
+   * The path to the Node.js executable.
+   */
+  nodePath: string
+  /**
+   * The path to the current script.
+   */
+  scriptPath: string
+}
+
+/**
+ * Parses the command line argv.
+ *
+ * @param argv The command line argv.
+ * @returns An object with options mapped to an object.
+ * @example
+ * parseArgv(['-f', '-bq', '--foo', 'bar', 'baz']) // returns { f: true, b: true, q: true, foo: 'bar' }
+ */
+export function parseArgv <T extends Record<string, any>>(argv: NodeJS.Process['argv']): ParseArgvReturnType<T> {
+  const parsedArgs = [] as string[]
+  const parsedOptions = {} as T
+  const [nodePath, scriptPath, ...parameters] = argv
+
+  // --- Parse the argv.
+  for (let index = 0; index < parameters.length; index++) {
+    const argument = parameters[index]
+
+    // --- If the parameter starts with a dash, it's an option.
+    if (argument.startsWith('-')) {
+      const next = parameters[index + 1]
+
+      // --- Parse option and increment the index if option has a value.
+      const parsedOption = next && !next.startsWith('-')
+        ? (index++, parseOption(argument, next))
+        : parseOption(argument)
+
+      // --- Merge the option into the result.
+      Object.assign(parsedOptions, parsedOption)
+    }
+
+    // --- Otherwise, it's an argument.
+    else { parsedArgs.push(parameters[index]) }
+  }
+
+  // --- Return the result.
+  return {
+    args: parsedArgs,
+    options: parsedOptions,
+    nodePath,
+    scriptPath,
+  }
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  it.each([
+
+    // --- Simple option.
+    ['foo bar', { options: {}, args: ['foo', 'bar'] }],
+    ['--foo bar', { options: { foo: 'bar' }, args: [] }],
+    ['--foo --bar', { options: { foo: true, bar: true }, args: [] }],
+
+    // --- Flags
+    ['-fbq', { options: { f: true, b: true, q: true }, args: [] }],
+    ['-f -b -q', { options: { f: true, b: true, q: true }, args: [] }],
+    ['foo -fbq', { options: { f: true, b: true, q: true }, args: ['foo'] }],
+    ['foo -f -b -q', { options: { f: true, b: true, q: true }, args: ['foo'] }],
+
+    // --- Combined option.
+    ['--baz.qux -f -bq --foo bar', { options: { baz: { qux: true }, f: true, b: true, q: true, foo: 'bar' }, args: [] }],
+
+  ])('should parse %j to %j is parsed', (argv, expected) => {
+    const wrappedArgv = ['/usr/local/bin/node', 'index.js', ...argv.split(' ')]
+    const wrappedExpected = { ...expected, nodePath: '/usr/local/bin/node', scriptPath: 'index.js' }
+    const result = parseArgv(wrappedArgv)
+    expect(result).toEqual(wrappedExpected)
+  })
+}
