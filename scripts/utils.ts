505c77e574f806ad4439d80646b72e76b7d18c69
Stanley Horwood <stanley@hsjm.io>
2023-10-15 19:40:03 +0200

    chore(scripts): consolidate utility functions and add `getPackageMetadata`
    This commit consolidates the utility scripts `getGitHash.ts` and `getGitRemoteUrl.ts` into a single `utils.ts` file. These functions are now exported from `utils.ts`, reducing redundancy and streamlining the codebase.
    
    Additionally, a new function `getPackageMetadata` has been added to `utils.ts`. This function retrieves the package metadata for a given package name, enhancing our ability to handle package-related tasks. This changes includes the addition of the `types-pkg-json` and `types-tsconfig` packages.

---
+++ scripts/utils.ts
@@ -0,0 +1,76 @@
+import { execFile } from 'node:child_process'
+import { relative, resolve } from 'node:path'
+import { cwd as getCwd } from 'node:process'
+import { loadObject } from '@unshared/fs/loadObject'
+import { toPascalCase } from '@unshared/string/toPascalCase'
+import { PackageJSON } from 'types-pkg-json'
+import { ROOT_PATH } from './constants'
+
+/** The current working directory. */
+const cwd = getCwd()
+
+/**
+ * Get the package metadata from a package name.
+ *
+ * @param packageName The name of the package.
+ * @returns The package metadata.
+ * @example getPackageMetadata('my-package') // { name: 'my-package', version: '0.0.1' }
+ */
+export async function getPackageMetadata(packageName: string) {
+  const globalName = toPascalCase('Unshared', packageName)
+  const outputPath = resolve(ROOT_PATH, 'packages', packageName, 'dist')
+  const packagePath = resolve(ROOT_PATH, 'packages', packageName)
+  const packageRelativePath = relative(ROOT_PATH, packagePath)
+  const packageJsonPath = resolve(packagePath, 'package.json')
+  const packageJsonFS = loadObject<PackageJSON>(packageJsonPath)
+  const packageJson = await packageJsonFS
+
+  return {
+    packageName,
+    globalName,
+    outputPath,
+    packagePath,
+    packageJson,
+    packageJsonFS,
+    packageJsonPath,
+    packageRelativePath,
+  }
+}
+
+/**
+ * Get the hash of a commit in a repository at a given path.
+ *
+ * @param path The path to the repository directory.
+ * @param name The name or refence of the commit.
+ * @returns The git hash of the current commit.
+ * @example getGitHash() // 'a1b2c3d4e5f6g7h8i9j0'
+ */
+export async function getGitHash(path: string = cwd, name = 'HEAD'): Promise<string> {
+  return await new Promise<string>((resolve, reject) => {
+    execFile(
+      'git',
+      ['-C', path, 'rev-parse', name],
+      { encoding: 'utf8' },
+      (error, stdout) => (error ? reject(error) : resolve(stdout.trim())),
+    )
+  })
+}
+
+/**
+ * Get the remote URL of a repository at a given path.
+ *
+ * @param path The path to the repository.
+ * @param name The name of the remote.
+ * @returns The remote URL of the repository.
+ * @example getGitRemoteUrl() // 'git@github.com:example/project.git'
+ */
+export async function getGitRemoteUrl(path: string = cwd, name = 'origin'): Promise<string> {
+  return await new Promise<string>((resolve, reject) => {
+    execFile(
+      'git',
+      ['-C', path, 'config', '--get', `remote.${name}.url`],
+      { encoding: 'utf8' },
+      (error, stdout) => (error ? reject(error) : resolve(stdout.trim())),
+    )
+  })
+}
