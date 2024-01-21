e3c4983d794eb7170f65a4f97db8ad6b545dbaf9
Stanley Horwood <stanley@hsjm.io>
2024-01-21 22:44:31 +0100

    chore(binary): continue WIP on archive extraction and creation methods
    Continued work-in-progress on the archive handling functionality within the `binary` package. This includes setting up scaffolding for new archive creation and extraction methods for various formats such as 7z, Rar, Tar, and Zip. The associated placeholder files for the previous implementation have been removed, and the new files have been added with TODO comments to indicate pending implementation. The refactor transitions towards a more modular and extensible approach for handling different archive types.

---
+++ packages/binary/extractArchiveZip.ts
@@ -0,0 +1,25 @@
+/**
+ * Extracts the content of a Zip archive into a map of `BinaryLike` values and their
+ * relative paths in the archive. The archive can be any binary-like value, including
+ * strings, buffers, streams, and more.
+ *
+ * This function returns an awaitable that synchronously returns an `AsyncIterable` of
+ * the entries in the archive. The entries are lazily extracted from the archive as
+ * they are iterated over. The result is also awaitable, and will resolve to a map of
+ * file paths to binary-like contents.
+ *
+ * @param archive The Zip archive to extract.
+ * @param options The options to use.
+ * @returns A map of file paths to binary-like contents.
+ * @example
+ * const readStream = fs.createReadStream('archive.zip')
+ * const entries = extractArchiveZip(readStream)
+ *
+ * // Iterate over the entries.
+ * for await (const [path, content] of entries) {}
+ *
+ * // Or get the entries map.
+ * const map = await entries // { ... }
+ */
+// TODO: Implement this function.
+// eslint-disable-next-line unicorn/no-empty-file
