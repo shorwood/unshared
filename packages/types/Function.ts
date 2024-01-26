b19b2ea55fb721a3ef864066d9869966a1ba416c
Stanley Horwood <stanley@hsjm.io>
2024-01-26 02:36:15 +0100

    feat(types): enhance `Function` and `FunctionAsync` typings for better inference
    Refined the typings for `Function` and `FunctionAsync` to allow more precise type inferences. The updates introduce better support for functions with varying numbers of parameters, improving type checking and developer experience. Now, functions with one parameter will have a dedicated typing, while functions with multiple parameters will continue to use the rest parameters approach. This change enhances type safety and predictability when working with these utility types.

--- packages/types/Function.ts
+++ packages/types/Function.ts
@@ -5,17 +5,29 @@
  * @template P The type of the parameters.
  * @example Function<boolean, [number, string]> // (a: number, b: string) => boolean
  */
-export type Function<U = unknown, P extends any[] = any[]> = (...parameters: P) => U
+export type Function<U = unknown, P = unknown[]> = P extends unknown[]
+  ? (...args: P) => U
+  : (arg0: P) => U
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should build a function with no parameters', () => {
+  it('should return a function type with no parameters', () => {
     type Result = Function<boolean>
-    expectTypeOf<Result>().toEqualTypeOf<(...p: any[]) => boolean>()
+    expectTypeOf<Result>().toEqualTypeOf<(...args: unknown[]) => boolean>()
   })
 
-  it('should build a function with parameters', () => {
+  it('should return a function type with 1 parameter', () => {
+    type Result = Function<string, number>
+    expectTypeOf<Result>().toEqualTypeOf<(arg0: number) => string>()
+  })
+
+  it('should build a function with 2 parameters', () => {
     type Result = Function<string, [number, string]>
-    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string) => string>()
+    expectTypeOf<Result>().toEqualTypeOf<(...args: [number, string]) => string>()
+  })
+
+  it('should build a function with 3 parameters', () => {
+    type Result = Function<string, [a: number, b: string, c: boolean]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c: boolean) => string>()
   })
 }
--- packages/types/FunctionAsync.ts
+++ packages/types/FunctionAsync.ts
@@ -1,21 +1,33 @@
 /**
- * A function that takes parameters and returns a promise.
+ * A FunctionAsync that takes parameters and returns a promise.
  *
- * @template U The type of the resolved value.
+ * @template U The type of the value returned by the promise.
  * @template P The type of the parameters.
- * @example FunctionAsync<boolean, [number, string]> // (a: number, b: string) => Promise<boolean>
+ * @example FunctionAsync<boolean, [number, string]> // (a: number, b: string) => boolean
  */
-export type FunctionAsync<U = unknown, P extends unknown[] = unknown[]> = (...parameters: P) => Promise<U>
+export type FunctionAsync<U = unknown, P = unknown[]> = P extends unknown[]
+  ? (...args: P) => Promise<U>
+  : (arg0: P) => Promise<U>
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should build a function with no parameters', () => {
+  it('should return a FunctionAsync type with no parameters', () => {
     type Result = FunctionAsync<boolean>
-    expectTypeOf<Result>().toEqualTypeOf<(...p: unknown[]) => Promise<boolean>>()
+    expectTypeOf<Result>().toEqualTypeOf<(...args: unknown[]) => Promise<boolean>>()
   })
 
-  it('should build a function with parameters', () => {
+  it('should return a FunctionAsync type with 1 parameter', () => {
+    type Result = FunctionAsync<string, number>
+    expectTypeOf<Result>().toEqualTypeOf<(arg0: number) => Promise<string>>()
+  })
+
+  it('should build a FunctionAsync with 2 parameters', () => {
     type Result = FunctionAsync<string, [number, string]>
-    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string) => Promise<string>>()
+    expectTypeOf<Result>().toEqualTypeOf<(...args: [number, string]) => Promise<string>>()
+  })
+
+  it('should build a FunctionAsync with 3 parameters', () => {
+    type Result = FunctionAsync<string, [a: number, b: string, c: boolean]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c: boolean) => Promise<string>>()
   })
 }
