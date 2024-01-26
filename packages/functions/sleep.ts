c0d9238e836122cf813fc1f5444e3c36b07ce5b1
Stanley Horwood <stanley@hsjm.io>
2024-01-26 03:29:17 +0100

    feat(functions): enforce integer type and validation for `sleep` function
    Enhanced the `sleep` function to enforce the use of a positive integer type for the delay parameter. The function now imports `NumberIntegerPositive` from `@unshared/types` to ensure that delay is an integer. Added checks to throw `TypeError` if the delay is not a safe integer and `RangeError` if it's negative. Updated the test suite to reflect these changes and to test for the new constraints, increasing the robustness and reliability of the `sleep` utility.

--- packages/functions/sleep.ts
+++ packages/functions/sleep.ts
@@ -1,3 +1,5 @@
+import { NumberIntegerPositive } from "@unshared/types"
+
 /**
  * Create a promise that resolves after a given delay.
  *
@@ -5,7 +7,8 @@
  * @returns A promise that resolves in `ms` milliseconds
  * @example await sleep(1000)
  */
-export function sleep(delay: number): Promise<void> {
+export function sleep<N extends number>(delay: NumberIntegerPositive<N>): Promise<void> {
+  if (Number.isSafeInteger(delay) === false) throw new TypeError('Expected an integer delay.')
   if (delay < 0) throw new RangeError('Expected delay to be a positive number.')
   return new Promise(resolve => setTimeout(resolve, delay))
 }
@@ -19,11 +22,23 @@ if (import.meta.vitest) {
   it('should resolve after a given delay', async() => {
     const result = sleep(1000)
     vi.advanceTimersByTime(1000)
-    expect(result).resolves.toBeUndefined()
+    await expect(result).resolves.toBeUndefined()
   })
 
-  it('should throw if delay is negative', async() => {
-    const shouldThrow = () => sleep(-1000)
+  it('should throw an error if the delay is negative', () => {
+    // @ts-expect-error: Delay is negative
+    const shouldThrow = () => sleep(-1)
     expect(shouldThrow).toThrow(RangeError)
+  })
+
+  it('should throw an error if the delay is not an integer', () => {
+    // @ts-expect-error: Delay is not an integer
+    const shouldThrow = () => sleep(1.5)
+    expect(shouldThrow).toThrow(TypeError)
+  })
+
+  it('should throw an error if the delay is not finite', () => {
+    const shouldThrow = () => sleep(Number.NaN)
+    expect(shouldThrow).toThrow(TypeError)
   })
 }
