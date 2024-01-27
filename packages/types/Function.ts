b42729e27d9ec5508403c99892005055271b6491
Stanley Horwood <stanley@hsjm.io>
2024-01-27 19:01:48 +0100

    fix(types): restrict `Function` type to only accept array for parameters
    The `Function` type was too permissive, accepting any type as its parameters. This commit corrects the type definition to ensure that `P` is always an array of unknown types. This change enforces the use of an array for function parameters, thereby eliminating the overload that allowed a single non-array type, which could lead to improper usage. This update improves type safety and aligns with the intended use of the `Function` type in TypeScript.

--- packages/types/Function.ts
+++ packages/types/Function.ts
@@ -5,9 +5,7 @@
  * @template P The type of the parameters.
  * @example Function<boolean, [number, string]> // (a: number, b: string) => boolean
  */
-export type Function<U = unknown, P = unknown[]> = P extends unknown[]
-  ? (...args: P) => U
-  : (arg0: P) => U
+export type Function<U = unknown, P extends unknown[] = unknown[]> = (...args: P) => U
 
 /** c8 ignore next */
 if (import.meta.vitest) {
@@ -16,18 +14,18 @@ if (import.meta.vitest) {
     expectTypeOf<Result>().toEqualTypeOf<(...args: unknown[]) => boolean>()
   })
 
-  it('should return a function type with 1 parameter', () => {
-    type Result = Function<string, number>
-    expectTypeOf<Result>().toEqualTypeOf<(arg0: number) => string>()
-  })
-
   it('should build a function with 2 parameters', () => {
     type Result = Function<string, [number, string]>
     expectTypeOf<Result>().toEqualTypeOf<(...args: [number, string]) => string>()
   })
 
-  it('should build a function with 3 parameters', () => {
-    type Result = Function<string, [a: number, b: string, c: boolean]>
-    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c: boolean) => string>()
+  it('should build a function with rest parameters', () => {
+    type Result = Function<string, [a: number, ...b: string[]]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, ...b: string[]) => string>()
+  })
+
+  it('should build a function with optional parameters', () => {
+    type Result = Function<string, [a: number, b: string, c?: boolean]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c?: boolean) => string>()
   })
 }
