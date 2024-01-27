3ada07fc93e44931dcb017a74e5744164bbce46c
Stanley Horwood <stanley@hsjm.io>
2024-01-27 19:02:01 +0100

    fix(types): refine Overloads type and ensure unique overload signatures
    Refactored the Overloads type to ensure that it only accepts functions with array types for parameters. Introduced the use of the Unique type to eliminate duplicate overload signatures. This change enhances the type safety and correctness of the Overloads utility, ensuring that it accurately represents the possible overloads of a given function. Additionally, this update includes adjustments to the test cases to align with the new Overloads type constraints.

--- packages/types/Overloads.ts
+++ packages/types/Overloads.ts
@@ -1,3 +1,4 @@
+import { Unique } from './Unique'
 import { Function } from './Function'
 
 /**
@@ -14,47 +15,49 @@ import { Function } from './Function'
  * declare function foo(x: string): boolean
  * Overloads<typeof foo> // [(x: number) => boolean, (x: string) => boolean]
  */
-export type Overloads<T extends Function> =
-T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5; (...p: infer P6): infer R6; (...p: infer P7): infer R7; (...p: infer P8): infer R8; (...p: infer P9): infer R9; (...p: infer P10): infer R10 }
-  ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5, (...p: P6) => R6, (...p: P7) => R7, (...p: P8) => R8, (...p: P9) => R9, (...p: P10) => R10]
-  : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5; (...p: infer P6): infer R6; (...p: infer P7): infer R7; (...p: infer P8): infer R8; (...p: infer P9): infer R9 }
-    ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5, (...p: P6) => R6, (...p: P7) => R7, (...p: P8) => R8, (...p: P9) => R9]
-    : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5; (...p: infer P6): infer R6; (...p: infer P7): infer R7; (...p: infer P8): infer R8 }
-      ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5, (...p: P6) => R6, (...p: P7) => R7, (...p: P8) => R8]
-      : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5; (...p: infer P6): infer R6; (...p: infer P7): infer R7 }
-        ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5, (...p: P6) => R6, (...p: P7) => R7]
-        : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5; (...p: infer P6): infer R6 }
-          ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5, (...p: P6) => R6]
-          : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4; (...p: infer P5): infer R5 }
-            ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4, (...p: P5) => R5]
-            : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3; (...p: infer P4): infer R4 }
-              ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3, (...p: P4) => R4]
-              : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2; (...p: infer P3): infer R3 }
-                ? [(...p: P1) => R1, (...p: P2) => R2, (...p: P3) => R3]
-                : T extends { (...p: infer P1): infer R1; (...p: infer P2): infer R2 }
-                  ? [(...p: P1) => R1, (...p: P2) => R2]
-                  : T extends { (...p: infer P1): infer R1 }
-                    ? [(...p: P1) => R1]
-                    : T extends (...p: infer P) => infer R
-                      ? [(...p: P) => R]
-                      : never
+export type Overloads<T extends Function<any, any[]>> =
+T extends {
+  (...p: infer P1): infer R1
+  (...p: infer P2): infer R2
+  (...p: infer P3): infer R3
+  (...p: infer P4): infer R4
+  (...p: infer P5): infer R5
+  (...p: infer P6): infer R6
+  (...p: infer P7): infer R7
+  (...p: infer P8): infer R8
+  (...p: infer P9): infer R9
+  (...p: infer P10): infer R10
+}
+  ? Unique<[
+    (...p: P1) => R1,
+    (...p: P2) => R2,
+    (...p: P3) => R3,
+    (...p: P4) => R4,
+    (...p: P5) => R5,
+    (...p: P6) => R6,
+    (...p: P7) => R7,
+    (...p: P8) => R8,
+    (...p: P9) => R9,
+    (...p: P10) => R10,
+  ]>
+  : never
 
 /** c8 ignore next */
 if (import.meta.vitest) {
   it('should return a single signature if function has no overloads', () => {
-    type Method = (a: number, b: string) => boolean
+    type Method = (a: string, b: string) => boolean
     type Result = Overloads<Method>
     expectTypeOf<Result>().toEqualTypeOf<[Method]>()
   })
 
   it('should return a union of signatures if function has 2 overloads', () => {
     interface Method {
-      (a: number, b: string): boolean
+      (a: number, b: number): boolean
       (a: string, b: number): boolean
     }
     type Result = Overloads<Method>
     type Expected = [
-      (a: number, b: string) => boolean,
+      (a: number, b: number) => boolean,
       (a: string, b: number) => boolean,
     ]
     expectTypeOf<Result>().toEqualTypeOf<Expected>()
