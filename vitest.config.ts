e5b886bc8bd363c046db30df4711024026a3660c
Stanley Horwood <stanley@hsjm.io>
2024-02-03 17:52:11 +0100

    chore: refine Vitest configuration to use constants for package names
    Refactors the Vitest configuration file to utilize the constants for package names when defining `includeSource` and `include` paths for tests. This change aligns with our efforts to centralize and standardize package-related information, ensuring that our testing configuration remains consistent with the actual packages in the project. The adjustment also switches the coverage reporter to 'lcovonly' for better integration with CI tools.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vitest/config'
+import { PACKAGES_NAMES } from './scripts/constants'
+
+const includeSource = PACKAGES_NAMES.map(name => `./packages/${name}/**/*.ts`)
 
 export default defineConfig({
   test: {
@@ -7,15 +10,16 @@ export default defineConfig({
     testTimeout: 100,
     reporters: ['basic'],
     setupFiles: './packages/setupTest.ts',
-    includeSource: ['./packages/**/*.ts'],
-    include: ['./packages/**/*.test.ts'],
+    // includeSource: ['./packages/**/*.ts'],
+    includeSource,
+    include: [],
     exclude: ['**/node_modules/**'],
 
     // --- Type-checking configuration.
     typecheck: {
       checker: 'tsc',
       ignoreSourceErrors: false,
-      include: ['./**/*.ts'],
+      include: includeSource,
     },
 
     // --- V8 coverage configuration.
@@ -23,7 +27,7 @@ export default defineConfig({
       enabled: false,
       clean: true,
       cleanOnRerun: true,
-      reporter: ['html'],
+      reporter: ['lcovonly'],
     },
   },
 })
