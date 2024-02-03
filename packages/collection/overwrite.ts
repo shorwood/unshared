55a69e4fdb48c8f90cccded9f1452e15f829a9e7
Stanley Horwood <stanley@hsjm.io>
2024-02-03 18:29:14 +0100

    feat(collection): enhance `overwrite` function for objects and arrays
    Refactor the `overwrite` function to handle both objects and arrays, ensuring type safety and correct behavior. The function now accepts arrays, enforces that both the target and source are of the same type (either both objects or both arrays), and sets the length of the target array to match the source array when applicable. Additional test cases have been added to verify the new behavior and ensure properties are correctly added or deleted. The documentation and examples have also been updated to reflect these changes.

--- packages/collection/overwrite.ts
+++ packages/collection/overwrite.ts
@@ -1,62 +1,99 @@
-import { Collection } from '@unshared/types/Collection'
-
 /**
  * Overwrites the values of `object` with the values of `source`. This method
- * is like `Object.assign` except that it deletes the properties of `object`
- * that are not present in `source`.
+ * is like `Object.assign` except that it deletes properties that are not in
+ * the source object.
  *
  * @param object The object to overwrite.
  * @param source The source object.
  * @returns The mutated object.
+ * @example
+ * // Define an object.
+ * const object = { foo: 'foo', bar: 'bar' }
+ *
+ * // Overwrite the object with the source object.
+ * overwrite(object, { baz: 1 }) // { baz: 1 }
  */
-export function overwrite<T extends Collection>(object: Collection, source: T): T {
-  if (typeof object !== 'object' || object === null)
-    throw new TypeError('Expected an object')
-  if (typeof source !== 'object' || source === null)
-    throw new TypeError('Expected an object')
+export function overwrite<T extends object>(object: object, source: T): T
+export function overwrite<T extends unknown[]>(object: unknown[], source: T): T
+export function overwrite(object: object, source: object): object {
 
-  // --- Assign the values of `source` to `object` and delete additional
-  // --- properties of `object` that are not present in `source`.
-  for (const key in source) {
-    // @ts-expect-error: The key is present in `source`.
+  // --- Assert that the object and source are of both arrays or objects.
+  const sourceIsArray = Array.isArray(source)
+  const objectIsArray = Array.isArray(object)
+  if (sourceIsArray !== objectIsArray)
+    throw new TypeError('The object and source must both be arrays or objects.')
+
+  // --- Get the keys of the object and source.
+  const objectKeys = Object.keys(object)
+  const sourceKeys = Object.keys(source)
+  const keys = new Set([...objectKeys, ...sourceKeys])
+
+  // --- If the object and source are arrays, set the length of the object.
+  if (Array.isArray(object) && Array.isArray(source))
+    object.length = source.length
+
+  // --- Loop through the keys and overwrite the object.
+  for (const key of keys) {
+    // @ts-expect-error: Indexing `object` with `key` is not type safe.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     if (key in source) object[key] = source[key]
-    // @ts-expect-error: The key is present in `source`.
+    // @ts-expect-error: Indexing `object` with `key` is not type safe.
     else delete object[key]
   }
 
-  // --- Return the mutated object.
-  return object as T
+  // --- Return the object.
+  return object
 }
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should overwrite an object', () => {
-    const object = { foo: 'foo', bar: 'bar' } as const
-    const source = { foo: 'FOO', baz: 'BAZ' } as const
+  it('should return the object with the source values', () => {
+    const object = { foo: 'foo', bar: 'bar' }
+    const source = { foo: 1, bar: 2 }
     const result = overwrite(object, source)
-    expect(result).toStrictEqual(object)
-    expect(result).toEqual(source)
-    expectTypeOf(result).toEqualTypeOf(source)
+    expect(result).toStrictEqual({ foo: 1, bar: 2 })
   })
 
-  it('should overwrite an array', () => {
-    const array = [1, 2, 3] as const
-    const source = [4, 5] as const
+  it('should mutate the object with the source values', () => {
+    const object = { foo: 'foo', bar: 'bar' }
+    const source = { foo: 1, bar: 2 }
+    const result = overwrite(object, source)
+    expect(object).toBe(result)
+    expect(object).toStrictEqual({ foo: 1, bar: 2 })
+  })
+
+  it('should delete properties that are not in the source', () => {
+    const object = { foo: 'foo', bar: 'bar' }
+    const source = { foo: 1 }
+    const result = overwrite(object, source)
+    const resultKeys = Object.keys(result)
+    expect(resultKeys).toEqual(['foo'])
+  })
+
+  it('should add properties that are not in the object', () => {
+    const object = { foo: 'foo' }
+    const source = { foo: 1, bar: 2 }
+    const result = overwrite(object, source)
+    const resultKeys = Object.keys(result)
+    expect(resultKeys).toEqual(['foo', 'bar'])
+  })
+
+  it('should return an object with the type of the source', () => {
+    const object = { foo: 'foo', bar: 'bar' }
+    const source = { foo: 1, baz: 2 }
+    const result = overwrite(object, source)
+    expectTypeOf(result).toEqualTypeOf<{ foo: number; baz: number }>()
+  })
+
+  it('should overwrite an array and set the length', () => {
+    const array = [1, 2, 3]
+    const source = [101, 102]
     const result = overwrite(array, source)
-    expect(result).toStrictEqual(array)
-    expect(result).toEqual(source)
-    expectTypeOf(result).toEqualTypeOf(source)
+    expect(result).toStrictEqual([101, 102])
   })
 
-  it('should throw an error if the object is not an object', () => {
-    // @ts-expect-error: Invalid argument.
-    const shouldThrow = () => overwrite(1, {})
-    expect(shouldThrow).toThrow(TypeError)
-  })
-
-  it('should throw an error if the source is not an object', () => {
-    // @ts-expect-error: Invalid argument.
-    const shouldThrow = () => overwrite({}, 1)
+  it('should throw an error if the object and source are not both arrays or objects', () => {
+    const shouldThrow = () => overwrite({}, [])
     expect(shouldThrow).toThrow(TypeError)
   })
 }
