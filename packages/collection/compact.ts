51b6d357456bcba8bfeff1a75d30dd05f0abef7e
Stanley Horwood <stanley@hsjm.io>
2024-02-21 07:41:52 +0100

    chore(collection): enhance `compact` function tests for better type checks
    Update the test suite for the `compact` function to cover a wider range of inputs and accurately assert the resulting types. The tests now include cases with mixed value types and tuples, ensuring the function filters out `null` and `undefined` values correctly while preserving all other falsy values like `false`, `0`, `0n`, and `''`. The improvements in the tests will help catch potential regressions and maintain the function's reliability.

--- packages/collection/compact.ts
+++ packages/collection/compact.ts
@@ -14,21 +14,27 @@ export function compact<T>(value: T[]): Array<NotNil<T>> {
 /* c8 ignore next */
 if (import.meta.vitest) {
   it('should filter out undefined values', () => {
-    const result = compact([1, undefined])
-    expect(result).toEqual([1])
-    expectTypeOf(result).toEqualTypeOf<number[]>()
+    const result = compact([true, undefined, true])
+    expect(result).toEqual([true, true])
+    expectTypeOf(result).toEqualTypeOf<boolean[]>()
   })
 
   it('should filter out null values', () => {
     // eslint-disable-next-line unicorn/no-null
-    const result = compact([true, null])
-    expect(result).toEqual([true])
+    const result = compact([true, null, true])
+    expect(result).toEqual([true, true])
     expectTypeOf(result).toEqualTypeOf<boolean[]>()
   })
 
   it('should not filter out falsy values', () => {
-    const result = compact([true, false])
-    expect(result).toEqual([true, false])
-    expectTypeOf(result).toEqualTypeOf<boolean[]>()
+    const result = compact([false, 0, 0n, ''])
+    expect(result).toEqual([false, 0, 0n, ''])
+    expectTypeOf(result).toEqualTypeOf<Array<bigint | boolean | number | string>>()
+  })
+
+  it('should infer the type of the result if the input is a tuple', () => {
+    const result = compact([1, undefined] as [1, undefined])
+    expect(result).toEqual([1])
+    expectTypeOf(result).toEqualTypeOf<Array<1>>()
   })
 }
