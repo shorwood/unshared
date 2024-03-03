b75a0ee77e10a7c558dab131d723461b5401d0ac
Stanley Horwood <stanley@hsjm.io>
2024-03-03 15:16:10 +0100

    feat(types): add `IteratorPath` type for enhanced path resolution
    Introduced a new type `IteratorPath` to the types package. This type is designed to aid in the resolution of paths within collections like arrays, objects, tuples, sets, and maps. It enhances auto-completion features in IDEs without compromising the ability to match non-literal string types. The implementation includes comprehensive examples and tests to ensure correctness and usability in various collection types.

---
+++ packages/types/IteratorPath.ts
@@ -0,0 +1,65 @@
+import { Path } from './Path'
+import { Values } from './Values'
+
+/**
+ * A type that may match the path of the elements in a collection. This type is
+ * used to allow auto-completion in IDEs without losing the ability to match
+ * non-literal string types.
+ *
+ * @template T Type of the collection to get the path from.
+ * @example
+ * interface Person {
+ *   age: number
+ *   name: {
+ *     first: string
+ *     last: string
+ *  }
+ * }
+ *
+ * // Wrap the `Person` type in an array.
+ * type Persons = Person[]
+ *
+ * // Get the paths of all items in the collection.
+ * type Paths = IteratorPath<Persons> // 'age' | 'name' | 'name.first' | 'name.last'
+ */
+export type IteratorPath<T> = Path<Values<T>> | (string & {})
+
+/** c8 ignore next */
+if (import.meta.vitest) {
+  interface Person { age: number; name: { first: string; last: string } }
+
+  it('should get the paths of an array', () => {
+    type Result = IteratorPath<Person[]>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of an object', () => {
+    type Result = IteratorPath<{ a: Person; b: Person }>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of a tuple', () => {
+    type Result = IteratorPath<[Person, Person]>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of a readonly array', () => {
+    type Result = IteratorPath<readonly Person[]>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of a readonly tuple', () => {
+    type Result = IteratorPath<readonly [Person, Person]>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of a set', () => {
+    type Result = IteratorPath<Set<Person>>
+    expectTypeOf<Result>().toEqualTypeOf<'age' | 'name.first' | 'name.last' | 'name' | string & {}>()
+  })
+
+  it('should get the paths of a map', () => {
+    type Result = IteratorPath<Map<string, Person>>
+    expectTypeOf<Result>().toEqualTypeOf<`${number}.age` | `${number}.name.first` | `${number}.name.last` | `${number}.name` | `${number}` | string & {}>()
+  })
+}
