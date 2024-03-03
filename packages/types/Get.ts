869d7849eaa51c0df81a52494083fcbd9b96100f
Stanley Horwood <stanley@hsjm.io>
2024-03-03 03:00:32 +0100

    feat(types): enhance `Get` type utility for better iterable support
    Improved the `Get` type utility to handle not only object paths but also iterables like arrays, Sets, and Maps. This enhancement includes support for accessing values within these iterables using paths and indices, significantly broadening the utility's applicability and flexibility. The test cases have been updated to reflect these new capabilities, ensuring robust type inference across a variety of data structures.

--- packages/types/Get.ts
+++ packages/types/Get.ts
@@ -9,54 +9,54 @@
  * @example Get<{ foo: { bar: { baz: 'baz' } } }, 'foo.bar.baz'> // 'baz'
  */
 export type Get<T, P extends string> =
-    // --- Extract the key of the first segment of the path.
-    P extends `${infer K}.${infer N}`
+  // --- Extract the key of the left-most segment.
+  P extends `${infer K}.${infer N}`
 
-      // --- If the segment matches the key of the object, continue.
-      ? K extends keyof T ? Get<T[K], N> : never
+    // --- If the segment matches the key of the object, continue.
+    ? K extends keyof T ? Get<T[K], N>
+      : T extends Iterable<infer U> ? `${K}` extends `${number}` ? Get<U, N>
+        : never : never
 
-      // --- Otherwise, return the value at the current path.
-      : P extends keyof T ? T[P]
-
-        // ---  Maybe the object is an array, if so, return the item type.
-        : T extends Array<infer U> ? U : never
+    // --- Otherwise, return the value at the current path.
+    : P extends keyof T ? T[P]
+      : T extends Iterable<infer U> ? `${P}` extends `${number}` ? U : never
+        : never
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should extract the value of a nested object', () => {
-    interface Object { foo: { bar: { baz: 'baz' } } }
-    type Result = Get<Object, 'foo.bar.baz'>
-    expectTypeOf<Result>().toEqualTypeOf<'baz'>()
-  })
-
-  it('should extract the value of a nested object in an array', () => {
-    type Object = [{ foo: 'foo' }]
-    type Result = Get<Object, '0.foo'>
+  it('should infer the value in an object at the given key', () => {
+    type Result = Get<{ foo: 'foo' }, 'foo'>
     expectTypeOf<Result>().toEqualTypeOf<'foo'>()
   })
 
-  it('should extract the value of a nested tuple in an object', () => {
-    interface Object { foo: { bar: { baz: [boolean] } } }
-    type Result = Get<Object, 'foo.bar.baz.0'>
-    expectTypeOf<Result>().toEqualTypeOf<boolean>()
-  })
-
-  it('should extract the value of a nested array in an object', () => {
-    interface Object { foo: { bar: { baz: boolean[] } } }
-    type Result = Get<Object, 'foo.bar.baz.0'>
-    expectTypeOf<Result>().toEqualTypeOf<boolean>()
-  })
-
-  it('should extract the value of a nested readonly array', () => {
-    type Object = Readonly<{ foo: readonly ['bar'] }>
-    type Result = Get<Object, 'foo.0'>
+  it('should infer the value in an object at the given path', () => {
+    type Result = Get<{ foo: { bar: 'bar' } }, 'foo.bar'>
     expectTypeOf<Result>().toEqualTypeOf<'bar'>()
   })
 
-  it('should extract the value of a nested readonly object', () => {
-    type Object = Readonly<{ foo: Readonly<{ bar: 'baz' }> }>
-    type Result = Get<Object, 'foo.bar'>
-    expectTypeOf<Result>().toEqualTypeOf<'baz'>()
+  it('should infer the value in an array at the given index', () => {
+    type Result = Get<[1, 2], '0'>
+    expectTypeOf<Result>().toEqualTypeOf<1>()
+  })
+
+  it('should infer the value in an array at the given path', () => {
+    type Result = Get<[1, [2, 3]], '1.0'>
+    expectTypeOf<Result>().toEqualTypeOf<2>()
+  })
+
+  it('should infer the value in a Set at the given index', () => {
+    type Result = Get<Set<number>, '42'>
+    expectTypeOf<Result>().toEqualTypeOf<number>()
+  })
+
+  it('should infer the value in a Map at the given index', () => {
+    type Result = Get<Map<string, number>, '0'>
+    expectTypeOf<Result>().toEqualTypeOf<[string, number]>()
+  })
+
+  it('should infer the value in a Map at the given path', () => {
+    type Result = Get<Map<string, { foo: number }>, '0.1.foo'>
+    expectTypeOf<Result>().toEqualTypeOf<number>()
   })
 
   it('should return never if the path is invalid', () => {
