92f2743de9c89731664f26d3fe12d66173f5bb1a
Stanley Horwood <stanley@hsjm.io>
2024-03-03 16:13:09 +0100

    chore(collection): refine `sample` function and documentation
    Refined the `sample` function implementation and updated the documentation to make it clearer. The function now more accurately reflects its purpose of sampling a single item or multiple items from a given collection. Simplified the parameter names and enhanced examples in the documentation for better understanding.

--- packages/collection/sample.ts
+++ packages/collection/sample.ts
@@ -1,35 +1,52 @@
 import { Collection, NumberIntegerPositive, Tuple } from '@unshared/types'
 
 /**
- * Returns a random item from an array, or an array of random items if a size is specified.
+ * Returns a single item randomly sampled from a collection.
  *
- * @param object The array to select from
- * @param size The number of items to select
+ * @param collection The collection to sample from
  * @returns A random item from the array
+ * @example
+ * // Create a collection.
+ * const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ *
+ * // Sample a random item from the collection.
+ * sample(collection) // => 3
  */
-export function sample<T>(object: Collection<T>, size: 1): T
+export function sample<T>(collection: Collection<T>): T
 /**
- * Returns a random item from an array, or an array of random items if a size is specified.
+ * Returns a single item randomly sampled from a collection.
  *
- * @param object The array to select from
+ * @param collection The collection to sample from
+ * @param size The number of items to select, if the size is 1, the output will be a single item.
  * @returns A random item from the array
+ * // Create a collection.
+ * const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ *
+ * // Sample a random item from the collection.
+ * sample(collection, 1) // => 3
  */
-export function sample<T>(object: Collection<T>): T
+export function sample<T>(collection: Collection<T>, size: 1): T
 /**
- * Returns a random item from an array, or an array of random items if a size is specified.
+ * Returns a sample array of random items selected from a collection.
  *
- * @param object The array to select from
- * @param size The number of items to select
- * @returns A random item from the array
+ * @param collection The collection to sample from
+ * @param size The number of items to sample
+ * @returns An array of random items from the collection
+ * @example
+ * // Create a collection.
+ * const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ *
+ * // Sample 3 random items from the collection.
+ * sample(collection, 3) // => [3, 7, 1]
  */
-export function sample<T, N extends number = number>(object: Collection<T>, size: NumberIntegerPositive<N>): Tuple<N, T>
+export function sample<T, N extends number = number>(collection: Collection<T>, size: NumberIntegerPositive<N>): Tuple<N, T>
 export function sample(object: Collection, size = 1) {
   if (typeof size === 'number' && size < 1)
     throw new RangeError('The sample size must be a positive number.')
 
+  // --- Pick random items from the cloned collection.
   const copy = Object.values(object)
   const result = Array.from({ length: size })
-
   for (let i = 0; i < size; i++) {
     const seed = Math.random()
     const index = Math.floor(seed * copy.length)
