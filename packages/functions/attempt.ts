32177b36b7084cf27179aaa74f0a198bf7c0a371
Stanley Horwood <stanley@hsjm.io>
2024-03-07 19:36:19 +0100

    feat(functions): refactor `attempt` function for better typing and readability
    Refactored the `attempt` function to improve typing and readability. Changed the return type from an array to an object, making the function more descriptive and easier to understand. This change also makes it easier to handle the results of the `attempt` function, especially when working with TypeScript, as it provides better type inference and error handling capabilities.
    
    The refactoring includes:
    - Changing the `Result` type from a tuple to an interface.
    - Simplifying the implementation of the `attempt` function to use the new `Result` interface.
    - Updating documentation and examples to reflect the new return type.
    - Improving test cases to cover the changes and ensure type correctness.

--- packages/functions/attempt.ts
+++ packages/functions/attempt.ts
@@ -9,16 +9,16 @@ import { MaybePromise, NotPromise } from '@unshared/types'
  * @template E The type of the error.
  * @example Result<string, Error> // [string | undefined, Error | undefined]
  */
-export type Result<T = unknown, E extends Error = Error> = [
-  value: T | undefined,
-  error: E | undefined,
-]
+export interface Result<T = unknown, E extends Error = Error> {
+  value: T | undefined
+  error: E | undefined
+}
 
 /**
  * Run a function and return the value or error in an array. If the function
  * throws an error, the error will be returned in the second element of the
  * array. Otherwise, the value will be returned in the first element of the
- * array. This allows for similar error handling to Rust's `Result` type.
+ * array. This allows for error handling similar to Rust's `Result` type.
  *
  * If the function is asynchronous, the result will be a promise that resolves
  * to the result asynchronously. If the function is synchronous, the result will
@@ -27,77 +27,70 @@ export type Result<T = unknown, E extends Error = Error> = [
  * @param fn The function to run.
  * @returns A tuple with the value and the error if any.
  * @example
- * const [result, error] = attempt(() => true) // [true, undefined]
- * const [result, error] = attempt(() => { throw new Error('I am failing') }) // [undefined, Error('I am failing')]
+ * // Synchronous function.
+ * attempt(() => true)
+ * // { value: true, error: undefined }
+ *
+ * // Asynchronous function.
+ * attempt(async() => true)
+ * // Promise<{ value: true, error: undefined }>
+ *
+ * // Failing synchronous function.
+ * attempt(() => { throw new Error('I am failing') })
+ * // { value: undefined, error: Error('I am failing') }
+ *
+ * // Failing asynchronous function.
+ * attempt(async() => { throw new Error('I am failing asynchronously') })
+ * // Promise<{ value: undefined, error: Error('I am failing asynchronously') }>
  */
-export function attempt<R, E extends Error = Error>(fn: () => Promise<R>): Promise<Result<R, E>>
-export function attempt<R, E extends Error = Error>(fn: () => NotPromise<R>): Result<R, E>
-export function attempt<R, E extends Error = Error>(fn: () => MaybePromise<R>): MaybePromise<Result<R, E>>
-export function attempt(fn: Function): unknown {
+export function attempt<T, E extends Error = Error>(fn: () => NotPromise<T>): Result<T, E>
+export function attempt<T, E extends Error = Error>(fn: () => Promise<T>): Promise<Result<T, E>>
+export function attempt<T, E extends Error = Error>(fn: () => MaybePromise<T>): MaybePromise<Result<T, E>>
+export function attempt(fn: () => unknown) {
   try {
-    const result = fn()
+    const value = fn()
 
     // --- Return a promise if the function is asynchronous.
-    return result instanceof Promise
-      ? result
-        .then(value => [value, undefined])
-        .catch(error => [undefined, error])
+    return value instanceof Promise
+      ? value
+        .then((value: unknown) => ({ value }))
+        .catch((error: Error) => ({ error }))
 
       // --- Return the result immediately if the function is synchronous.
-      : [result, undefined]
+      : { value }
   }
 
   // --- Catch sync error.
   catch (error: unknown) {
-    return [undefined, error]
+    return { error }
   }
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  const throws = (): number => { throw new Error('I am failing') }
-  const throwsAsync = async() => { throw new Error('I am failing asyncrhonously') }
-  const returns = () => 'I am valid'
-  const resolves = async() => 'I am valid asynchronously'
-
-  it('should returns value and error for a failing sync function', async() => {
-    const [result, error] = attempt(throws)
-    expect(result).toEqual(undefined)
-    expect(error).toBeInstanceOf(Error)
-    expect(error!.message).toEqual('I am failing')
+  it('should returns value using a valid sync function', () => {
+    const result = attempt(() => 'Hello, world!')
+    expect(result).toStrictEqual({ value: 'Hello, world!' })
+    expectTypeOf(result).toEqualTypeOf<Result<string, Error>>()
   })
 
-  it('should returns value and error for a failing async function', async() => {
-    const [result, error] = await attempt(throwsAsync)
-    expect(result).toEqual(undefined)
-    expect(error).toBeInstanceOf(Error)
-    expect(error!.message).toEqual('I am failing asyncrhonously')
+  it('should returns error using a failing sync function', () => {
+    const result = attempt(() => { throw new Error('Uh oh! Something went wrong!') })
+    expect(result).toStrictEqual({ error: new Error('Uh oh! Something went wrong!') })
+    expectTypeOf(result).toEqualTypeOf<Result<never, Error>>()
   })
 
-  it('should returns value and undefined for a valid sync function', async() => {
-    const [result, error] = attempt(returns)
-    expect(result).toEqual('I am valid')
-    expect(error).toEqual(undefined)
+  it('should returns value using a valid async function', async() => {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    const result = await attempt(async() => 'Hello, world!')
+    expect(result).toStrictEqual({ value: 'Hello, world!' })
+    expectTypeOf(result).toEqualTypeOf<Result<string, Error>>()
   })
 
-  it('should returns value and undefined for a valid async function', async() => {
-    const [result, error] = await attempt(resolves)
-    expect(result).toEqual('I am valid asynchronously')
-    expect(error).toEqual(undefined)
-  })
-
-  it('should type the return value if a syncronous function is passed', () => {
-    const result = attempt(returns)
-    expectTypeOf(result).toEqualTypeOf<[string | undefined, Error | undefined]>()
-  })
-
-  it('should type the return value if an asyncronous function is passed', async() => {
-    const result = attempt(resolves)
-    expectTypeOf(result).toEqualTypeOf<Promise<[string | undefined, Error | undefined]>>()
-  })
-
-  it('should type the return value if a maybe asyncronous function is passed', async() => {
-    const result = attempt(returns as () => MaybePromise<string>)
-    expectTypeOf(result).toEqualTypeOf<Promise<[string | undefined, Error | undefined]> | [string | undefined, Error | undefined]>()
+  it('should returns error using a failing async function', async() => {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    const result = await attempt(async() => { throw new Error('Uh oh! Something went wrong!') })
+    expect(result).toStrictEqual({ error: new Error('Uh oh! Something went wrong!') })
+    expectTypeOf(result).toEqualTypeOf<Result<never, Error>>()
   })
 }
