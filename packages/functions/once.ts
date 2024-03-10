6ea1cf368709fbcafde1482c014a10b81ebdbc92
Stanley Horwood <stanley@hsjm.io>
2024-03-10 22:52:01 +0100

    chore(functions): refactor `once` function and update tests
    Refactored the `once` function to simplify its structure and improve readability. The main change involves merging two lines into one within the function body to directly return the result of the function call. This modification aims to make the code more concise.
    
    Additionally, updated the unit tests for the `once` function to reflect the changes in its implementation. Replaced `toHaveBeenCalledTimes(1)` with `toHaveBeenCalledOnce()` to make the test assertions clearer and more expressive. Introduced a new test case to verify that the `once` function correctly handles and ignores parameters after the first call, ensuring that the initial result is consistently returned on subsequent calls. This test confirms the improved functionality and reliability of the `once` function in various scenarios.

--- packages/functions/once.ts
+++ packages/functions/once.ts
@@ -10,16 +10,15 @@ import { Function } from '@unshared/types/Function'
  * @returns A function that will be executed at most one time.
  * @example const initializeOnce = once(initialize)
  */
-export const once = <T extends Function>(fn: T): T => {
+export function once<T extends Function>(fn: T): T {
   let called = false
   let result: unknown
 
   // --- Wrap the function in a call guard.
-  const wrapped = (...args: unknown[]) => {
+  const wrapped = (...args: unknown[]): unknown => {
     if (called) return result
     called = true
-    result = fn(...args)
-    return result
+    return result = fn(...args)
   }
 
   // --- Return the wrapped function.
@@ -34,7 +33,7 @@ if (import.meta.vitest) {
     wrapped()
     wrapped()
     wrapped()
-    expect(fn).toHaveBeenCalledTimes(1)
+    expect(fn).toHaveBeenCalledOnce()
   })
 
   it('should always return the same result', () => {
@@ -44,12 +43,19 @@ if (import.meta.vitest) {
     const resultThird = wrapped()
     expect(resultFirst).toEqual(resultSecond)
     expect(resultSecond).toEqual(resultThird)
+    expectTypeOf(wrapped).toEqualTypeOf<() => number>()
   })
 
-  it('should preserve the function context', () => {
-    const context = { foo: 'foo', getFOO() { return this.foo.toUpperCase() } }
-    const wrapped = once(context.getFOO)
-    const result = wrapped()
-    expect(result).toEqual('FOO')
+  it('should take parameters but ignore them after the first call', () => {
+    const fn = vi.fn((n: number) => n)
+    const wrapped = once(fn)
+    const resultFirst = wrapped(1)
+    const resultSecond = wrapped(2)
+    const resultThird = wrapped(3)
+    expect(resultFirst).toEqual(1)
+    expect(resultSecond).toEqual(1)
+    expect(resultThird).toEqual(1)
+    expect(fn).toHaveBeenCalledOnce()
+    expect(fn).toHaveBeenCalledWith(1)
   })
 }
