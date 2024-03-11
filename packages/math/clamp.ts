d7de129252bda3974e184429e42e5d90cc4f5aeb
Stanley Horwood <stanley@hsjm.io>
2024-03-11 04:40:28 +0100

    chore(math): improve documentation and error messaging in `clamp`
    Updated the documentation for the `clamp` function to make it more concise. Additionally, enhanced the error message when the minimum value is greater than the maximum value to be more descriptive and helpful. This change aims to improve clarity and developer experience when using the `clamp` function.

--- packages/math/clamp.ts
+++ packages/math/clamp.ts
@@ -1,9 +1,8 @@
 /**
  * [Clamps](https://en.wikipedia.org/wiki/Clamping_(graphics)) a number to a given range.
- *
- * Clamping a number means to limit it to a given range. If the number is smallerthan
- * the minimum value, the minimum value is returned. If the number is larger than the
- * maximum value, the maximum value is returned. Otherwise, the number is returned.
+ * If the number is smaller than  the minimum value, the minimum value is returned. If
+ * the number is larger than the maximum value, the maximum value is returned. Otherwise,
+ * the number is returned.
  *
  * @param n The number to clamp
  * @param min The minimum value
@@ -12,7 +11,7 @@
  * @example clamp(100, 0, 10) // 10
  */
 export function clamp(n: number, min: number, max: number): number {
-  if (min > max) throw new RangeError('Expected min to be less than or equal to max')
+  if (min > max) throw new RangeError('Expected the minimum value to be less than or equal to the maximum value')
   if (n <= min) return min
   if (n >= max) return max
   return n
@@ -43,5 +42,6 @@ if (import.meta.vitest) {
   it('should throw an error if min is greater than max', () => {
     const shouldThrow = () => clamp(0, 10, 0)
     expect(shouldThrow).toThrow(RangeError)
+    expect(shouldThrow).toThrow('Expected the minimum value to be less than or equal to the maximum value')
   })
 }
