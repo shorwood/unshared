651e581c12512f2034b422619b7bccbf7eeabaca
Stanley Horwood <stanley@hsjm.io>
2024-03-11 06:17:42 +0100

    feat(functions): enhance `memoize` function with advanced options and cache handling
    The `memoize` function within the `functions` package has been significantly enhanced to support advanced caching options and custom cache key generation. This update introduces the ability to use custom cache implementations, such as LRU caches, and provides a method for generating cache keys based on the function's parameters. This flexibility allows for more efficient memoization, especially in scenarios where the default cache mechanism and key generation may not be optimal.
    
    The changes include:
    - The cache is now a `Map` instance by default, allowing for more complex data structures to be memoized.
    - A new `MemoizeOptions` interface has been introduced, which includes options for custom cache instances (`cache`) and custom cache key generation functions (`getKey`).
    - The memoized function's cache can now be inspected or replaced, providing more control over the memoization process.
    - Several examples and tests have been added to demonstrate the new functionality and ensure its correctness.
    
    This enhancement makes the `memoize` function more versatile and powerful, catering to a wider range of use cases and improving performance in applications that rely heavily on memoization.

--- packages/functions/memoize.ts
+++ packages/functions/memoize.ts
@@ -1,29 +1,75 @@
+import { Function } from '@unshared/types'
+
 export type Memoized<T extends Function> = T & {
   /**
-   * The cache of the arguments and their results
+   * The cache of the arguments and their results that have been memoized.
+   * The cache is a `Map` instance that can be used to inspect the memoized
+   * results and reset the cache if needed. It is also possible to replace
+   * the default cache implementation with a more advanced one like `lru-cache`.
+   *
+   * @example memoized.cache.get('["foo"]') // => 'bar'
    */
-  cache: Record<string, any>
+  cache: Map<string, unknown>
+}
+
+export interface MemoizeOptions<T extends Function> {
+  /**
+   * The `Map` instance or constructor to use as the cache. It defaults to the
+   * native `Map` constructor but can be replaced with a more advanced cache
+   * like `lru-cache`.
+   *
+   * @default Map
+   */
+  cache?: Map<string, unknown> | (new () => Map<string, unknown>)
+  /**
+   * A function to generate the cache key given the parameters of the function.
+   * This will be used to index the results of the function and retrieve them
+   * from the cache when the function is called with the same parameters.
+   *
+   * @default JSON.stringify
+   */
+  getKey?: (...parameters: Parameters<T>) => string
 }
 
 /**
  * Wrap a function to cache it's results indexed by their parameters
  *
- * @param fn The function to be cached
- * @returns The memoized function
+ * @param fn The function to memoize.
+ * @param options The options to use.
+ * @returns The memoized function.
+ * @example
+ * // Create a slow function.
+ * const slow = (value: number) => `The value is ${Math.random()}`
+ *
+ * // Wrap the function in a memoize guard.
+ * const memoized = memoize(slow)
+ *
+ * // Call the memoized function.
+ * memoized(10) // => 'The value is 0.123456789'
+ * memoized(10) // => 'The value is 0.123456789'
+ * memoized(9) // => 'The value is 0.987654321'
  */
-export const memoize = <T extends Function>(fn: T): Memoized<T> => {
+export function memoize<T extends Function>(fn: T, options: MemoizeOptions<T> = {}): Memoized<T> {
   // --- Instantiate a cache
-  const cache = new Map<unknown[], unknown>()
+  const {
+    cache: getCache = new Map<string, unknown>(),
+    getKey = (...args) => JSON.stringify(args),
+  } = options
 
-  // --- Wrap the function
-  const memoized = (...parameters: unknown[]) => {
-    const cacheHit = cache.get(parameters)
+  // --- Create or use the cache provided in the options.
+  const cache = typeof getCache === 'function' ? new getCache() : getCache
+
+  // --- Wrap the function in a memoize guard.
+  const memoized = (...parameters: Parameters<T>) => {
+    const key = getKey(...parameters)
+
+    // --- Return the cached result if it exists.
+    const cacheHit = cache.get(key)
     if (cacheHit) return cacheHit
-    const result = fn(...parameters)
-    cache.set(parameters, result)
 
-    if (cache.size > 1000) cache.clear()
-
+    // --- Call the function and cache the result.
+    const result = fn(...parameters) as unknown
+    cache.set(key, result)
     return result
   }
 
@@ -34,49 +80,72 @@ export const memoize = <T extends Function>(fn: T): Memoized<T> => {
   return memoized as unknown as Memoized<T>
 }
 
-/* c8 ignore next */
+/* v8 ignore start */
 if (import.meta.vitest) {
   beforeAll(() => {
     vi.useFakeTimers()
   })
 
-  it('should pass the parameters to the memoized function', async() => {
-    const memoized = memoize((value: number) => value + 1)
+  it('should call the function with the given parameters', () => {
+    const fn = vi.fn()
+    const memoized = memoize(fn)
+    memoized(1)
+    expect(fn).toHaveBeenCalledWith(1)
+  })
+
+  it('should return the result of the function', () => {
+    const memoized = memoize((value: number) => value)
     const result = memoized(10)
-    expect(result).toEqual(11)
+    expect(result).toEqual(10)
   })
 
-  it('should return the cached result if the parameters are the same', async() => {
-    const memoized = memoize((value: number) => value + 1)
-    memoized(10)
-    memoized(10)
-    memoized(10)
-    const result = memoized(10)
-    expect(result).toEqual(11)
+  it('should return the cached result if the parameters are the same', () => {
+    const fn = vi.fn((n: number) => n)
+    const memoized = memoize(fn)
+    memoized(1)
+    memoized(1)
+    memoized(1)
+    expect(fn).toBeCalledWith(1)
+    expect(fn).toHaveBeenCalledTimes(1)
+    expect(memoized.cache.size).toEqual(1)
   })
 
-  it('should return a different result if the parameters are different', async() => {
-    const memoized = memoize((value: number) => value + 1)
-    memoized(10)
-    memoized(10)
-    memoized(10)
-    const result = memoized(11)
-    expect(result).toEqual(12)
+  it('should return a different result if the parameters are different', () => {
+    const fn = vi.fn((n: number) => n)
+    const memoized = memoize(fn)
+    memoized(1)
+    memoized(2)
+    memoized(3)
+    expect(fn).toHaveBeenCalledWith(1)
+    expect(fn).toHaveBeenCalledWith(2)
+    expect(fn).toHaveBeenCalledWith(3)
+    expect(fn).toHaveBeenCalledTimes(3)
+    expect(memoized.cache.size).toEqual(3)
   })
 
-  it('should expose the cache', async() => {
-    const memoized = memoize((value: number) => value + 1)
-    memoized(10)
-    memoized(10)
-    memoized(10)
-    expect(memoized.cache.get([10])).toEqual(11)
-    expect(memoized.cache.get([11])).toEqual(12)
-    expect(memoized.cache.get([12])).toEqual(undefined)
+  it('should register the parameters and the result in the cache', () => {
+    const memoized = memoize((value: number) => value)
+    memoized(1)
+    const result = [...memoized.cache.entries()]
+    expect(result).toEqual([['[1]', 1]])
   })
 
-  it('should infer the return type', async() => {
-    const memoized = memoize((value: number) => value + 1)
-    const result = memoized(10)
-    expect(result).toEqual(11)
+  it('should use the custom cache provided in the options', () => {
+    const cache = new Map<string, unknown>()
+    const memoized = memoize((value: number) => value, { cache })
+    expect(memoized.cache).toBe(cache)
+  })
+
+  it('should use the custom key generator provided in the options', () => {
+    const getKey = vi.fn((n: number) => Math.floor(n).toString())
+    const fn = vi.fn((n: number) => n)
+    const memoized = memoize(fn, { getKey })
+    memoized(1.25)
+    memoized(1.75)
+    expect(getKey).toHaveBeenCalledWith(1.25)
+    expect(getKey).toHaveBeenCalledWith(1.75)
+    expect(getKey).toHaveBeenCalledTimes(2)
+    expect(memoized.cache.size).toEqual(1)
+    expect(fn).toHaveBeenCalledTimes(1)
   })
 }
