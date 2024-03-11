cea6cf8f722aead3c24c72ec63f210e5b02b4160
Stanley Horwood <stanley@hsjm.io>
2024-03-11 01:55:15 +0100

    feat(functions): add `flow` function to transform values with transformers
    Implemented a new `flow` function in the `functions` package. This function allows users to transform a value through a series of synchronous or asynchronous transformer functions. The `flow` function is designed to take an initial value and apply each transformer in sequence, passing the result of one as the input to the next.
    
    If any transformer returns a `Promise`, the `flow` function ensures that the promise is resolved before proceeding to the next transformer, ultimately returning a `Promise` if asynchronous operations are involved. This makes the `flow` function versatile, supporting both synchronous and asynchronous transformations without requiring separate handling by the caller.
    
    The implementation supports an arbitrary number of transformers, leveraging TypeScript's type inference to provide accurate return types, including proper handling for promises. This addition enhances the library's utility by enabling complex transformations with minimal boilerplate, catering to both common and advanced use cases.
    
    The commit also includes comprehensive tests to verify the function's behavior with both synchronous and asynchronous transformers, ensuring correctness and reliability of the implementation.

---
+++ packages/functions/createFlow.ts
@@ -0,0 +1,72 @@
+import { Function } from '@unshared/types'
+
+/**
+ * The result of the `flow` function. If one of the `transformer` functions
+ * returns a `Promise`, the final value will be a `Promise` as well.
+ *
+ * @template T The list of transformers.
+ * @returns The result of the last transformer.
+ * @example FlowResult<[number, Promise<string>, boolean]> // => Promise<boolean>
+ */
+type FlowResult<T extends unknown[]> =
+  T extends [...Array<infer U>, infer R]
+    ? U extends Promise<any>
+      ? Promise<R>
+      : R
+    : never
+
+export function flow<V, R>(value: V, transformer: (value: V) => R): R
+export function flow<V, R1, R2>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2): FlowResult<[R1, R2]>
+export function flow<V, R1, R2, R3>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3): FlowResult<[R1, R2, R3]>
+export function flow<V, R1, R2, R3, R4>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3, transformer4: (value: Awaited<R3>) => R4): FlowResult<[R1, R2, R3, R4]>
+export function flow<V, R1, R2, R3, R4, R5>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3, transformer4: (value: Awaited<R3>) => R4, transformer5: (value: Awaited<R4>) => R5): FlowResult<[R1, R2, R3, R4, R5]>
+export function flow<V, R1, R2, R3, R4, R5, R6>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3, transformer4: (value: Awaited<R3>) => R4, transformer5: (value: Awaited<R4>) => R5, transformer6: (value: Awaited<R5>) => R6): FlowResult<[R1, R2, R3, R4, R5, R6]>
+export function flow<V, R1, R2, R3, R4, R5, R6, R7>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3, transformer4: (value: Awaited<R3>) => R4, transformer5: (value: Awaited<R4>) => R5, transformer6: (value: Awaited<R5>) => R6, transformer7: (value: Awaited<R6>) => R7): FlowResult<[R1, R2, R3, R4, R5, R6, R7]>
+export function flow<V, R1, R2, R3, R4, R5, R6, R7, R8>(value: V, transformer: (value: V) => R1, transformer2: (value: Awaited<R1>) => R2, transformer3: (value: Awaited<R2>) => R3, transformer4: (value: Awaited<R3>) => R4, transformer5: (value: Awaited<R4>) => R5, transformer6: (value: Awaited<R5>) => R6, transformer7: (value: Awaited<R6>) => R7, transformer8: (value: Awaited<R7>) => R8): FlowResult<[R1, R2, R3, R4, R5, R6, R7, R8]>
+/**
+ * Transform a value using a chain of transformers. The `flow` function takes a
+ * value and a list of transformers and applies consecutively each transformer to
+ * the value. The result of the previous transformer is passed as the first
+ * parameter of the next transformer.
+ *
+ * If one of the `transformer` functions returns a `Promise`, the final value
+ * will be a `Promise` as well.
+ *
+ * @param value The value to transform.
+ * @param transformers The transformers to apply.
+ * @returns The transformed value.
+ * @example flow(Math.PI, Math.round, String) // => "3"
+ */
+export function flow(value: unknown, ...transformers: Function[]) {
+  let result: unknown = value
+
+  // --- Apply each transformer to the value.
+  for (let i = 0; i < transformers.length; i++) {
+    const transformer = transformers[i]
+    result = transformer(result)
+
+    // --- If the result is a promise, wait for it to resolve.
+    if (result instanceof Promise) {
+      const nextTransformers = transformers.slice(i + 1)
+      // @ts-expect-error: Spread argument is of the correct type.
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return result.then(value => flow(value, ...nextTransformers))
+    }
+  }
+  return result
+}
+
+/* c8 ignore next */
+if (import.meta.vitest) {
+  it('should transform a value using the given transformers', () => {
+    const result = flow(1, n => n + 1, n => n * 2, n => n.toString())
+    expect(result).toEqual('4')
+    expectTypeOf(result).toEqualTypeOf<string>()
+  })
+
+  it('should be able to transform a value using async transformers', async() => {
+    const result = flow(1, n => Promise.resolve(n), n => n.toFixed(1) )
+    await expect(result).resolves.toEqual('1.0')
+    expectTypeOf(result).toEqualTypeOf<Promise<string>>()
+  })
+}
