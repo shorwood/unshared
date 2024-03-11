c750beeebce7a90fa689433db09a791d9beb0736
Stanley Horwood <stanley@hsjm.io>
2024-03-11 06:24:05 +0100

    chore(functions): cleanup `getStack` function and tests
    Removed unnecessary ESLint disable comment and simplified the example in the JSDoc. Also, cleaned up the test assertions for better clarity and focus on the primary functionality of the `getStack` function. This change makes the codebase cleaner and the tests more focused on validating the correct behavior of the `getStack` function.

--- packages/functions/getStack.ts
+++ packages/functions/getStack.ts
@@ -5,10 +5,9 @@
  * from the error.
  *
  * @returns The file paths of the stack frames.
- * @example resolveStack() // ['/home/user/project/foo.js', '/home/user/project/index.js']
+ * @example getStack() // ['/home/user/project/foo.js', '/home/user/project/index.js']
  */
 export function getStack() {
-  // eslint-disable-next-line unicorn/error-message
   return new Error().stack
     ?.match(/(?:file:\/\/)?(\/.*)\.\w+/g)
     ?.map(file => file.replace('file://', '')) ?? []
@@ -19,10 +18,6 @@ if (import.meta.vitest) {
   it('should return the stack of the current function', () => {
     const result = getStack()
     const expected = import.meta.url.slice(7)
-    const isArrayString = result.every(item => typeof item === 'string')
-    const isArrayAbsolute = result.every(item => item.startsWith('/'))
-    expect(isArrayString, 'Expected the stack to be an array of strings')
-    expect(isArrayAbsolute, 'Expected the stack to be an array of absolute paths')
     expect(result[0]).toEqual(expected)
   })
 }
