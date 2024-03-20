6499ddafc7117c1ba86ec8b270ff1e7e2046ca35
Stanley Horwood <stanley@hsjm.io>
2024-03-20 02:24:04 +0100

    feat(fs): refactor `findAncestors` to return an awaitable iterator
    Refactored the `findAncestors` function to return an awaitable iterator, allowing for more flexible and efficient traversal of ancestor files. This change enhances the usability of `findAncestors` by enabling asynchronous iteration over found ancestor files from a given starting directory.
    
    The updated implementation introduces an optional `from` parameter, defaulting to the current working directory, and utilizes async generator functions to yield ancestor paths as they are found. This approach streamlines the process of finding ancestor files, making it more responsive and adaptable to various use cases.
    
    Additionally, the refactor includes the use of the `awaitable` utility, ensuring that the function can be used both as a traditional promise, returning an array of all ancestors, or as an async iterator for on-demand processing of each ancestor found. This dual functionality broadens the potential applications of `findAncestors`, enhancing its utility in diverse development scenarios.
    
    Comprehensive test coverage has been added to validate the function's behavior in different contexts, ensuring reliability and consistent performance across use cases.

--- packages/fs/findAncestors.ts
+++ packages/fs/findAncestors.ts
@@ -1,5 +1,7 @@
 import { access, constants } from 'node:fs/promises'
 import { dirname, resolve } from 'node:path'
+import { cwd } from 'node:process'
+import { awaitable, Awaitable } from '@unshared/functions/awaitable'
 import { vol } from 'memfs'
 
 /**
@@ -8,49 +10,92 @@ import { vol } from 'memfs'
  * If the file is not found, an empty array will be returned.
  *
  * @param name The file name to find.
- * @param path The path to start from.
- * @returns The absolute paths of the files found.
- * @example findAncestors('.npmrc', cwd()) // ['/home/user/project/.npmrc', '/home/user/.npmrc']
+ * @param from The path to start from.
+ * @returns An awaitable iterator of the absolute paths of the files found.
+ * @example
+ * // Get all ancestors as an array.
+ * const ancestors = await findAncestors('file', '/home/user/project')
+ *
+ * // Or, iterate over the ancestors one by one.
+ * const ancestors = findAncestors('file', '/home/user/project')
+ * for await (const ancestor of ancestors) console.log(ancestor)
  */
-export async function findAncestors(name: string, path: string): Promise<string[]> {
-  const paths: string[] = []
-  while (path !== '/') {
-    const absolutePath = resolve(path, name)
-    try {
-      await access(absolutePath, constants.F_OK)
-      paths.push(absolutePath)
+export function findAncestors(name: string, from: string = cwd()): Awaitable<AsyncIterable<string>, string[]> {
+  async function *createIterator() {
+    while (from !== '') {
+      const absolutePath = resolve(from, name)
+      try {
+        await access(absolutePath, constants.F_OK)
+        yield absolutePath
+      }
+      catch {
+        /** Ignore error. */
+      }
+      if (from === '/') break
+      from = dirname(from)
     }
-    catch {}
-    path = dirname(path)
   }
-  return paths
+
+  // --- Instantiate the iterator and wrap it in an awaitable.
+  const iterator = createIterator()
+  return awaitable(iterator)
 }
 
 /** c8 ignore next */
 if (import.meta.vitest) {
-  it('should resolve ancestors', async() => {
-    const json = {
+  it('should resolve ancestors from current directory', async() => {
+    vi.mock('node:process', () => ({ cwd: () => '/home/user/project' }))
+    vol.fromJSON({
       '/home/user/project/file': '',
       '/home/user/file': '',
       '/home/file': '',
       '/file': '',
-    }
-    vol.fromJSON(json)
-    const result = await findAncestors('file', '/home/user/project')
-    const expected = Object.keys(json)
-    expect(result).toEqual(expected)
+    })
+    const result = await findAncestors('file')
+    expect(result).toEqual([
+      '/home/user/project/file',
+      '/home/user/file',
+      '/home/file',
+      '/file',
+    ])
   })
 
-  it('should resolve ancestors at root', async() => {
-    const json = { '/file': '' }
-    vol.fromJSON(json)
-    const result = await findAncestors('file', '/')
-    const expected = Object.keys(json)
-    expect(result).toEqual(expected)
+  it('should resolve ancestors at from given directory', async() => {
+    vol.fromJSON({
+      '/home/user/project/file': '',
+      '/home/user/file': '',
+      '/home/file': '',
+      '/file': '',
+    })
+    const result = await findAncestors('file', '/home/user/project')
+    expect(result).toEqual([
+      '/home/user/project/file',
+      '/home/user/file',
+      '/home/file',
+      '/file',
+    ])
   })
 
-  it('should return empty array if no ancestors', async() => {
-    const result = await findAncestors('file', '/home/user/project')
+  it('should be iterable', async() => {
+    vol.fromJSON({
+      '/home/user/project/file': '',
+      '/home/user/file': '',
+      '/home/file': '',
+      '/file': '',
+    })
+    const result = findAncestors('file', '/home/user/project')
+    const items = []
+    for await (const item of result) items.push(item)
+    expect(items).toEqual([
+      '/home/user/project/file',
+      '/home/user/file',
+      '/home/file',
+      '/file',
+    ])
+  })
+
+  it('should return empty array if no ancestors were found', async() => {
+    const result = await findAncestors('filename')
     expect(result).toEqual([])
   })
 }
