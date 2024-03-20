eeb44133dc24d65c7cd8c1158e6f156e43e53ef8
Stanley Horwood <stanley@hsjm.io>
2024-03-20 06:32:14 +0100

    feat(functions): enhance `tries` function with promise support and improved typing
    The `tries` function has been significantly enhanced to support promises and improve type inference. This update introduces a more comprehensive type signature for the `TriesResult` that accounts for async functions and aggregates results into a union type, including `undefined` to represent cases where all functions have thrown an error.
    
    The implementation now correctly handles promises, waiting for the promise to resolve before returning the result. If a promise is rejected, the `tries` function will continue to the next function in the list. This change ensures that the `tries` function can be used with both synchronous and asynchronous functions, providing more flexibility in handling different types of operations.
    
    Furthermore, the update includes improvements to the internal logic of the `tries` function, such as using a for loop for better control over iteration and handling promises more effectively.
    
    This enhancement also comes with additional test cases to cover scenarios with promises, ensuring the functionality works as expected in both synchronous and asynchronous use cases. The tests validate the behavior of the `tries` function when dealing with rejected promises, confirming that it correctly moves to the next function and that each function is called only once.

--- packages/functions/tries.ts
+++ packages/functions/tries.ts
@@ -1,4 +1,4 @@
-import { Function } from '@unshared/types'
+import { Fallback, Function } from '@unshared/types'
 
 /**
  * The result of trying multiple functions.
@@ -7,10 +7,24 @@ import { Function } from '@unshared/types'
  * @returns The first non-undefined result.
  * @example type Result = TriesResult<[() => string, () => number]> // string | number | undefined
  */
-export type TriesResult<F extends Function[]> =
-  F extends Array<Function<infer U>>
-    ? Exclude<U, void>
-    : never
+export type TriesResult<F extends Function[], IsPromise = false, Result extends unknown[] = []> =
+  F extends [() => infer U, ...infer FNext extends Function[]]
+
+    // --- Some functions might return `never` and may override the resulting type.
+    // --- To avoid this, we fallback to `undefined` since thrown errors are caught and ignored.
+    ? Fallback<U, void> extends (infer R)
+
+      // --- If an async function was found before, wrap the next return types in a promise.
+      ? R extends Promise<unknown>
+        ? TriesResult<FNext, true, [...Result, R]>
+        : TriesResult<FNext, IsPromise, [...Result, IsPromise extends true ? Promise<R>: R]>
+
+      // --- Stop the recursion if no more functions are left.
+      : TriesResult<[], IsPromise, Result>
+
+    // --- At the end of the recursion, return the result as a union.
+    // --- Also add `undefined` to represent the case where all functions have thrown.
+    : Exclude<Result[number], void> | (IsPromise extends true ? Promise<void> : void)
 
 /**
  * Try multiple functions and return the result of the first one that does not throw.
@@ -20,13 +34,23 @@ export type TriesResult<F extends Function[]> =
  * @returns A promise that resolves to the first non-undefined result.
  * @example tries(throws, Date.now) // 1658682347132
  */
-export function tries<F extends Array<Function<unknown>>>(...functions: F): TriesResult<F> | undefined
-export function tries<T>(...functions: Array<Function<unknown>>): T | undefined
-export function tries(...functions: Array<Function<unknown>>): unknown | undefined {
-  for (const fn of functions) {
+export function tries<F extends Function[]>(...functions: F): TriesResult<F>
+export function tries<T>(...functions: Function[]): T
+export function tries(...functions: Function[]) {
+  // eslint-disable-next-line @typescript-eslint/prefer-for-of
+  for (let i = 0; i < functions.length; i++) {
+    const fn = functions[i] as () => unknown
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return fn()
+      const result = fn()
+
+      // --- If the result is a promise, wait for it to resolve.
+      // --- If an error occurs, try the next function but return
+      // --- wrapped in a promise to keep the result type consistent.
+      if (result instanceof Promise)
+        return result.catch(() => tries(...functions.slice(1)))
+
+      // --- If the result is not a promise, we can return it immediately.
+      return result
     }
     catch {
       // --- Continue to the next function if the current one throws.
@@ -36,27 +60,68 @@ export function tries(...functions: Array<Function<unknown>>): unknown | undefin
 
 /* v8 ignore start */
 if (import.meta.vitest) {
-  const noop = () => {}
-  const throws = () => { throw new Error('Error') }
-  const returnsBoolean = () => true
-  const returnsString = () => 'string'
-  const resolvesNumber = () => Promise.resolve(1)
-
   it('should return the first result that does not throw', () => {
-    const result = tries(throws, returnsBoolean, returnsString, resolvesNumber)
-    expect(result).toEqual(true)
-    expectTypeOf(result).toEqualTypeOf<Promise<number> | boolean | string | undefined>()
+    const fn1 = vi.fn(() => { throw new Error('Error') })
+    const fn2 = vi.fn(() => 'Hello, World!')
+    const fn3 = vi.fn(() => 1)
+    const result = tries(fn1, fn2, fn3)
+    expect(result).toEqual('Hello, World!')
+    expect(fn1).toHaveBeenCalledOnce()
+    expect(fn2).toHaveBeenCalledOnce()
+    expect(fn3).not.toHaveBeenCalled()
+    expectTypeOf(result).toEqualTypeOf<number | string | void>()
   })
 
-  it('should return a value of the type given in the generic', () => {
-    const result = tries<'foo'>(returnsBoolean, returnsString)
-    expect(result).toEqual(true)
-    expectTypeOf(result).toEqualTypeOf<'foo' | undefined>()
+  it('should return a promise if the result is a promise after the first function throws', async() => {
+    const fn1 = vi.fn(() => { throw new Error('Error') })
+    const fn2 = vi.fn(() => Promise.resolve(1))
+    const result = tries(fn1, fn2)
+    await expect(result).resolves.toEqual(1)
+    expect(fn1).toHaveBeenCalledOnce()
+    expect(fn2).toHaveBeenCalledOnce()
+    expectTypeOf(result).toEqualTypeOf<Promise<number> | Promise<void>>()
+  })
+
+  it('should return a promise if a previous function rejects', async() => {
+    const fn1 = vi.fn(() => Promise.reject(new Error('Error')))
+    const fn2 = vi.fn(() => 1)
+    const result = tries(fn1, fn2)
+    await expect(result).resolves.toEqual(1)
+    expect(fn1).toHaveBeenCalledOnce()
+    expect(fn2).toHaveBeenCalledOnce()
+    expectTypeOf(result).toEqualTypeOf<Promise<never> | Promise<number> | Promise<void>>()
   })
 
   it('should return undefined if all functions throw or return undefined', () => {
-    const result = tries(throws, noop)
-    expect(result).toEqual(undefined)
-    expectTypeOf(result).toEqualTypeOf<undefined>()
+    const fn1 = vi.fn(() => { throw new Error('Error') })
+    const fn2 = vi.fn(() => {})
+    const result = tries(fn1, fn2)
+    expect(result).toBeUndefined()
+    expect(fn1).toHaveBeenCalledOnce()
+    expect(fn2).toHaveBeenCalledOnce()
+    expectTypeOf(result).toEqualTypeOf<void>()
+  })
+
+  it('should only call each function once when switching to a promise', async() => {
+    const fn1 = vi.fn(() => { throw new Error('Error') })
+    const fn2 = vi.fn(() => { throw new Error('Error') })
+    const fn3 = vi.fn(() => Promise.resolve(1))
+    const result = tries(fn1, fn2, fn3)
+    await expect(result).resolves.toEqual(1)
+    expect(fn1).toHaveBeenCalledOnce()
+    expect(fn2).toHaveBeenCalledOnce()
+    expect(fn3).toHaveBeenCalledOnce()
+  })
+
+  it('should return undefined if no functions are provided', () => {
+    const result = tries()
+    expect(result).toBeUndefined()
+    expectTypeOf(result).toEqualTypeOf<void>()
+  })
+
+  it('should override the return type using the generic', () => {
+    const result = tries<'foo'>(() => 'not-foo')
+    expect(result).toEqual('not-foo')
+    expectTypeOf(result).toEqualTypeOf<'foo'>()
   })
 }
