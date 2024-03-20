3bd10d5169ed68d939cad16ee25836bc8bc31294
Stanley Horwood <stanley@hsjm.io>
2024-03-21 00:50:34 +0100

    fix(collection): refine parameter types in `overwrite` function
    Adjusted the parameter types in the `overwrite` function to use the `object` type instead of the more restrictive `ObjectLike` interface. This change enhances the function's flexibility by allowing it to accept a broader range of object types, thereby making the `overwrite` function more versatile and user-friendly. Removed unnecessary import statements to clean up the code.

--- packages/collection/overwrite.ts
+++ packages/collection/overwrite.ts
@@ -1,5 +1,3 @@
-import { ObjectLike } from '@unshared/types'
-
 /**
  * Overwrites the values of `object` with the values of `source`. This method
  * is like `Object.assign` except that it deletes properties that are not in
@@ -15,8 +13,8 @@ import { ObjectLike } from '@unshared/types'
  * // Overwrite the object with the source object.
  * overwrite(object, { baz: 1 }) // { baz: 1 }
  */
-export function overwrite<T extends ObjectLike>(object: ObjectLike, source: T): T
-export function overwrite(object: ObjectLike, source: ObjectLike): ObjectLike {
+export function overwrite<T extends object>(object: object, source: T): T
+export function overwrite(object: object, source: object): object {
 
   // --- Assert that the object and source are of both arrays or objects.
   const sourceIsArray = Array.isArray(source)
