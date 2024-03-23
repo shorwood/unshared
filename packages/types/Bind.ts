0c223e443d68d89c2e6a4637c44490cacd1b7969
Stanley Horwood <stanley@hsjm.io>
2024-03-23 20:54:44 +0100

    feat!(types): introduce `Bind` type and remove `BoundThis`
    Introduced a new type `Bind` in the `types` package to replace the `BoundThis` type. The `Bind` type offers enhanced functionality for binding the `this` context and additional parameters to functions. It supports binding just the `this` context, binding `this` with other parameters, and binding parameters without specifying `this`.
    
    The `BoundThis` type has been removed as it was limited to binding only the `this` context without the flexibility of additional parameters. The removal of `BoundThis` and introduction of `Bind` is a breaking change as it may affect consumers relying on the `BoundThis` type.
    
    The implementation includes comprehensive type tests to ensure the `Bind` type works as expected across different use cases, including binding with and without the `this` context and with varying numbers of parameters.

---
+++ packages/types/Bind.ts
@@ -0,0 +1,57 @@
+import { Function } from './Function'
+import { TupleLength } from './TupleLength'
+import { TupleSlice } from './TupleSlice'
+
+/**
+ * A function where the first parameter is already bound to `this`.
+ *
+ * @template T The function to bind.
+ * @example BoundThis<(x: number, y: string) => boolean> // (this: number, y: string) => boolean
+ */
+export type Bind<T extends Function, P extends unknown[] = []> =
+  P extends [] ? T
+
+    // --- If only the this parameter is provided, bind it to the function.
+    : P extends [this: infer This]
+      ? (this: This, ...parameters: Parameters<T>) => ReturnType<T>
+
+      // --- If the this parameter is provided as well as other parameters, bind the this parameter and append the other parameters.
+      : P extends [this: infer This, ...infer R]
+        ? (this: This, ...parameters: TupleSlice<Parameters<T>, TupleLength<R>>) => ReturnType<T>
+
+        // --- Otherwise, just bind the parameters.
+        : (...parameters: TupleSlice<Parameters<T>, TupleLength<P>>) => ReturnType<T>
+
+/** v8 ignore start */
+if (import.meta.vitest) {
+  it('should return the type as is if no parameters are provided', () => {
+    type Result = Bind<(x: number, y: number) => boolean>
+    type Expected = (x: number, y: number) => boolean
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
+  })
+
+  it('should return a bound function as is if no other parameters are provided', () => {
+    type Result = Bind<(this: string, x: number, y: number) => boolean>
+    type Expected = (this: string, x: number, y: number) => boolean
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
+  })
+
+  it('should bind the this parameter if using named tuple parameters', () => {
+    type Result = Bind<(x: number, y: number) => boolean, [this: string]>
+    type Expected = (this: string, x: number, y: number) => boolean
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
+  })
+
+  it('should bind the this parameter and append the other parameters', () => {
+    type Result = Bind<(x: number, y: number) => boolean, [this: string, x: string]>
+    type Expected = (this: string, y: number) => boolean
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
+  })
+
+  it('should only bind the parameters if no this parameter is provided', () => {
+    type Result = Bind<(x: number, y: number) => boolean, [undefined, x: string]>
+    type Expected = (y: number) => boolean
+    expectTypeOf<Result>().toEqualTypeOf<Expected>()
+  })
+}
+
