2708f28fc048a85596c00bbc3a0d9e6e71d34735
Stanley Horwood <stanley@hsjm.io>
2024-03-28 03:50:49 +0100

    chore(types): improve `OmitNever` type visibility in IDE
    Adjusted the definition of the `OmitNever` type to enhance its visibility and utility within IDEs. This change makes it easier for developers to understand and utilize the `OmitNever` type by ensuring that the type's structure and purpose are more clearly presented in the coding environment. This improvement aids in the readability and maintainability of code using the `OmitNever` type.

--- packages/types/OmitNever.ts
+++ packages/types/OmitNever.ts
@@ -4,7 +4,10 @@
  * @template T The object to omit properties from.
  * @example type Result = OmitNever<{ a: string; b: never; c: number }> // { a: string; c: number }
  */
-export type OmitNever<T> = { [K in keyof T as T[K] extends never ? never : K]: T[K] }
+export type OmitNever<T> =
+  { [K in keyof T as T[K] extends never ? never : K]: T[K] } extends infer U
+    ? { [K in keyof U]: U[K] }
+    : never
 
 /** c8 ignore next */
 if (import.meta.vitest) {
