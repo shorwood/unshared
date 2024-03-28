c18b50284c0e12d95e8ccf582a2d2eeea2fb18cf
Stanley Horwood <stanley@hsjm.io>
2024-03-28 05:47:53 +0100

    feat(functions): enhance typing of `isConstructor` function
    Improved the typing of the `isConstructor` function by adding a generic type parameter. This allows for more specific type inference when the function is used, enabling developers to assert that the passed value is not only a constructor but also of a specific constructor type. The update includes additional test cases to verify the correct behavior and type inference, ensuring that the function can now predicate more accurately over the constructor functions.
    
    This enhancement supports better type safety and developer experience when working with constructor checks in TypeScript.

--- packages/functions/isConstructor.ts
+++ packages/functions/isConstructor.ts
@@ -7,7 +7,7 @@ import { Constructor } from '@unshared/types'
  * @returns `true` if the value is a constructor function.
  * @example isConstructor(Boolean) // true
  */
-export function isConstructor(value: unknown): value is Constructor {
+export function isConstructor<T extends Constructor>(value: unknown): value is T {
   return typeof value === 'function'
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     && value.prototype?.constructor === value
@@ -28,5 +28,17 @@ if (import.meta.vitest) {
   it('should return false for a non-function value', () => {
     const result = isConstructor(10)
     expect(result).toEqual(false)
+  })
+
+  it('should predicate a constructor function', () => {
+    const value: unknown = Boolean
+    const result = isConstructor(value)
+    if (result) expectTypeOf(value).toEqualTypeOf<Constructor>()
+  })
+
+  it('should predicate a constructor function with a given type', () => {
+    const value: unknown = Boolean
+    const result = isConstructor<BooleanConstructor>(value)
+    if (result) expectTypeOf(value).toEqualTypeOf<BooleanConstructor>()
   })
 }
