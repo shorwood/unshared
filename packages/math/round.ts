13a2990c07126b8a9fe3385b939824a6724c22eb
Stanley Horwood <stanley@hsjm.io>
2024-04-02 18:44:15 +0200

    feat(math): enhance `round` function with stricter typing and error validation
    The `round` function within the math package has been significantly improved to enforce stricter typing and enhance error handling. The function now leverages the `NumberIntegerPositive` type from `@unshared/types` for the `precision` parameter, ensuring that only positive integers or zero are accepted. This change makes the function more robust and reliable, preventing unexpected behavior when incorrect values are passed.
    
    Error handling has been introduced to check for and reject negative values for precision and to ensure that the precision value is an integer. This adds an additional layer of safety, making the function more user-friendly and easier to debug in scenarios where incorrect values are passed.
    
    Unit tests have been expanded to cover these new error conditions, including tests for negative and non-integer precision values. This ensures the reliability and correctness of the `round` function across a broader range of inputs.

--- packages/math/round.ts
+++ packages/math/round.ts
@@ -1,26 +1,52 @@
+import { NumberIntegerPositive } from '@unshared/types'
+
 /**
- * [Round](https://en.wikipedia.org/wiki/Rounding) a number to a specified number of decimal places.
+ * [Round](https://en.wikipedia.org/wiki/Floor_and_ceiling_functions) a number to a given precision.
  *
  * @param number The number to round.
  * @param precision The precision to round to.
  * @returns The rounded number.
- * @example round(1.2345, 2) // 1.23
+ * @example round(1.234, 2) // 1.23
  */
-export function round(number: number, precision = 0): number {
+export function round<N extends number>(number: number, precision: NumberIntegerPositive<N> | 0 = 0): number {
+
+  // --- Handle the edge cases.
+  if (Number.isSafeInteger(number)) return number
+  if (precision < 0) throw new RangeError('Expected a positive precision')
+  if (Number.isSafeInteger(precision) === false) throw new RangeError('Expected an integer precision')
   if (precision <= 0) return Math.round(number)
+
+  // --- Otherwise, round the number to the given precision.
   const factor = 10 ** precision
   return Math.round(number * factor) / factor
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it('should round a number', () => {
-    const result = round(1.2345)
+  it('should round an integer', () => {
+    const result = round(1)
     expect(result).toEqual(1)
   })
 
-  it('should round a number to a specified number of decimal places', () => {
-    const result = round(1.2345, 2)
+  it('should round a number', () => {
+    const result = round(1.234)
+    expect(result).toEqual(1)
+  })
+
+  it('should round a number to a given precision', () => {
+    const result = round(1.234, 2)
     expect(result).toEqual(1.23)
+  })
+
+  it('should throw an error if the precision is negative', () => {
+    // @ts-expect-error: Precision is negative
+    const shouldThrow = () => round(1.234, -1)
+    expect(shouldThrow).toThrow(RangeError)
+  })
+
+  it('should throw an error if the precision is not an integer', () => {
+    // @ts-expect-error: Precision is not an integer
+    const shouldThrow = () => round(1.234, 1.5)
+    expect(shouldThrow).toThrow(RangeError)
   })
 }
