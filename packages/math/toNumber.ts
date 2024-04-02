e609cb2d3f4f4ea6387ff5a50893132a0bcbcd9c
Stanley Horwood <stanley@hsjm.io>
2024-04-02 18:24:06 +0200

    chore(math): improve error handling in `toNumber` function
    Updated the `toNumber` function to enhance its robustness and error handling. Now, if a string that cannot be parsed as a number is passed, it explicitly throws an error, preventing silent failures or incorrect assumptions about the input value's nature. Additionally, the error message for non-number-like values now includes the input value's type, making debugging easier by providing more context about the failure. The conversion of the function from an arrow to a traditional function format does not affect its functionality but aligns with the codebase's style consistency.

--- packages/math/toNumber.ts
+++ packages/math/toNumber.ts
@@ -1,7 +1,7 @@
 /**
  * A value that can be converted to a `number` type.
  */
-export type NumberLike = number | bigint | boolean | string
+export type NumberLike = bigint | boolean | number | string
 
 /**
  * Cast a number-like value to a number.
@@ -16,9 +16,14 @@ export type NumberLike = number | bigint | boolean | string
  * toNumber('foo') // 0
  * toNumber({}) // 0
  */
-export const toNumber = (value: NumberLike): number => {
+export function toNumber(value: NumberLike): number {
   // --- If the value is a string, try to parse it as a number.
-  if (typeof value === 'string') value = Number.parseFloat(value)
+  if (typeof value === 'string') {
+    const castValue = Number.parseFloat(value)
+    if (Number.isFinite(castValue) === false)
+      throw new Error(`Cannot cast non-number string "${value}" to a number.`)
+    return castValue
+  }
 
   // --- If the value is a number, return it.
   if (typeof value === 'number') {
@@ -34,7 +39,8 @@ export const toNumber = (value: NumberLike): number => {
   if (typeof value === 'bigint') return Number(value)
 
   // --- If the value is not a number-like value, throw an error.
-  throw new Error(`Cannot cast ${value} to a number.`)
+  const valueToString = Object.prototype.toString.call(value)
+  throw new Error(`Cannot cast ${valueToString} to a number.`)
 }
 
 /* c8 ignore next */
@@ -59,13 +65,14 @@ if (import.meta.vitest) {
     expect(result).toEqual(42)
   })
 
-  it('should cast a string to a number', () => {
-    const result = toNumber('foo')
-    expect(result).toEqual(0)
+  it('should throw an error if the string is not a number', () => {
+    const shouldThrow = () => toNumber('foo')
+    expect(shouldThrow).toThrow('Cannot cast non-number string "foo" to a number.')
   })
 
-  it('should fallback to 0', () => {
-    const result = toNumber({})
-    expect(result).toEqual(0)
+  it('should throw an error if the value is not a number-like value', () => {
+    // @ts-expect-error: Test non-number-like value
+    const shouldThrow = () => toNumber({})
+    expect(shouldThrow).toThrow('Cannot cast [object Object] to a number.')
   })
 }
