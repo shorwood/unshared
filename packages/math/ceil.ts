36dc47df2b2dc006f489bcded934257962ee2abd
Stanley Horwood <stanley@hsjm.io>
2024-04-02 18:31:38 +0200

    feat!(math): enhance `ceil` and `floor` functions with precision control
    Introduced significant improvements to both the `ceil` and `floor` functions within the math package. These functions have been enhanced to accept a `precision` parameter, allowing numbers to be rounded to a specified number of decimal places. This change leverages the `NumberIntegerPositive` type from `@unshared/types` to ensure that the precision argument is a positive integer, enhancing the functions' robustness and reliability.
    
    Additionally, comprehensive error handling has been implemented. Both functions will now throw a `RangeError` if provided with a negative precision or a precision that is not an integer, preventing unexpected behavior and facilitating easier debugging. These enhancements ensure that the functions are more versatile and user-friendly, catering to a broader range of use cases.
    
    The unit tests have been expanded to cover these new functionalities and error conditions. Tests now confirm that integers are returned unchanged, a `RangeError` is thrown for invalid precision values, and that numbers are correctly rounded to the desired precision, ensuring the reliability and accuracy of these functions.

--- packages/math/ceil.ts
+++ packages/math/ceil.ts
@@ -1,19 +1,33 @@
+import { NumberIntegerPositive } from '@unshared/types'
+
 /**
  * [Ceil](https://en.wikipedia.org/wiki/Floor_and_ceiling_functions) a number to a given precision.
  *
  * @param number The number to ceil.
- * @param precision The precision to ceil to. (Default: 0)
+ * @param precision The precision to ceil to.
  * @returns The ceiled number.
- * @example ceil(1.234, 2) // 1.24
+ * @example ceil(1.234, 2) // 1.23
  */
-export function ceil(number: number, precision = 0): number {
+export function ceil<N extends number>(number: number, precision: NumberIntegerPositive<N> | 0 = 0): number {
+
+  // --- Handle the edge cases.
+  if (Number.isSafeInteger(number)) return number
+  if (precision < 0) throw new RangeError('Expected a positive precision')
+  if (Number.isSafeInteger(precision) === false) throw new RangeError('Expected an integer precision')
   if (precision <= 0) return Math.ceil(number)
-  const factor = Math.pow(10, precision)
+
+  // --- Otherwise, ceil the number to the given precision.
+  const factor = 10 ** precision
   return Math.ceil(number * factor) / factor
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
+  it('should ceil an integer', () => {
+    const result = ceil(1)
+    expect(result).toEqual(1)
+  })
+
   it('should ceil a number', () => {
     const result = ceil(1.234)
     expect(result).toEqual(2)
@@ -22,5 +36,17 @@ if (import.meta.vitest) {
   it('should ceil a number to a given precision', () => {
     const result = ceil(1.234, 2)
     expect(result).toEqual(1.24)
+  })
+
+  it('should throw an error if the precision is negative', () => {
+    // @ts-expect-error: Precision is negative
+    const shouldThrow = () => ceil(1.234, -1)
+    expect(shouldThrow).toThrow(RangeError)
+  })
+
+  it('should throw an error if the precision is not an integer', () => {
+    // @ts-expect-error: Precision is not an integer
+    const shouldThrow = () => ceil(1.234, 1.5)
+    expect(shouldThrow).toThrow(RangeError)
   })
 }
--- packages/math/floor.ts
+++ packages/math/floor.ts
@@ -1,3 +1,5 @@
+import { NumberIntegerPositive } from '@unshared/types'
+
 /**
  * [Floor](https://en.wikipedia.org/wiki/Floor_and_ceiling_functions) a number to a given precision.
  *
@@ -6,14 +8,26 @@
  * @returns The floored number.
  * @example floor(1.234, 2) // 1.23
  */
-export function floor(number: number, precision = 0): number {
+export function floor<N extends number>(number: number, precision: NumberIntegerPositive<N> | 0 = 0): number {
+
+  // --- Handle the edge cases.
+  if (Number.isSafeInteger(number)) return number
+  if (precision < 0) throw new RangeError('Expected a positive precision')
+  if (Number.isSafeInteger(precision) === false) throw new RangeError('Expected an integer precision')
   if (precision <= 0) return Math.floor(number)
+
+  // --- Otherwise, floor the number to the given precision.
   const factor = 10 ** precision
   return Math.floor(number * factor) / factor
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
+  it('should floor an integer', () => {
+    const result = floor(1)
+    expect(result).toEqual(1)
+  })
+
   it('should floor a number', () => {
     const result = floor(1.234)
     expect(result).toEqual(1)
@@ -22,5 +36,17 @@ if (import.meta.vitest) {
   it('should floor a number to a given precision', () => {
     const result = floor(1.234, 2)
     expect(result).toEqual(1.23)
+  })
+
+  it('should throw an error if the precision is negative', () => {
+    // @ts-expect-error: Precision is negative
+    const shouldThrow = () => floor(1.234, -1)
+    expect(shouldThrow).toThrow(RangeError)
+  })
+
+  it('should throw an error if the precision is not an integer', () => {
+    // @ts-expect-error: Precision is not an integer
+    const shouldThrow = () => floor(1.234, 1.5)
+    expect(shouldThrow).toThrow(RangeError)
   })
 }
