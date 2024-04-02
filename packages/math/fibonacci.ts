e3139d7a24c3f92d9c5bf9121cf247f3283c6c99
Stanley Horwood <stanley@hsjm.io>
2024-04-02 18:33:18 +0200

    feat(math): enhance `fibonacci` function with stricter typing and error handling
    Refined the `fibonacci` function to only accept non-negative integers and zero as valid inputs, leveraging the `NumberIntegerPositive` type from `@unshared/types`. This update introduces checks for safe integers, ensuring that the function can handle large numbers accurately and efficiently.
    
    Error handling has been significantly improved. The function now throws a `TypeError` for non-integer inputs and a `RangeError` for negative inputs, making it more robust and user-friendly. These changes prevent unexpected behavior and facilitate easier debugging when incorrect values are passed.
    
    Unit tests have been expanded to cover these new error conditions and edge cases, including tests for zero and one, ensuring the function's reliability across a broader range of inputs.

--- packages/math/fibonacci.ts
+++ packages/math/fibonacci.ts
@@ -1,3 +1,5 @@
+import { NumberIntegerPositive } from '@unshared/types'
+
 /**
  * Compute [Fibonacci number](https://en.wikipedia.org/wiki/Fibonacci_number) at index `n`.
  *
@@ -9,35 +11,60 @@
  * @returns The Fibonacci number at position N
  * @example fibonacci(10) // 55
  */
-export function fibonacci(n: number): number {
-  if (n < 0) throw new RangeError('Cannot compute Fibonacci number at negative index')
+export function fibonacci<N extends number>(n: NumberIntegerPositive<N> | 0): number {
+  // --- Handle edge cases.
+  if (Number.isSafeInteger(n) === false) throw new TypeError('Cannot calculate Fibonacci number at non-integer index')
+  if (n < 0) throw new RangeError('Cannot calculate Fibonacci number at negative index')
 
-  // --- Define two big integers
+  // --- Return early for first two numbers.
+  if (n === 0) return 0
+  if (n === 1) return 1
+
+  // --- Compute Fibonacci number using bigint.
   let a = 0n
   let b = 1n
   let c = 0n
-
-  // --- Loop N times
   for (let index = 2; index <= n; index++) {
     c = a + b
     a = b
     b = c
   }
 
-  // --- Return result as number
+  // --- Cast bigint to number and return.
   return Number(c)
 }
 
 /* c8 ignore next */
 if (import.meta.vitest) {
+  it('should compute Fibonacci number at 0', () => {
+    const result = fibonacci(0)
+    expect(result).toEqual(0)
+  })
+
+  it('should compute Fibonacci number at 1', () => {
+    const result = fibonacci(1)
+    expect(result).toEqual(1)
+  })
+
   it('should compute Fibonacci number at N', () => {
     const result = fibonacci(50)
     expect(result).toEqual(12586269025)
   })
 
-  it('should fail when N is negative', () => {
-    // eslint-disable-next-line unicorn/consistent-function-scoping
+  it('should throw when the argument is negative', () => {
+    // @ts-expect-error: Test negative number
     const shouldThrow = () => fibonacci(-1)
-    expect(shouldThrow).toThrowError(RangeError)
+    expect(shouldThrow).toThrow('Cannot calculate Fibonacci number at negative index')
+  })
+
+  it('should throw when the argument is not an integer', () => {
+    // @ts-expect-error: Test non-integer number
+    const shouldThrow = () => fibonacci(1.5)
+    expect(shouldThrow).toThrow('Cannot calculate Fibonacci number at non-integer index')
+  })
+
+  it('should throw when the argument is not a safe integer', () => {
+    const shouldThrow = () => fibonacci(Number.MAX_SAFE_INTEGER + 1)
+    expect(shouldThrow).toThrow('Cannot calculate Fibonacci number at non-integer index')
   })
 }
