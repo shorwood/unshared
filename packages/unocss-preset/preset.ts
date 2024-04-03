0a2d7d885395d74603bde5f9f2fd8eed4f25d1ab
Stanley Horwood <stanley@hsjm.io>
2024-04-03 03:55:52 +0200

    chore(unocss-preset): changed typing setup for preset configuration
    Refactored the preset configuration in `unocss-preset` package to use `PresetFactory` instead of the direct `Preset` function. This change aligns with the updated typing requirements and enhances the modularity of the preset configuration. The update simplifies the preset's structure by transitioning from a function declaration to an arrow function expression, which is more concise and aligns with modern JavaScript practices. Additionally, the refactor introduces a more explicit declaration of the `PresetHsjmOptions` type, ensuring better type safety and clarity in options handling.

--- packages/unocss-preset/preset.ts
+++ packages/unocss-preset/preset.ts
@@ -1,4 +1,4 @@
-import { Preset } from '@unocss/core'
+import { PresetFactory } from '@unocss/core'
 import { colorsNord, colorsBrand } from './constants'
 import * as RULES from './rules'
 import * as SHORTCUTS from './shortcuts'
@@ -6,22 +6,20 @@ import * as VARIANTS from './variants'
 
 export interface PresetHsjmOptions {}
 
-export function presetHsjm(options = {} as PresetHsjmOptions): Preset {
-  return {
-    name: '@hsjm/unocss-preset',
-    rules: Object.values(RULES),
-    variants: Object.values(VARIANTS),
-    shortcuts: Object.values(SHORTCUTS),
-    options,
-    theme: {
-      easing: {
-        bounce: 'cubic-bezier(0.29, 2.81, 0.29, -0.03)',
-        landing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-      },
-      colors: {
-        ...colorsNord,
-        ...colorsBrand,
-      },
+export const presetHsjm: PresetFactory<object, PresetHsjmOptions> = (options = {}) => ({
+  name: '@hsjm/unocss-preset',
+  rules: Object.values(RULES),
+  variants: Object.values(VARIANTS),
+  shortcuts: Object.values(SHORTCUTS),
+  options,
+  theme: {
+    easing: {
+      bounce: 'cubic-bezier(0.29, 2.81, 0.29, -0.03)',
+      landing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
     },
-  }
-}
+    colors: {
+      ...colorsNord,
+      ...colorsBrand,
+    },
+  },
+})
