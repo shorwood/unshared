eba0951aa617b1d3132ceafa26044667546979d0
Stanley Horwood <stanley@hsjm.io>
2024-04-03 02:17:47 +0200

    chore(math): enhance `clamp` function with safe number conversion
    Improved the `clamp` function in the `math` package by integrating `toSafeNumber` to ensure that all input values are converted to safe numbers before proceeding with the clamping logic. This change enhances robustness by guarding against potential issues with unsafe or unexpected number values. The modification includes updated parameter names for clarity and additional comments for better understanding of the process.

--- packages/math/clamp.ts
+++ packages/math/clamp.ts
@@ -1,20 +1,29 @@
+import { toSafeNumber } from './toSafeNumber'
+
 /**
  * [Clamps](https://en.wikipedia.org/wiki/Clamping_(graphics)) a number to a given range.
  * If the number is smaller than  the minimum value, the minimum value is returned. If
  * the number is larger than the maximum value, the maximum value is returned. Otherwise,
  * the number is returned.
  *
- * @param n The number to clamp
- * @param min The minimum value
- * @param max The maximum value
- * @returns The clamped value
+ * @param number The number to clamp.
+ * @param min The minimum value.
+ * @param max The maximum value.
+ * @returns The clamped value.
  * @example clamp(100, 0, 10) // 10
  */
-export function clamp(n: number, min: number, max: number): number {
+export function clamp(number: number, min: number, max: number): number {
   if (min > max) throw new RangeError('Expected the minimum value to be less than or equal to the maximum value')
-  if (n <= min) return min
-  if (n >= max) return max
-  return n
+
+  // --- Make sure the values are safe numbers.
+  number = toSafeNumber(number)
+  min = toSafeNumber(min)
+  max = toSafeNumber(max)
+
+  // --- Clamp the value.
+  if (number <= min) return min
+  if (number >= max) return max
+  return number
 }
 
 /* c8 ignore next */
