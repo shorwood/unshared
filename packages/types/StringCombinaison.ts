9d73c0dfe1388a2b72cb847395d2e63f0319e1c6
Stanley Horwood <stanley@hsjm.io>
2024-04-11 05:00:58 +0200

    feat(types): add `StringCombinaison` utility type
    Introduce the `StringCombinaison` utility type in the `types` package to generate all unique combinations of tuple strings as a union of literal strings. This new type supports generating permutations of string literals, enabling more flexible type manipulation and compile-time string operations. The implementation includes test cases to verify correct type generation for tuples of various lengths and combinations, demonstrating its utility in complex type operations.

---
+++ packages/types/StringCombinaison.ts
@@ -0,0 +1,38 @@
+/**
+ * Given a tuple of literal strings, return all unique combinations as a union of literal strings.
+ *
+ * @template S Tuple of literal strings.
+ * @returns All possible combinations of the strings.
+ * @example StringCombinaison<['a', 'b', 'c']> // 'abc' | 'acb' | 'bac' | 'bca' | 'cab' | 'cba'
+ */
+export type StringCombinaison<S extends string[]> =
+  (S extends [...infer T extends string[], infer H extends string]
+    ? `${H}${StringCombinaison<T>}` | `${StringCombinaison<T>}${H}`
+    : '') |
+
+  (S extends [infer H extends string, ...infer T extends string[]]
+    ? `${H}${StringCombinaison<T>}` | `${StringCombinaison<T>}${H}`
+    : '')
+
+/** v8 ignore start */
+if (import.meta.vitest) {
+  it('should return all possible combinations of 2 characters', () => {
+    type Result = StringCombinaison<['a', 'b']>
+    expectTypeOf<Result>().toEqualTypeOf<'ab' | 'ba'>()
+  })
+
+  it('should return all possible combinations of 3 characters', () => {
+    type Result = StringCombinaison<['a', 'b', 'c']>
+    expectTypeOf<Result>().toEqualTypeOf<'abc' | 'acb' | 'bac' | 'bca' | 'cab' | 'cba'>()
+  })
+
+  it('should return all possible combinations of 4 characters', () => {
+    type Result = StringCombinaison<['a', 'b', 'c', 'd']>
+    expectTypeOf<Result>().toEqualTypeOf<'abcd' | 'abdc' | 'acbd' | 'acdb' | 'adbc' | 'adcb' | 'bacd' | 'bcad' | 'bcda' | 'bdca' | 'cabd' | 'cbad' | 'cbda' | 'cdba' | 'dabc' | 'dacb' | 'dbac' | 'dbca' | 'dcab' | 'dcba'>()
+  })
+
+  it('should return all possible combinations of 2 or 3 characters', () => {
+    type Result = StringCombinaison<['a', 'b', '' | 'c']>
+    expectTypeOf<Result>().toEqualTypeOf<'ab' | 'abc' | 'acb' | 'ba' | 'bac' | 'bca' | 'cab' | 'cba'>()
+  })
+}
