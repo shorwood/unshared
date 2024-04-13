1fe378bd15bb4026d55d28fee1dbcc7e1ce36e8c
Stanley Horwood <stanley@hsjm.io>
2024-04-13 16:05:43 +0200

    feat(fs): enhance `createTemporaryFile` with options and examples
    Added options and documentation examples to the `createTemporaryFile` utility in the `fs` package. This enhancement introduces the ability to specify a directory, file extension, and a custom function for generating random file names. Additionally, the documentation now includes examples demonstrating how to use the utility, making it more accessible for users to implement in their projects.
    
    The changes include:
    - Introduction of `CreateTemporaryFileOptions` interface to allow for customizable file creation parameters.
    - Extension support, enabling files to be created with specific file extensions.
    - Custom random string generator function support, providing flexibility in naming temporary files.
    - Updated test cases to cover the new functionality, ensuring the utility's reliability and ease of use.

--- packages/fs/createTemporaryFile.ts
+++ packages/fs/createTemporaryFile.ts
@@ -2,22 +2,64 @@ import { writeFile, rm, mkdir } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
 import { join } from 'node:path'
 
+export interface CreateTemporaryFileOptions {
+  /**
+   * The directory to create the temporary file in.
+   * Defaults to the system's temporary directory.
+   *
+   * @default tmpdir()
+   */
+  directory?: string
+  /**
+   * The file extension to use for the temporary file.
+   *
+   * @default ''
+   */
+  extension?: string
+  /**
+   * A function that generates a random string.
+   *
+   * @default () => Math.random().toString(36).slice(2)
+   */
+  random?: () => string
+}
+
 /**
  * Create a temporary file with a random name and return
  * an object containing the file path, and a function to
  * remove the file.
  *
  * @param content The content to write to the temporary file.
- * @param directory The directory to create the temporary file in.
+ * @param options The options to create the temporary file.
  * @returns A promise that resolves to the temporary file object.
+ * @example
+ * // Create a temporary file with the specified content.
+ * const [path, remove] = await createTemporaryFile('Hello, world!')
+ *
+ * // Do something with the file.
+ * exec(`openssl sha1 ${path}`)
+ *
+ * // Remove the file.
+ * await remove()
  */
-export async function createTemporaryFile(content?: Parameters<typeof writeFile>[1], directory?: string) {
-  if (!directory) directory = tmpdir()
-  const name = Math.random().toString(36).slice(2)
+export async function createTemporaryFile(content?: Parameters<typeof writeFile>[1], options: CreateTemporaryFileOptions = {}) {
+  const {
+    extension,
+    directory = tmpdir(),
+    random = () => Math.random().toString(36).slice(2),
+  } = options
+
+  // --- Generate a random name.
+  const rand = random()
+  const name = extension ? `${rand}.${extension}` : rand
   const path = join(directory, name)
-  const remove = async() => await rm(path, { force: true })
+
+  // --- Write the content to the file.
   await mkdir(directory, { recursive: true })
   await writeFile(path, content ?? '')
+
+  // --- Return the path and a function to remove the file.
+  const remove = () => rm(path, { force: true })
   return [path, remove] as const
 }
 
@@ -39,13 +81,23 @@ if (import.meta.vitest) {
   })
 
   it('should create a temporary file in the specified directory', async() => {
-    const [path] = await createTemporaryFile(undefined, '/cache')
+    const [path] = await createTemporaryFile(undefined, { directory: '/cache' })
     expect(path).toMatch(/^\/cache\/[\da-z]+$/)
   })
 
   it('should recursively create the specified directory', async() => {
-    const [path] = await createTemporaryFile(undefined, '/tmp/foo/bar')
+    const [path] = await createTemporaryFile(undefined, { directory: '/tmp/foo/bar' })
     expect(path).toMatch(/^\/tmp\/foo\/bar\/[\da-z]+$/)
+  })
+
+  it('should create a temporary file with the specified extension', async() => {
+    const [path] = await createTemporaryFile(undefined, { extension: 'txt' })
+    expect(path).toMatch(/^\/tmp\/[\da-z]+\.txt$/)
+  })
+
+  it('should create a temporary file with the given random function', async() => {
+    const [path] = await createTemporaryFile(undefined, { random: () => 'foo' })
+    expect(path).toMatch(/^\/tmp\/foo$/)
   })
 
   it('should remove the temporary file after calling the remove function', async() => {
