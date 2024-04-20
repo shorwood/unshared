654feba2555d76bff8bb9bf8b6304d87621847b3
Stanley Horwood <stanley@hsjm.io>
2024-04-20 08:43:09 +0200

    feat(vue): add `ready` utility for reactive condition tracking
    Introduces a new utility function `ready` within the Vue package to facilitate the creation of promises that resolve based on reactive condition fulfillment. This utility is designed to enhance the development experience by allowing easier tracking of reactive state changes until they meet specified conditions.
    
    The `ready` function accepts a Vue ref and an optional condition, which can be a value or a function returning a boolean. It returns a promise that resolves when the specified condition is met by the ref's value. This addition is particularly useful for scenarios where developers need to wait for a reactive state to reach a certain condition without resorting to cumbersome and less readable setups involving watchers or manual promise handling.
    
    Included are also unit tests to ensure the functionality works as expected across different use cases, such as waiting for a boolean ref to become true, waiting for a ref's value to match a specific value, and conditions evaluated through a function.

---
+++ packages/vue/utils/ready.ts
@@ -0,0 +1,75 @@
+import { Ref, ref, watch } from 'vue-demi'
+
+export type ReadyCondition<T> = T | ((value: T) => boolean)
+
+/**
+ * Create a promise that is resolved once the ref is equal to the value.
+ *
+ * @param reference The ref to watch.
+ * @param condition The value to watch for. (Default: `true`)
+ * @returns A promise that resolves once the ref is equal to the value.
+ * @example
+ * // Create a ref.
+ * const ref = ref(false)
+ *
+ * // Make the `ref` truthy after 1 second.
+ * setTimeout(() => { ref.value = true }, 1000)
+ *
+ * // Create a promise that resolves once the ref is truthy.
+ * const promise = ready(ref) // Resolves after 1 second.
+ */
+export function ready<T = unknown>(reference: Ref<T>, condition?: ReadyCondition<T>): Promise<T> {
+  return new Promise<T>((resolve) => {
+    const unwatch = watch(reference, (newValue) => {
+
+      // --- If no condition is provided, resolve once the value is truthy.
+      if (condition === undefined) {
+        if (!newValue) return
+      }
+
+      // --- If the condition is a function, resolve once the function returns true.
+      else if (typeof condition === 'function') {
+        // @ts-expect-error: T may also be a function, ignore this edge case.
+        const result = condition(newValue) as boolean
+        if (!result) return
+      }
+
+      // --- If the condition is a value, resolve once the value is equal.
+      else if (newValue !== condition) { return }
+
+      // --- Finaly, resolve the promise and stop watching.
+      unwatch()
+      resolve(newValue)
+    })
+  })
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  it('should create a promise that resolves once the ref is equal to truthy', async() => {
+    const reference = ref<boolean>()
+    const promise = ready(reference)
+    expect(promise).toBeInstanceOf(Promise)
+    reference.value = true
+    await expect(promise).resolves.toBe(true)
+    expectTypeOf(promise).toEqualTypeOf<Promise<boolean | undefined>>()
+  })
+
+  it('should create a promise that resolves once the ref is equal to the value', async() => {
+    const reference = ref('NOT READY')
+    const promise = ready(reference, 'READY')
+    expect(promise).toBeInstanceOf(Promise)
+    reference.value = 'READY'
+    await expect(promise).resolves.toEqual('READY')
+    expectTypeOf(promise).toEqualTypeOf<Promise<string>>()
+  })
+
+  it('should create a promise that resolves once the condition is met', async() => {
+    const reference = ref(0)
+    const promise = ready(reference, value => value === 5)
+    expect(promise).toBeInstanceOf(Promise)
+    reference.value = 5
+    await expect(promise).resolves.toEqual(5)
+    expectTypeOf(promise).toEqualTypeOf<Promise<number>>()
+  })
+}
