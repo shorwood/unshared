e0e1b904d6c0bb9c929d8826dbaa2e77d9cb398b
Stanley Horwood <stanley@hsjm.io>
2024-04-25 00:29:18 +0200

    chore(eslint-config): update rules for Node.js imports and version compatibility
    Update the ESLint configuration for Node.js projects to enable built-in rules up to version 20 of Node.js, allowing the use of newer ECMAScript features and APIs. Additionally, the `n/prefer-node-protocol` rule has been adjusted to enforce the use of the `node:` protocol for importing Node.js built-in modules with a version specification. The `n/no-missing-import` rule description has been updated for clarity.

--- packages/eslint-config/configs/node.ts
+++ packages/eslint-config/configs/node.ts
@@ -11,16 +11,20 @@ export function node(): Linter.FlatConfig[] {
         ...nodePlugin.configs.recommended.rules,
 
         /**
-         * Enforce using the node: protocol when importing Node.js builtin
-         * modules. This highlights the use of Node.js built-in modules.
+         * Enable node built-in rules up to version 20 of Node.js. This will
+         * allow the use of newer ECMAScript features and Node.js APIs.
          *
          * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-node-protocol.md
          */
-        'n/prefer-node-protocol': 'error',
+        'n/prefer-node-protocol': ['error', {
+          version: '>=20.0.0',
+        }],
 
         /**
          * Disable the no-missing-import as module resolution is already checked
          * by other tools and IDEs extensively. This rule is redundant.
+         *
+         * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/no-missing-import.md
          */
         'n/no-missing-import': 'off',
         'n/no-missing-require': 'off',
