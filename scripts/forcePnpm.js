151b86f7ef62c60a97acab7d1823de175191c3fb
Stanley Horwood <stanley@hsjm.io>
2024-04-25 00:13:12 +0200

    chore(scripts): update `forcePnpm.js` to use ESM syntax
    Refactored `forcePnpm.js` to adopt ESM syntax by replacing `require` and `process` global with explicit imports from 'node:process'. This change aligns the script with modern JavaScript standards, ensuring consistency and future-proofing the codebase.

--- scripts/forcePnpm.js
+++ scripts/forcePnpm.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env node
+import { env, exit, stderr } from 'node:process'
 
 // --- Check if pnpm is being used
-const agent = process.env.npm_config_user_agent || 'npm'
-if (agent.startsWith('pnpm')) process.exit(0)
+const agent = env.npm_config_user_agent || 'npm'
+if (agent.startsWith('pnpm')) exit(0)
 
 // --- If not, throw an error
-console.error('\nPlease use pnpm to manage dependencies in this repository.\n  $ npm i pnpm -g\n')
-process.exit(1)
+stderr.write('\nPlease use pnpm to manage dependencies in this repository.\n  $ npm i pnpm -g\n')
+exit(1)
