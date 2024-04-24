c66665ed1e852ce17f52a1d0509d78d8046f83d1
Stanley Horwood <stanley@hsjm.io>
2024-04-24 07:45:18 +0200

    fix(string): correct double globstar handling in `createPattern`
    Adjusted the `createPattern` function to correctly handle patterns with double globstars. Prior to this fix, patterns with double globstars were not being processed correctly, leading to inaccurate pattern matching. This update ensures that double globstar patterns match as intended, improving the reliability and accuracy of pattern matching within the library.

--- packages/string/createPattern.ts
+++ packages/string/createPattern.ts
@@ -10,19 +10,21 @@ import { escapeRegexp } from './escapeRegexp'
  */
 export function createPattern(pattern: string): RegExp {
   const patternExp = escapeRegexp(pattern, ['\\', '^', '$', '.', '|', '+', '(', ')'])
+
+  // --- Normalize paths
+    .replace(/^\\.\//, '')
+    .replaceAll(/\/+/g, '/')
+    .replace(/\/$/, '')
+
     // --- Globstar(s)
-    .replaceAll(/(\/)?(\*+)\/?/g, (_, $1 = '', $2: string) => ($2.length === 1 ? `${$1}[^/]+` : '(\/?.*)'))
+    .replaceAll(/(\/)?(\*+)(\/)?/g, (_, $1 = '', $2: string, $3 = '') =>
+      ($2.length === 1 ? `${$1}[^/]+${$3}` : `\/?.*${$3}`))
 
     // --- Wildcard
     .replaceAll(/{(.+?)}/g, (_, p1: string) => `(?:${p1.replaceAll(',', '|')})`)
 
     // --- Remove leading !
     .replace(/^!/, '')
-
-    // --- Normalize paths
-    .replace(/^\\.\//, '')
-    .replaceAll(/\/+/g, '/')
-    .replace(/\/$/, '')
 
     // --- POSIX bracket expressions
     .replaceAll('[[:alnum:]]', '[\\dA-Za-z]')
@@ -52,120 +54,168 @@ export function createPattern(pattern: string): RegExp {
 
 /* c8 ignore next */
 if (import.meta.vitest) {
-  it('should create RegExp from globstar pattern', () => {
-    const result = createPattern('foo/*')
-    expect(result).toEqual(/^foo\/[^/]+$/)
+
+  describe('globstar', () => {
+    it('should create RegExp from globstar pattern', () => {
+      const result = createPattern('foo/*')
+      const test = result.test('foo/bar')
+      expect(result).toEqual(/^foo\/[^/]+$/)
+      expect(test).toBe(true)
+    })
+
+    it('should create RegExp from double globstar pattern', () => {
+      const result = createPattern('foo/**')
+      const test = result.test('foo/bar/baz')
+      expect(result).toEqual(/^foo\/?.*$/)
+      expect(test).toBe(true)
+    })
+
+    it('should create RegExp for nested paths with file extension', () => {
+      const result = createPattern('*/*.ts')
+      const test = result.test('foo/bar.ts')
+      expect(result).toEqual(/^[^/]+\/[^/]+\.ts$/)
+      expect(test).toBe(true)
+    })
+
+    it('should create RegExp for deeply nested paths with file extension', () => {
+      const result = createPattern('**/src/*.ts')
+      const test = result.test('foo/bar/src/baz.ts')
+      expect(result).toEqual(/^\/?.*\/src\/[^/]+\.ts$/)
+      expect(test).toBe(true)
+    })
   })
 
-  it('should create RegExp from double globstar pattern', () => {
-    const result = createPattern('foo/**')
-    expect(result).toEqual(/^foo(\/?.*)$/)
+  describe('wildcard', () => {
+    it('should create RegExp from pattern with brackets', () => {
+      const result = createPattern('foo[abr]')
+      expect(result).toEqual(/^foo[abr]$/)
+    })
+
+    it('should create RegExp from pattern with question mark', () => {
+      const result = createPattern('foo?bar')
+      expect(result).toEqual(/^foo?bar$/)
+    })
+
+    it('should create RegExp with wildcard', () => {
+      const result = createPattern('{foo,bar}')
+      expect(result).toEqual(/^(?:foo|bar)$/)
+    })
+
+    it('should create RegExp with multiple wildcards', () => {
+      const result = createPattern('{foo,bar,baz,qux}')
+      expect(result).toEqual(/^(?:foo|bar|baz|qux)$/)
+    })
   })
 
-  it('should create RegExp from pattern with brackets', () => {
-    const result = createPattern('foo[abr]')
-    expect(result).toEqual(/^foo[abr]$/)
+  describe('POSIX', () => {
+    it('should create RegExp with [[:alnum:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:alnum:]]')
+      expect(result).toEqual(/^foo[\dA-Za-z]$/)
+    })
+
+    it('should create RegExp with [[:alpha:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:alpha:]]')
+      expect(result).toEqual(/^foo[A-Za-z]$/)
+    })
+
+    it('should create RegExp with [[:ascii:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:ascii:]]')
+      // eslint-disable-next-line no-control-regex
+      expect(result).toEqual(/^foo[\u0000-\u007F]$/)
+    })
+
+    it('should create RegExp with [[:blank:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:blank:]]')
+      expect(result).toEqual(/^foo[\t ]$/)
+    })
+
+    it('should create RegExp with [[:cntrl:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:cntrl:]]')
+      // eslint-disable-next-line no-control-regex
+      expect(result).toEqual(/^foo[\u0000-\u001F\u007F]$/)
+    })
+
+    it('should create RegExp with [[:digit:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:digit:]]')
+      expect(result).toEqual(/^foo\d$/)
+    })
+
+    it('should create RegExp with [[:graph:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:graph:]]')
+      expect(result).toEqual(/^foo[\u0021-\u007E]$/)
+    })
+
+    it('should create RegExp with [[:lower:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:lower:]]')
+      expect(result).toEqual(/^foo[a-z]$/)
+    })
+
+    it('should create RegExp with [[:print:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:print:]]')
+      expect(result).toEqual(/^foo[\u0020-\u007E]$/)
+    })
+
+    it('should create RegExp with [[:punct:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:punct:]]')
+      expect(result).toEqual(/^foo[\u0021-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]$/)
+    })
+
+    it('should create RegExp with [[:space:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:space:]]')
+      expect(result).toEqual(/^foo[\t\n\v\f\r ]$/)
+    })
+
+    it('should create RegExp with [[:upper:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:upper:]]')
+      expect(result).toEqual(/^foo[A-Z]$/)
+    })
+
+    it('should create RegExp with [[:word:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:word:]]')
+      expect(result).toEqual(/^foo\w$/)
+    })
+
+    it('should create RegExp with [[:xdigit:]] POSIX bracket expressions', () => {
+      const result = createPattern('foo[[:xdigit:]]')
+      expect(result).toEqual(/^foo[\dA-Fa-f]$/)
+    })
   })
 
-  it('should create RegExp from pattern with question mark', () => {
-    const result = createPattern('foo?bar')
-    expect(result).toEqual(/^foo?bar$/)
-  })
+  describe('edge cases', () => {
+    it('should create RegExp from pattern with negation', () => {
+      const result = createPattern('!foo*')
+      const testPositive = result.test('foobar')
+      const testNegative = result.test('barfoo')
+      expect(testPositive).toBe(false)
+      expect(testNegative).toBe(true)
+    })
 
-  it('should create RegExp with wildcard', () => {
-    const result = createPattern('{foo,bar}')
-    expect(result).toEqual(/^(?:foo|bar)$/)
-  })
+    it('should omit leading ./ from the pattern', () => {
+      const result = createPattern('./foo/*')
+      const test = result.test('foo/bar')
+      expect(result).toEqual(/^foo\/[^/]+$/)
+      expect(test).toBe(true)
+    })
 
-  it('should create RegExp with multiple wildcards', () => {
-    const result = createPattern('{foo,bar,baz,qux}')
-    expect(result).toEqual(/^(?:foo|bar|baz|qux)$/)
-  })
+    it('should remove duplicate slashes from the pattern', () => {
+      const result = createPattern('foo//bar')
+      const test = result.test('foo/bar')
+      expect(result).toEqual(/^foo\/bar$/)
+      expect(test).toBe(true)
+    })
 
-  it('should create RegExp with [[:alnum:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:alnum:]]')
-    expect(result).toEqual(/^foo[\dA-Za-z]$/)
-  })
+    it('should remove trailing slash from the pattern', () => {
+      const result = createPattern('foo/')
+      const test = result.test('foo')
+      expect(result).toEqual(/^foo$/)
+      expect(test).toBe(true)
+    })
 
-  it('should create RegExp with [[:alpha:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:alpha:]]')
-    expect(result).toEqual(/^foo[A-Za-z]$/)
-  })
-
-  it('should create RegExp with [[:ascii:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:ascii:]]')
-    // eslint-disable-next-line no-control-regex
-    expect(result).toEqual(/^foo[\u0000-\u007F]$/)
-  })
-
-  it('should create RegExp with [[:blank:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:blank:]]')
-    expect(result).toEqual(/^foo[\t ]$/)
-  })
-
-  it('should create RegExp with [[:cntrl:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:cntrl:]]')
-    // eslint-disable-next-line no-control-regex
-    expect(result).toEqual(/^foo[\u0000-\u001F\u007F]$/)
-  })
-
-  it('should create RegExp with [[:digit:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:digit:]]')
-    expect(result).toEqual(/^foo\d$/)
-  })
-
-  it('should create RegExp with [[:graph:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:graph:]]')
-    expect(result).toEqual(/^foo[\u0021-\u007E]$/)
-  })
-
-  it('should create RegExp with [[:lower:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:lower:]]')
-    expect(result).toEqual(/^foo[a-z]$/)
-  })
-
-  it('should create RegExp with [[:print:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:print:]]')
-    expect(result).toEqual(/^foo[\u0020-\u007E]$/)
-  })
-
-  it('should create RegExp with [[:punct:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:punct:]]')
-    expect(result).toEqual(/^foo[\u0021-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]$/)
-  })
-
-  it('should create RegExp with [[:space:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:space:]]')
-    expect(result).toEqual(/^foo[\t\n\v\f\r ]$/)
-  })
-
-  it('should create RegExp with [[:upper:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:upper:]]')
-    expect(result).toEqual(/^foo[A-Z]$/)
-  })
-
-  it('should create RegExp with [[:word:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:word:]]')
-    expect(result).toEqual(/^foo\w$/)
-  })
-
-  it('should create RegExp with [[:xdigit:]] POSIX bracket expressions', () => {
-    const result = createPattern('foo[[:xdigit:]]')
-    expect(result).toEqual(/^foo[\dA-Fa-f]$/)
-  })
-
-  it('should create RegExp from pattern with negation', () => {
-    const result = createPattern('!foo*')
-    const testPositive = result.test('foobar')
-    const testNegative = result.test('barfoo')
-    expect(testPositive).toBe(false)
-    expect(testNegative).toBe(true)
-  })
-
-  it('should escape certain RegExp special characters', () => {
-    const result = createPattern('foo$foo(foo)foo*foo+foo.foo?foo[fo]foo^foo{foo}foo|foo')
-    // eslint-disable-next-line unicorn/better-regex
-    const expected = /^foo\$foo\(foo\)foo[^/]+foo\+foo\.foo?foo[fo]foo\^foo(?:foo)foo\|foo$/
-    expect(result).toEqual(expected)
+    it('should escape certain RegExp special characters', () => {
+      const result = createPattern('foo$foo(foo)foo*foo+foo.foo?foo[fo]foo^foo{foo}foo|foo')
+      // eslint-disable-next-line unicorn/better-regex
+      const expected = /^foo\$foo\(foo\)foo[^/]+foo\+foo\.foo?foo[fo]foo\^foo(?:foo)foo\|foo$/
+      expect(result).toEqual(expected)
+    })
   })
 }
