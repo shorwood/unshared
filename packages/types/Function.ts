5d9c556e56dec9050f3be49b503328494f3ce733
Stanley Horwood <stanley@hsjm.io>
2024-04-25 00:21:56 +0200

    feat(types): add `this` context to `Function` and `FunctionAsync` types
    Enhanced the `Function` and `FunctionAsync` types by introducing a template for the `this` context in both synchronous and asynchronous function types. This change allows developers to define the type of the context (`this`) within which the function executes, improving the flexibility and type safety of higher-order functions and methods in TypeScript-based projects. This is particularly useful for scenarios where functions need a bound context to operate correctly.

--- packages/types/Function.ts
+++ packages/types/Function.ts
@@ -3,29 +3,47 @@
  *
  * @template U The type of the return value.
  * @template P The type of the parameters.
+ * @template T The type of the `this` context.
  * @example Function<boolean, [number, string]> // (a: number, b: string) => boolean
  */
-export type Function<U = any, P extends any[] = any[]> = (...args: P) => U
+export type Function<U = any, P extends any[] = any[], T = void> =
+  T extends void
+    ? (...args: P) => U
+    : (this: T, ...args: P) => U
 
 /* v8 ignore next */
 if (import.meta.vitest) {
-  test('should return a function type with no parameters', () => {
+  test('should return a function type', () => {
+    expectTypeOf<Function>().toEqualTypeOf<(...args: any[]) => any>()
+  })
+
+  test('should return an function type with no parameters', () => {
     type Result = Function<boolean>
     expectTypeOf<Result>().toEqualTypeOf<(...args: any[]) => boolean>()
   })
 
-  test('should build a function with 2 parameters', () => {
-    type Result = Function<string, [number, string]>
-    expectTypeOf<Result>().toEqualTypeOf<(...args: [number, string]) => string>()
+  test('should return a function type with 1 parameter', () => {
+    type Result = Function<string, [value: number]>
+    expectTypeOf<Result>().toEqualTypeOf<(value: number) => string>()
   })
 
-  test('should build a function with rest parameters', () => {
+  test('should return a function with 3 parameters', () => {
+    type Result = Function<string, [a: number, b: string, c: boolean]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c: boolean) => string>()
+  })
+
+  test('should return a function with rest parameters', () => {
     type Result = Function<string, [a: number, ...b: string[]]>
     expectTypeOf<Result>().toEqualTypeOf<(a: number, ...b: string[]) => string>()
   })
 
-  test('should build a function with optional parameters', () => {
+  test('should return a function with optional parameters', () => {
     type Result = Function<string, [a: number, b: string, c?: boolean]>
     expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c?: boolean) => string>()
+  })
+
+  test('should return a function with `this` context', () => {
+    type Result = Function<string, [number, string], { foo: string }>
+    expectTypeOf<Result>().toEqualTypeOf<(this: { foo: string }, a: number, b: string) => string>()
   })
 }
--- packages/types/FunctionAsync.ts
+++ packages/types/FunctionAsync.ts
@@ -3,31 +3,47 @@
  *
  * @template U The type of the value returned by the promise.
  * @template P The type of the parameters.
- * @example FunctionAsync<boolean, [number, string]> // (a: number, b: string) => boolean
+ * @template T The type of the `this` context.
+ * @example FunctionAsync<boolean, [a: number, b: string]> // (a: number, b: string) => boolean
  */
-export type FunctionAsync<U = unknown, P = unknown[]> = P extends unknown[]
-  ? (...args: P) => Promise<U>
-  : (argument0: P) => Promise<U>
+export type FunctionAsync<U = any, P extends any[] = any[], T = void> =
+  T extends void
+    ? (...args: P) => Promise<U>
+    : (this: T, ...args: P) => Promise<U>
 
 /* v8 ignore next */
 if (import.meta.vitest) {
-  test('should return a FunctionAsync type with no parameters', () => {
+  test('should return an asyncronous function type', () => {
+    expectTypeOf<FunctionAsync>().toEqualTypeOf<(...args: any[]) => Promise<any>>()
+  })
+
+  test('should return an asyncronous function type with no parameters', () => {
     type Result = FunctionAsync<boolean>
-    expectTypeOf<Result>().toEqualTypeOf<(...args: unknown[]) => Promise<boolean>>()
+    expectTypeOf<Result>().toEqualTypeOf<(...args: any[]) => Promise<boolean>>()
   })
 
-  test('should return a FunctionAsync type with 1 parameter', () => {
-    type Result = FunctionAsync<string, number>
-    expectTypeOf<Result>().toEqualTypeOf<(argument0: number) => Promise<string>>()
+  test('should return an asyncronous function type with 1 parameter', () => {
+    type Result = FunctionAsync<string, [value: number]>
+    expectTypeOf<Result>().toEqualTypeOf<(value: number) => Promise<string>>()
   })
 
-  test('should build a FunctionAsync with 2 parameters', () => {
-    type Result = FunctionAsync<string, [number, string]>
-    expectTypeOf<Result>().toEqualTypeOf<(...args: [number, string]) => Promise<string>>()
-  })
-
-  test('should build a FunctionAsync with 3 parameters', () => {
+  test('should return an asyncronous function with 3 parameters', () => {
     type Result = FunctionAsync<string, [a: number, b: string, c: boolean]>
     expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c: boolean) => Promise<string>>()
+  })
+
+  test('should return an asyncronous with rest parameters', () => {
+    type Result = FunctionAsync<string, [a: number, ...b: string[]]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, ...b: string[]) => Promise<string>>()
+  })
+
+  test('should return an asyncronous with optional parameters', () => {
+    type Result = FunctionAsync<string, [a: number, b: string, c?: boolean]>
+    expectTypeOf<Result>().toEqualTypeOf<(a: number, b: string, c?: boolean) => Promise<string>>()
+  })
+
+  test('should return an asyncronous function with `this` context', () => {
+    type Result = FunctionAsync<string, [number, string], { foo: string }>
+    expectTypeOf<Result>().toEqualTypeOf<(this: { foo: string }, a: number, b: string) => Promise<string>>()
   })
 }
