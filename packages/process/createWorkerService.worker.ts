06bda9431872f480bdf19da2bc6aae6eb1e37359
Stanley Horwood <stanley@hsjm.io>
2024-04-25 04:51:44 +0200

    chore(process): add tests for `createWorkerService.worker`
    Added a comprehensive set of tests to `createWorkerService.worker.ts` to ensure its functionality across various scenarios. These tests include checking the execution of named functions from both built-in and local modules, handling default exports, and properly managing error cases such as non-existent modules or exports. This addition aims to bolster the reliability and maintainability of the worker service creation process.

--- packages/process/createWorkerService.worker.ts
+++ packages/process/createWorkerService.worker.ts
@@ -27,3 +27,62 @@ async function callback(request: WorkerServicePayload) {
 
 // --- Register the dynamic runner function.
 workerRegister('WORKER_SERVICE', callback)
+
+/* v8 ignore next */
+if(import.meta.vitest) {
+  test('should call a named function from a built-in module and return the result', async() => {
+    const result = await callback({ moduleId: 'node:util', name: 'format', parameters: ['Hello', 'World!'] })
+    expect(result).toBe('Hello World!')
+  })
+
+  test('should call a named sync function from a local module and return the result', async() => {
+    const result = await callback({ moduleId: './__fixtures__/module', name: 'factorial', parameters: [10] })
+    expect(result).toBe(3628800)
+  })
+
+  test('should call a named async function from a local module and return the resolved value', async() => {
+    const result = await callback({ moduleId: './__fixtures__/module', name: 'factorialAsync', parameters: [10] })
+    expect(result).toBe(3628800)
+  })
+
+  test('should default to the default export if no name is provided', async() => {
+    const result = await callback({ moduleId: './__fixtures__/module', parameters: [10] })
+    expect(result).toBe(3628800)
+  })
+
+  test('should throw an error if the named export does not exist', async() => {
+    const shouldReject = callback({ moduleId: './__fixtures__/module', name: 'doesNotExist' })
+    await expect(shouldReject).rejects.toThrow(/The module ".+?" does not have the named export "doesNotExist"/)
+  })
+
+  test('should throw an error if the module does not exist', async() => {
+    const shouldReject = callback({ moduleId: './__fixtures__/doesNotExist' })
+    await expect(shouldReject).rejects.toThrow(/Cannot find module '.+?'/)
+  })
+
+  test('should throw an error if the module throws', async() => {
+    const shouldReject = callback({ moduleId: './__fixtures__/module', name: 'throws' })
+    await expect(shouldReject).rejects.toThrow(SyntaxError)
+    await expect(shouldReject).rejects.toThrow('Thrown')
+  })
+
+  test('should throw an error if the module rejects', async() => {
+    const shouldReject = callback({ moduleId: './__fixtures__/module', name: 'rejects' })
+    await expect(shouldReject).rejects.toThrow(SyntaxError)
+    await expect(shouldReject).rejects.toThrow('Rejected')
+  })
+
+  test('should return the own property names of the module', async() => {
+    const result = await callback({ moduleId: './__fixtures__/module', name: 'getOwnPropertyNames' })
+    expect(result).toMatchObject([
+      'factorial',
+      'factorialAsync',
+      'throws',
+      'rejects',
+      'buffer',
+      'getThreadId',
+      'constant',
+      'default',
+    ])
+  })
+}
