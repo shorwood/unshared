830d508bd5344ec44c9c8eac9c7021d13acff5e1
Stanley Horwood <stanley@hsjm.io>
2024-04-26 07:46:03 +0200

    feat(unocss-preset): enhance `ruleGradientMask` with dynamic start/end points
    Refactored `ruleGradientMask` to support dynamic start and end points for gradient masks, allowing for more flexible design options. The regular expression for matching the rule has been updated to capture the direction, start, and end percentages, enabling the definition of gradient masks that start and end at specific points other than the defaults. This change improves the utility and flexibility of the gradient mask feature in the `unocss-preset` package, making it more adaptable to various design requirements.

--- packages/unocss-preset/rules/ruleGradientMask.ts
+++ packages/unocss-preset/rules/ruleGradientMask.ts
@@ -1,6 +1,6 @@
 import { Rule, RuleContext } from '@unocss/core'
 
-const directionKeys = {
+const DIRECTIONS_KEYS = {
   b: 'to bottom',
   bl: 'to bottom left',
   br: 'to bottom right',
@@ -12,25 +12,25 @@ const directionKeys = {
 }
 
 export const ruleGradientMask: Rule = [
-  new RegExp(`^gradient-mask-(${Object.keys(directionKeys).join('|')})(?:-(\\d{1,3}))?$`),
+  /^gradient-mask-(?<dir>\w{1,2})(?:-(?<start>\d{1,3})(?:\/(?<end>\d{1,3}))?)?$/,
 
   // --- Resolve the direction and opacity and return CSS properties.
-  ([, directionKey, opacity = '0']: string[]) => {
-    const direction = directionKeys[directionKey as keyof typeof directionKeys]
+  (match) => {
+    const { dir, start = '0', end = '100' } = match.groups!
+    const direction = DIRECTIONS_KEYS[dir as keyof typeof DIRECTIONS_KEYS]
     if (!direction) return
-
-    // --- Return CSS properties.
     return {
-      '-webkit-mask-image': `linear-gradient(${direction}, rgba(0, 0, 0, 1.0) ${opacity}%, transparent 100%)`,
-      'mask-image': `linear-gradient(${direction}, rgba(0, 0, 0, 1.0) ${opacity}%, transparent 100%)`,
+      '-webkit-mask-image': `linear-gradient(${direction}, black ${start}%, transparent ${end}%)`,
+      'mask-image': `linear-gradient(${direction}, black ${start}%, transparent ${end}%)`,
     }
   },
 
   // --- Provide autocomplete suggestions.
   {
     autocomplete: [
-      `gradient-mask-(${Object.keys(directionKeys).join('|')})`,
-      `gradient-mask-(${Object.keys(directionKeys).join('|')})-<num>`,
+      'gradient-mask-<directions>',
+      'gradient-mask-<directions>-<percent>',
+      'gradient-mask-<directions>-<percent>/<percent>',
     ],
   },
 ]
@@ -39,102 +39,121 @@ export const ruleGradientMask: Rule = [
 if (import.meta.vitest) {
   const [matcher, handler] = ruleGradientMask
 
-  test('should create a top gradient mask', () => {
-    const match = matcher.exec('gradient-mask-t')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to top, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to top, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
+  describe('directions', () => {
+    it('should create a top gradient mask', () => {
+      const match = matcher.exec('gradient-mask-t')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to top, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to top, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a top right gradient mask', () => {
+      const match = matcher.exec('gradient-mask-tr')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to top right, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to top right, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a right gradient mask', () => {
+      const match = matcher.exec('gradient-mask-r')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to right, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to right, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a bottom right gradient mask', () => {
+      const match = matcher.exec('gradient-mask-br')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to bottom right, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to bottom right, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a bottom gradient mask', () => {
+      const match = matcher.exec('gradient-mask-b')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to bottom, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to bottom, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a bottom left gradient mask', () => {
+      const match = matcher.exec('gradient-mask-bl')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to bottom left, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to bottom left, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a left gradient mask', () => {
+      const match = matcher.exec('gradient-mask-l')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to left, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to left, black 0%, transparent 100%)',
+      })
+    })
+
+    it('should create a top left gradient mask', () => {
+      const match = matcher.exec('gradient-mask-tl')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to top left, black 0%, transparent 100%)',
+        'mask-image': 'linear-gradient(to top left, black 0%, transparent 100%)',
+      })
     })
   })
 
-  test('should create a top right gradient mask', () => {
-    const match = matcher.exec('gradient-mask-tr')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to top right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to top right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
+  describe('position', () => {
+    it('should create a top gradient mask that starts at 50%', () => {
+      const match = matcher.exec('gradient-mask-t-50')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to top, black 50%, transparent 100%)',
+        'mask-image': 'linear-gradient(to top, black 50%, transparent 100%)',
+      })
+    })
+
+    it('should create a top right gradient mask that starts at 25% and fades to transparent at 75%', () => {
+      const match = matcher.exec('gradient-mask-tr-25/75')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toStrictEqual({
+        '-webkit-mask-image': 'linear-gradient(to top right, black 25%, transparent 75%)',
+        'mask-image': 'linear-gradient(to top right, black 25%, transparent 75%)',
+      })
     })
   })
 
-  test('should create a right gradient mask', () => {
-    const match = matcher.exec('gradient-mask-r')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
+  describe('autocomplete', () => {
+    it('should provide autocomplete suggestions', () => {
+      const result = ruleGradientMask[2].autocomplete
+      expect(result).toMatchObject([
+        'gradient-mask-<directions>',
+        'gradient-mask-<directions>-<percent>',
+        'gradient-mask-<directions>-<percent>/<percent>',
+      ])
     })
   })
 
-  test('should create a bottom right gradient mask', () => {
-    const match = matcher.exec('gradient-mask-br')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to bottom right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to bottom right, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
+  describe('invalid', () => {
+    it('should not match invalid gradient mask', () => {
+      const match = matcher.exec('gradient-mask-aa')!
+      const result = handler(match, {} as RuleContext)
+      expect(result).toBeUndefined()
     })
-  })
 
-  test('should create a bottom gradient mask', () => {
-    const match = matcher.exec('gradient-mask-b')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to bottom, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to bottom, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
+    it('should not match invalid gradient value', () => {
+      const match = matcher.exec('gradient-mask-t-1000')
+      expect(match).toBeNull()
     })
-  })
-
-  test('should create a bottom left gradient mask', () => {
-    const match = matcher.exec('gradient-mask-bl')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to bottom left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to bottom left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-    })
-  })
-
-  test('should create a left gradient mask', () => {
-    const match = matcher.exec('gradient-mask-l')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-    })
-  })
-
-  test('should create a top left gradient mask', () => {
-    const match = matcher.exec('gradient-mask-tl')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to top left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-      'mask-image': 'linear-gradient(to top left, rgba(0, 0, 0, 1.0) 0%, transparent 100%)',
-    })
-  })
-
-  test('should create a top gradient mask that fades to transparent at 50%', () => {
-    const match = matcher.exec('gradient-mask-t-50')!
-    const result = handler(match, {} as RuleContext)
-    expect(result).toStrictEqual({
-      '-webkit-mask-image': 'linear-gradient(to top, rgba(0, 0, 0, 1.0) 50%, transparent 100%)',
-      'mask-image': 'linear-gradient(to top, rgba(0, 0, 0, 1.0) 50%, transparent 100%)',
-    })
-  })
-
-  test('should provide autocomplete suggestions', () => {
-    const result = ruleGradientMask[2].autocomplete
-    expect(result).toMatchObject([
-      'gradient-mask-(b|bl|br|l|r|t|tl|tr)',
-      'gradient-mask-(b|bl|br|l|r|t|tl|tr)-<num>',
-    ])
-  })
-
-  test('should not match invalid gradient mask', () => {
-    const match = matcher.exec('gradient-mask-invalid')
-    expect(match).toBeNull()
-  })
-
-  test('should not match invalid gradient mask with opacity', () => {
-    const match = matcher.exec('gradient-mask-t-1000')
-    expect(match).toBeNull()
   })
 }
