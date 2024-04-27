d86d1f46518e257f2f28611aaf4b8a0645c90ab8
Stanley Horwood <stanley@hsjm.io>
2024-04-27 18:00:17 +0200

    fix(unocss-preset): ensure default color for `shortcutInput`
    Updated the `shortcutInput` function to include a default color of 'white' when no specific color is provided. This change addresses an issue where the background would not render correctly if the color parameter was omitted. The default setting ensures consistent styling across all uses of this shortcut, improving the robustness of UI components that rely on it.

--- packages/unocss-preset/shortcuts/shortcutInput.ts
+++ packages/unocss-preset/shortcuts/shortcutInput.ts
@@ -3,14 +3,17 @@ import { Shortcut } from '@unocss/core'
 
 export const shortcutInput: Shortcut = [
   /^input(?:-(.+))?$/,
-  ([, color]) => dedent(`
-    rounded-2xl text-lg rounded-xl p-3 py-3
-    w-full transition-all outline-none !font-sans
-    ring-1 ring-transparent bg-${color}/5
-    focus:(ring-${color} bg-${color}/10)
-    hover:(ring-${color}/50 bg-${color}/10)
-    hover:focus:(ring-${color} bg-${color}/10)
-  `),
+  ([, color]) => {
+    if (!color) color = 'white'
+    return dedent(`
+      rounded-2xl text-lg rounded-xl p-3 py-3
+      w-full transition-all outline-none !font-sans
+      ring-1 ring-transparent bg-${color}/5
+      focus:(ring-${color} bg-${color}/10)
+      hover:(ring-${color}/50 bg-${color}/10)
+      hover:focus:(ring-${color} bg-${color}/10)
+    `)
+  },
 ]
 
 export const shortcutInputLabel: Shortcut = [
