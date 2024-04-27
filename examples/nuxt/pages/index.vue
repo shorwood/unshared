e70ced37446f48e6c99657d49ccd516514d81dd3
Stanley Horwood <stanley@hsjm.io>
2024-04-27 18:00:47 +0200

    chore(nuxt): apply linting auto-fixes to `index.vue`
    Ran automatic linting tools on the `index.vue` file in the Nuxt example directory, which rearranged the attributes in the `<InputToggle>` and `<Button>` components for better readability and consistency with linting rules. This adjustment ensures that the codebase adheres to the established coding standards.

--- examples/nuxt/pages/index.vue
+++ examples/nuxt/pages/index.vue
@@ -8,29 +8,29 @@ const state = ref<'bar' | 'foo' | symbol>('foo')
 <template>
   <main class="flex flex-col items-center justify-center h-screen w-screen">
     <InputToggle
+      v-slot="{ isActive }"
+      v-model="state"
       class-active="bg-red"
       type="radio"
-      v-model="state"
-      v-slot="{ isActive }"
       value="bar">
       {{ isActive }}
     </InputToggle>
 
     <InputToggle
+      v-slot="{ isActive }"
+      v-model="state"
       class-active="bg-red"
       type="radio"
-      v-model="state"
-      v-slot="{ isActive }"
       value="foo">
       {{ isActive }}
     </InputToggle>
 
     <Button
+      v-slot="{ isLink }"
       :debounce="500"
       :disabled="state === 'foo'"
       class="bg-blue gradient-mask-tr-50/80 text-black active:text-black p-4 cursor-pointer"
-      label="YES"
-      v-slot="{ isLink }">
+      label="YES">
       {{ isLink }}
     </Button>
 
