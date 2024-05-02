421dbce063d6e99c8e32f8d7d93b0a7607d3deb9
Stanley Horwood <stanley@hsjm.io>
2024-05-02 13:49:16 +0200

    feat(functions): add `toPredicate` function for type-safe assertions
    Introduced a new function `toPredicate` in the `functions` package that converts assertion functions into predicates. This enhancement allows developers to perform type checks without having to handle exceptions manually, providing a more robust and cleaner way to validate types at runtime.
    
    The function takes an assertion function as an argument and returns a predicate that signifies whether the input satisfies the specified type assertion. This functionality is particularly useful in scenarios where type safety is critical but the overhead of exception handling needs to be avoided. Additionally, unit tests have been added to ensure the reliability of this new function.

---
+++ packages/functions/toPredicate.ts
@@ -0,0 +1,49 @@
+import { Assertor } from '@unshared/types'
+import { Predicator } from '@unshared/types'
+
+/**
+ * Given a typed assertion function, returns a predicate function
+ * that can be used to infer the type of the value without the need
+ * to catch the error.
+ *
+ * @param fn The assertion function to convert to a predicate.
+ * @returns A predicate function that returns `true` if the value passes the assertion, `false` otherwise.
+ * @example
+ * // Declare a type assertion function.
+ * function assertIsString(value: unknown): asserts value is string {
+ *   if (typeof value !== 'string') throw new Error('Value is not a string')
+ * }
+ *
+ * // Convert the assertion function to a predicate.
+ * const isString = toPredicate(assertIsString) // (value: unknown) => value is string
+ */
+export function toPredicate<T>(fn: Assertor<T>): Predicator<T> {
+  return (value: unknown): value is T => {
+    try {
+      fn(value)
+      return true
+    }
+    catch {
+      return false
+    }
+  }
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  function assertIsString(value: unknown): asserts value is string {
+    if (typeof value !== 'string') throw new Error('Value is not a string')
+  }
+
+  test('should return true if value is a string', () => {
+    const isString = toPredicate(assertIsString)
+    const result = isString('hello')
+    expect(result).toBeTruthy()
+  })
+
+  test('should return false if value is not a string', () => {
+    const isString = toPredicate(assertIsString)
+    const result = isString(1)
+    expect(result).toBeFalsy()
+  })
+}
