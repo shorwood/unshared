ceef1d3f6ae270167c78d4baeef890bcfe30b243
Stanley Horwood <stanley@hsjm.io>
2024-05-02 13:49:15 +0200

    feat(types): enhance `Predicated` type to return `never` for non-predicate functions
    Updated the `Predicated` type in TypeScript to now return `never` if the input function does not match a proper predicate pattern. This change ensures more accurate type inference by enforcing stricter type checks. The adjustment prevents incorrect type assignments that could lead to subtle bugs in type-dependent logic. This update further refines the utility and reliability of the `Predicated` type when used in complex TypeScript applications, making it more robust against improper use.

--- packages/types/Predicated.ts
+++ packages/types/Predicated.ts
@@ -1,34 +1,37 @@
 /**
- * Infer the predicate type from a predicator function.
+ * Infer the predicate type from a predicator or assertor function.
  *
  * @template T The predicator function.
  * @example Predicated<(value: unknown) => value is string> // string
  */
-export type Predicated<T> = T extends (value: any, ...args: any[]) => value is infer R ? R : never
+export type Predicated<T> =
+  T extends (value: any, ...args: any[]) => value is infer R
+    ? (unknown extends R ? never : R)
+    : never
 
 /** v8 ignore start */
 if (import.meta.vitest) {
-  test('should infer string from a predicator for strings', () => {
+  test('should give string from a predicator of strings', () => {
     type Result = Predicated<(value: unknown) => value is string>
     expectTypeOf<Result>().toEqualTypeOf<string>()
   })
 
-  test('should infer number from a predicator for numbers', () => {
-    type Result = Predicated<(value: unknown) => value is number>
-    expectTypeOf<Result>().toEqualTypeOf<number>()
+  test('should give never from an assertor of numbers', () => {
+    type Result = Predicated<(value: unknown) => asserts value is number>
+    expectTypeOf<Result>().toEqualTypeOf<never>()
   })
 
-  test('should infer unknown from a predicator for unknowns', () => {
+  test('should give unknown from a predicator for unknowns', () => {
     type Result = Predicated<(value: unknown) => value is unknown>
-    expectTypeOf<Result>().toEqualTypeOf<unknown>()
+    expectTypeOf<Result>().toEqualTypeOf<never>()
   })
 
-  test('should give never for non-predicator functions', () => {
+  test('should give unknown for non-predicator functions', () => {
     type Result = Predicated<(value: unknown) => boolean>
     expectTypeOf<Result>().toEqualTypeOf<never>()
   })
 
-  test('should match function with more than one parameter', () => {
+  test('should match predicator with more than one parameter', () => {
     type Result = Predicated<(value: unknown, key: string) => value is string>
     expectTypeOf<Result>().toEqualTypeOf<string>()
   })
