db2688bfa1518f5ca9693aefa95010ce311fdad8
Stanley Horwood <stanley@hsjm.io>
2024-05-05 18:23:52 +0200

    chore(vue): enhance `useBaseRenderable` to improve component flexibility and integration
    Refactored `useBaseRenderable` composable in the Vue package to increase its flexibility and better integrate with the Vue component instance lifecycle. The update includes changes that allow the composable to accept an instance parameter, which is used to handle the composable state more efficiently within the context of the Vue component instance. Additionally, the definition of the `as` prop has been clarified to be more descriptive about its purpose and usage, which enhances readability and maintainability.
    
    This enhancement aims to provide a more robust and flexible infrastructure for rendering components dynamically, which could be beneficial for developers when building complex component systems that rely on conditional rendering logic.

--- packages/vue/composables/useBaseRenderable.ts
+++ packages/vue/composables/useBaseRenderable.ts
@@ -1,13 +1,17 @@
-import { Component, ExtractPropTypes, Prop, computed, provide, resolveComponent } from 'vue'
+import { Component, ExtractPropTypes, Prop, computed, getCurrentInstance, resolveComponent } from 'vue'
 import { toReactive } from '@vueuse/core'
 
+/** The symbol to provide the composable into the component. */
+export const BASE_RENDERABLE_SYMBOL = Symbol('baseRenderable')
+
+/** The properties of the base renderable component. */
 export const BASE_RENDERABLE_PROPS = {
 
   /**
-   * The HTML tag, component name, or component object to render. This represents
-   * what will encapsulate the content of the component and how it will be rendered.
+   * The HTML tag or component (constructor or name) to render. This represents what will
+   * encapsulate the content of the component and how it will be rendered.
    *
-   * @default 'div'
+   * @example 'div'
    */
   as: [String, Object] as Prop<{} & string | Component | keyof HTMLElementTagNameMap | undefined>,
 }
@@ -15,32 +19,118 @@ export const BASE_RENDERABLE_PROPS = {
 /** The properties of the base renderable component. */
 export type BaseRenderableProps = ExtractPropTypes<typeof BASE_RENDERABLE_PROPS>
 
+/** The base properties of the renderable composable. */
+export interface BaseRenderable {
+  is: Component | string | undefined
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentInternalInstance {
+    [BASE_RENDERABLE_SYMBOL]?: BaseRenderable
+  }
+}
+
 /**
  * A composable that provides properties and methods to create a Vue link or
  * external hyperlink. This composable will dynamically determine the type of
  * link to use based on the provided properties.
  *
  * @param props The properties of the component passed by the `setup` function.
+ * @param instance The instance of the component to provide the composable.
  * @returns An object with the computed classes and attributes.
  * @example
  * defineComponent({
+ *   props: BASE_RENDERABLE_PROPS,
  *   setup(props, context) {
- *     return useBaseRenderable(props, context)
+ *     const renderable = useBaseRenderable(props)
+ *     return () => h(renderable.is ?? 'div')
  *   }
  * })
  */
-export function useBaseRenderable(props: BaseRenderableProps) {
+export function useBaseRenderable(props: BaseRenderableProps = {}, instance = getCurrentInstance()) {
+  if (instance?.[BASE_RENDERABLE_SYMBOL]) return instance[BASE_RENDERABLE_SYMBOL]
 
   // --- Compute component type.
   const is = computed(() => {
     if (props.as === undefined) return
+    if (typeof props.as !== 'string') return props.as
     const isHtmlTag = typeof props.as === 'string' && props.as.toLowerCase() === props.as
-    if (isHtmlTag) return props.as as keyof HTMLElementTagNameMap
-    return resolveComponent(props.as as string)
+    if (isHtmlTag) return props.as
+    return resolveComponent(props.as)
   })
 
   // --- Provide the composable into the component and return it.
   const composable = toReactive({ is })
-  provide('baseRenderable', composable)
+  if (instance) instance[BASE_RENDERABLE_SYMBOL] = composable
   return composable
+}
+
+/* v8 ignore next */
+// @vitest-environment happy-dom
+if (import.meta.vitest) {
+  const { isReactive, defineComponent } = await import('vue')
+  const { mount } = await import('@vue/test-utils')
+
+  describe('composable', () => {
+    it('should return a reactive object', () => {
+      const result = useBaseRenderable()
+      const reactive = isReactive(result)
+      expect(reactive).toBe(true)
+    })
+
+    it('should provide the composable into the component', () => {
+      mount(() => {
+        const result = useBaseRenderable()
+        const instance = getCurrentInstance()
+        const injected = instance?.[BASE_RENDERABLE_SYMBOL]
+        expect(result).toStrictEqual(injected)
+      })
+    })
+
+    it('should return the same instance when called multiple times', () => {
+      mount(() => {
+        const result1 = useBaseRenderable()
+        const result2 = useBaseRenderable()
+        expect(result1).toBe(result2)
+      })
+    })
+
+    it('should return different instances for different components', () => {
+      const result1 = useBaseRenderable()
+      const result2 = useBaseRenderable()
+      expect(result1).not.toBe(result2)
+    })
+  })
+
+  describe('html tag', () => {
+    it('should return a div tag', () => {
+      const result = useBaseRenderable({ as: 'div' })
+      expect(result.is).toBe('div')
+    })
+
+    it('should return a span tag', () => {
+      const result = useBaseRenderable({ as: 'span' })
+      expect(result.is).toBe('span')
+    })
+  })
+
+  describe('component', () => {
+    it('should return a component', () => {
+      const component = defineComponent({ template: '<div></div>' })
+      const result = useBaseRenderable({ as: component })
+      expect(result.is).toBe(component)
+    })
+
+    it('should return a component by name', () => {
+      const Component = defineComponent({ template: '<div></div>' })
+      mount({
+        components: { Component },
+        setup() {
+          const result = useBaseRenderable({ as: 'Component' })
+          expect(result.is).toBe(Component)
+          return () => ''
+        },
+      })
+    })
+  })
 }
