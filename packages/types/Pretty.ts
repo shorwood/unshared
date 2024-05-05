b9ba33f3a1f6fe2720b45eb40c6a783f5db36216
Stanley Horwood <stanley@hsjm.io>
2024-05-05 13:13:46 +0200

    feat(types): add `Pretty` type to enhance IDE readability
    Introduced a new `Pretty` type in the `types` package to improve the readability of complex, nested type signatures in the IDE. This type utility unwraps deeply nested types, making them easier to understand and work with during development. This addition aims to enhance developer productivity by decluttering the type signatures displayed in the IDE, particularly useful for projects with extensive use of TypeScript's advanced type features.

---
+++ packages/types/Pretty.ts
@@ -0,0 +1,38 @@
+/**
+ * Unwrap the type making it easier to read in the IDE. This type is useful when
+ * working with deeply nested types that produce implicit signatures that are
+ * hard to read and understand.
+ *
+ * @template T The type to wrap.
+ * @example
+ *
+ * // Create a type that deeply modifies the keys of an object.
+ * type UppercaseKeys<T extends object> = {
+ *   [K in keyof T as Uppercase<K & string>]:
+ *     T[K] extends object ? UppercaseKeys<T[K]> : T[K]
+ * }
+ *
+ * // Create a type that is not unwrapped in the IDE.
+ * // Notice how the `FLAGS` property is not expanded.
+ * type Input = UppercaseKeys<{ name: 'JOHN'; flags: { isAdmin: true } }>
+ * // { NAME: 'JOHN'; FLAGS: UppercaseKeys<{ isAdmin: true }> }
+ *
+ * // Wrap the type in a `Pretty` type to declutter the type signature.
+ * // The `FLAGS` property is now expanded and easier to read.
+ * type Output = Pretty<Input>
+ * // { NAME: 'JOHN'; FLAGS: { ISADMIN: true } }
+ */
+export type Pretty<T> = T extends object ? { [K in keyof T]: Pretty<T[K]> } : T
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  type UppercaseKeys<T extends object> = {
+    [K in keyof T as Uppercase<K & string>]: T[K] extends object ? UppercaseKeys<T[K]> : T[K]
+  }
+
+  test('should return the same type as the input', () => {
+    type Input = UppercaseKeys<{ name: 'JOHN'; flags: { isAdmin: true } }>
+    type Output = Pretty<Input>
+    expectTypeOf<Output>().toEqualTypeOf<Input>()
+  })
+}
