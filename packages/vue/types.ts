b26261af5798506b42c1e681c087d2784a27a307
Stanley Horwood <stanley@hsjm.io>
2024-05-14 21:00:37 +0200

    feat(vue): introduce `MaybeRef` type for flexible referencing
    Added a new type `MaybeRef` in the vue package to enhance flexibility when working with Vue's reactivity system. This type allows a value to be either a direct instance or wrapped in a `Ref` object, facilitating easier integration in scenarios where the use of references might vary. The addition aims to streamline development by reducing the need for explicit ref checks or conversions, thereby improving code readability and maintainability.

--- packages/vue/types.ts
+++ packages/vue/types.ts
@@ -1,4 +1,10 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 import { Ref } from 'vue'
 
+/**
+ * A type that represents a value that may be wrapped in a `Ref` object.
+ *
+ * @template T The type of the value.
+ * @example MaybeRef<number> // Ref<number> | number
+ */
 export type MaybeRef<T = any> = Ref<T> | T
