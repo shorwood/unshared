9825cbd80387cf0468287222ec728ecdfb72e2a7
Stanley Horwood <stanley@hsjm.io>
2024-05-15 14:55:14 +0200

    chore(eslint-config): migrate sonarjs to use flat config syntax
    Refactored the sonarjs configuration in the eslint-config package to utilize the newer flat config format provided by ESLint. This update simplifies the configuration structure and aligns with the latest ESLint standards for better maintainability and clarity in configuration files. The migration involves adjusting the import statements and modifying how configurations and rules are declared within the file.

--- packages/eslint-config/configs/sonarjs.ts
+++ packages/eslint-config/configs/sonarjs.ts
@@ -1,52 +1,49 @@
+import pluginSonarjs from 'eslint-plugin-sonarjs'
 import { Linter } from 'eslint'
-import { FlatCompat } from '@eslint/eslintrc'
 import { ESLintConfigOptions } from './all'
 
-export function sonarjs(options: ESLintConfigOptions): Array<Linter.FlatConfig<Linter.RulesRecord>> {
-  return new FlatCompat().config({
-    extends: [
-      'plugin:sonarjs/recommended',
-    ],
-    plugins: [
-      'sonarjs',
-    ],
-    rules: {
+export function sonarjs(options: ESLintConfigOptions): Linter.FlatConfig[] {
+  return [
+    pluginSonarjs.configs.recommended as Linter.FlatConfig,
+    {
+      rules: {
 
-      /**
-       * Cognitive Complexity is a measure of how hard the control flow of a function
-       * is to understand. Functions with high Cognitive Complexity will be difficult
-       * to maintain.
-       *
-       * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/cognitive-complexity.md
-       */
-      'sonarjs/cognitive-complexity': ['error', 30],
+        /**
+         * Cognitive Complexity is a measure of how hard the control flow of a function
+         * is to understand. Functions with high Cognitive Complexity will be difficult
+         * to maintain.
+         *
+         * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/cognitive-complexity.md
+         */
+        'sonarjs/cognitive-complexity': ['error', 30],
 
-      /**
-       * Duplicated string literals make the process of refactoring error-prone,
-       * since you must be sure to update all occurrences. On the other hand,
-       * constants can be referenced from many places, but only need to be
-       * updated in a single place.
-       *
-       * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/no-duplicate-string.md
-       */
-      'sonarjs/no-duplicate-string': ['error', {
-        threshold: 10,
-      }],
+        /**
+         * Duplicated string literals make the process of refactoring error-prone,
+         * since you must be sure to update all occurrences. On the other hand,
+         * constants can be referenced from many places, but only need to be
+         * updated in a single place.
+         *
+         * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/no-duplicate-string.md
+         */
+        'sonarjs/no-duplicate-string': ['error', {
+          threshold: 10,
+        }],
 
-      /**
-       * Those rules are crashing ESLint at startup, so they are disabled for now.
-       */
-      'sonarjs/no-empty-collection': 'off',
-      'sonarjs/no-extra-arguments': 'off',
-      'sonarjs/no-gratuitous-expressions': 'off',
-      'sonarjs/no-one-iteration-loop': 'off',
-      'sonarjs/no-redundant-jump': 'off',
-      'sonarjs/no-unused-collection': 'off',
-      'sonarjs/no-use-of-empty-return-value': 'off',
-      'sonarjs/prefer-immediate-return': 'off',
+        /**
+         * Those rules are crashing ESLint at startup, so they are disabled for now.
+         */
+        'sonarjs/no-empty-collection': 'off',
+        'sonarjs/no-extra-arguments': 'off',
+        'sonarjs/no-gratuitous-expressions': 'off',
+        'sonarjs/no-one-iteration-loop': 'off',
+        'sonarjs/no-redundant-jump': 'off',
+        'sonarjs/no-unused-collection': 'off',
+        'sonarjs/no-use-of-empty-return-value': 'off',
+        'sonarjs/prefer-immediate-return': 'off',
 
-      /** User-defined rules */
-      ...options.rules,
+        /** User-defined rules */
+        ...options.rules,
+      },
     },
-  })
+  ]
 }
