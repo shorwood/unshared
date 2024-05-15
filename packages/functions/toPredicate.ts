8f45f7b9f5f404a95815498de27f196822da07c3
Stanley Horwood <stanley@hsjm.io>
2024-05-15 15:13:26 +0200

    feat(functions): enhance typing for `toPredicate` utility
    Updated the `toPredicate` utility in the `functions` package to support broader and more precise type handling. This improvement involves using generics to define the types of assertion functions more accurately, converting these to predicate functions that maintain type integrity.
    
    This change ensures that the utility can handle various assertion functions and return the appropriate predicate types, enhancing the utility's versatility and type safety in consumer code.

--- packages/functions/toPredicate.ts
+++ packages/functions/toPredicate.ts
@@ -1,5 +1,16 @@
-import { Assertor } from '@unshared/types'
-import { Predicator } from '@unshared/types'
+import { Function } from '@unshared/types'
+
+/**
+ * Converts a typed assertion function to a predicate function.
+ *
+ * @template T The type of the assertion function.
+ * @returns A function that predicates instead of asserts.
+ * @example ToPredicate<(value: unknown) => asserts value is string> // (value: unknown) => value is string
+ */
+export type ToPredicate<T extends Function> =
+  T extends (value: unknown, ...args: infer A) => asserts value is infer U
+    ? (value: unknown, ...args: A) => value is U
+    : (...args: Parameters<T>) => boolean
 
 /**
  * Given a typed assertion function, returns a predicate function
@@ -17,22 +28,27 @@ import { Predicator } from '@unshared/types'
  * // Convert the assertion function to a predicate.
  * const isString = toPredicate(assertIsString) // (value: unknown) => value is string
  */
-export function toPredicate<T>(fn: Assertor<T>): Predicator<T> {
-  return (value: unknown): value is T => {
+export function toPredicate<T extends Function>(fn: T): ToPredicate<T> {
+  return ((...args: unknown[]) => {
     try {
-      fn(value)
+      fn(...args)
       return true
     }
     catch {
       return false
     }
-  }
+  }) as ToPredicate<T>
 }
 
 /* v8 ignore start */
 if (import.meta.vitest) {
   function assertIsString(value: unknown): asserts value is string {
     if (typeof value !== 'string') throw new Error('Value is not a string')
+  }
+
+  function assertIsLowerThan(value: unknown, max: number): asserts value is number {
+    if (typeof value !== 'number') throw new Error('Value is not a number')
+    if (value >= max) throw new Error('Value is not lower than max')
   }
 
   test('should return true if value is a string', () => {
@@ -45,5 +61,21 @@ if (import.meta.vitest) {
     const isString = toPredicate(assertIsString)
     const result = isString(1)
     expect(result).toBe(false)
+  })
+
+  test('should pass additional arguments to the assertion function', () => {
+    const isLowerThan = toPredicate(assertIsLowerThan)
+    const result = isLowerThan(1, 2)
+    expect(result).toBe(true)
+  })
+
+  test('should return a type predicate', () => {
+    const isString = toPredicate(assertIsString)
+    expectTypeOf(isString).toEqualTypeOf<(value: unknown) => value is string>()
+  })
+
+  test('should return a type predicate with additional arguments', () => {
+    const isLowerThan = toPredicate(assertIsLowerThan)
+    expectTypeOf(isLowerThan).toEqualTypeOf<(value: unknown, max: number) => value is number>()
   })
 }
