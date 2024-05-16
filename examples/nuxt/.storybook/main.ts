7aa4b63077af112ce38062b644e39b0338f3a367
Stanley Horwood <stanley@hsjm.io>
2024-05-16 14:41:57 +0200

    chore(examples): update Storybook configuration and dependencies for Nuxt example
    Updated the Storybook configuration in the Nuxt example to include additional addons and refined the stories path to target components specifically. This enhances our Storybook integration, making it more aligned with our current component structure. Also, adjusted the Nuxt configuration to include essential modules and Storybook settings, ensuring a smoother development experience. Moreover, updated package.json to align with the latest compatible versions of dependencies and added scripts for running and building Storybook. These changes aim to maintain an updated development environment that facilitates easier onboarding and testing.

--- examples/nuxt/.storybook/main.ts
+++ examples/nuxt/.storybook/main.ts
@@ -1,21 +1,24 @@
-import type { StorybookConfig } from '@storybook-vue/nuxt'
+import type { StorybookConfig } from "@storybook-vue/nuxt";
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook-vue/nuxt',
+    name: "@storybook-vue/nuxt",
     options: {},
+  },
+  core: {
+    disableTelemetry: true,
   },
   docs: {
     autodocs: 'tag',
   },
-}
-export default config
+};
+export default config;
--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -7,12 +7,19 @@ export default defineNuxtConfig({
 
   modules: [
     '@unocss/nuxt',
+    '@vueuse/nuxt',
     '@unshared/vue',
+    '@nuxtjs/storybook',
   ],
 
   unocss: {
     preflight: true,
     configFile: 'uno.config.ts',
+  },
+
+  storybook: {
+    storybookRoute: '/_/storybook',
+    port: 6006,
   },
 
   vite: {
@@ -21,9 +28,5 @@ export default defineNuxtConfig({
         '@unshared/vue',
       ],
     },
-    // esbuild: {
-    //   minifySyntax: true,
-    //   define: { 'import.meta.vitest': 'false' },
-    // },
   },
 })
