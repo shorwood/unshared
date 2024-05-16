4565c77a67ecf06c2fa6460dc85f95da219d0f8b
Stanley Horwood <stanley@hsjm.io>
2024-05-17 01:13:37 +0200

    feat(vue): introduce `BaseInputList` component and `useBaseInputList` composable
    Added the `BaseInputList` component and `useBaseInputList` composable to the Vue package. This enhancement provides a flexible and reusable solution for creating input lists that can be easily integrated into forms and other UI elements. The new component supports customizable attributes, slots, and accessibility features, making it versatile for various use cases. The `useBaseInputList` composable offers detailed control over list behavior, including search functionality, selection handling, and custom value entries, enhancing the dynamic interaction capabilities of Vue applications.

---
+++ packages/vue/BaseInputList.ts
@@ -0,0 +1,301 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import { Prop, VNode, computed, getCurrentInstance, h, mergeProps, ref } from 'vue'
+import { refDebounced } from '@vueuse/core'
+import { BASE_STATE_OPTIONS, BaseStateOptions, useBaseState } from './useBaseState'
+import { BASE_RENDERABLE_OPTIONS, BaseRenderableOptions } from './useBaseRenderable'
+import { BASE_INPUT_LIST_OPTIONS, ListOption, UseBaseInputListOptions, useBaseInputList } from './useBaseInputList'
+import { exposeToDevtool } from './exposeToDevtool'
+import { DefineComponentContext, defineSetupComponent } from './defineSetupComponent'
+import { cleanAttributes } from './cleanAttributes'
+
+const BASE_INPUT_LIST_PROPS = {
+  ...BASE_STATE_OPTIONS,
+  ...BASE_RENDERABLE_OPTIONS,
+  ...BASE_INPUT_LIST_OPTIONS,
+  label: String,
+  native: Boolean,
+  classSearch: {},
+  classOption: {},
+  classOptions: {},
+  classValues: {},
+  classValue: {},
+} satisfies Record<keyof Props<any, any, any>, Prop<unknown>>
+
+interface Props<T, V, M extends boolean> extends
+  BaseStateOptions,
+  BaseRenderableOptions,
+  UseBaseInputListOptions<T, V, M> {
+
+  /**
+   * The label for the input list. This is used to create an accessible
+   * label for the input list.
+   *
+   * @example 'Select a color'
+   */
+  label?: string
+
+  /**
+   * If true, the input list will be a native input element. This will render
+   * a native `<select>` or `<datalist>` element instead of a custom list.
+   * This is useful when you want to use the native browser features for
+   * accessibility and usability.
+   *
+   * @default false
+   */
+  native?: boolean
+
+  /**
+   * The classes to apply to the search input. This can be used to style the
+   * search input field when the list is searchable and is a custom implementation.
+   *
+   * @example 'bg-white'
+   */
+  classSearch?: string
+
+  /**
+   * The classes to apply to the option element. This can be used to style the
+   * option elements when the list is either a native input or a custom
+   * implementation.
+   *
+   * @example 'bg-white text-black'
+   */
+  classOption?: string
+
+  /**
+   * The classes to apply to the options container. This can be used to style
+   * the options container when the list is either a native input or a custom
+   * implementation.
+   *
+   * @example 'bg-white text-black'
+   */
+  classOptions?: string
+
+  /**
+   * The classes to apply to the value container. This can be used to style
+   * the values container when the list is not a native input.
+   *
+   * @example 'bg-white text-black'
+   */
+  classValues?: string
+
+  /**
+   * The classes to apply to a single value. This can be used to style the
+   * value when the list is not a native input.
+   *
+   * @example 'bg-white text-black'
+   */
+  classValue?: string
+}
+
+/** The slots of the `BaseInputList` component. */
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type Slots<T, V> = {
+  default: (options: Array<ListOption<T, V>>) => VNode
+  search: (options: Array<ListOption<T, V>>) => VNode
+  option: (option: ListOption<T, V>) => VNode
+  values: (options: Array<ListOption<T, V>>) => VNode
+  value: (options: ListOption<T, V>) => VNode
+  empty: () => VNode
+}
+
+export const BaseInputList = defineSetupComponent(
+  <T, V, M extends boolean>(props: Props<T, V, M>, { attrs, slots }: DefineComponentContext<Slots<T, V>>) => {
+
+    // --- Initialize reactive properties.
+    const instance = getCurrentInstance()
+    const state = useBaseState(props, instance)
+    const input = useBaseInputList<T, V, M>(props, instance)
+    const is = computed(() => props.as ?? 'div')
+    const isOpen = ref(false)
+    const isOpenDebounced = refDebounced(isOpen, 20)
+
+    // --- Expose to Vue Devtools for debugging.
+    exposeToDevtool({ is, state, input })
+
+    // --- Get input props.
+    const attributes = computed(() => mergeProps(
+      attrs,
+      state.attributes,
+    ))
+
+    /**
+     * Create an <option> node for the input list. If a slot is provided, it will
+     * be used to create the node.
+     *
+     * @param option The option to create the option for.
+     * @returns The VNode for the `<option>` node.
+     */
+    function createOption(option: ListOption<T, V>): VNode {
+      return h('option', cleanAttributes({
+        'key': option.value,
+        'value': option.value,
+        'disabled': option.isDisabled || undefined,
+        'selected': option.isSelected,
+        'aria-disabled': option.isDisabled || undefined,
+        'aria-selected': option.isSelected || undefined,
+        'class': props.classOption,
+      }), option.text)
+    }
+
+    /**
+     * Create the VNode for the input list if the tag is a <select> or <input>
+     * with a list attribute.
+     *
+     * @returns The VNode for the input list.
+     */
+    function createSelect(): VNode {
+      const options = input.options.filter(option => option.isVisible).map(createOption)
+      return h('select', mergeProps(
+        attributes.value,
+        cleanAttributes({
+          multiple: props.multiple || undefined,
+          onInput: (event: { target: HTMLSelectElement } & InputEvent) => {
+            event.preventDefault()
+            event.stopPropagation()
+            // @ts-expect-error: `model` is a `V[]`.
+            input.model = props.multiple
+              ? [...event.target.selectedOptions].map(option => option.value as V)
+              : event.target.value as V
+          },
+        }),
+      ), options)
+    }
+
+    /**
+     * Create the VNode for the input list if the tag is a <datalist>.
+     *
+     * @returns The VNode for the input list.
+     */
+    // function createDatalist(): VNode {
+    //   const id = instance!.uid.toString()
+    //   const options = input.options.filter(option => option.isVisible).map(createOption)
+    //   return h('div', attributes.value, [
+    //     h('input', { list: `list-${id}` }),
+    //     h('datalist', { id: `list-${id}` }, options),
+    //   ])
+    // }
+
+    /**
+     * Create the VNode for the search input if the list is searchable.
+     *
+     * @returns The VNode for the search input.
+     */
+    function createSearchInput(): VNode {
+      if (slots.search) return slots.search(input.options)
+      return h('input', cleanAttributes({
+        type: 'text',
+        class: props.classSearch,
+        disabled: props.disabled,
+        readonly: props.readonly ?? !props.optionFilter,
+        value: input.search,
+        onFocusin: () => isOpen.value = true,
+        onFocusout: () => isOpen.value = false,
+        onKeydown: (event: KeyboardEvent) => {
+          if (event.key === 'Enter') {
+            event.preventDefault()
+            event.stopPropagation()
+            input.pushSearch()
+          }
+        },
+        onInput: (event: { target: HTMLInputElement } & InputEvent) => {
+          event.preventDefault()
+          event.stopPropagation()
+          input.search = event.target.value
+        },
+      }))
+    }
+
+    /**
+     * Create the VNode for the list of items in the input list.
+     *
+     * @returns The VNode for the list of items.
+     */
+    function createList() {
+      if (slots.default) return slots.default(input.options)
+
+      // --- Create the VNodes for the list of items.
+      const options = input.options
+        .filter(option => option.isVisible)
+        .map(option => h('li', cleanAttributes({
+          'role': 'option',
+          'disabled': option.isDisabled || undefined,
+          'selected': option.isSelected || undefined,
+          'aria-disabled': option.isDisabled || undefined,
+          'aria-selected': option.isSelected || undefined,
+          'class': props.classOption,
+          'onClick': () => option.toggle(),
+        }), slots.option?.(option) ?? option.text))
+
+      // --- If the list is empty, use the `empty` slot.
+      return h(
+        'ul',
+        { role: 'list', class: props.classOptions },
+        options.length === 0 ? slots.empty?.() : options,
+      )
+    }
+
+    /**
+     * Create a single VNode for the value of the input list.
+     *
+     * @param option The option to create the value for.
+     * @returns The VNode for the value of the input list.
+     */
+    function createListValue(option: ListOption<T, V>): VNode {
+      return h(
+        'span',
+        cleanAttributes({ class: props.classValue }),
+        slots.value?.(option) ?? option.text,
+      )
+    }
+
+    /**
+     * Create a VNode to display the current value of the input list.
+     *
+     * @returns The VNode for the value of the input list.
+     */
+    function createListValues(): VNode {
+      const vnodeValues = slots.values?.(input.selected) ?? input.selected.map(createListValue)
+      return h('div', cleanAttributes({ class: props.classValues }), vnodeValues)
+    }
+
+    /**
+     * Create the custom VNode for the input list if the tag is not a
+     * native input or select element. This will create a search input
+     * and a list of items.
+     *
+     * @returns The VNode for the input list.
+     */
+    function createListGroup(): VNode {
+      const vnodeSearch = createSearchInput()
+      const vnodeValue = createListValues()
+      const vnodeList = createList()
+      return h(
+        is.value,
+        mergeProps(
+          attrs,
+          state.attributes,
+          {
+            'tabindex': '0',
+            'role': 'listbox',
+            'aria-multiselectable': props.multiple,
+            'aria-expanded': isOpen.value,
+            'onFocus': (event: { target: HTMLElement } & Event) => event.target.querySelector('input')?.focus(),
+          },
+        ),
+        isOpenDebounced.value
+          ? [vnodeValue, vnodeSearch, vnodeList]
+          : [vnodeValue, vnodeSearch],
+      )
+    }
+
+    // --- Return virtual DOM node.
+    return () => {
+      if (!props.native) return createListGroup()
+      return createSelect()
+    }
+  },
+  {
+    name: 'BaseInputList',
+    props: BASE_INPUT_LIST_PROPS,
+  },
+)
---
+++ packages/vue/useBaseInputList.ts
@@ -0,0 +1,389 @@
+import { Prop, Ref, computed, getCurrentInstance, ref } from 'vue'
+import { toReactive, useVModel } from '@vueuse/core'
+
+/** The symbol to provide the `useBaseInputList` composable. */
+export const BASE_INPUT_LIST_SYMBOL = Symbol()
+
+/** The props when using the `useBaseInputList` composable. */
+export const BASE_INPUT_LIST_OPTIONS = {
+  'options': {},
+  'modelValue': {},
+  'onUpdate:modelValue': [Function, Array],
+  'search': String,
+  'onUpdate:search': [Function, Array],
+  'allowCustomValue': Boolean,
+  'optionValue': Function,
+  'optionLabel': Function,
+  'optionDisabled': Function,
+  'optionFilter': Function,
+  'multiple': Boolean,
+} satisfies Record<keyof UseBaseInputListOptions<any, any, any>, Prop<unknown>>
+
+/** The options for the `useBaseInputList` composable. */
+export interface UseBaseInputListOptions<
+  T,
+  V = T,
+  M extends boolean = boolean,
+> {
+
+  /**
+   * Items available for selection as an object or array. Each option should
+   * be different as to allow for unique values and avoid duplicates.
+   *
+   * @example { '1': { name: 'Alice' }, '2': { name: 'Bob' } }
+   */
+  options?: Record<PropertyKey, T> | T[]
+
+  /**
+   * The current value of the input. It can be a single value or an array of
+   * values if `multiple` is set to `true`.
+   *
+   * @example ['Alice', 'Bob']
+   */
+  modelValue?: M extends true ? V[] : V
+  'onUpdate:modelValue'?: (value: M extends true ? V[] : V) => void
+
+  /**
+   * The search query to filter options by. It can be used to filter the current
+   * list of options by the provided query or to search for options in a remote
+   * source.
+   *
+   * @example 'Alice'
+   */
+  search?: string
+  'onUpdate:search'?: (value: string) => void
+
+  /**
+   * If `true`, allow multiple options to be selected at once. If
+   * `false`, only one option can be selected at a time.
+   *
+   * @default false
+   */
+  multiple?: M
+
+  /**
+   * Allow the user to set the value from the search input. If `true`, custom
+   * value can be set by the user by pressing `Enter` when the search input is
+   * focused.
+   */
+  allowCustomValue?: boolean
+
+  /**
+   * Function that returns the value that should be used as the option's value.
+   * This value will be used when selecting the option and comparing it with the
+   * list of selected options.
+   *
+   * @example (option) => option.id
+   */
+  optionValue?: (option: T) => V
+
+  /**
+   * Function that returns the text that should be used as the option's text.
+   * This text will be displayed in the selection list and in the selected
+   * options list.
+   *
+   * @example (option) => option.name
+   */
+  optionLabel?: (option: T) => number | string
+
+  /**
+   * Function that returns if the option should be disabled. If `true`, the
+   * option will not be selectable and will be displayed as disabled.
+   *
+   * @example (option) => option.disabled
+   */
+  optionDisabled?: (option: T) => boolean
+
+  /**
+   * Function that returns if the option should be visible when a search query
+   * is provided. If `false`, the option will not be displayed in the list.
+   *
+   * @example (searchQuery, option) => option.name.startsWith(searchQuery)
+   */
+  optionFilter?: (searchQuery: string, option: T) => boolean
+}
+
+/** The `ListOption` object returned by the `useBaseInputList` composable. */
+export interface ListOption<T, V = T> {
+
+  /**
+   * The option object as provided in the `options` prop. If the options are an
+   * array, this will be the option at the current index. If the options are an
+   * object, this will be the value of the current key.
+   *
+   * @example { id: '0000-0000-0000-0000', name: 'Alice' }
+   */
+  option: T
+
+  /**
+   * The value of the option. If the `optionValue` function is provided, this will
+   * be the value returned by the function. If not, this will be the key of
+   * the option.
+   *
+   * @example 'Alice'
+   */
+  value: V
+
+  /**
+   * The text of the option. If the `optionLabel` function is provided, this will
+   * be the text returned by the function. If not, this will be the key of
+   * the option.
+   *
+   * @example 'Alice'
+   */
+  text: number | string
+
+  /**
+   * If the option is disabled. If the `optionDisabled` function is provided, this
+   * will be the value returned by the function. If not, this will be `false`.
+   * If `true`, the option will not be selectable and will be displayed as
+   *
+   * @example false
+   */
+  isDisabled: boolean
+
+  /**
+   * If the option is selected by comparing the option's value with the current
+   * model value. If `optionValue` is not defined, compare with the option's key.
+   * If `true`, the option will be displayed as selected.
+   *
+   * @example false
+   */
+  isSelected: boolean
+
+  /**
+   * If the option is visible. If the `optionFilter` function is provided, this
+   * will be the value returned by the function. If not, this will be `true`.
+   * If `false`, the option will not be displayed in the list.
+   *
+   * @example true
+   */
+  isVisible: boolean
+
+  /**
+   * Toggle the option by adding or removing the option's value from the model
+   * value. If `multiple` is `false`, set the model value to the option's value.
+   * If `selected` is `true`, remove the option's value from the model value.
+   *
+   * @example toggle() // => undefined
+   */
+  toggle: () => void
+
+  /**
+   * Add option's value to value array / set value to option's value.
+   *
+   * @example on() // => undefined
+   */
+  on: () => void
+
+  /**
+   * Remove option's value from value array.
+   *
+   * @example off() // => undefined
+   */
+  off: () => void
+}
+
+/** The composable properties returned by the `useBaseInputList` composable. */
+export interface UseBaseInputListComposable<T, V = T, M extends boolean = boolean> {
+
+  /**
+   * List of options available for selection. This is a collection of `ListOption`
+   * objects that keep track of the option's value, text, and state.
+   */
+  options: Array<ListOption<T, V>>
+
+  /**
+   * List of selected `ListOption` options. This allows you to keep track of the selected
+   * options even if they dissapear from the list of `options` due to a search
+   * query or a filter change.
+   */
+  selected: Array<ListOption<T, V>>
+
+  /**
+   * Current value of the input. If `multiple` is `true`, this will be an array
+   * of values. If `multiple` is `false`, this will be a single value or `undefined`.
+   *
+   * @example ['Alice', 'Bob']
+   */
+  model: M extends true ? V[] : V | undefined
+
+  /**
+   * The search query to filter options by or `undefined` if no search query is
+   * provided. This can be used to filter the list of options by the provided
+   */
+  search: string | undefined
+
+  /**
+   * Select an option by passing the value of the option. If `multiple` is `true`,
+   * add the option's value to the model value. Optionally, pass a boolean value
+   * to select or deselect the option.
+   *
+   * @param option The option to toggle.
+   * @param key The key of the option.
+   * @param value If `true`, remove the option's value from the model value.
+   * @example toggle(option, true) // => undefined
+   */
+  toggle: (option: T, value?: boolean) => void
+
+  /**
+   * Add a custom value to the model value by pressing `Enter` when the search
+   * input is focused. If `allowCustomValue` is `false`, this function will do
+   * nothing.
+   */
+  pushSearch: () => void
+
+  /**
+   * Deslect all selected options by setting the model value to an empty array
+   * or to `undefined` if `multiple` is `false`.
+   */
+  clear: () => void
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentInternalInstance {
+    [BASE_INPUT_LIST_SYMBOL]?: UseBaseInputListComposable<any, any, any>
+  }
+}
+
+export function useBaseInputList<T, V, M extends boolean>(options: UseBaseInputListOptions<T, V, M>, instance = getCurrentInstance()): UseBaseInputListComposable<T, V, M> {
+  if (instance?.[BASE_INPUT_LIST_SYMBOL]) return instance[BASE_INPUT_LIST_SYMBOL] as UseBaseInputListComposable<T, V, M>
+
+  // --- Initialize model value.
+  const emit = instance?.emit
+  const model = useVModel(options, 'modelValue', emit, { passive: true }) as Ref<V | V[] | undefined>
+  const search = useVModel(options, 'search', undefined, { passive: true })
+  const selected = ref([]) as Ref<Array<ListOption<T, V>>>
+
+  /**
+   * Check if the option is selected by comparing the option's value with the
+   * current model value. If `optionValue` is not defined, compare with the
+   * option's key.
+   *
+   * @param option The option to check.
+   * @returns `true` if the option is selected, `false` otherwise.
+   * @example isSelected(option) // => true
+   */
+  function isSelected(option: T): boolean {
+    const value = options.optionValue ? options.optionValue(option) : option
+    if (options.multiple) return Array.isArray(options.modelValue) && (options.modelValue).includes(value)
+    return options.modelValue === value
+  }
+
+  /**
+   * Check if the option is visible by comparing the option with the search query.
+   * If `optionFilter` is not defined, return `true`.
+   *
+   * @param searchQuery The search query to filter options by.
+   * @param option The option to check.
+   * @returns `true` if the option is visible, `false` otherwise.
+   * @example isVisible('Alice', option) // => true
+   */
+  function isVisible(searchQuery: string | undefined, option: T): boolean {
+    if (!searchQuery) return true
+    if (!options.optionFilter) return true
+    return options.optionFilter(searchQuery, option)
+  }
+
+  /**
+   * Clear the selected options by setting the model value to an empty array or
+   * to `undefined` if `multiple` is `false`.
+   */
+  function clear(): void {
+    model.value = options.multiple ? [] as V[] : undefined
+  }
+
+  /**
+   * Wrap the option in a `ListOption` object to keep track of the option's value,
+   * text, and state. Additionally, provide a `toggle` function to toggle the
+   * option's state.
+   *
+   * @param option The option to wrap.
+   * @returns The `ListOption` object.
+   */
+  function wrapOption(option: T): ListOption<T, V> {
+    return {
+      option,
+      value: options.optionValue?.(option) ?? option as unknown as V,
+      text: options.optionLabel?.(option) ?? option as unknown as number | string,
+      isDisabled: !!options.optionDisabled?.(option),
+      isSelected: isSelected(option),
+      isVisible: isVisible(search.value, option),
+      toggle: () => toggle(option),
+      off: () => toggle(option, false),
+      on: () => toggle(option, true),
+    }
+  }
+
+  /**
+   * Toggle the option by adding or removing the option's value from the model
+   * value. If `multiple` is `false`, set the model value to the option's value.
+   * If `selected` is `true`, remove the option's value from the model value.
+   *
+   * @param option The option to toggle.
+   * @param state The state to set the option to.
+   * @example toggle(option, true) // => undefined
+   */
+  function toggle(option: T, state?: boolean): void {
+    const value = options.optionValue ? options.optionValue(option) : option as unknown as V
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    const listOption = listOptions.value.find(option => option.value === value) ?? wrapOption(option)
+
+    // --- If not multiple, set value.
+    if (!options.multiple) {
+      model.value = state === false ? undefined : value
+      selected.value = state === false ? [] : [listOption]
+    }
+
+    // --- If multiple and value is already an array, push or remove value.
+    // --- Additionally, keep track of selected options in a separate array.
+    else if (Array.isArray(model.value)) {
+      model.value = state ?? !listOption.isSelected
+        ? [...model.value, value]
+        : [...model.value].filter(x => x !== value)
+      selected.value = state ?? !listOption.isSelected
+        ? [...selected.value, listOption]
+        : [...selected.value].filter(x => x.value !== value)
+    }
+
+    // --- Otherwise, initialize array with value.
+    else {
+      model.value = [value]
+      selected.value = [listOption]
+    }
+  }
+
+  /**
+   * Add a custom value to the model value by pressing `Enter` when the search
+   * input is focused. If `allowCustomValue` is `false`, this function will do
+   * nothing.
+   */
+  function pushSearch(): void {
+    if (!search.value) return
+    if (!options.allowCustomValue) return
+    for (const { value } of selected.value) if (value === search.value) return
+    // @ts-expect-error: Voluntary type mismatch.
+    toggle(search.value, search.value, true)
+    search.value = ''
+  }
+
+  // --- Computed list options
+  const listOptions = computed(() => {
+    if (!options.options) return []
+    void model.value
+    return Object.values(options.options).map(wrapOption)
+  })
+
+  const composables = toReactive({
+    options: listOptions,
+    model,
+    selected,
+    search,
+    clear,
+    toggle,
+    pushSearch,
+  }) as unknown as UseBaseInputListComposable<T, V, M>
+
+  if (instance) instance[BASE_INPUT_LIST_SYMBOL] = composables
+  return composables
+}
