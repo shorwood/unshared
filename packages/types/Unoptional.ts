ce627daa005c6ed02063c53af776eac27a12da87
Stanley Horwood <stanley@hsjm.io>
2024-05-25 13:17:42 +0200

    feat(types): introduce `Unoptional` type utility
    Introduced a new type utility `Unoptional` in the `types` package. This utility makes properties that are `undefined` or `void` optional while keeping other types unchanged. This enhancement aims to provide more precise type handling, especially beneficial in cases where optional chaining and default parameters are used frequently. The addition enhances the utility library by offering a more nuanced alternative to TypeScript's `Partial` type.

---
+++ packages/types/Unoptional.ts
@@ -0,0 +1,29 @@
+import { Pretty } from './Pretty'
+
+/**
+ * Make all `undefined` or `void` properties of T optional. This type is
+ * similar to `Partial` but it only makes properties optional if they are
+ * `undefined` or `void`. Meaning they non-`undefined` or non-`void` properties
+ * will remain required.
+ *
+ * @template T The type to make optional
+ * @returns A new type with all `undefined` or `void` properties of T optional
+ * @example Unoptional<{ a: string | undefined, b: number }> // { a?: string, b: number }
+ */
+export type Unoptional<T> = Pretty<
+  { [P in keyof T as undefined extends T[P] ? P : never]?: T[P]; } &
+  { [P in keyof T as undefined extends T[P] ? never : P]: T[P]; }
+>
+
+/* v8 ignore next */
+if (import.meta.vitest) {
+  test('should make all `undefined` properties of T optional', () => {
+    type Result = Unoptional<{ a: string | undefined; b: number }>
+    expectTypeOf<Result>().toEqualTypeOf<{ a?: string; b: number }>()
+  })
+
+  test('should make all `void` properties of T optional', () => {
+    type Result = Unoptional<{ a: string | void; b: number }>
+    expectTypeOf<Result>().toEqualTypeOf<{ a?: string | void | undefined; b: number }>()
+  })
+}
