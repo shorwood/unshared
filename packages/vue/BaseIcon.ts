3302383bd0163881b873c282358343433eafece8
Stanley Horwood <stanley@hsjm.io>
2024-05-29 18:57:03 +0200

    feat(vue): enhance `BaseIcon` component with dynamic icon handling and improved accessibility
    The `BaseIcon` component has been significantly enhanced to support dynamic icon types, including URLs to SVG files, icon fonts, and raw SVG content. It also introduces properties like `keepSize`, `load`, and `inline` to provide more control over icon appearance and behavior. For icons loaded from URLs, an asynchronous fetch mechanism has been implemented to securely fetch and embed SVG content, which helps in customizing icons further based on color and size.
    
    Additionally, the component now better supports accessibility standards by dynamically setting `aria-label` based on the icon type and using computed properties to manage the display and attributes of icons more effectively. These improvements make the `BaseIcon` component more versatile and user-friendly across different usage scenarios.

--- packages/vue/BaseIcon.ts
+++ packages/vue/BaseIcon.ts
@@ -1,5 +1,6 @@
-import { Prop, computed, h, mergeProps } from 'vue'
+import { Prop, computed, h, mergeProps, onMounted, ref, watch } from 'vue'
 import { BASE_RENDERABLE_OPTIONS, BaseRenderableOptions, useBaseRenderable } from './useBaseRenderable'
+import { exposeToDevtool } from './exposeToDevtool'
 import { DefineComponentContext, defineSetupComponent } from './defineSetupComponent'
 
 /** The base props for the `BaseIcon` component. */
@@ -7,18 +8,22 @@ export const BASE_ICON_PROPS = {
   ...BASE_RENDERABLE_OPTIONS,
   icon: String,
   label: String,
+  keepSize: Boolean,
+  load: Boolean,
+  inline: Boolean,
 } satisfies Record<keyof Props, Prop<unknown>>
 
 /** The properties of the `BaseIcon` component. */
 interface Props extends BaseRenderableOptions {
 
   /**
-   * The class name of the icon to display. This is expected to be a valid
-   * UnoCSS icon class name comming from the `@unocss/icons` package.
+   * The icon to display. It can be an URL to an SVG file, a class name of an
+   * icon font or the raw SVG content. If the icon is an URL, it must be prefixed
+   * with `https://`, `http://`, or `/`.
    *
-   * @example 'i-mdi:home'
+   * @example 'i-mdi:home' or 'https://example.com/icon.svg'
    */
-  icon: string
+  icon?: string
 
   /**
    * The label of the icon. This is used to set the `aria-label` attribute of the
@@ -26,22 +31,140 @@ interface Props extends BaseRenderableOptions {
    * the `icon` prop is used as the label.
    */
   label?: string
+
+  /**
+   * If `true`, keep the `width` and `height` attributes of the icon. This is
+   * useful when the icon is used as a background image.
+   *
+   * @default false
+   */
+  keepSize?: boolean
+
+  /**
+   * If `true` and the `icon` prop is an URL, the icon will be loaded using the
+   * `fetch` API. The result data will be used as the icon content. This allows
+   * more control over the icon content such as the color and size.
+   *
+   * Be aware that this can be a security risk if the URL is not trusted or if
+   * the URL is user-provided as the content is injected into the DOM.
+   *
+   * @default false
+   */
+  load?: boolean
+
+  /**
+   * If `true` the `display: inline-block;` style will be applied to the icon.
+   *
+   * @default false
+   */
+  inline?: boolean
 }
 
 export const BaseIcon = /* #__PURE__ */ defineSetupComponent(
   (props: Props, { attrs }: DefineComponentContext) => {
     const renderable = useBaseRenderable(props)
+    const iconSvg = ref<string>()
+
+    // --- Detect what kind of icon we have.
+    const type = computed(() => {
+      if (!props.icon) return 'none'
+      if (props.icon.startsWith('<svg')) return 'svg'
+      if (props.icon.startsWith('data:image/svg+xml')) return 'data-uri'
+      if (props.icon.startsWith('/')) return 'url'
+      if (props.icon.startsWith('http://')) return 'url'
+      if (props.icon.startsWith('https://')) return 'url'
+      return 'class'
+    })
+
+    // --- Compute the label based on the icon type.
+    const label = computed(() => {
+      if (props.label) return props.label
+      if (type.value === 'svg') return 'svg'
+      if (type.value === 'data-uri') return 'data-uri'
+      if (type.value === 'url') return props.icon!.match(/[^/]+([^#?]+)/)?.[1]
+      return props.icon
+    })
+
+    // --- Compute the tag based on the icon type.
+    const is = computed(() => {
+      if (props.as) return renderable.is
+      if (type.value === 'svg') return 'div'
+      if (type.value === 'url') return props.load ? 'div' : 'img'
+      if (type.value === 'data-uri') return 'img'
+      return 'i'
+    })
+
+    // --- Compute the inner HTML based on the icon type.
+    const innerHTML = computed(() => {
+      if (!props.icon) return
+      if (type.value === 'svg') return cleanSvgSize(props.icon)
+      if (type.value === 'url' && props.load) return iconSvg.value
+      return
+    })
+
+    // --- Compute the `src` attribute based on the icon type.
+    const source = computed(() => {
+      if (props.load) return
+      if (type.value === 'url' || type.value === 'data-uri') return props.icon
+      return
+    })
+
+    /**
+     * Load the icon content using the `fetch` API and use it as the inner HTML
+     * of the icon element. This allows more control over the icon content such
+     * as the color and size.
+     *
+     * @returns A promise that resolves when the icon content is loaded.
+     */
+    async function loadIcon(): Promise<void> {
+      if (!props.load) return iconSvg.value = undefined
+      if (!props.icon) return iconSvg.value = undefined
+      if (type.value !== 'url') return iconSvg.value = undefined
+      const response = await fetch(props.icon)
+      if (!response.ok) return
+      const svg = await response.text()
+      iconSvg.value = cleanSvgSize(svg)
+    }
+
+    /**
+     * Given an SVG string, remove the `width` and `height` attributes.
+     * This allows the icon to be styled using CSS without the need to
+     * override the `width` and `height` attributes.
+     *
+     * @param svg The SVG string to clean.
+     * @returns The cleaned SVG string.
+     */
+    function cleanSvgSize(svg: string) {
+      if (props.keepSize) return svg
+      return svg.replaceAll(/(width|height)="[^"]+"/g, '')
+    }
+
+    // --- Load the icon content if the `load` prop is `true`.
+    watch([() => props.icon], loadIcon)
+    onMounted(loadIcon)
 
     // --- Build the attributes.
     const attributes = computed(() => mergeProps(attrs, {
-      'role': 'img',
+      'role': is.value === 'img' ? undefined : 'img',
       'aria-hidden': 'true',
-      'aria-label': props.label ?? props.icon,
-      'class': props.icon,
+      'aria-label': label.value,
+      'innerHTML': innerHTML.value,
+      'class': type.value === 'class' ? props.icon : undefined,
+      'src': source.value,
+      'style': { display: props.inline ? 'inline-block' : 'block' },
     }))
 
+    // --- Expose properties.
+    exposeToDevtool({
+      is,
+      label,
+      source,
+      innerHTML,
+      iconSvg,
+    })
+
     // --- Return virtual DOM node.
-    return () => h(renderable.is ?? 'div', attributes.value)
+    return () => h(is.value ?? 'div', attributes.value)
   },
   {
     name: 'BaseIcon',
