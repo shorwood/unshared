6d40253bb2aa471d75d7444ab40bd983f2e2a6ed
Stanley Horwood <stanley@hsjm.io>
2024-05-30 21:10:27 +0200

    feat(validation): add `assertObject` function to validation package
    Introduced a new function `assertObject` in the `validation` package to validate that a given value is of type object. This function throws a `ValidationError` if the input value does not meet the criteria, enhancing robustness in data validation by ensuring that only object type values are processed where expected. This update is essential for developers needing to enforce data integrity at runtime, especially when dealing with user inputs or API responses that must satisfy specific object structure criteria.

---
+++ packages/validation/assert/assertObject.ts
@@ -0,0 +1,57 @@
+import { kindOf } from '@unshared/functions/kindOf'
+import { ValidationError } from '../ValidationError'
+
+/**
+ * Assert that a value is an object.
+ *
+ * @param value The value to assert as an object.
+ * @throws `ValidationError` if the value is not an object.
+ * @example assertObject({}) // void
+ */
+export function assertObject<T extends object>(value: unknown): asserts value is T {
+  if (kindOf(value) === 'object') return
+  throw new ValidationError({
+    name: 'E_NOT_OBJECT',
+    message: `Expected value to be an object but received: ${kindOf(value)}`,
+  })
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  test('should pass if value is an object', () => {
+    const result = assertObject({})
+    expect(result).toBeUndefined()
+  })
+
+  test('should throw if value is not an object', () => {
+    const shouldThrow = () => assertObject([])
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an object but received: Array')
+  })
+
+  test('should throw if value is undefined', () => {
+    // eslint-disable-next-line unicorn/no-useless-undefined
+    const shouldThrow = () => assertObject(undefined)
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an object but received: undefined')
+  })
+
+  test('should throw if value is null', () => {
+    // eslint-disable-next-line unicorn/no-null
+    const shouldThrow = () => assertObject(null)
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an object but received: null')
+  })
+
+  test('should predicate an object', () => {
+    const value = {} as unknown
+    assertObject(value)
+    expectTypeOf(value).toEqualTypeOf<object>()
+  })
+
+  test('should predicate an object of Record<string, unknown> if a generic is provided', () => {
+    const value = {} as unknown
+    assertObject<Record<string, unknown>>(value)
+    expectTypeOf(value).toEqualTypeOf<Record<string, unknown>>()
+  })
+}
