bc6aa874cc4b6bcdf77c0f62fdcd3a619679b1e4
Stanley Horwood <stanley@hsjm.io>
2024-05-31 04:49:02 +0200

    feat(validation): introduced `assertInstance` and related functions
    Added new functions `assertInstance` and `createAssertInstance` in the validation package to enhance type checking capabilities. These functions allow for asserting that a value is an instance of a specified class, throwing a `ValidationError` if the assertion fails. This feature aids in enforcing type safety throughout the application by ensuring that values match expected types at runtime.
    
    The implementation includes comprehensive tests to ensure the functionality works as expected under various scenarios, including edge cases where values are undefined or null. This addition aims to streamline type validation processes and improve code reliability by providing clearer, type-specific assertions.

---
+++ packages/validation/assert/assertInstance.ts
@@ -0,0 +1,58 @@
+import { Constructor } from '@unshared/types'
+import { kindOf } from '@unshared/functions/kindOf'
+import { ValidationError } from '../ValidationError'
+
+/**
+ * Assert that a value is an instance of the given class.
+ *
+ * @param value The value to assert as an instance of the given class.
+ * @param ctor The class to assert the value is an instance of.
+ * @throws `ValidationError` if the value is not an instance of the given class.
+ * @example assertInstance(new Date(), Date) // void
+ */
+export function assertInstance<T extends object>(value: unknown, ctor: Constructor<T>): asserts value is T {
+  if (value instanceof ctor) return
+  throw new ValidationError({
+    name: 'E_NOT_INSTANCE_OF',
+    message: `Expected value to be an instance of ${ctor.name} but received: ${kindOf(value)}`,
+  })
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  test('should pass if value is an instance of the given class', () => {
+    const result = assertInstance(new Date(), Date)
+    expect(result).toBeUndefined()
+  })
+
+  test('should throw if value is not an instance of the given class', () => {
+    const shouldThrow = () => assertInstance([], Date)
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an instance of Date but received: Array')
+  })
+
+  test('should throw if value is undefined', () => {
+    const shouldThrow = () => assertInstance(undefined, Date)
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an instance of Date but received: undefined')
+  })
+
+  test('should throw if value is null', () => {
+    // eslint-disable-next-line unicorn/no-null
+    const shouldThrow = () => assertInstance(null, Date)
+    expect(shouldThrow).toThrow(ValidationError)
+    expect(shouldThrow).toThrow('Expected value to be an instance of Date but received: null')
+  })
+
+  test('should predicate an instance of the given class', () => {
+    const value = new Date() as unknown
+    assertInstance(value, Date)
+    expectTypeOf(value).toEqualTypeOf<Date>()
+  })
+
+  test('should predicate an instance of the given class if a generic is provided', () => {
+    const value = new Date() as unknown
+    assertInstance<Date>(value, Date)
+    expectTypeOf(value).toEqualTypeOf<Date>()
+  })
+}
---
+++ packages/validation/assert/createAssertInstance.ts
@@ -0,0 +1,26 @@
+import { Constructor } from '@unshared/types'
+import { assertInstance } from './assertInstance'
+
+/**
+ * Create an assertion function that asserts a value is an instance of the given class.
+ *
+ * @param ctor The class to assert the value is an instance of.
+ * @returns An assertion function that asserts a value is an instance of the given class.
+ * @example const assertDate = createAssertInstance(Date) // (value: unknown) => asserts value is Date
+ */
+export function createAssertInstance<T extends object>(ctor: Constructor<T>) {
+  return (value: unknown): asserts value is T => assertInstance(value, ctor)
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  test('should pass if value is a string ending with the given string', () => {
+    const result = createAssertInstance(Date)(new Date())
+    expect(result).toBeUndefined()
+  })
+
+  test('should predicate a string ending with the given string', () => {
+    const result = createAssertInstance(Date)
+    expectTypeOf(result).toEqualTypeOf<(value: unknown) => asserts value is Date>()
+  })
+}
