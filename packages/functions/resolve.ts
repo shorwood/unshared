f10c40ea79e276eb4acc604253ad2a7c5b01ddf0
Stanley Horwood <stanley@hsjm.io>
2024-07-07 03:46:48 +0200

    feat(functions): add `resolve` utility function
    Introduced a new utility function `resolve` in the `functions` package. This function is designed to handle values that may either be direct values, promises, or functions returning promises. It simplifies the process of resolving these different types of values, providing a unified approach to handle asynchronous and synchronous operations seamlessly. This addition enhances the module's capability to deal with complex data resolution scenarios in a more efficient way.

---
+++ packages/functions/resolve.ts
@@ -0,0 +1,79 @@
+import { MaybeFunction, MaybePromise } from '@unshared/types'
+
+/**
+ * A value that contains a value as the result of a function or a promise.
+ *
+ * @template T The type of the value.
+ * @template P The type of the arguments of the function.
+ * @example MaybeResolvable<string> // MaybeFunction<MaybePromise<T>>
+ */
+export type MaybeResolvable<T, P extends any[] = []> = MaybeFunction<MaybePromise<T>, P>
+
+/**
+ * Call and resolve a value that may be a function or a promise or
+ * both. The value is resolved by calling the function if it is a
+ * function and / or by awaiting the promise if it is a promise.
+ *
+ * @param value The value to call and resolve.
+ * @param args The arguments to pass to the function.
+ * @returns The resolved value.
+ * @example
+ *
+ * // Return the value directly
+ * const value1 = resolve('Hello, World!') // 'Hello, World!'
+ *
+ * // Return the value by calling the function
+ * const value2 = resolve(() => 'Hello, World!') // 'Hello, World!'
+ *
+ * // Return the value by awaiting the promise
+ * const value3 = resolve(Promise.resolve('Hello, World!')) // 'Hello, World!'
+ *
+ * // Return the value by calling the function and awaiting the promise
+ * const value4 = resolve(() => Promise.resolve('Hello, World!')) // 'Hello, World!'
+ */
+export async function resolve<T, P extends any[]>(value: MaybeResolvable<T, P>, ...args: P): Promise<T> {
+  let result = value
+
+  // --- Call the function if it is a function
+  // @ts-expect-error: The value is a function
+  if (typeof value === 'function') result = value(...args) as MaybePromise<T>
+
+  // --- Await the promise if it is a promise
+  // @ts-expect-error: The value is a promise
+  if (result instanceof Promise) result = await result as T
+
+  // --- Return the result
+  return result as T
+}
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  test('should return the value directly', async() => {
+    const value = await resolve('Hello, World!')
+    expect(value).toBe('Hello, World!')
+  })
+
+  test('should return the value by awaiting the promise', async() => {
+    const contained = Promise.resolve('Hello, World!')
+    const value = await resolve(contained)
+    expect(value).toBe('Hello, World!')
+  })
+
+  test('should return the value by calling the function', async() => {
+    const contained = () => 'Hello, World!'
+    const value = await resolve(contained)
+    expect(value).toBe('Hello, World!')
+  })
+
+  test('should return the value by calling the function and awaiting the promise', async() => {
+    const contained = () => Promise.resolve('Hello, World!')
+    const value = await resolve(contained)
+    expect(value).toBe('Hello, World!')
+  })
+
+  test('should return the value by calling the function with arguments', async() => {
+    const contained = (name: string) => `Hello, ${name}!`
+    const value = await resolve(contained, 'World')
+    expect(value).toBe('Hello, World!')
+  })
+}
