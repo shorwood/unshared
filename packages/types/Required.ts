b24c2e2d616e539afa822cbffd9821a44321caa8
Stanley Horwood <stanley@hsjm.io>
2024-07-08 02:40:04 +0200

    feat(types): add `Required` type and update exports
    Introduced a new utility type `Required` that enhances TypeScript's built-in `Required` utility by allowing a subset of properties to be specified as required. This utility uses the `Loose` type internally to achieve non-nullability of selected properties. Additionally, updated the exports in the `index.ts` file to include the new `Required` type and adjusted the package configuration to reflect these changes. The inclusion of `PartialStrict` and removal of `Optional` and `Unoptional` are aligned with previous refactoring for consistency across utility types.

---
+++ packages/types/Required.ts
@@ -0,0 +1,29 @@
+import { Loose } from './Loose'
+
+/**
+ * Make all or some properties in T required and non-nullable.
+ * This utility is similar to the built-in `Required` utility in TypeScript
+ * but allows specifying a subset of properties to be required
+ * instead of requiring all properties.
+ *
+ * @template T The type to make some properties required.
+ * @template K The subset of properties to make required.
+ * @example Required<{ a?: string, b?: string }, 'a'> // { a: string, b?: string }
+ */
+export type Required<T, K extends keyof T = keyof T> =
+  Loose<{ [P in Exclude<keyof T, K>]: T[P] } & { [P in K]-?: T[P] }>
+
+/* v8 ignore start */
+if (import.meta.vitest) {
+  test('should make all properties required', () => {
+    type Actual = Required<{ a?: string; b?: string }>
+    interface Expected { a: string; b: string }
+    expectTypeOf<Actual>().toEqualTypeOf<Expected>()
+  })
+
+  test('should make some properties required', () => {
+    type Actual = Required<{ a?: string; b?: string }, 'a'>
+    interface Expected { a: string; b?: string }
+    expectTypeOf<Actual>().toEqualTypeOf<Expected>()
+  })
+}
