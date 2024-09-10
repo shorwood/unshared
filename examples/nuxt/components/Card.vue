1f5717a1768254f289d61a13c284406bc56a870d
Stanley Horwood <stanley@hsjm.io>
2024-09-10 02:39:24 +0200

    chore(nuxt): enhance interaction and styling of `Card` component
    Refactored the `Card` component to include an expandable content section controlled by a toggle. Implemented the toggle functionality using `isOpen` reactive property and updated the template to use `BaseButton` for the header and `BaseCollapse` for content animation. These changes improve the component's interactivity and visual coherence, making it more flexible and user-friendly in UI layouts.

--- examples/nuxt/components/Card.vue
+++ examples/nuxt/components/Card.vue
@@ -2,15 +2,37 @@
 defineProps<{
   title?: string
 }>()
+
+const isOpen = ref(false)
 </script>
 
 <template>
-  <div class="bg-blue/5 border border-blue/10 p-4 rounded-lg w-full h-full">
-    <h3 class="text-lg font-bold mb-4">
-      {{ title }}
-    </h3>
-    <div class="flex space-x-3">
-      <slot></slot>
-    </div>
+  <div class="bg-blue/5 border border-blue/10 rounded-lg w-full h-full">
+
+    <!-- Header -->
+    <BaseButton
+      as="h3"
+      class="flex items-center space-x-4 cursor-pointer p-4"
+      @click="() => { isOpen = !isOpen }">
+      <span class="text-lg font-bold">
+        {{ title }}
+      </span>
+      <BaseIcon
+        icon="i-carbon:chevron-down"
+        class="transform transition-transform"
+        :class="{ 'rotate-180': isOpen }"
+      />
+    </BaseButton>
+
+    <!-- Content -->
+    <BaseCollapse
+      vertical
+      :isOpen="isOpen"
+      :class="{ 'opacity-0': !isOpen }"
+      class="transition-all ease-in-out duration-200">
+      <div class="flex space-x-3 p-4 pt-px">
+        <slot/>
+      </div>
+    </BaseCollapse>
   </div>
 </template>
