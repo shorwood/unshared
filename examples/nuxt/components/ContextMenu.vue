230448075220be371fd9ab1336f4c46858b54d49
Stanley Horwood <stanley@hsjm.io>
2024-09-10 02:39:14 +0200

    chore(nuxt): update `ContextMenu` component styling and interaction
    Updated the `ContextMenu` component in the Nuxt examples to enhance its usability and visual appeal. Changed the position of the menu to appear below and to the left of the trigger area, improving accessibility and intuitiveness. Additionally, replaced the previous transition implementation with `BaseCollapse` for smoother animations and added conditional classes to manage the visual state based on whether the menu is open. This ensures a more consistent and visually appealing interaction for users.

--- examples/nuxt/components/ContextMenu.vue
+++ examples/nuxt/components/ContextMenu.vue
@@ -3,30 +3,35 @@ import { BaseMenu } from '@unshared/vue/BaseMenu'
 </script>
 
 <template>
-  <BaseMenu x="after" y="center">
+  <BaseMenu x="left" y="below">
 
+    <!-- Trigger -->
     <template #default="{ isOpen, open }">
-      <div @contextmenu.prevent="() => open()">
+      <div
+        class="bg-blue/5 border border-blue/10 rounded-lg"
+        :class="{ '!bg-blue/10': isOpen }"
+        @contextmenu.prevent="() => open()">
         <slot/>
       </div>
     </template>
 
+    <!-- Menu -->
     <template #menu="{ close, isOpen }">
-      <Transition
-        name="fade"
-        :duration="200"
-        enter-active-class="transition duration-100"
-        enter-from-class="opacity-0 translate-x-1/2"
-        enter-to-class="opacity-100"
-        leave-active-class="transition duration-150"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0 translate-x-1/2">
-
-        <p v-if="isOpen" class="p-4 bg-red/20">
-          <button class="bg-red" @click="() => close()">Close</button>
-        </p>
-
-      </Transition>
+      <BaseCollapse
+        vertical
+        :isOpen="isOpen"
+        :class="{ 'opacity-0': !isOpen }"
+        class="transition-all ease-in-out duration-100 bg-blue/5 border border-blue/10 rounded-lg backdrop-blur-2xl">
+        <div class="flex flex-col space-y-1 p-4 w-48">
+          <BaseButton
+            v-for="i in 5"
+            :key="i"
+            class="block w-full p-2 text-left whitespace-nowrap hover:bg-blue/10 rounded-lg"
+            @click="() => { close(); return 1 }">
+            Item {{ i }}
+          </BaseButton>
+        </div>
+      </BaseCollapse>
     </template>
   </BaseMenu>
 </template>
