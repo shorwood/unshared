7f204bc4593e5f931922beb440530e32f31e7291
Stanley Horwood <stanley@hsjm.io>
2024-10-26 05:26:59 +0200

    chore(types): enhance `Assertor` type with generic parameter support
    The `Assertor` type now supports a generic parameter for additional arguments. This enhancement allows for more flexible assertion functions that can take extra parameters. The update introduces a default value for the parameter array, maintaining backward compatibility while offering extended functionality. This change is reflected in the updated tests which demonstrate how to use the new parameter feature for boolean assertions with an additional string parameter.

--- packages/types/Assertor.ts
+++ packages/types/Assertor.ts
@@ -4,7 +4,7 @@
  * @template T The type to assert the value is.
  * @example Assertor<string> = (value: unknown) => asserts value is string
  */
-export type Assertor<T> = (value: unknown, ...args: any[]) => asserts value is T
+export type Assertor<T, P extends any[] = any[]> = (value: unknown, ...args: P) => asserts value is T
 
 /** v8 ignore start */
 if (import.meta.vitest) {
@@ -16,5 +16,10 @@ if (import.meta.vitest) {
   test('should give a function that asserts a value is a number', () => {
     type Result = Assertor<number>
     expectTypeOf<Result>().toEqualTypeOf<(value: unknown, ...args: any[]) => asserts value is number>()
+  })
+
+  test('should give a function that asserts a value is a boolean and takes a parameter', () => {
+    type Result = Assertor<boolean, [other: string]>
+    expectTypeOf<Result>().toEqualTypeOf<(value: unknown, other: string) => asserts value is boolean>()
   })
 }
