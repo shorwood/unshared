fb1cf012eda76f1d8d889c5902aeb5f8cb351cbf
Stanley Horwood <stanley@hsjm.io>
2024-10-27 06:16:49 +0100

    chore(eslint-config): streamline SonarJS rules configuration
    Refactored SonarJS configuration in `eslint-config` package to simplify and clarify the rules setup. Removed unnecessary comments and legacy configurations that were redundant with TypeScript implementations. Also adjusted thresholds for cognitive complexity and duplicate strings to better align with current project standards. This change aims to enhance the maintainability and readability of SonarJS rules without impacting existing functionality.

--- packages/eslint-config/configs/sonarjs.ts
+++ packages/eslint-config/configs/sonarjs.ts
@@ -1,48 +1,32 @@
 import type { Linter } from 'eslint'
-import type { ESLintConfigOptions } from './all'
 import pluginSonarjs from 'eslint-plugin-sonarjs'
 
-export function sonarjs(options: ESLintConfigOptions): Linter.FlatConfig[] {
+/**
+ * @see https://github.com/SonarSource/SonarJS/blob/master/packages/jsts/src/rules/README.md
+ * @returns ESLint Config for SonarJS.
+ */
+export function sonarjs(): Linter.Config[] {
   return [
-    pluginSonarjs.configs.recommended as Linter.FlatConfig,
+    pluginSonarjs.configs.recommended as Linter.Config,
     {
       rules: {
 
-        /**
-         * Cognitive Complexity is a measure of how hard the control flow of a function
-         * is to understand. Functions with high Cognitive Complexity will be difficult
-         * to maintain.
-         *
-         * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/cognitive-complexity.md
-         */
+        // --- Increase the default threshold to 30.
         'sonarjs/cognitive-complexity': ['error', 30],
 
-        /**
-         * Duplicated string literals make the process of refactoring error-prone,
-         * since you must be sure to update all occurrences. On the other hand,
-         * constants can be referenced from many places, but only need to be
-         * updated in a single place.
-         *
-         * @see https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/no-duplicate-string.md
-         */
-        'sonarjs/no-duplicate-string': ['error', {
-          threshold: 10,
-        }],
+        // --- Increase the default threshold to 10.
+        'sonarjs/no-duplicate-string': ['error', { threshold: 10 }],
 
-        /**
-         * Those rules are crashing ESLint at startup, so they are disabled for now.
-         */
-        'sonarjs/no-empty-collection': 'off',
-        'sonarjs/no-extra-arguments': 'off',
-        'sonarjs/no-gratuitous-expressions': 'off',
-        'sonarjs/no-one-iteration-loop': 'off',
-        'sonarjs/no-redundant-jump': 'off',
-        'sonarjs/no-unused-collection': 'off',
+        // --- We may want to use `undefined` as a return value.
         'sonarjs/no-use-of-empty-return-value': 'off',
-        'sonarjs/prefer-immediate-return': 'off',
 
-        /** User-defined rules */
-        ...options.rules,
+        // --- Disable redundant with TypeScript.
+        'sonarjs/function-return-type': 'off',
+        'sonarjs/different-types-comparison': 'off',
+
+        // --- Allow control characters in regex.
+        'no-control-regex': 'off',
+        'sonarjs/sonar-no-control-regex': 'off',
       },
     },
   ]
