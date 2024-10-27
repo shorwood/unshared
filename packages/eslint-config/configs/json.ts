f9bb4f12980fbaaa536321a3603904eb427193db
Stanley Horwood <stanley@hsjm.io>
2024-10-27 05:56:33 +0100

    chore(eslint-config): streamline and update json config to use flat presets
    Simplified the JSON configuration in the `eslint-config` package by removing specific file targeting and redundant comments. The update includes using `eslint-plugin-jsonc` with its `flat/recommended-with-json` preset. This change enhances clarity and maintains consistency with the new flat config standards applied across other configurations in the project. Reduced complexity in the configuration setup aligns better with modern JSON handling practices.

--- packages/eslint-config/configs/json.ts
+++ packages/eslint-config/configs/json.ts
@@ -1,31 +1,18 @@
 import type { Linter } from 'eslint'
-import type { ESLintConfigOptions } from './all'
-import jsonc from 'eslint-plugin-jsonc'
+import pluginJsonc from 'eslint-plugin-jsonc'
 
 /**
- * @param options The configuration options.
  * @returns The configuration for JSON files.
  * @see https://ota-meshi.github.io/eslint-plugin-jsonc/
  */
-export function configJson(options: ESLintConfigOptions): Linter.FlatConfig[] {
+export function json(): Linter.Config[] {
   return [
-    ...jsonc.configs['flat/recommended-with-json'],
+    ...pluginJsonc.configs['flat/recommended-with-json'],
     {
-      files: [
-        '**/*.json',
-        '**/*.json5',
-      ],
       rules: {
 
-        /**
-         * Automatically apply jsonc rules similar to your configured ESLint core rules to JSON.
-         *
-         * @see https://ota-meshi.github.io/eslint-plugin-jsonc/rules/auto.html
-         */
+        // --- Automatically apply jsonc rules similar to your configured ESLint core rules to JSON.
         'jsonc/auto': 'error',
-
-        /** User-defined rules */
-        ...options.rules,
       },
     },
   ]
