2f76cce341ff9d2164ef8b505b58ebd6d35b6fb7
Stanley Horwood <stanley@hsjm.io>
2024-10-28 20:56:52 +0100

    chore: enhance test setup with additional mocks
    Added mocks for both the `fs` and `fs/promises` modules as well as the `vue-router` package in the setupTest.ts file. This allows for better isolation in tests and prevents side effects from interacting with the file system or router state. Now, each test run will simulate a consistent and controlled environment, enhancing the reliability of our test suite.

--- packages/setupTest.ts
+++ packages/setupTest.ts
@@ -1,8 +1,17 @@
 import { fs, vol } from 'memfs'
 
 beforeEach(() => {
+
+  // --- Mock the `fs` and `fs/promises` modules.
   vi.mock('node:fs', () => fs)
   vi.mock('node:fs/promises', () => fs.promises)
+
+  // --- Mock the `vue-router` package.
+  vi.mock('vue-router', async() => {
+    const VueRouter: typeof import('vue-router') = await vi.importActual('vue-router')
+    const router = VueRouter.createRouter({ history: VueRouter.createMemoryHistory(), routes: [{ path: '/', component: { template: '<div></div>' } }] })
+    return { ...VueRouter, router, useRoute: () => router.currentRoute, useRouter: () => router }
+  })
 })
 
 afterEach(() => {
