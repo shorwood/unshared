64e3233cbd2e140d65e649b73440030bf2167d90
Stanley Horwood <stanley@hsjm.io>
2024-11-22 19:13:26 +0100

    feat(client): enhance response handling and error management in `createClient`
    Updated the `createClient` function to handle API responses more effectively, ensuring that successful responses are parsed as JSON and appropriate errors are thrown for non-successful responses based on the OpenAPI response descriptions. This change improves the reliability and maintainability of the client by providing clearer error messages and handling different response scenarios robustly.

--- packages/client/createClient.ts
+++ packages/client/createClient.ts
@@ -6,19 +6,24 @@ import { getOperationById } from './openapi/getOperationById'
 import { getBaseUrl, type OpenAPIV2, type OpenAPIV3 } from './openapi/index'
 
 type ClientBaseUrl<T> =
-  MaybeLiteral<T extends V2.Document ? OpenAPIV2.ServerUrl<T>
-    : T extends V3.Document ? OpenAPIV3.ServerUrl<T>
-      : T extends V3_1.Document ? OpenAPIV3.ServerUrl<T>
-        : string>
+  MaybeLiteral<
+    T extends V2.Document ? OpenAPIV2.ServerUrl<T>
+      : T extends V3.Document ? OpenAPIV3.ServerUrl<T>
+        : T extends V3_1.Document ? OpenAPIV3.ServerUrl<T>
+          : string
+  >
 
 type ClientFetch<T> =
-  T extends V2.Document ? OpenAPIV2.Fetch<T>
-    : T extends V3.Document ? OpenAPIV3.Fetch<T>
-      : T extends V3_1.Document ? OpenAPIV3.Fetch<T>
+  T extends V2.Document ? <P extends OpenAPIV2.Route<T>>(name: P, options: OpenAPIV2.RequestInit<OpenAPIV2.OperationByRoute<T, P>>) => Promise<OpenAPIV2.Response<OpenAPIV2.OperationByRoute<T, P>>>
+    : T extends V3.Document ? <P extends OpenAPIV2.Route<T>>(name: P, options: OpenAPIV3.RequestInit<T, OpenAPIV2.OperationByRoute<T, P>>) => Promise<OpenAPIV3.Response<OpenAPIV2.OperationByRoute<T, P>>>
+      : T extends V3_1.Document ? <P extends OpenAPIV2.Route<T>>(name: P, options: OpenAPIV3.RequestInit<T, OpenAPIV2.OperationByRoute<T, P>>) => Promise<OpenAPIV3.Response<OpenAPIV2.OperationByRoute<T, P>>>
         : typeof globalThis.fetch
 
 type ClientFetchOperation<T, U extends OpenAPIV2.OperationId<T>> =
-  (options: OpenAPIV2.RequestInit<OpenAPIV2.OperationById<T, U>>) => Promise<OpenAPIV2.Response<OpenAPIV2.OperationById<T, U>>>
+  T extends V2.Document ? (options: OpenAPIV2.RequestInit<OpenAPIV2.OperationById<T, U>>) => Promise<OpenAPIV2.ResponseBody<OpenAPIV2.OperationById<T, U>>>
+    : T extends V3.Document ? (options: OpenAPIV3.RequestInit<T, OpenAPIV2.OperationById<T, U>>) => Promise<OpenAPIV3.ResponseBody<OpenAPIV2.OperationById<T, U>>>
+      : T extends V3_1.Document ? (options: OpenAPIV3.RequestInit<T, OpenAPIV2.OperationById<T, U>>) => Promise<OpenAPIV3.ResponseBody<OpenAPIV2.OperationById<T, U>>>
+        : (options: RequestOptions) => Promise<Response>
 
 export type Client<T = OpenAPI.Document> =
   Pretty<
@@ -26,7 +31,7 @@ export type Client<T = OpenAPI.Document> =
     & { fetch: ClientFetch<T> }
   >
 
-export type ClientOptions<T = OpenAPI.Document> = Pretty<Override<RequestOptions, {
+export type ClientOptions<T> = Pretty<Override<RequestOptions, {
   baseUrl?: ClientBaseUrl<T>
 
   /**
@@ -44,17 +49,32 @@ export function createClient<T extends OpenAPI.Document>(url: ClientBaseUrl<T>, 
 export function createClient(documentOrUrl: Readonly<OpenAPI.Document> | string, initialOptions: ClientOptions<any> = {}): Client {
   const specifications = typeof documentOrUrl === 'string' ? undefined : documentOrUrl
 
-  function fetchByOperationId(operationId: string, options: Record<string, unknown>) {
+  async function fetchByOperationId(operationId: string, options: ClientOptions<any>) {
     if (!specifications) throw new Error('No OpenAPI specification provided.')
-    const operation = getOperationById(specifications, operationId)
+    const operation = getOperationById(specifications, operationId) as { method: string; path: string } & OpenAPI.Operation
     if (!operation) throw new Error(`Operation ID "${operationId}" not found.`)
-    const { method, path } = operation
-    return fetch(path, {
+    const { method, path, responses = {} } = operation
+    const response = await fetch(path, {
       method,
       baseUrl: getBaseUrl(specifications),
       ...initialOptions,
       ...options,
     })
+
+    // --- Return the JSON response if successful.
+    if (response.ok) return response.json() as Promise<unknown>
+
+    // --- Throw an error if the response was not successful.
+    const status = response.status.toString()
+    if (status in responses
+      && typeof responses[status] === 'object'
+      && responses[status] !== null
+      && 'description' in responses[status]
+      && typeof responses[status].description === 'string')
+      throw new Error(responses[status].description)
+
+    // --- Throw a generic error if the response was not successful.
+    throw new Error(response.statusText)
   }
 
   return new Proxy({}, {
