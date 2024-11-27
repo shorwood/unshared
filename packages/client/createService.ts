3aeabf72b9c16ee999db5736050ce053fa332229
Stanley Horwood <stanley@hsjm.io>
2024-11-27 18:26:07 +0100

    feat(client): add `createService` function for OpenAPI powered service creation
    Introduced a new `createService` function in the `client` package that facilitates the creation of service instances based on OpenAPI specifications. This function utilizes dynamic proxy generation to handle API operations dynamically, improving the ease of integration with various APIs by automatically handling operation resolutions, requests, and responses based on the provided OpenAPI document. This enhancement aims to streamline the development process for applications interfacing with different backend services through standardized OpenAPI contracts.

---
+++ packages/client/createService.ts
@@ -0,0 +1,69 @@
+import type { MaybeLiteral } from '@unshared/types'
+import type { OpenAPIV3, OperationById, OperationId, OperationOptions, OperationResult, ServerUrl } from './openapi'
+import type { RequestOptions } from './utils/request'
+import { getServerUrl } from './openapi/getServerUrl'
+import { resolveOperation } from './openapi/resolveOperation'
+import { fetch } from './utils/fetch'
+import { handleResponse } from './utils/handleResponse'
+
+/** A service instance for the given OpenAPI specification. */
+export type Service<T> = {
+  [K in OperationId<T>]:
+  OperationById<T, K> extends infer U extends OperationById<T, K>
+    ? (data: OperationOptions<T, U>['data'], options?: OperationOptions<T, U>) => Promise<OperationResult<T, U>>
+    : never
+}
+
+/** The options to pass to the service client. */
+export interface ServiceOptions<T = any> extends RequestOptions {
+  baseUrl?: MaybeLiteral<ServerUrl<T>>
+  headers?: T extends { openapi: string } ? OpenAPIV3.ServerHeaders<T> : never
+}
+
+/**
+ * Create a new client instance for the given OpenAPI specification.
+ *
+ * @param document The OpenAPI specification document.
+ * @param initialOptions The initial options to use for every request.
+ * @returns The client instance.
+ * @example
+ *
+ * // Import the Giphy OpenAPI specification.
+ * import Giphy from './giphy.openapi.json'
+ *
+ * // Create a new service instance for the Giphy API.
+ * const service = createService(Giphy, { headers: { 'Bearer' } })
+ *
+ * // Request the "searchGifs" operation from the Giphy API.
+ * await service.getGifs({ limit: 10, offset: 0, q: 'cats' }) // => { data: [...] }
+ */
+export function createService<T extends object>(document: Readonly<T>, initialOptions?: ServiceOptions<T>): Service<T> {
+  return new Proxy({}, {
+    get(_, id: OperationId<T>) {
+      return async(data: object, options: RequestOptions) => {
+
+        // --- Find the operation in the OpenAPI specification.
+        const baseUrl = getServerUrl(document)
+        const operation = resolveOperation(document, id)
+        if (!operation) throw new Error(`Operation ID "${id}" not found.`)
+
+        // --- Fetch the relevant resource from the server.
+        const { method, path, responses = {} } = operation
+        const response = await fetch(path, { method, baseUrl, data, ...initialOptions, ...options })
+        if (response.ok) return handleResponse(response, { ...initialOptions, ...options })
+
+        // --- Throw an error if the response was not successful.
+        const status = response.status.toString()
+        if (status in responses
+          && typeof responses[status] === 'object'
+          && responses[status] !== null
+          && 'description' in responses[status]
+          && typeof responses[status].description === 'string')
+          throw new Error(responses[status].description)
+
+        // --- Throw a generic error if the response was not successful.
+        throw new Error(response.statusText)
+      }
+    },
+  }) as Service<T>
+}
