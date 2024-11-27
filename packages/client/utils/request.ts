86ca577def7d12cea7ebb45762bd59e6974ce282
Stanley Horwood <stanley@hsjm.io>
2024-11-27 18:24:21 +0100

    feat(client): add enhanced request handling utilities
    Introduced a new `request.ts` module within the client utils package to streamline API request operations. This module includes robust request handling functionalities such as callbacks for error handling, data reception, success, failure, and completion of requests. It leverages improved typing from `@unshared/types` and extends existing `FetchOptions` to provide a more flexible and powerful API interaction layer. This addition aims to support more complex request handling scenarios and improve overall data fetching reliability and maintainability in client applications.

---
+++ packages/client/utils/request.ts
@@ -0,0 +1,70 @@
+import type { ObjectLike } from '@unshared/types'
+import type { FetchMethod, FetchOptions } from './parseRequest'
+import { fetch } from './fetch'
+import { handleResponse } from './handleResponse'
+
+export interface RequestOptions<
+  Method extends FetchMethod = FetchMethod,
+  BaseUrl extends string = string,
+  Parameters extends ObjectLike = ObjectLike,
+  Query extends ObjectLike = ObjectLike,
+  Body = unknown,
+  Headers extends ObjectLike = ObjectLike,
+  Data = any,
+  Response = globalThis.Response,
+> extends
+  FetchOptions<Method, BaseUrl, Parameters, Query, Body, Headers> {
+
+  /**
+   * The callback that is called when an error occurs during the request.
+   */
+  onError?: (error: Error) => any
+
+  /**
+   * The callback that is called when data is received from the request. This callback
+   * will be called for each chunk of data that is received from the request.
+   */
+  onData?: (data: Data) => any
+
+  /**
+   * The callback that is called when the request is successful. This callback will be
+   * called after the request is complete and all data has been received.
+   */
+  onSuccess?: (response: Response) => any
+
+  /**
+   * The callback that is called when the status code is not OK. This callback will be called
+   * after the request is complete and before the data is consumed.
+   */
+  onFailure?: (response: Response) => any
+
+  /**
+   * The callback that is called when the request is complete. This callback will be called
+   * after the request is complete and all data has been received.
+   */
+  onEnd?: (response: Response) => any
+}
+
+/**
+ * Fetch a route from the API and return the data. If the client was instantiated with an
+ * application, the route name will be inferred from the application routes. Otherwise, you
+ * can pass the route name as a string.
+ *
+ * @param route The name of the route to fetch.
+ * @param options The options to pass to the request.
+ * @returns The data from the API.
+ * @example
+ * // Declare the application type.
+ * type App = Application<[ModuleProduct]>
+ *
+ * // Create a type-safe client for the application.
+ * const request = createClient<App>()
+ *
+ * // Fetch the data from the API.
+ * const data = request('GET /api/product/:id', { data: { id: '1' } })
+ */
+export async function request(route: string, options?: RequestOptions): Promise<unknown>
+export async function request(route: string, options: RequestOptions = {}): Promise<unknown> {
+  const response = await fetch(route, options)
+  return await handleResponse(response, options)
+}
