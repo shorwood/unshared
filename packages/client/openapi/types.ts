8b111ba1499d8b242dc3dcfe9f6522c1f66bdda4
Stanley Horwood <stanley@hsjm.io>
2024-11-28 00:10:45 +0100

    chore(client): refine type definitions in OpenAPI client types
    This commit updates the type definitions in the `openapi/types.ts` file within the `client` package, enhancing the modularity and readability of the OpenAPI client types. Key changes include the introduction of `OpenAPIV2Like` and `OpenAPIV3Like` interfaces for better type abstraction and the removal of redundant type parameters such as `FetchMethod`. This cleanup aligns the type structures more closely with the OpenAPI spec versions they represent, and ensures consistency across type usage in the client package.

--- packages/client/openapi/types.ts
+++ packages/client/openapi/types.ts
@@ -1,47 +1,48 @@
 import type { CollectKey, MaybeLiteral, Pretty, StringSplit } from '@unshared/types'
-import type { FetchMethod } from '../utils'
 import type { RequestOptions } from '../utils/request'
 import type { ServerUrl } from './getServerUrl'
 import type { OpenAPIV2 } from './OpenApiV2'
 import type { OpenAPIV3 } from './OpenApiV3'
 import type { Operation } from './resolveOperation'
 
+export interface OpenAPIV2Like { swagger: string }
+export interface OpenAPIV3Like { openapi: string }
+export type OpenAPILike = OpenAPIV2Like | OpenAPIV3Like
+
 /** The options to pass to the request. */
 export type OperationOptions<T, V extends Operation> =
-  T extends { swagger: string }
+  T extends OpenAPIV2Like
     ? RequestOptions<
-      FetchMethod,
+      never,
       MaybeLiteral<ServerUrl<T>>,
       OpenAPIV2.RequestParameters<V>,
       OpenAPIV2.RequestQuery<V>,
       OpenAPIV2.RequestBody<V>,
       OpenAPIV2.RequestHeaders<V>,
-      OpenAPIV2.ResponseBody<V>,
-      Response
+      OpenAPIV2.ResponseBody<V>
     >
-    : T extends { openapi: string }
+    : T extends OpenAPIV3Like
       ? RequestOptions<
-        FetchMethod,
+        never,
         MaybeLiteral<ServerUrl<T>>,
         OpenAPIV2.RequestParameters<V>,
         OpenAPIV3.RequestQuery<V> & OpenAPIV3.ServerQuery<T>,
         OpenAPIV3.RequestBody<V>,
         OpenAPIV2.RequestHeaders<V>,
-        OpenAPIV3.ResponseBody<V>,
-        Response
+        OpenAPIV3.ResponseBody<V>
       >
       : RequestOptions
 
 /** The response data from the operation. */
 export type OperationResult<T, V extends Operation> =
-  T extends { swagger: string } ? OpenAPIV2.ResponseBody<V>
-    : T extends { openapi: string } ? OpenAPIV3.ResponseBody<V>
+  T extends OpenAPIV2Like ? OpenAPIV2.ResponseBody<V>
+    : T extends OpenAPIV3Like ? OpenAPIV3.ResponseBody<V>
       : unknown
 
 /** The `Response` object from the operation. */
 export type OperationResponse<T, V extends Operation> =
-  T extends { swagger: string } ? OpenAPIV2.Response<V>
-    : T extends { openapi: string } ? OpenAPIV3.Response<V>
+  T extends OpenAPIV2Like ? OpenAPIV2.Response<V>
+    : T extends OpenAPIV3Like ? OpenAPIV3.Response<V>
       : globalThis.Response
 
 /** Union of all operation route names in the specification. */
@@ -69,6 +70,6 @@ export type OperationByRoute<T, U extends OperationRoute<T>> =
     : never
 
 /** The `ClientRoutes` inferred from the OpenAPI specification. */
-export type OpenAPIRoutes<T> = {
+export type OpenAPIOptionsMap<T> = {
   [K in OperationRoute<T>]: OperationOptions<T, OperationByRoute<T, K>>
 }
