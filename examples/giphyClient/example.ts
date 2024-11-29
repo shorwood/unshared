d2ef75d3b91855aadcaca5a4734020370f9932c1
Stanley Horwood <stanley@hsjm.io>
2024-11-29 03:08:02 +0100

    chore(giphyClient): implement token option in example
    Updated the `giphyClient` example to use the token option in the `createService` call. The example now passes the `GIPHY_API_KEY` from environment variables to the service, aligning with recent changes in the token handling logic.
    
    Additionally, adjusted the function to be asynchronous and updated the method from `searchGifs` to `translateGif`, which better demonstrates the capabilities of the updated client. The output formatting has also been enhanced for better readability.

--- examples/giphyClient/example.ts
+++ examples/giphyClient/example.ts
@@ -1,13 +1,14 @@
+/* eslint-disable unicorn/prefer-top-level-await */
 import { createService } from '../../packages/client/createService'
 import { parseCliArguments } from '../../packages/process/parseCliArguments'
 import { giphy } from './giphy'
 
-function main() {
-  const { options } = parseCliArguments()
-  const Giphy = createService(giphy)
-  const ghhifs = Giphy.searchGifs({ q: 'cats', ...options })
-  const ghhifsJson = JSON.stringify(ghhifs)
-  process.stdout.write(ghhifsJson)
+async function main() {
+  const { parameters } = parseCliArguments()
+  const Giphy = createService(giphy, { token: process.env.GIPHY_API_KEY })
+  const ghhifs = await Giphy.translateGif({ s: parameters.join(' ') })
+  const ghhifsJson = JSON.stringify(ghhifs, undefined, 2)
+  process.stdout.write(`${ghhifsJson}\n`)
 }
 
-main()
+void main()
